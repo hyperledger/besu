56b0a10942542fc07d385485232ece16698ba374
S. Matthew English <s.matthew.english@gmail.com>
2018-10-12 19:56:09 -0400

    parameterize BlockchainUtilTest (#57)
    Signed-off-by: Adrian Sutton <adrian.sutton@consensys.net>

--- ethereum/core/src/main/java/tech/pegasys/pantheon/ethereum/util/BlockchainUtil.java
+++ ethereum/core/src/main/java/tech/pegasys/pantheon/ethereum/util/BlockchainUtil.java
@@ -12,6 +12,17 @@ public class BlockchainUtil {
 
   private BlockchainUtil() {}
 
+  /**
+   * General utility to process a list of headers and a blockchain, sussing out which header in the
+   * input list is simultaneously the highest order block number and a direct match with one of the
+   * headers of the local chain. The purpose of which being to determine the point of departure in
+   * fork scenarios.
+   *
+   * @param blockchain our local copy of the blockchain
+   * @param headers the list of remote headers
+   * @param ascendingHeaderOrder whether the headers are sorted in ascending or descending order
+   * @return index of the highest known header, or an empty value if no header is known
+   */
   public static OptionalInt findHighestKnownBlockIndex(
       final Blockchain blockchain,
       final List<BlockHeader> headers,
