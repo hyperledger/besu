6b2470274a7876ffa466d9e337b718e3ebd8d8a4
Rob Dawson <robert@rojotek.com>
2018-10-17 17:51:16 +1000

    Use consistent ports for quickstart (#62)
    * specify the port for the ports command in the docker compose file so we get consistent ports to bind to.
    * add options for fixed ports and skip build
    Signed-off-by: Adrian Sutton <adrian.sutton@consensys.net>

--- quickstart/runPantheonPrivateNetwork.sh
+++ quickstart/runPantheonPrivateNetwork.sh
@@ -1,8 +1,54 @@
 #!/bin/sh -e
+me=`basename "$0"`
+
+PARAMS=""
+while (( "$#" )); do
+  case "$1" in
+    -h|--help)
+      echo "Usage:"
+      exit 0
+      ;;
+    -s|--skip-build)
+      SKIP_BUILD=true
+      shift 1
+      ;;
+    -d|--use-default-static-ports)
+      export RPC_PORT_MAPPING="8545:"
+      export WS_PORT_MAPPING="8546:"
+      export EXPLORER_PORT_MAPPING="3000:"
+
+      break 2
+      ;;
+    --rpc-port)
+      export RPC_PORT_MAPPING="${2}:"
+      shift 2
+      ;;
+    --ws-port)
+      export WS_PORT_MAPPING="${2}:"
+      shift 2
+      ;;
+    --explorer-port)
+      export EXPLORER_PORT_MAPPING="${2}:"
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1, try ${me} -h or ${me} --help for complete usage help." >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
 
 QUICKSTART_FOLDER=quickstart
-
-me=`basename "$0"`
 
 if [ ! -f gradlew ]; then
     echo "Please, run this script from the project root using : ${QUICKSTART_FOLDER}/${me}"
@@ -12,7 +58,10 @@ fi
 COMPOSE_CONFIG_FILE_OPTION="-f ${QUICKSTART_FOLDER}/docker-compose.yml"
 
 # Build and run containers and network
-docker-compose ${COMPOSE_CONFIG_FILE_OPTION} build --force-rm
+
+if [ ! ${SKIP_BUILD} ];then
+  docker-compose ${COMPOSE_CONFIG_FILE_OPTION} build --force-rm
+fi
 docker-compose ${COMPOSE_CONFIG_FILE_OPTION} up -d --scale node=4
 
 ${QUICKSTART_FOLDER}/listQuickstartServices.sh
