4c47b101d0f237c6ab90a0a3dafb60351802d54f
CJ Hare <CjHare@users.noreply.github.com>
2018-10-26 15:18:50 +1000

    PantheonInfo updated after first build (#167)
    Signed-off-by: Adrian Sutton <adrian.sutton@consensys.net>

--- pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
+++ pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
@@ -2,7 +2,7 @@ package tech.pegasys.pantheon;
 
 // This file is generated via a gradle task and should not be edited directly.
 public class PantheonInfo {
-  private static final String version = "pantheon/0.8.1";
+  private static final String version = "pantheon/0.9.0-SNAPSHOT";
 
   private PantheonInfo() {}
 
