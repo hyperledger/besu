33f44ff21690f6bfa7c636fa047705cb791eb64e
PegaSys Admin <pegasys.manager@gmail.com>
2018-10-26 03:27:38 +0000

    [Gradle Release Plugin] - pre tag commit:  '0.8.1'.

--- pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
+++ pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
@@ -2,7 +2,7 @@ package tech.pegasys.pantheon;
 
 // This file is generated via a gradle task and should not be edited directly.
 public class PantheonInfo {
-  private static final String version = "pantheon/0.8.0-SNAPSHOT";
+  private static final String version = "pantheon/0.8.1";
 
   private PantheonInfo() {}
 
