002d9ceef3bbca644af6f27d7df3cb9b15d8978e
Danno Ferrin <danno.ferrin@shemnon.com>
2018-11-14 08:08:45 -0700

    Update Client Version to be ethstats friendly (#258)
    Update value returned by web3_clientVersion to be ethstats friendly
    
    * Version part starts with a v
    * SNAPSHOT builds report 32 bits of the git hash
    * OS and architecture are sniffed out and reported
    * JVM version and branding are sniffed out and reported
    
    Example values (not all real):
    pantheon/v0.9.0-dev-f800a0b1/osx-x86_64/oracle-java-1.8
    pantheon/v0.9.0-dev-27960b57/osx-x86_64/zulu-java-11
    pantheon/v0.9.0/linux-arm64/openjdk-java-12

--- pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
+++ pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
@@ -1,12 +1,26 @@
 package tech.pegasys.pantheon;
 
+import tech.pegasys.pantheon.util.PlatformDetector;
+
 // This file is generated via a gradle task and should not be edited directly.
-public class PantheonInfo {
-  private static final String version = "pantheon/0.9.0-SNAPSHOT";
+public final class PantheonInfo {
+  private static final String CLIENT_IDENTITY = "pantheon";
+  private static final String VERSION =
+      CLIENT_IDENTITY
+          + "/v"
+          + PantheonInfo.class.getPackage().getImplementationVersion()
+          + "/"
+          + PlatformDetector.getOS()
+          + "/"
+          + PlatformDetector.getVM();
 
   private PantheonInfo() {}
 
+  public static String clientIdentity() {
+    return CLIENT_IDENTITY;
+  }
+
   public static String version() {
-    return version;
+    return VERSION;
   }
 }
