9d88a55155b548d148ebfe0bad7171a0ef58dd8d
tmohay <37158202+rain-on@users.noreply.github.com>
2018-11-19 08:10:32 +1100

    Added IbftProtocolSchedule (#273)
    A ProtocolSchedule has been added for use with IBFT 2.0 consensus
    mechanism.

---
+++ consensus/ibft/src/main/java/tech/pegasys/pantheon/consensus/ibft/IbftProtocolSchedule.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.pantheon.consensus.ibft;
+
+import static tech.pegasys.pantheon.consensus.ibft.IbftBlockHeaderValidationRulesetFactory.ibftBlockHeaderValidator;
+
+import tech.pegasys.pantheon.config.GenesisConfigOptions;
+import tech.pegasys.pantheon.config.IbftConfigOptions;
+import tech.pegasys.pantheon.consensus.common.EpochManager;
+import tech.pegasys.pantheon.consensus.common.VoteTallyUpdater;
+import tech.pegasys.pantheon.ethereum.core.Wei;
+import tech.pegasys.pantheon.ethereum.mainnet.MainnetBlockBodyValidator;
+import tech.pegasys.pantheon.ethereum.mainnet.MainnetBlockImporter;
+import tech.pegasys.pantheon.ethereum.mainnet.ProtocolSchedule;
+import tech.pegasys.pantheon.ethereum.mainnet.ProtocolScheduleBuilder;
+import tech.pegasys.pantheon.ethereum.mainnet.ProtocolSpecBuilder;
+
+import java.math.BigInteger;
+
+/** Defines the protocol behaviours for a blockchain using IBFT. */
+public class IbftProtocolSchedule {
+
+  private static final int DEFAULT_CHAIN_ID = 1;
+
+  public static ProtocolSchedule<IbftContext> create(final GenesisConfigOptions config) {
+    final IbftConfigOptions ibftConfig = config.getIbftConfigOptions();
+    final long epochLength = ibftConfig.getEpochLength();
+    final long blockPeriod = ibftConfig.getBlockPeriodSeconds();
+    final EpochManager epochManager = new EpochManager(epochLength);
+
+    return new ProtocolScheduleBuilder<>(
+            config,
+            DEFAULT_CHAIN_ID,
+            builder -> applyIbftChanges(blockPeriod, epochManager, builder))
+        .createProtocolSchedule();
+  }
+
+  private static ProtocolSpecBuilder<IbftContext> applyIbftChanges(
+      final long secondsBetweenBlocks,
+      final EpochManager epochManager,
+      final ProtocolSpecBuilder<Void> builder) {
+    return builder
+        .<IbftContext>changeConsensusContextType(
+            difficultyCalculator -> ibftBlockHeaderValidator(secondsBetweenBlocks),
+            difficultyCalculator -> ibftBlockHeaderValidator(secondsBetweenBlocks),
+            MainnetBlockBodyValidator::new,
+            (blockHeaderValidator, blockBodyValidator, blockProcessor) ->
+                new IbftBlockImporter(
+                    new MainnetBlockImporter<>(
+                        blockHeaderValidator, blockBodyValidator, blockProcessor),
+                    new VoteTallyUpdater(epochManager, new IbftVotingBlockInterface())),
+            (time, parent, protocolContext) -> BigInteger.ONE)
+        .blockReward(Wei.ZERO)
+        .blockHashFunction(IbftBlockHashing::calculateHashOfIbftBlockOnChain);
+  }
+}
