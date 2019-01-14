99bded7dd41d9e627c4d9bb70a9bde835b7d4b65
Adrian Sutton <adrian@symphonious.net>
2019-01-15 08:51:26 +1000

    [NC-1977] Provide a better error message when RocksDB reports an unsupported operation system/jvm combination (like 32bit Java on Windows). (#547)
    Signed-off-by: Adrian Sutton <adrian.sutton@consensys.net>

--- util/src/main/java/tech/pegasys/pantheon/util/InvalidConfigurationException.java
+++ util/src/main/java/tech/pegasys/pantheon/util/InvalidConfigurationException.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package tech.pegasys.pantheon.ethereum.util;
+package tech.pegasys.pantheon.util;
 
 public class InvalidConfigurationException extends IllegalArgumentException {
   public InvalidConfigurationException(final String message) {
