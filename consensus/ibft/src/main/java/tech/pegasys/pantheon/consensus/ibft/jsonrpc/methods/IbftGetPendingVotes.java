27007629ca4894269e9f0dab8146ef178c4b59c7
Rob Dawson <robert@rojotek.com>
2019-01-21 15:48:58 +1000

    Added IBFT get pending votes (#603)

--- consensus/ibft/src/main/java/tech/pegasys/pantheon/consensus/ibft/jsonrpc/methods/IbftGetPendingVotes.java
+++ consensus/ibft/src/main/java/tech/pegasys/pantheon/consensus/ibft/jsonrpc/methods/IbftGetPendingVotes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 ConsenSys AG.
+ * Copyright 2019 ConsenSys AG.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -10,20 +10,20 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package tech.pegasys.pantheon.consensus.clique.jsonrpc.methods;
+package tech.pegasys.pantheon.consensus.ibft.jsonrpc.methods;
 
 import tech.pegasys.pantheon.consensus.common.VoteProposer;
 import tech.pegasys.pantheon.consensus.common.jsonrpc.AbstractVoteProposerMethod;
 import tech.pegasys.pantheon.ethereum.jsonrpc.internal.methods.JsonRpcMethod;
 
-public class CliqueProposals extends AbstractVoteProposerMethod implements JsonRpcMethod {
+public class IbftGetPendingVotes extends AbstractVoteProposerMethod implements JsonRpcMethod {
 
-  public CliqueProposals(final VoteProposer voteProposer) {
+  public IbftGetPendingVotes(final VoteProposer voteProposer) {
     super(voteProposer);
   }
 
   @Override
   public String getName() {
-    return "clique_proposals";
+    return "ibft_getPendingVotes";
   }
 }
