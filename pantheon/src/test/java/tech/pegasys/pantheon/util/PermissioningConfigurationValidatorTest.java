257cddf0caccd3016a41f48656a7bca42cc9882d
Sally MacFarlane <sally.macfarlane@consensys.net>
2019-02-19 16:42:07 +1000

    [PAN-1930] bootnodes not on whitelist - improve errors (#904)
    * enhanced test

--- pantheon/src/test/java/tech/pegasys/pantheon/util/PermissioningConfigurationValidatorTest.java
+++ pantheon/src/test/java/tech/pegasys/pantheon/util/PermissioningConfigurationValidatorTest.java
@@ -69,7 +69,13 @@ public class PermissioningConfigurationValidatorTest {
           ethNetworkConfig, permissioningConfiguration);
       fail("expected exception because ropsten bootnodes are not in node-whitelist");
     } catch (Exception e) {
-      assertThat(e.getMessage().startsWith("Bootnode")).isTrue();
+      assertThat(e.getMessage()).startsWith("Bootnode(s) not in nodes-whitelist");
+      assertThat(e.getMessage())
+          .contains(
+              "enode://6332792c4a00e3e4ee0926ed89e0d27ef985424d97b6a45bf0f23e51f0dcb5e66b875777506458aea7af6f9e4ffb69f43f3778ee73c81ed9d34c51c4b16b0b0f@52.232.243.152:30303");
+      assertThat(e.getMessage())
+          .contains(
+              "enode://94c15d1b9e2fe7ce56e458b9a3b672ef11894ddedd0c6f247e0f1d3487f52b66208fb4aeb8179fce6e3a749ea93ed147c37976d67af557508d199d9594c35f09@192.81.208.223:30303");
     }
   }
 }
