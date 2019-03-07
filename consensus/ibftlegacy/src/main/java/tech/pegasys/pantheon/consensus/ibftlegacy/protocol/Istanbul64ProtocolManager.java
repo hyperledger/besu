75f920c0c994695a7182420c01c82fd9c7246a09
Trent Mohay <37158202+rain-on@users.noreply.github.com>
2019-03-07 14:51:21 +1100

    Repair Istanbul to handle Eth/62 & Eth/63 (#1063)
    With re-enabling IBFT 1.0, an error was introduced whereby the Eth/62
    and ETH/63 protocols did not have a mapped ProtocolManager (resulting
    in an eror at runtime).
    
    This change ensures all protocols are handled by the Istanbul64
    ProtocolManager.

--- consensus/ibftlegacy/src/main/java/tech/pegasys/pantheon/consensus/ibftlegacy/protocol/Istanbul64ProtocolManager.java
+++ consensus/ibftlegacy/src/main/java/tech/pegasys/pantheon/consensus/ibftlegacy/protocol/Istanbul64ProtocolManager.java
@@ -12,17 +12,15 @@
  */
 package tech.pegasys.pantheon.consensus.ibftlegacy.protocol;
 
+import static java.util.Collections.singletonList;
+
 import tech.pegasys.pantheon.ethereum.chain.Blockchain;
-import tech.pegasys.pantheon.ethereum.eth.EthProtocol;
 import tech.pegasys.pantheon.ethereum.eth.manager.EthProtocolManager;
-import tech.pegasys.pantheon.ethereum.p2p.api.Message;
 import tech.pegasys.pantheon.ethereum.p2p.wire.Capability;
 import tech.pegasys.pantheon.ethereum.worldstate.WorldStateArchive;
 import tech.pegasys.pantheon.metrics.MetricsSystem;
 
 import java.util.List;
-
-import com.google.common.collect.Lists;
 
 /** This allows for interoperability with Quorum, but shouldn't be used otherwise. */
 public class Istanbul64ProtocolManager extends EthProtocolManager {
@@ -48,21 +46,8 @@ public class Istanbul64ProtocolManager extends EthProtocolManager {
   }
 
   @Override
-  public void processMessage(final Capability cap, final Message message) {
-    if (cap.equals(Istanbul64Protocol.ISTANBUL64)) {
-      if (message.getData().getCode() != Istanbul64Protocol.INSTANBUL_MSG) {
-        super.processMessage(EthProtocol.ETH63, message);
-      } else {
-        // TODO(tmm): Determine if the message should be routed to ibftController at a later date.
-      }
-    }
-  }
-
-  @Override
   public List<Capability> getSupportedCapabilities() {
-    final List<Capability> result = Lists.newArrayList(Istanbul64Protocol.ISTANBUL64);
-    result.addAll(super.getSupportedCapabilities());
-    return result;
+    return singletonList(Istanbul64Protocol.ISTANBUL64);
   }
 
   @Override
