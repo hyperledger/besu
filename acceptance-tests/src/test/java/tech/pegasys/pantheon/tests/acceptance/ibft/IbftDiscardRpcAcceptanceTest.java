553ff840f20fceb3760e756b8526921e8eff742e
Jason Frame <909467+jframe@users.noreply.github.com>
2019-05-10 14:22:50 +1000

    Change AT discard RPC tests to check votes are discarded using proposals API (#1411)

--- acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/ibft/IbftDiscardRpcAcceptanceTest.java
+++ acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/ibft/IbftDiscardRpcAcceptanceTest.java
@@ -23,26 +23,21 @@ public class IbftDiscardRpcAcceptanceTest extends AcceptanceTestBase {
 
   @Test
   public void shouldDiscardVotes() throws IOException {
-    final String[] validators = {"validator1", "validator2"};
+    final String[] validators = {"validator1", "validator3"};
     final PantheonNode validator1 = pantheon.createIbftNodeWithValidators("validator1", validators);
     final PantheonNode validator2 = pantheon.createIbftNodeWithValidators("validator2", validators);
     final PantheonNode validator3 = pantheon.createIbftNodeWithValidators("validator3", validators);
     cluster.start(validator1, validator2, validator3);
 
-    validator1.execute(ibftTransactions.createRemoveProposal(validator2));
-    validator1.execute(ibftTransactions.createAddProposal(validator3));
-
-    validator2.execute(ibftTransactions.createRemoveProposal(validator2));
-    validator2.execute(ibftTransactions.createAddProposal(validator3));
+    validator1.execute(ibftTransactions.createAddProposal(validator2));
+    validator1.execute(ibftTransactions.createRemoveProposal(validator3));
+    validator1.verify(
+        ibft.pendingVotesEqual().addProposal(validator2).removeProposal(validator3).build());
 
     validator1.execute(ibftTransactions.createDiscardProposal(validator2));
+    validator1.verify(ibft.pendingVotesEqual().removeProposal(validator3).build());
+
     validator1.execute(ibftTransactions.createDiscardProposal(validator3));
-
-    validator1.waitUntil(wait.chainHeadHasProgressedByAtLeast(validator1, 2));
-
-    cluster.verify(ibft.validatorsEqual(validator1, validator2));
-    validator1.verify(ibft.noProposals());
-    validator2.verify(
-        ibft.pendingVotesEqual().removeProposal(validator2).addProposal(validator3).build());
+    cluster.verify(ibft.noProposals());
   }
 }
