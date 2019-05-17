356dc8303426c8aa8aa21fd8f8cbb1cf19566aca
Danno Ferrin <danno.ferrin@consensys.net>
2019-05-17 10:30:15 -0600

    [PAN-2705] helpful graphql error when an account doesn't exist (#1460)
    When we are asking for an account at an address that does not exist the
    error message should state that the account doesn't exist.

--- ethereum/graphqlrpc/src/test/java/tech/pegasys/pantheon/ethereum/graphqlrpc/EthGraphQLRpcHttpBySpecTest.java
+++ ethereum/graphqlrpc/src/test/java/tech/pegasys/pantheon/ethereum/graphqlrpc/EthGraphQLRpcHttpBySpecTest.java
@@ -72,6 +72,8 @@ public class EthGraphQLRpcHttpBySpecTest extends AbstractEthGraphQLRpcHttpServic
     specs.add("eth_call_BlockLatest");
     specs.add("eth_getBalance_latest");
     specs.add("eth_getBalance_0x19");
+    specs.add("eth_getBalance_invalidAccountBlockNumber");
+    specs.add("eth_getBalance_invalidAccountLatest");
     specs.add("eth_gasPrice");
 
     specs.add("eth_getTransactionReceipt");
