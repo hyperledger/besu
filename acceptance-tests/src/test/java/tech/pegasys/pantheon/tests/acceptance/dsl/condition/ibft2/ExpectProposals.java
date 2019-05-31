0a7d20a5bfa1937c16b12001ac74feb2b7ede875
CJ Hare <CjHare@users.noreply.github.com>
2019-05-31 14:41:22 +1000

    AT DSL - Renaming ibft to ibft2 (#1516)
    Signed-off-by: Adrian Sutton <adrian.sutton@consensys.net>

--- acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/dsl/condition/ibft2/ExpectProposals.java
+++ acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/dsl/condition/ibft2/ExpectProposals.java
@@ -10,30 +10,29 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package tech.pegasys.pantheon.tests.acceptance.dsl.condition.ibft;
+package tech.pegasys.pantheon.tests.acceptance.dsl.condition.ibft2;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static tech.pegasys.pantheon.tests.acceptance.dsl.WaitUtils.waitFor;
-import static tech.pegasys.pantheon.tests.acceptance.dsl.transaction.ibft2.Ibft2Transactions.LATEST;
 
 import tech.pegasys.pantheon.ethereum.core.Address;
 import tech.pegasys.pantheon.tests.acceptance.dsl.condition.Condition;
 import tech.pegasys.pantheon.tests.acceptance.dsl.node.Node;
 import tech.pegasys.pantheon.tests.acceptance.dsl.transaction.ibft2.Ibft2Transactions;
 
-public class ExpectValidators implements Condition {
-  private final Ibft2Transactions ibft;
-  private final Address[] validators;
+import java.util.Map;
 
-  public ExpectValidators(final Ibft2Transactions ibft, final Address... validators) {
-    this.ibft = ibft;
-    this.validators = validators;
+public class ExpectProposals implements Condition {
+  private final Ibft2Transactions ibftTwo;
+  private final Map<Address, Boolean> proposers;
+
+  public ExpectProposals(final Ibft2Transactions ibftTwo, final Map<Address, Boolean> proposers) {
+    this.ibftTwo = ibftTwo;
+    this.proposers = proposers;
   }
 
   @Override
   public void verify(final Node node) {
-    waitFor(
-        () ->
-            assertThat(node.execute(ibft.createGetValidators(LATEST))).containsExactly(validators));
+    waitFor(() -> assertThat(node.execute(ibftTwo.createProposals())).isEqualTo(proposers));
   }
 }
--- acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/dsl/condition/ibft2/ExpectValidators.java
+++ acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/dsl/condition/ibft2/ExpectValidators.java
@@ -10,29 +10,30 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package tech.pegasys.pantheon.tests.acceptance.dsl.condition.ibft;
+package tech.pegasys.pantheon.tests.acceptance.dsl.condition.ibft2;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static tech.pegasys.pantheon.tests.acceptance.dsl.WaitUtils.waitFor;
+import static tech.pegasys.pantheon.tests.acceptance.dsl.transaction.ibft2.Ibft2Transactions.LATEST;
 
 import tech.pegasys.pantheon.ethereum.core.Address;
 import tech.pegasys.pantheon.tests.acceptance.dsl.condition.Condition;
 import tech.pegasys.pantheon.tests.acceptance.dsl.node.Node;
 import tech.pegasys.pantheon.tests.acceptance.dsl.transaction.ibft2.Ibft2Transactions;
 
-import java.util.Map;
+public class ExpectValidators implements Condition {
+  private final Ibft2Transactions ibft;
+  private final Address[] validators;
 
-public class ExpectProposals implements Condition {
-  private final Ibft2Transactions ibftTwo;
-  private final Map<Address, Boolean> proposers;
-
-  public ExpectProposals(final Ibft2Transactions ibftTwo, final Map<Address, Boolean> proposers) {
-    this.ibftTwo = ibftTwo;
-    this.proposers = proposers;
+  public ExpectValidators(final Ibft2Transactions ibft, final Address... validators) {
+    this.ibft = ibft;
+    this.validators = validators;
   }
 
   @Override
   public void verify(final Node node) {
-    waitFor(() -> assertThat(node.execute(ibftTwo.createProposals())).isEqualTo(proposers));
+    waitFor(
+        () ->
+            assertThat(node.execute(ibft.createGetValidators(LATEST))).containsExactly(validators));
   }
 }
