b01ea4207e38abff52cc72ce7d02fa1f50329090
Abdelhamid Bakhta <45264458+abdelhamidbakhta@users.noreply.github.com>
2019-07-09 18:56:26 +0200

    Factorise metrics code for KeyValueStorage database (#1663)
    * Factorise metrics code for KeyValueStorage database
    
    - introduce `MonitorableKeyValueStorage`
    - factorise code
    - remove metrics instanciation in `RocksDbKeyValueStorage` and `ColumnarRocksDbKeyValueStorage`
    
    * Rename class

--- services/kvstore/src/main/java/tech/pegasys/pantheon/services/kvstore/RocksDbKeyValueStorage.java
+++ services/kvstore/src/main/java/tech/pegasys/pantheon/services/kvstore/RocksDbKeyValueStorage.java
@@ -12,12 +12,8 @@
  */
 package tech.pegasys.pantheon.services.kvstore;
 
-import tech.pegasys.pantheon.metrics.Counter;
 import tech.pegasys.pantheon.metrics.MetricsSystem;
 import tech.pegasys.pantheon.metrics.OperationTimer;
-import tech.pegasys.pantheon.metrics.PantheonMetricCategory;
-import tech.pegasys.pantheon.metrics.prometheus.PrometheusMetricsSystem;
-import tech.pegasys.pantheon.metrics.rocksdb.RocksDBStats;
 import tech.pegasys.pantheon.services.util.RocksDbUtil;
 import tech.pegasys.pantheon.util.bytes.BytesValue;
 
@@ -46,13 +42,7 @@ public class RocksDbKeyValueStorage implements KeyValueStorage, Closeable {
   private final TransactionDBOptions txOptions;
   private final TransactionDB db;
   private final AtomicBoolean closed = new AtomicBoolean(false);
-
-  private final OperationTimer readLatency;
-  private final OperationTimer removeLatency;
-  private final OperationTimer writeLatency;
-  private final OperationTimer commitLatency;
-  private final Counter rollbackCount;
-  private final Statistics stats;
+  private final RocksDBMetricsHelper rocksDBMetricsHelper;
 
   public static KeyValueStorage create(
       final RocksDbConfiguration rocksDbConfiguration, final MetricsSystem metricsSystem)
@@ -64,7 +54,7 @@ public class RocksDbKeyValueStorage implements KeyValueStorage, Closeable {
       final RocksDbConfiguration rocksDbConfiguration, final MetricsSystem metricsSystem) {
     RocksDbUtil.loadNativeLibrary();
     try {
-      stats = new Statistics();
+      final Statistics stats = new Statistics();
       options =
           new Options()
               .setCreateIfMissing(true)
@@ -76,78 +66,8 @@ public class RocksDbKeyValueStorage implements KeyValueStorage, Closeable {
 
       txOptions = new TransactionDBOptions();
       db = TransactionDB.open(options, txOptions, rocksDbConfiguration.getDatabaseDir().toString());
-
-      readLatency =
-          metricsSystem
-              .createLabelledTimer(
-                  PantheonMetricCategory.KVSTORE_ROCKSDB,
-                  "read_latency_seconds",
-                  "Latency for read from RocksDB.",
-                  "database")
-              .labels(rocksDbConfiguration.getLabel());
-      removeLatency =
-          metricsSystem
-              .createLabelledTimer(
-                  PantheonMetricCategory.KVSTORE_ROCKSDB,
-                  "remove_latency_seconds",
-                  "Latency of remove requests from RocksDB.",
-                  "database")
-              .labels(rocksDbConfiguration.getLabel());
-      writeLatency =
-          metricsSystem
-              .createLabelledTimer(
-                  PantheonMetricCategory.KVSTORE_ROCKSDB,
-                  "write_latency_seconds",
-                  "Latency for write to RocksDB.",
-                  "database")
-              .labels(rocksDbConfiguration.getLabel());
-      commitLatency =
-          metricsSystem
-              .createLabelledTimer(
-                  PantheonMetricCategory.KVSTORE_ROCKSDB,
-                  "commit_latency_seconds",
-                  "Latency for commits to RocksDB.",
-                  "database")
-              .labels(rocksDbConfiguration.getLabel());
-
-      if (metricsSystem instanceof PrometheusMetricsSystem) {
-        RocksDBStats.registerRocksDBMetrics(stats, (PrometheusMetricsSystem) metricsSystem);
-      }
-
-      metricsSystem.createLongGauge(
-          PantheonMetricCategory.KVSTORE_ROCKSDB,
-          "rocks_db_table_readers_memory_bytes",
-          "Estimated memory used for RocksDB index and filter blocks in bytes",
-          () -> {
-            try {
-              return db.getLongProperty("rocksdb.estimate-table-readers-mem");
-            } catch (final RocksDBException e) {
-              LOG.debug("Failed to get RocksDB metric", e);
-              return 0L;
-            }
-          });
-
-      metricsSystem.createLongGauge(
-          PantheonMetricCategory.KVSTORE_ROCKSDB,
-          "rocks_db_files_size_bytes",
-          "Estimated database size in bytes",
-          () -> {
-            try {
-              return db.getLongProperty("rocksdb.live-sst-files-size");
-            } catch (final RocksDBException e) {
-              LOG.debug("Failed to get RocksDB metric", e);
-              return 0L;
-            }
-          });
-
-      rollbackCount =
-          metricsSystem
-              .createLabelledCounter(
-                  PantheonMetricCategory.KVSTORE_ROCKSDB,
-                  "rollback_count",
-                  "Number of RocksDB transactions rolled back.",
-                  "database")
-              .labels(rocksDbConfiguration.getLabel());
+      rocksDBMetricsHelper =
+          RocksDBMetricsHelper.of(metricsSystem, rocksDbConfiguration, db, stats);
     } catch (final RocksDBException e) {
       throw new StorageException(e);
     }
@@ -162,7 +82,8 @@ public class RocksDbKeyValueStorage implements KeyValueStorage, Closeable {
   public Optional<BytesValue> get(final BytesValue key) throws StorageException {
     throwIfClosed();
 
-    try (final OperationTimer.TimingContext ignored = readLatency.startTimer()) {
+    try (final OperationTimer.TimingContext ignored =
+        rocksDBMetricsHelper.getReadLatency().startTimer()) {
       return Optional.ofNullable(db.get(key.getArrayUnsafe())).map(BytesValue::wrap);
     } catch (final RocksDBException e) {
       throw new StorageException(e);
@@ -240,7 +161,8 @@ public class RocksDbKeyValueStorage implements KeyValueStorage, Closeable {
 
     @Override
     protected void doPut(final BytesValue key, final BytesValue value) {
-      try (final OperationTimer.TimingContext ignored = writeLatency.startTimer()) {
+      try (final OperationTimer.TimingContext ignored =
+          rocksDBMetricsHelper.getWriteLatency().startTimer()) {
         innerTx.put(key.getArrayUnsafe(), value.getArrayUnsafe());
       } catch (final RocksDBException e) {
         throw new StorageException(e);
@@ -249,7 +171,8 @@ public class RocksDbKeyValueStorage implements KeyValueStorage, Closeable {
 
     @Override
     protected void doRemove(final BytesValue key) {
-      try (final OperationTimer.TimingContext ignored = removeLatency.startTimer()) {
+      try (final OperationTimer.TimingContext ignored =
+          rocksDBMetricsHelper.getRemoveLatency().startTimer()) {
         innerTx.delete(key.getArrayUnsafe());
       } catch (final RocksDBException e) {
         throw new StorageException(e);
@@ -258,7 +181,8 @@ public class RocksDbKeyValueStorage implements KeyValueStorage, Closeable {
 
     @Override
     protected void doCommit() throws StorageException {
-      try (final OperationTimer.TimingContext ignored = commitLatency.startTimer()) {
+      try (final OperationTimer.TimingContext ignored =
+          rocksDBMetricsHelper.getCommitLatency().startTimer()) {
         innerTx.commit();
       } catch (final RocksDBException e) {
         throw new StorageException(e);
@@ -271,7 +195,7 @@ public class RocksDbKeyValueStorage implements KeyValueStorage, Closeable {
     protected void doRollback() {
       try {
         innerTx.rollback();
-        rollbackCount.inc();
+        rocksDBMetricsHelper.getRollbackCount().inc();
       } catch (final RocksDBException e) {
         throw new StorageException(e);
       } finally {
