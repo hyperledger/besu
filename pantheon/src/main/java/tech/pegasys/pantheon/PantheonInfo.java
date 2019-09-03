0a3785604ee35aa9f4b7e7bf1902518ab2902d0b
Adrian Sutton <adrian.sutton@consensys.net>
2019-09-03 12:27:21 +1000

    Stop autogenerating PantheonInfo.java (#1899)
    The version is now read from a manifest file so there's nothing dynamic about the file.
    Signed-off-by: Adrian Sutton <adrian.sutton@consensys.net>

--- pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
+++ pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
@@ -1,8 +1,19 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package tech.pegasys.pantheon;
 
 import tech.pegasys.pantheon.util.PlatformDetector;
 
-// This file is generated via a gradle task and should not be edited directly.
 public final class PantheonInfo {
   private static final String CLIENT_IDENTITY = "pantheon";
   private static final String VERSION =
@@ -15,10 +26,6 @@ public final class PantheonInfo {
           + PlatformDetector.getVM();
 
   private PantheonInfo() {}
-
-  public static String clientIdentity() {
-    return CLIENT_IDENTITY;
-  }
 
   public static String version() {
     return VERSION;
