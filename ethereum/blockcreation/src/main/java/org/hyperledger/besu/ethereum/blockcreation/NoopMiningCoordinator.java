0bb72fbb0558c9d3c26d4cdb166e914e772710a2
Meredith Baxter <meredith.baxter@consensys.net>
2019-10-11 16:31:25 -0400

    Update IbftLegacyControllerBuilder to return a MiningCoordinator
    Signed-off-by: Meredith Baxter <meredith.baxter@consensys.net>

---
+++ ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/NoopMiningCoordinator.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.hyperledger.besu.ethereum.blockcreation;
+
+import org.hyperledger.besu.ethereum.core.Address;
+import org.hyperledger.besu.ethereum.core.Block;
+import org.hyperledger.besu.ethereum.core.BlockHeader;
+import org.hyperledger.besu.ethereum.core.MiningParameters;
+import org.hyperledger.besu.ethereum.core.Transaction;
+import org.hyperledger.besu.ethereum.core.Wei;
+import org.hyperledger.besu.util.bytes.BytesValue;
+
+import java.util.List;
+import java.util.Optional;
+
+public class NoopMiningCoordinator implements MiningCoordinator {
+
+  private final MiningParameters miningParameters;
+
+  public NoopMiningCoordinator(final MiningParameters miningParameters) {
+    this.miningParameters = miningParameters;
+  }
+
+  @Override
+  public void enable() {}
+
+  @Override
+  public void disable() {}
+
+  @Override
+  public boolean isRunning() {
+    return false;
+  }
+
+  @Override
+  public Wei getMinTransactionGasPrice() {
+    return miningParameters.getMinTransactionGasPrice();
+  }
+
+  @Override
+  public void setExtraData(final BytesValue extraData) {}
+
+  @Override
+  public Optional<Address> getCoinbase() {
+    return miningParameters.getCoinbase();
+  }
+
+  @Override
+  public Optional<Block> createBlock(
+      final BlockHeader parentHeader,
+      final List<Transaction> transactions,
+      final List<BlockHeader> ommers) {
+    return Optional.empty();
+  }
+}
