7c009816cd48d5808c60a9564e853edd8a2a380a
Danno Ferrin <danno.ferrin@gmail.com>
2019-10-17 16:21:52 -0600

    [PAN-3242] Programatically enforce plugin CLI variable names (#117)
    Enforce that plugin variable names are either `--plugin-<namespace>-`
    or `--Xplugin-<namespace>-` when registered with the
    PicoCLIOptionsService.  If the names don't match a RuntimeException is
    thrown, and unless that exception is caught the plugin will not have
    start or stop lifecycle messages called.
    
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>
    Signed-off-by: edwardmack <ed@edwardmack.com>

--- besu/src/main/java/org/hyperledger/besu/services/PicoCLIOptionsImpl.java
+++ besu/src/main/java/org/hyperledger/besu/services/PicoCLIOptionsImpl.java
@@ -16,9 +16,15 @@ package org.hyperledger.besu.services;
 
 import org.hyperledger.besu.plugin.services.PicoCLIOptions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import picocli.CommandLine;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Model.OptionSpec;
 
 public class PicoCLIOptionsImpl implements PicoCLIOptions {
+
+  private static final Logger LOG = LogManager.getLogger();
 
   private final CommandLine commandLine;
 
@@ -28,6 +34,24 @@ public class PicoCLIOptionsImpl implements PicoCLIOptions {
 
   @Override
   public void addPicoCLIOptions(final String namespace, final Object optionObject) {
-    commandLine.addMixin("Plugin " + namespace, optionObject);
+    final String pluginPrefix = "--plugin-" + namespace + "-";
+    final String unstablePrefix = "--Xplugin-" + namespace + "-";
+    final CommandSpec mixin = CommandSpec.forAnnotatedObject(optionObject);
+    boolean badOptionName = false;
+
+    for (final OptionSpec optionSpec : mixin.options()) {
+      for (final String optionName : optionSpec.names()) {
+        if (!optionName.startsWith(pluginPrefix) && !optionName.startsWith(unstablePrefix)) {
+          badOptionName = true;
+          LOG.error(
+              "Plugin option {} did not have the expected prefix of {}", optionName, pluginPrefix);
+        }
+      }
+    }
+    if (badOptionName) {
+      throw new RuntimeException("Error loading CLI options");
+    } else {
+      commandLine.getCommandSpec().addMixin("Plugin " + namespace, mixin);
+    }
   }
 }
