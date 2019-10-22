7530cad9b2c1cb2dcdf9a912fbf868b74d1014d0
Usman Saleem <usman@usmans.info>
2019-10-22 18:07:08 +1000

    [PIE-2052] Besu CLI -V to print plugin versions (#123)
     -- Use (PicoCLI) custom factory to construct version provider which can return optional plugin versions 
     -- Use plugin's jar manifest implementation and version to build plugin version during plugin registration
    
    Signed-off-by: Usman Saleem <usman@usmans.info>
    Signed-off-by: edwardmack <ed@edwardmack.com>

---
+++ besu/src/test/java/org/hyperledger/besu/cli/util/VersionProviderTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.cli.util;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.hyperledger.besu.BesuInfo;
+import org.hyperledger.besu.services.PluginVersionsProvider;
+
+import java.util.Collections;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VersionProviderTest {
+
+  @Mock private PluginVersionsProvider pluginVersionsProvider;
+
+  @Test
+  public void validateEmptyListGenerateBesuInfoVersionOnly() {
+    when(pluginVersionsProvider.getPluginVersions()).thenReturn(emptyList());
+    final VersionProvider versionProvider = new VersionProvider(pluginVersionsProvider);
+    assertThat(versionProvider.getVersion()).containsOnly(BesuInfo.version());
+  }
+
+  @Test
+  public void validateVersionListGenerateValidValues() {
+    when(pluginVersionsProvider.getPluginVersions()).thenReturn(Collections.singletonList("test"));
+    final VersionProvider versionProvider = new VersionProvider(pluginVersionsProvider);
+    assertThat(versionProvider.getVersion()).containsExactly(BesuInfo.version(), "test");
+  }
+}
