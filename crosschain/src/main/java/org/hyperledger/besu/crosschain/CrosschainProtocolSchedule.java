ad8644a1c0f5384f97a258ba5e57ca95494730d0
Horacio Mijail <hmijail@gmail.com>
2019-10-30 03:29:03 +1000

    Prepare placeholders similar to what other protocols have.
    Signed-off-by: Horacio Mijail Anton Quiles <hmijail@gmail.com>

---
+++ crosschain/src/main/java/org/hyperledger/besu/crosschain/CrosschainProtocolSchedule.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2018 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.hyperledger.besu.crosschain;
+
+import org.checkerframework.checker.units.qual.C;
+import org.hyperledger.besu.config.GenesisConfigOptions;
+import org.hyperledger.besu.ethereum.MainnetBlockValidator;
+import org.hyperledger.besu.ethereum.core.PrivacyParameters;
+import org.hyperledger.besu.ethereum.core.Wei;
+import org.hyperledger.besu.ethereum.mainnet.*;
+import org.hyperledger.besu.ethereum.privacy.PrivateTransactionValidator;
+
+import java.math.BigInteger;
+import java.util.OptionalLong;
+
+public class CrosschainProtocolSchedule {
+
+  private static final BigInteger DEFAULT_CHAIN_ID = BigInteger.ONE;
+
+
+}
