eb752bb502d8dbc0bee74401a523d84084902483
Joshua Richardson <joshua@richardson.tech>
2019-11-18 13:14:00 +0000

    Refactorings for new web3j version
    Signed-off-by: Joshua Richardson <joshua@richardson.tech>

--- acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/web3j/privacy/EthSignerAcceptanceTest.java
+++ acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/web3j/privacy/EthSignerAcceptanceTest.java
@@ -28,8 +28,8 @@ import java.util.Collections;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.web3j.protocol.pantheon.response.privacy.PrivacyGroup;
-import org.web3j.protocol.pantheon.response.privacy.PrivateTransactionReceipt;
+import org.web3j.protocol.besu.response.privacy.PrivacyGroup;
+import org.web3j.protocol.besu.response.privacy.PrivateTransactionReceipt;
 import org.web3j.utils.Base64String;
 
 public class EthSignerAcceptanceTest extends PrivacyAcceptanceTestBase {
