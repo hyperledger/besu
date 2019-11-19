29ec2e372d9e19a6a5ab6c4fd8ad5abe1017ac5a
Josh Richardson <josh-richardson@users.noreply.github.com>
2019-11-19 09:15:54 +0000

    [PAN-2984] Refactors transaction receipt to contain more members (#85)
    Signed-off-by: Joshua Richardson <joshua@richardson.tech>

--- acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/privacy/contract/LoadPrivateSmartContractTransaction.java
+++ acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/privacy/contract/LoadPrivateSmartContractTransaction.java
@@ -27,13 +27,13 @@ import org.web3j.tx.Contract;
 import org.web3j.tx.LegacyPrivateTransactionManager;
 import org.web3j.tx.PrivateTransactionManager;
 import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.BesuPrivacyGasProvider;
 import org.web3j.tx.gas.ContractGasProvider;
-import org.web3j.tx.gas.PantheonPrivacyGasProvider;
 import org.web3j.utils.Base64String;
 
 public class LoadPrivateSmartContractTransaction<T extends Contract> implements Transaction<T> {
-  private static final PantheonPrivacyGasProvider GAS_PROVIDER =
-      new PantheonPrivacyGasProvider(BigInteger.valueOf(1000));
+  private static final BesuPrivacyGasProvider GAS_PROVIDER =
+      new BesuPrivacyGasProvider(BigInteger.valueOf(1000));
   private static final Object METHOD_IS_STATIC = null;
 
   private final Class<T> clazz;
