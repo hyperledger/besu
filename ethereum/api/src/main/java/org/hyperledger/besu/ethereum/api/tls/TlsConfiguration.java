6b324a42a44f057d7f15c8ba1b60c4f9b2600a72
Usman Saleem <usman@usmans.info>
2019-12-19 16:33:41 +1000

    headers
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/tls/TlsConfiguration.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/tls/TlsConfiguration.java
@@ -1,10 +1,18 @@
 /*
- * Copyright (C) 2019 ConsenSys AG.
+ * Copyright ConsenSys AG.
  *
- * The source code is provided to licensees of PegaSys Plus for their convenience and internal
- * business use. These files may not be copied, translated, and/or distributed without the express
- * written permission of an authorized signatory for ConsenSys AG.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  */
+
 package org.hyperledger.besu.ethereum.api.tls;
 
 import java.util.Optional;
