37becfee3191b804126724561a848335959b822b
Usman Saleem <usman@usmans.info>
2019-12-19 10:35:13 +1000

    wip - tls configuration classes
    Signed-off-by: Usman Saleem <usman@usmans.info>

---
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/tls/TlsConfiguration.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2019 ConsenSys AG.
+ *
+ * The source code is provided to licensees of PegaSys Plus for their convenience and internal
+ * business use. These files may not be copied, translated, and/or distributed without the express
+ * written permission of an authorized signatory for ConsenSys AG.
+ */
+package org.hyperledger.besu.ethereum.api.tls;
+
+import java.util.List;
+
+public class TlsConfiguration {
+  private Boolean tlsEnabled = false;
+
+  // server certificate/key configuration
+  private TlsKeyStoreConfiguration serverKeyStore;
+
+  // optional - trust store for server to verify identify of clients
+  private TlsTrustStoreConfiguration clientTrustKeyStore;
+
+  // optional
+  private List<String> cipherSuite;
+
+  // optional - TLS protocols to remove from enabled list
+  private List<String> removeEnabledTlsProtocol;
+
+  //optional - default TLS versions already enabled in VertX - SSLv2Hello, TLSv1, TLSv1.1 and TLSv1.2
+  private List<String> addEnabledTlsProtocol;
+
+  private boolean useOpenSSLEngine = false; //Use OpenSSL engine instead of JDK implementation
+
+
+
+
+
+
+
+
+}
