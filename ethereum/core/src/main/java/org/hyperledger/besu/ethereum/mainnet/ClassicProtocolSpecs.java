42df3fa7105107717d08e01f7bb07a94391ed9ee
edwardmack <ed@edwardmack.com>
2019-12-23 15:32:37 -0500

    Implement Aztlan updates config definition
    - Adds code defining aztlan block number in config files.
    - Adds code for Aztlan definition
    
    Signed-off-by: edwardmack <ed@edwardmack.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/ClassicProtocolSpecs.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/ClassicProtocolSpecs.java
@@ -150,6 +150,16 @@ public class ClassicProtocolSpecs {
         .name("Agharta");
   }
 
+  public static ProtocolSpecBuilder<Void> aztlanDefinition(
+      final Optional<BigInteger> chainId,
+      final OptionalInt configContractSizeLimit,
+      final OptionalInt configStackSizeLimit,
+      final boolean enableRevertReason) {
+    return aghartaDefinition(
+            chainId, configContractSizeLimit, configStackSizeLimit, enableRevertReason)
+        .name("Aztlan");
+  }
+
   private static TransactionReceipt byzantiumTransactionReceiptFactory(
       final TransactionProcessor.Result result, final WorldState worldState, final long gasUsed) {
     return new TransactionReceipt(
