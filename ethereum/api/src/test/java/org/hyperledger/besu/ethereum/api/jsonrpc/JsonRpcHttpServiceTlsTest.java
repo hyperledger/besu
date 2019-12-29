ae6be9d669ab7d3211c27e69c3b837c6f6527985
Usman Saleem <usman@usmans.info>
2019-12-29 15:28:25 +1000

    ssl all debug
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/JsonRpcHttpServiceTlsTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/JsonRpcHttpServiceTlsTest.java
@@ -83,7 +83,7 @@ import org.junit.rules.TemporaryFolder;
 
 public class JsonRpcHttpServiceTlsTest {
   static {
-    System.setProperty("javax.net.debug", "ssl:handshake:verbose:keymanager");
+    System.setProperty("javax.net.debug", "all");
   }
 
   @ClassRule public static final TemporaryFolder folder = new TemporaryFolder();
