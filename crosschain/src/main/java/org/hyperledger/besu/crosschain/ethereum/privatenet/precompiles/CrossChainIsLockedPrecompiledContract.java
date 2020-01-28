e70192bd394311629bc63f2376bb3849223f466d
John Brainard <john.brainard@consensys.net>
2020-01-28 16:26:09 -0500

    fix to isLocked() precompile

--- crosschain/src/main/java/org/hyperledger/besu/crosschain/ethereum/privatenet/precompiles/CrossChainIsLockedPrecompiledContract.java
+++ crosschain/src/main/java/org/hyperledger/besu/crosschain/ethereum/privatenet/precompiles/CrossChainIsLockedPrecompiledContract.java
@@ -1,15 +1,22 @@
 package org.hyperledger.besu.crosschain.ethereum.privatenet.precompiles;
 
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hyperledger.besu.ethereum.core.Gas;
+import org.hyperledger.besu.ethereum.core.*;
 import org.hyperledger.besu.ethereum.mainnet.AbstractPrecompiledContract;
 import org.hyperledger.besu.ethereum.vm.GasCalculator;
 import org.hyperledger.besu.ethereum.vm.MessageFrame;
 import org.hyperledger.besu.util.bytes.BytesValue;
 
-public class CrossChainIsLockedPrecompiledContract extends AbstractPrecompiledContract {
+import javax.swing.*;
+
+public class CrossChainIsLockedPrecompiledContract extends AbstractCrossChainPrecompiledContract {
     protected static final Logger LOG = LogManager.getLogger();
+    private static final int addressLength = 20;
 
     // TODO: Not sure what this should really be
     private static final long FIXED_GAS_COST = 10L;
@@ -28,8 +35,35 @@ public class CrossChainIsLockedPrecompiledContract extends AbstractPrecompiledCo
         LOG.info(
                 "CrosschainIsLocked Precompile called with " + input.size() + "bytes:" + input.toString());
 
+       BigInteger contractAddress = extractAddress(input, 0, addressLength);
+       if (contractAddress.equals(BigInteger.ZERO))
+       {
+           LOG.error("Invalid contract address");
+           return(BytesValue.of(0));
+       }
 
-        return BytesValue.of(1);
+        //MutableAccount contract = WorldUpdater.getMutable(contractAddress)null;
+        MutableAccount contract = null;
+        if (contract.isLocked()){
+            return BytesValue.of(1);
+        }
+       else {
+           return(BytesValue.of(0));
+        }
+
     }
 
+    protected boolean isMatched(final CrosschainTransaction ct) {
+        return ct.getType().isOriginatingTransaction() || ct.getType().isSubordinateTransaction();
+    }
+
+
+    private static BigInteger extractAddress(
+            final BytesValue input, final int offset, final int length) {
+        if (offset > input.size() || length == 0) {
+            return BigInteger.ZERO;
+        }
+        final byte[] raw = Arrays.copyOfRange(input.extractArray(), offset, offset + length);
+        return new BigInteger(1, raw);
+    }
 }
