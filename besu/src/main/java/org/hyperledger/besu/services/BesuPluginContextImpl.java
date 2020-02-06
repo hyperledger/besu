10789abd57fc90bc4b5f6cf94c34cfc98c9a7705
Danno Ferrin <danno.ferrin@gmail.com>
2020-02-06 10:51:20 -0700

    Plugin error stack traces
    Because of how the Log4J2 api works exception stack traces were not
    being printed.  Update to use the explicit "throwable" overloaded
    methods.
    
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- besu/src/main/java/org/hyperledger/besu/services/BesuPluginContextImpl.java
+++ besu/src/main/java/org/hyperledger/besu/services/BesuPluginContextImpl.java
@@ -96,8 +96,9 @@ public class BesuPluginContextImpl implements BesuContext, PluginVersionsProvide
         addPluginVersion(plugin);
       } catch (final Exception e) {
         LOG.error(
-            "Error registering plugin of type {}, start and stop will not be called. \n{}",
-            plugin.getClass(),
+            "Error registering plugin of type "
+                + plugin.getClass()
+                + ", start and stop will not be called.",
             e);
         continue;
       }
@@ -140,9 +141,7 @@ public class BesuPluginContextImpl implements BesuContext, PluginVersionsProvide
         LOG.debug("Started plugin of type {}.", plugin.getClass().getName());
       } catch (final Exception e) {
         LOG.error(
-            "Error starting plugin of type {}, stop will not be called. \n{}",
-            plugin.getClass(),
-            e);
+            "Error starting plugin of type " + plugin.getClass() + ", stop will not be called.", e);
         pluginsIterator.remove();
       }
     }
@@ -164,7 +163,7 @@ public class BesuPluginContextImpl implements BesuContext, PluginVersionsProvide
         plugin.stop();
         LOG.debug("Stopped plugin of type {}.", plugin.getClass().getName());
       } catch (final Exception e) {
-        LOG.error("Error stopping plugin of type {}. \n{}", plugin.getClass(), e);
+        LOG.error("Error stopping plugin of type " + plugin.getClass(), e);
       }
     }
 
