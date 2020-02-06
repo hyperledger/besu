599de6788f9a0be7fcd0d915d14a0a22aabf5c93
Karim TAAM <karim.t2am@gmail.com>
2020-02-06 19:09:25 +0100

    resolve tests issues
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- besu/src/test/java/org/hyperledger/besu/cli/BesuCommandTest.java
+++ besu/src/test/java/org/hyperledger/besu/cli/BesuCommandTest.java
@@ -1391,6 +1391,9 @@ public class BesuCommandTest extends CommandTestAbstract {
     parseCommand("--nat-method", "AUTO");
     verify(mockRunnerBuilder).natMethod(eq(NatMethod.AUTO));
 
+    parseCommand("--nat-method", "DOCKER");
+    verify(mockRunnerBuilder).natMethod(eq(NatMethod.DOCKER));
+
     assertThat(commandOutput.toString()).isEmpty();
     assertThat(commandErrorOutput.toString()).isEmpty();
   }
@@ -1403,7 +1406,7 @@ public class BesuCommandTest extends CommandTestAbstract {
     assertThat(commandOutput.toString()).isEmpty();
     assertThat(commandErrorOutput.toString())
         .contains(
-            "Invalid value for option '--nat-method': expected one of [UPNP, MANUAL, AUTO, NONE] (case-insensitive) but was 'invalid'");
+            "Invalid value for option '--nat-method': expected one of [UPNP, MANUAL, DOCKER, AUTO, NONE] (case-insensitive) but was 'invalid'");
   }
 
   @Test
