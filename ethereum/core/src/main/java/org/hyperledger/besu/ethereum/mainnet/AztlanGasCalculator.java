350352de0a873cfae49f295c6a28f672f8e87024
Edward Mack <ed@edwardmack.com>
2020-02-10 15:47:34 -0500

    Update SLOAD_GAS cost to 200 in Aztlan Gas Calculator (#23)
    Change SLOAD_GAS cost in Aztlan Gas Calculator from 800 to 200 and
    update functions that use SLOAD_GAS.
    
    Signed-off-by: edwardmack <ed@edwardmack.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/AztlanGasCalculator.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/AztlanGasCalculator.java
@@ -14,7 +14,10 @@
  */
 package org.hyperledger.besu.ethereum.mainnet;
 
+import org.hyperledger.besu.ethereum.core.Account;
 import org.hyperledger.besu.ethereum.core.Gas;
+
+import org.apache.tuweni.units.bigints.UInt256;
 
 /*
  * This class implements changes to gas calculation for ECIP-1061 https://ecips.ethereumclassic.org/ECIPs/ecip-1061
@@ -23,6 +26,78 @@ import org.hyperledger.besu.ethereum.core.Gas;
 public class AztlanGasCalculator extends IstanbulGasCalculator {
   private static final Gas BALANCE_OPERATION_GAS_COST = Gas.of(400);
   private static final Gas EXTCODE_HASH_COST = Gas.of(400);
+  private static final Gas SLOAD_GAS = Gas.of(200);
+  private static final Gas SSTORE_SET_GAS = Gas.of(20_000);
+  private static final Gas SSTORE_RESET_GAS = Gas.of(5_000);
+  private static final Gas SSTORE_CLEARS_SCHEDULE = Gas.of(15_000);
+
+  private static final Gas SSTORE_SET_GAS_LESS_SLOAD_GAS = SSTORE_SET_GAS.minus(SLOAD_GAS);
+  private static final Gas SSTORE_RESET_GAS_LESS_SLOAD_GAS = SSTORE_RESET_GAS.minus(SLOAD_GAS);
+  private static final Gas NEGATIVE_SSTORE_CLEARS_SCHEDULE = Gas.ZERO.minus(SSTORE_CLEARS_SCHEDULE);
+
+  @Override
+  // As per https://eips.ethereum.org/EIPS/eip-2200
+  public Gas calculateStorageCost(
+      final Account account, final UInt256 key, final UInt256 newValue) {
+
+    final UInt256 currentValue = account.getStorageValue(key);
+    if (currentValue.equals(newValue)) {
+      return SLOAD_GAS;
+    } else {
+      final UInt256 originalValue = account.getOriginalStorageValue(key);
+      if (originalValue.equals(currentValue)) {
+        return originalValue.isZero() ? SSTORE_SET_GAS : SSTORE_RESET_GAS;
+      } else {
+        return SLOAD_GAS;
+      }
+    }
+  }
+
+  @Override
+  // As per https://eips.ethereum.org/EIPS/eip-2200
+  public Gas calculateStorageRefundAmount(
+      final Account account, final UInt256 key, final UInt256 newValue) {
+
+    final UInt256 currentValue = account.getStorageValue(key);
+    if (currentValue.equals(newValue)) {
+      return Gas.ZERO;
+    } else {
+      final UInt256 originalValue = account.getOriginalStorageValue(key);
+      if (originalValue.equals(currentValue)) {
+        if (originalValue.isZero()) {
+          return Gas.ZERO;
+        } else if (newValue.isZero()) {
+          return SSTORE_CLEARS_SCHEDULE;
+        } else {
+          return Gas.ZERO;
+        }
+      } else {
+        Gas refund = Gas.ZERO;
+        if (!originalValue.isZero()) {
+          if (currentValue.isZero()) {
+            refund = NEGATIVE_SSTORE_CLEARS_SCHEDULE;
+          } else if (newValue.isZero()) {
+            refund = SSTORE_CLEARS_SCHEDULE;
+          }
+        }
+
+        if (originalValue.equals(newValue)) {
+          refund =
+              refund.plus(
+                  originalValue.isZero()
+                      ? SSTORE_SET_GAS_LESS_SLOAD_GAS
+                      : SSTORE_RESET_GAS_LESS_SLOAD_GAS);
+        }
+        return refund;
+      }
+    }
+  }
+
+  @Override
+  // As per https://eips.ethereum.org/EIPS/eip-1884
+  public Gas getSloadOperationGasCost() {
+    return SLOAD_GAS;
+  }
 
   @Override
   public Gas getBalanceOperationGasCost() {
