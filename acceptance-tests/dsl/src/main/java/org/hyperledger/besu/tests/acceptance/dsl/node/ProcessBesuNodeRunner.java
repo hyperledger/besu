e28ce7ecbe086fa98d52916302f8d76002e1bfbc
Horacio Mijail Anton Quiles <hmijail@gmail.com>
2020-02-19 20:29:59 +1000

    Close a leaked cluster
    Signed-off-by: Horacio Mijail Anton Quiles <hmijail@gmail.com>

--- acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/node/ProcessBesuNodeRunner.java
+++ acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/node/ProcessBesuNodeRunner.java
@@ -45,7 +45,6 @@ import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.awaitility.Awaitility;
 
 public class ProcessBesuNodeRunner implements BesuNodeRunner {
 
