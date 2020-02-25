a7703f615fa4322aa2ce140f838e43174616c924
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-02-25 15:28:25 +0100

    Test CI
    Signed-off-by: Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>

--- acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/database/DatabaseMigrationAcceptanceTest.java
+++ acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/database/DatabaseMigrationAcceptanceTest.java
@@ -26,20 +26,26 @@ import org.hyperledger.besu.tests.acceptance.dsl.node.BesuNode;
 import org.hyperledger.besu.tests.acceptance.dsl.node.configuration.BesuNodeConfigurationBuilder;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Enumeration;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
-import net.lingala.zip4j.ZipFile;
-import net.lingala.zip4j.exception.ZipException;
+import org.apache.commons.compress.utils.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -92,7 +98,7 @@ public class DatabaseMigrationAcceptanceTest extends AcceptanceTestBase {
   }
 
   @Before
-  public void ignore() throws Exception {
+  public void setUp() throws Exception {
     System.out.println("Setting up database migration test");
     final URL rootURL = DatabaseMigrationAcceptanceTest.class.getResource(dataPath);
     System.out.printf("Root URL: %s\n", rootURL.toString());
@@ -132,7 +138,7 @@ public class DatabaseMigrationAcceptanceTest extends AcceptanceTestBase {
   }
 
   @Test
-  public void shouldReturnCorrectBlockHeight() throws ZipException {
+  public void shouldReturnCorrectBlockHeight() {
     blockchain.currentHeight(expectedChainHeight).verify(node);
   }
 
@@ -141,12 +147,12 @@ public class DatabaseMigrationAcceptanceTest extends AcceptanceTestBase {
     testAccount.balanceEquals(Amount.wei(expectedBalance.toBigInteger())).verify(node);
   }
 
-  private static void unzip(final Path zipFile, final String destDirectoryPath) throws IOException {
+  /*private static void unzip(final Path zipFile, final String destDirectoryPath) throws IOException {
     final ZipFile zip = new ZipFile(zipFile.toFile());
     zip.extractAll(destDirectoryPath);
-  }
+  }*/
 
-  /*private static void unzip(final Path path, final String destDirectory) throws IOException {
+  private static void unzip(final Path path, final String destDirectory) throws IOException {
     System.out.println("Unzip using IOUtils.");
     final Path dest = Paths.get(destDirectory);
     try (ZipFile zipFile = new ZipFile(path.toFile(), ZipFile.OPEN_READ, StandardCharsets.UTF_8)) {
@@ -166,7 +172,7 @@ public class DatabaseMigrationAcceptanceTest extends AcceptanceTestBase {
         }
       }
     }
-  }*/
+  }
 
   private Path copyDataDir(final URL url) {
     if (url == null) {
