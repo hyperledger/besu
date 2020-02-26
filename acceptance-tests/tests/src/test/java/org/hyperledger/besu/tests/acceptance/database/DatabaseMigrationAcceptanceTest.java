7f67b8671acd3c3cc6e6d2aa9d39d0a846925073
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-02-26 09:28:58 +0100

    Test CI
    Signed-off-by: Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>

--- acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/database/DatabaseMigrationAcceptanceTest.java
+++ acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/database/DatabaseMigrationAcceptanceTest.java
@@ -123,7 +123,7 @@ public class DatabaseMigrationAcceptanceTest extends AcceptanceTestBase {
     final String genesisData = getGenesisConfiguration();
     return nodeBuilder
         .devMode(false)
-        .dataPath(hostDataPath.resolve("data"))
+        .dataPath(hostDataPath)
         .genesisConfigProvider((nodes) -> Optional.of(genesisData))
         .jsonRpcEnabled();
   }
@@ -131,7 +131,7 @@ public class DatabaseMigrationAcceptanceTest extends AcceptanceTestBase {
   private String getGenesisConfiguration() {
     try {
       return Resources.toString(
-          hostDataPath.resolve("data").resolve("genesis.json").toUri().toURL(), Charsets.UTF_8);
+          hostDataPath.resolve("genesis.json").toUri().toURL(), Charsets.UTF_8);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
