cbd012ac32ea938d287a363200089294a3da99e4
Karim TAAM <karim.t2am@gmail.com>
2020-02-26 22:51:39 +0100

    resolve pipeline
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/sync/fastsync/FastSyncDownloaderTest.java
+++ ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/sync/fastsync/FastSyncDownloaderTest.java
@@ -16,6 +16,7 @@ package org.hyperledger.besu.ethereum.eth.sync.fastsync;
 
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -32,8 +33,10 @@ import org.hyperledger.besu.ethereum.eth.sync.worldstate.WorldStateDownloader;
 import org.hyperledger.besu.services.tasks.TaskCollection;
 
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -360,19 +363,13 @@ public class FastSyncDownloaderTest {
 
   @SuppressWarnings("unchecked")
   @Test
-  public void shouldResetFastSyncStateAndRestartProcessIfANonFastSyncExceptionOccurs() {
+  public void shouldScheduleNewRestartProcessIfNonFastSyncExceptionOccurs() {
     final CompletableFuture<Void> firstWorldStateFuture = new CompletableFuture<>();
-    final CompletableFuture<Void> secondWorldStateFuture = new CompletableFuture<>();
     final CompletableFuture<Void> chainFuture = new CompletableFuture<>();
-    final ChainDownloader secondChainDownloader = mock(ChainDownloader.class);
     final FastSyncState selectPivotBlockState = new FastSyncState(50);
     final FastSyncState secondSelectPivotBlockState = new FastSyncState(90);
     final BlockHeader pivotBlockHeader = new BlockHeaderTestFixture().number(50).buildHeader();
-    final BlockHeader secondPivotBlockHeader =
-        new BlockHeaderTestFixture().number(90).buildHeader();
     final FastSyncState downloadPivotBlockHeaderState = new FastSyncState(pivotBlockHeader);
-    final FastSyncState secondDownloadPivotBlockHeaderState =
-        new FastSyncState(secondPivotBlockHeader);
     // First attempt
     when(fastSyncActions.waitForSuitablePeers(FastSyncState.EMPTY_SYNC_STATE)).thenReturn(COMPLETE);
     when(fastSyncActions.selectPivotBlock(FastSyncState.EMPTY_SYNC_STATE))
@@ -384,15 +381,6 @@ public class FastSyncDownloaderTest {
         .thenReturn(chainDownloader);
     when(chainDownloader.start()).thenReturn(chainFuture);
     when(worldStateDownloader.run(pivotBlockHeader)).thenReturn(firstWorldStateFuture);
-
-    // Second attempt
-    when(fastSyncActions.downloadPivotBlockHeader(secondSelectPivotBlockState))
-        .thenReturn(completedFuture(secondDownloadPivotBlockHeaderState));
-
-    when(fastSyncActions.createChainDownloader(secondDownloadPivotBlockHeaderState))
-        .thenReturn(secondChainDownloader);
-    when(secondChainDownloader.start()).thenReturn(completedFuture(null));
-    when(worldStateDownloader.run(secondPivotBlockHeader)).thenReturn(secondWorldStateFuture);
 
     final CompletableFuture<FastSyncState> result = downloader.start();
 
@@ -413,17 +401,7 @@ public class FastSyncDownloaderTest {
     // A real chain downloader would cause the chainFuture to complete when cancel is called.
     chainFuture.completeExceptionally(new CancellationException());
 
-    verify(fastSyncActions, times(2)).waitForSuitablePeers(FastSyncState.EMPTY_SYNC_STATE);
-    verify(fastSyncActions, times(2)).selectPivotBlock(FastSyncState.EMPTY_SYNC_STATE);
-    verify(fastSyncActions).downloadPivotBlockHeader(secondSelectPivotBlockState);
-    verify(storage).storeState(secondDownloadPivotBlockHeaderState);
-    verify(fastSyncActions).createChainDownloader(secondDownloadPivotBlockHeaderState);
-    verify(worldStateDownloader).run(secondPivotBlockHeader);
-    verifyNoMoreInteractions(fastSyncActions, worldStateDownloader, storage);
-
-    secondWorldStateFuture.complete(null);
-
-    assertThat(result).isCompletedWithValue(secondDownloadPivotBlockHeaderState);
+    verify(fastSyncActions).scheduleFutureTask(any(Supplier.class), any(Duration.class));
   }
 
   @Test
