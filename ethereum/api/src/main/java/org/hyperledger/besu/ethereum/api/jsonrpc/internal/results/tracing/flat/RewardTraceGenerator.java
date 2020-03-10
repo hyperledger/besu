0344ca474b81c65657fc705b789bc6c31008d571
Karim TAAM <karim.t2am@gmail.com>
2020-03-10 13:59:22 +0100

    init trace_block api
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

---
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/results/tracing/flat/RewardTraceGenerator.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.ethereum.api.jsonrpc.internal.results.tracing.flat;
+
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.results.tracing.Trace;
+import org.hyperledger.besu.ethereum.core.Block;
+import org.hyperledger.besu.ethereum.core.BlockHeader;
+import org.hyperledger.besu.ethereum.core.Wei;
+import org.hyperledger.besu.ethereum.mainnet.MiningBeneficiaryCalculator;
+import org.hyperledger.besu.ethereum.mainnet.ProtocolSchedule;
+import org.hyperledger.besu.ethereum.mainnet.ProtocolSpec;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class RewardTraceGenerator {
+
+  private static final String REWARD_LABEL = "reward";
+  private static final String BLOCK_LABEL = "block";
+  private static final String UNCLE_LABEL = "uncle";
+
+  public static Stream<Trace> generateFromBlock(
+      final ProtocolSchedule<?> protocolSchedule, final Block block) {
+
+    final List<Trace> flatTraces = new ArrayList<>();
+
+    final BlockHeader blockHeader = block.getHeader();
+    final ProtocolSpec<?> protocolSpec = protocolSchedule.getByBlockNumber(blockHeader.getNumber());
+    final MiningBeneficiaryCalculator miningBeneficiaryCalculator =
+        protocolSpec.getMiningBeneficiaryCalculator();
+    final Wei blockReward = protocolSpec.getBlockReward();
+
+    // add block reward trace
+    final Action.Builder blockActionBuilder =
+        Action.builder()
+            .author(miningBeneficiaryCalculator.calculateBeneficiary(blockHeader).toHexString())
+            .rewardType(BLOCK_LABEL)
+            .value(blockReward.toShortHexString());
+    flatTraces.add(
+        RewardTrace.builder()
+            .actionBuilder(blockActionBuilder)
+            .blockHash(block.getHash().toHexString())
+            .blockNumber(blockHeader.getNumber())
+            .type(REWARD_LABEL)
+            .build());
+
+    // add uncle reward traces
+    block
+        .getBody()
+        .getOmmers()
+        .forEach(
+            ommerBlockHeader -> {
+              final long distance = blockHeader.getNumber() - ommerBlockHeader.getNumber();
+              final Wei ommerReward =
+                  blockReward.subtract(blockReward.multiply(distance).divide(8));
+              final Action.Builder uncleActionBuilder =
+                  Action.builder()
+                      .author(
+                          miningBeneficiaryCalculator
+                              .calculateBeneficiary(ommerBlockHeader)
+                              .toHexString())
+                      .rewardType(UNCLE_LABEL)
+                      .value(ommerReward.toShortHexString());
+              flatTraces.add(
+                  RewardTrace.builder()
+                      .actionBuilder(uncleActionBuilder)
+                      .blockHash(block.getHash().toHexString())
+                      .blockNumber(blockHeader.getNumber())
+                      .type(REWARD_LABEL)
+                      .build());
+            });
+
+    return flatTraces.stream();
+  }
+}
