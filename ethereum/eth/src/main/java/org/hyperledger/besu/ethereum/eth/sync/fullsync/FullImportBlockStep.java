480f0a54be1ec44ac7316e59f4e743d7d88860ec
Raw Pong Ghmoa <58883403+q9f@users.noreply.github.com>
2020-04-08 15:47:28 +0200

    eth/sync: augment log with peer count information (#631)
    Signed-off-by: Tim Beiko <t.beiko23@gmail.com>
    
    * ethereum/fullsync: augment log with peer count information
    
    Signed-off-by: q9f <58883403+q9f@users.noreply.github.com>
    
    * ethereum/fastsync: augment log with peer count information
    
    Signed-off-by: q9f <58883403+q9f@users.noreply.github.com>
    
    * ethereum/manager: augment log with peer count information
    
    Signed-off-by: q9f <58883403+q9f@users.noreply.github.com>
    
    * ethereum/manager: print full block hash in debug log information
    
    Signed-off-by: q9f <58883403+q9f@users.noreply.github.com>
    
    * ethereum/*sync: fix tests without passing ethContext
    
    Signed-off-by: q9f <58883403+q9f@users.noreply.github.com>
    
    * ethereum/sync: restore full block hash in info log
    
    Signed-off-by: /raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/fullsync/FullImportBlockStep.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/fullsync/FullImportBlockStep.java
@@ -17,6 +17,7 @@ package org.hyperledger.besu.ethereum.eth.sync.fullsync;
 import org.hyperledger.besu.ethereum.ProtocolContext;
 import org.hyperledger.besu.ethereum.core.Block;
 import org.hyperledger.besu.ethereum.core.BlockImporter;
+import org.hyperledger.besu.ethereum.eth.manager.EthContext;
 import org.hyperledger.besu.ethereum.eth.sync.tasks.exceptions.InvalidBlockException;
 import org.hyperledger.besu.ethereum.mainnet.HeaderValidationMode;
 import org.hyperledger.besu.ethereum.mainnet.ProtocolSchedule;
@@ -30,23 +31,37 @@ public class FullImportBlockStep<C> implements Consumer<Block> {
   private static final Logger LOG = LogManager.getLogger();
   private final ProtocolSchedule<C> protocolSchedule;
   private final ProtocolContext<C> protocolContext;
+  private final EthContext ethContext;
 
   public FullImportBlockStep(
-      final ProtocolSchedule<C> protocolSchedule, final ProtocolContext<C> protocolContext) {
+      final ProtocolSchedule<C> protocolSchedule,
+      final ProtocolContext<C> protocolContext,
+      final EthContext ethContext) {
     this.protocolSchedule = protocolSchedule;
     this.protocolContext = protocolContext;
+    this.ethContext = ethContext;
   }
 
   @Override
   public void accept(final Block block) {
     final long blockNumber = block.getHeader().getNumber();
+    final String blockHash = block.getHash().toHexString();
+    final String shortHash =
+        String.format(
+            "%s..%s",
+            blockHash.substring(0, 6),
+            blockHash.substring(blockHash.length() - 4, blockHash.length()));
     final BlockImporter<C> importer =
         protocolSchedule.getByBlockNumber(blockNumber).getBlockImporter();
     if (!importer.importBlock(protocolContext, block, HeaderValidationMode.SKIP_DETACHED)) {
       throw new InvalidBlockException("Failed to import block", blockNumber, block.getHash());
     }
+    int peerCount = -1; // ethContext is not available in tests
+    if (ethContext != null && ethContext.getEthPeers().peerCount() >= 0) {
+      peerCount = ethContext.getEthPeers().peerCount();
+    }
     if (blockNumber % 200 == 0) {
-      LOG.info("Import reached block {}", blockNumber);
+      LOG.info("Import reached block {} ({}), Peers: {}", blockNumber, shortHash, peerCount);
     }
   }
 }
