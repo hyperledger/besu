2129920eac67ce38bec952e81b21afcd96ad9e48
Danno Ferrin <danno.ferrin@gmail.com>
2020-04-12 09:16:25 -0600

    BLS12-381 precompiled contract support
    initial binding onto eth_parings via JNA, with test cases
    
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

---
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/precompiles/BLS12MapFieldToCurvePrecompiledContract.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+package org.hyperledger.besu.ethereum.mainnet.precompiles;
+
+import org.hyperledger.besu.ethereum.core.Gas;
+
+import org.apache.tuweni.bytes.Bytes;
+
+public class BLS12MapFieldToCurvePrecompiledContract extends AbstractBLS12PrecompiledContract {
+
+  public BLS12MapFieldToCurvePrecompiledContract() {
+    super("BLS12_MAP_FIELD_TO_CURVE", LibEthPairings.BLS12_MAP_OPERATION_RAW_VALUE);
+  }
+
+  @Override
+  public Gas gasRequirement(final Bytes input) {
+    if (input.size() >= 128) {
+      return Gas.of(110000);
+    } else {
+      return Gas.of(5500);
+    }
+  }
+}
