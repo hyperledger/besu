b41b35374db2204e9a7b90896fbc65daa2a8c831
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-04-14 17:11:57 +0200

    Besu transaction sign subcommand Testing purpose only. EIP-1559
    Signed-off-by: Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>

--- besu/src/main/java/org/hyperledger/besu/cli/subcommands/RpcClient.java
+++ besu/src/main/java/org/hyperledger/besu/cli/subcommands/RpcClient.java
@@ -1,16 +1,28 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.hyperledger.besu.cli.subcommands;
 
-import static org.hyperledger.besu.cli.subcommands.RpcClient.RpcMethod.ETH_SEND_RAW_TRANSACTION;
-
-import org.hyperledger.besu.ethereum.api.jsonrpc.RpcMethod;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 
 import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.RequestBody;
+import static org.hyperledger.besu.cli.subcommands.RpcClient.RpcMethod.ETH_SEND_RAW_TRANSACTION;
 
 public class RpcClient {
 
