02ca0557b624da1fe44d206155961a7c97d26b0a
Ratan Rai Sur <ratan.r.sur@gmail.com>
2020-04-21 15:21:24 -0400

    set default lock timeout to 1ms
    Signed-off-by: Ratan Rai Sur <ratan.r.sur@gmail.com>

--- plugins/rocksdb/src/main/java/org/hyperledger/besu/plugin/services/storage/rocksdb/configuration/RocksDBFactoryConfiguration.java
+++ plugins/rocksdb/src/main/java/org/hyperledger/besu/plugin/services/storage/rocksdb/configuration/RocksDBFactoryConfiguration.java
@@ -20,16 +20,19 @@ public class RocksDBFactoryConfiguration {
   private final int maxBackgroundCompactions;
   private final int backgroundThreadCount;
   private final long cacheCapacity;
+  private final int defaultLockTimeout;
 
   public RocksDBFactoryConfiguration(
       final int maxOpenFiles,
       final int maxBackgroundCompactions,
       final int backgroundThreadCount,
-      final long cacheCapacity) {
+      final long cacheCapacity,
+      final int defaultLockTimeout) {
     this.maxBackgroundCompactions = maxBackgroundCompactions;
     this.backgroundThreadCount = backgroundThreadCount;
     this.maxOpenFiles = maxOpenFiles;
     this.cacheCapacity = cacheCapacity;
+    this.defaultLockTimeout = defaultLockTimeout;
   }
 
   public int getMaxOpenFiles() {
@@ -46,5 +49,9 @@ public class RocksDBFactoryConfiguration {
 
   public long getCacheCapacity() {
     return cacheCapacity;
+  }
+
+  public int getDefaultLockTimeout() {
+    return defaultLockTimeout;
   }
 }
