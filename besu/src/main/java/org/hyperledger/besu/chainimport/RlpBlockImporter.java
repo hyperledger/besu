117656fae0d21e5f14a9152cce3c09dc3c8bbcd1
Danno Ferrin <danno.ferrin@gmail.com>
2020-04-28 15:57:23 -0600

    Add `--skip-pow-validation-enabled` to block import command
    To support Ethereum Foundation Hive testing an option to ignore the
    validity of the mixHash is needed in the block importer command.  All
    other validation for the blocks is still performed.
    
    Fixes #803
    
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- besu/src/main/java/org/hyperledger/besu/chainimport/RlpBlockImporter.java
+++ besu/src/main/java/org/hyperledger/besu/chainimport/RlpBlockImporter.java
@@ -64,7 +64,8 @@ public class RlpBlockImporter {
    * @throws IOException On Failure
    */
   public <C> RlpBlockImporter.ImportResult importBlockchain(
-      final Path blocks, final BesuController<C> besuController) throws IOException {
+      final Path blocks, final BesuController<C> besuController, final boolean skipPowValidation)
+      throws IOException {
     final ProtocolSchedule<C> protocolSchedule = besuController.getProtocolSchedule();
     final ProtocolContext<C> context = besuController.getProtocolContext();
     final MutableBlockchain blockchain = context.getBlockchain();
@@ -98,7 +99,8 @@ public class RlpBlockImporter {
 
         final CompletableFuture<Void> validationFuture =
             CompletableFuture.runAsync(
-                () -> validateBlock(protocolSpec, context, lastHeader, header), validationExecutor);
+                () -> validateBlock(protocolSpec, context, lastHeader, header, skipPowValidation),
+                validationExecutor);
 
         final CompletableFuture<Void> extractingFuture =
             CompletableFuture.runAsync(() -> extractSignatures(block));
@@ -119,7 +121,7 @@ public class RlpBlockImporter {
         previousBlockFuture =
             validationFuture.runAfterBothAsync(
                 calculationFutures,
-                () -> evaluateBlock(context, block, header, protocolSpec),
+                () -> evaluateBlock(context, block, header, protocolSpec, skipPowValidation),
                 importExecutor);
 
         ++count;
@@ -162,11 +164,17 @@ public class RlpBlockImporter {
       final ProtocolSpec<C> protocolSpec,
       final ProtocolContext<C> context,
       final BlockHeader previousHeader,
-      final BlockHeader header) {
+      final BlockHeader header,
+      final boolean skipPowValidation) {
     final BlockHeaderValidator<C> blockHeaderValidator = protocolSpec.getBlockHeaderValidator();
     final boolean validHeader =
         blockHeaderValidator.validateHeader(
-            header, previousHeader, context, HeaderValidationMode.DETACHED_ONLY);
+            header,
+            previousHeader,
+            context,
+            skipPowValidation
+                ? HeaderValidationMode.LIGHT_DETACHED_ONLY
+                : HeaderValidationMode.DETACHED_ONLY);
     if (!validHeader) {
       throw new IllegalStateException("Invalid header at block number " + header.getNumber() + ".");
     }
@@ -176,11 +184,20 @@ public class RlpBlockImporter {
       final ProtocolContext<C> context,
       final Block block,
       final BlockHeader header,
-      final ProtocolSpec<C> protocolSpec) {
+      final ProtocolSpec<C> protocolSpec,
+      final boolean skipPowValidation) {
     try {
       final BlockImporter<C> blockImporter = protocolSpec.getBlockImporter();
       final boolean blockImported =
-          blockImporter.importBlock(context, block, HeaderValidationMode.SKIP_DETACHED);
+          blockImporter.importBlock(
+              context,
+              block,
+              skipPowValidation
+                  ? HeaderValidationMode.LIGHT_SKIP_DETACHED
+                  : HeaderValidationMode.SKIP_DETACHED,
+              skipPowValidation
+                  ? HeaderValidationMode.LIGHT
+                  : HeaderValidationMode.FULL);
       if (!blockImported) {
         throw new IllegalStateException(
             "Invalid block at block number " + header.getNumber() + ".");
--- besu/src/test/java/org/hyperledger/besu/chainimport/RlpBlockImporterTest.java
+++ besu/src/test/java/org/hyperledger/besu/chainimport/RlpBlockImporterTest.java
@@ -36,9 +36,11 @@ import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.concurrent.CompletionException;
 
 import com.google.common.io.Resources;
 import org.apache.tuweni.units.bigints.UInt256;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -72,10 +74,66 @@ public final class RlpBlockImporterTest {
             .targetGasLimit(GasLimitCalculator.DEFAULT)
             .build();
     final RlpBlockImporter.ImportResult result =
-        rlpBlockImporter.importBlockchain(source, targetController);
+        rlpBlockImporter.importBlockchain(source, targetController, false);
     // Don't count the Genesis block
     assertThat(result.count).isEqualTo(999);
     assertThat(result.td).isEqualTo(UInt256.valueOf(21991996248790L));
+  }
+
+  @Test
+  public void blockImportRejectsBadPow() throws IOException {
+    final Path dataDir = folder.newFolder().toPath();
+    final Path source = dataDir.resolve("badpow.blocks");
+    BlockTestUtil.writeBadPowBlocks(source);
+    final BesuController<?> targetController =
+        new BesuController.Builder()
+            .fromGenesisConfig(GenesisConfigFile.mainnet())
+            .synchronizerConfiguration(SynchronizerConfiguration.builder().build())
+            .ethProtocolConfiguration(EthProtocolConfiguration.defaultConfig())
+            .storageProvider(new InMemoryStorageProvider())
+            .networkId(BigInteger.ONE)
+            .miningParameters(new MiningParametersTestBuilder().enabled(false).build())
+            .nodeKey(NodeKeyUtils.generate())
+            .metricsSystem(new NoOpMetricsSystem())
+            .privacyParameters(PrivacyParameters.DEFAULT)
+            .dataDirectory(dataDir)
+            .clock(TestClock.fixed())
+            .transactionPoolConfiguration(TransactionPoolConfiguration.builder().build())
+            .targetGasLimit(GasLimitCalculator.DEFAULT)
+            .build();
+
+    Assert.assertThrows(
+        "Invalid header at block number 2.",
+        CompletionException.class,
+        () -> rlpBlockImporter.importBlockchain(source, targetController, false));
+  }
+
+  @Test
+  public void blockImportCanSkipPow() throws IOException {
+    final Path dataDir = folder.newFolder().toPath();
+    final Path source = dataDir.resolve("badpow.blocks");
+    BlockTestUtil.writeBadPowBlocks(source);
+    final BesuController<?> targetController =
+        new BesuController.Builder()
+            .fromGenesisConfig(GenesisConfigFile.mainnet())
+            .synchronizerConfiguration(SynchronizerConfiguration.builder().build())
+            .ethProtocolConfiguration(EthProtocolConfiguration.defaultConfig())
+            .storageProvider(new InMemoryStorageProvider())
+            .networkId(BigInteger.ONE)
+            .miningParameters(new MiningParametersTestBuilder().enabled(false).build())
+            .nodeKey(NodeKeyUtils.generate())
+            .metricsSystem(new NoOpMetricsSystem())
+            .privacyParameters(PrivacyParameters.DEFAULT)
+            .dataDirectory(dataDir)
+            .clock(TestClock.fixed())
+            .transactionPoolConfiguration(TransactionPoolConfiguration.builder().build())
+            .targetGasLimit(GasLimitCalculator.DEFAULT)
+            .build();
+
+    final RlpBlockImporter.ImportResult result =
+        rlpBlockImporter.importBlockchain(source, targetController, true);
+    assertThat(result.count).isEqualTo(1);
+    assertThat(result.td).isEqualTo(UInt256.valueOf(34351349760L));
   }
 
   @Test
@@ -112,7 +170,7 @@ public final class RlpBlockImporterTest {
             .targetGasLimit(GasLimitCalculator.DEFAULT)
             .build();
     final RlpBlockImporter.ImportResult result =
-        rlpBlockImporter.importBlockchain(source, controller);
+        rlpBlockImporter.importBlockchain(source, controller, false);
 
     // Don't count the Genesis block
     assertThat(result.count).isEqualTo(958);
