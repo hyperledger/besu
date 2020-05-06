cec87497c818480cfd51e50763c9cc29815ce08c
Ratan Rai Sur <ratan.r.sur@gmail.com>
2020-05-06 16:34:12 -0400

    add back throws javadoc
    Signed-off-by: Ratan Rai Sur <ratan.r.sur@gmail.com>

--- plugin-api/src/main/java/org/hyperledger/besu/plugin/services/storage/KeyValueStorage.java
+++ plugin-api/src/main/java/org/hyperledger/besu/plugin/services/storage/KeyValueStorage.java
@@ -76,6 +76,7 @@ public interface KeyValueStorage extends Closeable {
    * @param retainCondition predicate to evaluate each key against, unless the result is {@code
    *     null}, both the key and associated value must be removed.
    * @return the number of keys removed.
+   * @throws StorageException problem encountered during the retrieval attempt.
    */
   long removeAllKeysUnless(Predicate<byte[]> retainCondition) throws StorageException;
 
