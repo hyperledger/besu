8cffe1e1909df45d3692ee283da3ea046c011a95
Danno Ferrin <danno.ferrin@gmail.com>
2020-05-15 13:38:49 -0600

    Support rocks DBs in EVMTool
    Allows the DB of a stopped node to be used for benchmarking.
    
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- ethereum/evmtool/src/main/java/org/hyperledger/besu/evmtool/EvmToolCommandOptionsModule.java
+++ ethereum/evmtool/src/main/java/org/hyperledger/besu/evmtool/EvmToolCommandOptionsModule.java
@@ -16,10 +16,19 @@
 
 package org.hyperledger.besu.evmtool;
 
+import static org.hyperledger.besu.cli.DefaultCommandValues.getDefaultBesuDataPath;
+
+import org.hyperledger.besu.controller.BesuController;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.parameters.BlockParameter;
+import org.hyperledger.besu.plugin.services.BesuConfiguration;
+import org.hyperledger.besu.services.BesuConfigurationImpl;
+
+import java.nio.file.Path;
 import javax.inject.Named;
 
 import dagger.Module;
 import dagger.Provides;
+import picocli.CommandLine;
 import picocli.CommandLine.Option;
 
 @SuppressWarnings("WeakerAccess")
@@ -36,7 +45,45 @@ public class EvmToolCommandOptionsModule {
 
   @Provides
   @Named("RevertReasonEnabled")
-  boolean isRevertReasonEnabled() {
+  boolean provideRevertReasonEnabled() {
     return revertReasonEnabled;
+  }
+
+  @SuppressWarnings({"FieldCanBeFinal", "FieldMayBeFinal"}) // PicoCLI requires non-final Strings.
+  @Option(
+      names = {"--key-value-storage"},
+      description =
+          "Identity for the key-value storage to be used (default: 'memory' alternate: 'rocksdb')",
+      arity = "1")
+  private String keyValueStorageName = "memory";
+
+  @Provides
+  @Named("KeyValueStorageName")
+  String provideKeyValueStorageName() {
+    return keyValueStorageName;
+  }
+
+  @CommandLine.Option(
+      names = {"--data-path"},
+      paramLabel = "<PATH>",
+      description =
+          "If using RocksDB storage, the path to Besu data directory (default: ${DEFAULT-VALUE})")
+  final Path dataPath = getDefaultBesuDataPath(this);
+
+  @Provides
+  BesuConfiguration provideBesuConfiguration() {
+    return new BesuConfigurationImpl(dataPath, dataPath.resolve(BesuController.DATABASE_PATH));
+  }
+
+  @Option(
+      names = {"--block-number"},
+      description =
+          "Block number to evaluate against (default: 'PENDING', or 'EARLIEST', 'LATEST', or a number)",
+      arity = "1")
+  final private BlockParameter blockParameter = BlockParameter.PENDING;
+
+  @Provides
+  BlockParameter provideBlockParameter() {
+    return blockParameter;
   }
 }
