9c37887df42f70e695cacb71c3ea2195c05c4c62
Abdelhamid Bakhta <45264458+abdelhamidbakhta@users.noreply.github.com>
2020-06-02 19:43:32 +0200

    Reintroduce consortium network friendly EIP-2124 fork id management. (#1018)
    * Handle backward compatible fork id.
    
    Signed-off-by: Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/ForkIdManager.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/ForkIdManager.java
@@ -14,6 +14,9 @@
  */
 package org.hyperledger.besu.ethereum.eth.manager;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Collections.singletonList;
+
 import org.hyperledger.besu.ethereum.chain.Blockchain;
 import org.hyperledger.besu.ethereum.core.Hash;
 import org.hyperledger.besu.ethereum.rlp.BytesValueRLPOutput;
@@ -21,7 +24,10 @@ import org.hyperledger.besu.ethereum.rlp.RLPInput;
 import org.hyperledger.besu.ethereum.rlp.RLPOutput;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.zip.CRC32;
 
@@ -30,22 +36,60 @@ import org.apache.tuweni.bytes.Bytes32;
 
 public class ForkIdManager {
 
-  private final Blockchain blockchain;
   private final Hash genesisHash;
-  private final List<Long> forks;
-  private long forkNext;
-  private final long highestKnownFork;
-  private List<ForkId> forkAndHashList;
+  private final List<ForkId> forkAndHashList;
+
+  private final List<Predicate<ForkId>> forkIDCheckers;
 
   public ForkIdManager(final Blockchain blockchain, final List<Long> forks) {
-    this.blockchain = blockchain;
+    checkNotNull(blockchain);
+    checkNotNull(forks);
     this.genesisHash = blockchain.getGenesisBlock().getHash();
-    // de-dupe and sanitize forks
-    this.forks =
-        forks.stream().filter(fork -> fork > 0).distinct().collect(Collectors.toUnmodifiableList());
-    highestKnownFork = forks.size() > 0 ? forks.get(forks.size() - 1) : 0L;
-    createForkIds();
-  };
+    this.forkAndHashList = new ArrayList<>();
+    final Predicate<ForkId> legacyForkIdChecker =
+        createForkIDChecker(
+            blockchain,
+            genesisHash,
+            forks,
+            fs ->
+                fs.stream()
+                    .filter(fork -> fork > 0)
+                    .distinct()
+                    .collect(Collectors.toUnmodifiableList()),
+            forkAndHashList);
+    // if the fork list contains only zeros then we may be in a consortium/dev network
+    if (onlyZerosForkBlocks(forks)) {
+      this.forkIDCheckers = singletonList(forkId -> true);
+    } else {
+      final Predicate<ForkId> newForkIdChecker =
+          createForkIDChecker(
+              blockchain,
+              genesisHash,
+              forks,
+              fs -> fs.stream().distinct().collect(Collectors.toUnmodifiableList()),
+              new ArrayList<>());
+      this.forkIDCheckers = Arrays.asList(newForkIdChecker, legacyForkIdChecker);
+    }
+  }
+
+  private static Predicate<ForkId> createForkIDChecker(
+      final Blockchain blockchain,
+      final Hash genesisHash,
+      final List<Long> forks,
+      final Function<List<Long>, List<Long>> sanitizer,
+      final List<ForkId> forkIds) {
+    final List<Long> sanitizedForks = sanitizer.apply(forks);
+    final long forkNext = createForkIds(genesisHash, sanitizedForks, forkIds);
+    return eip2124(blockchain, forkNext, forkIds, highestKnownFork(sanitizedForks));
+  }
+
+  private static boolean onlyZerosForkBlocks(final List<Long> forks) {
+    return forks.stream().allMatch(value -> 0L == value);
+  }
+
+  private static long highestKnownFork(final List<Long> forks) {
+    return !forks.isEmpty() ? forks.get(forks.size() - 1) : 0L;
+  }
 
   public List<ForkId> getForkAndHashList() {
     return this.forkAndHashList;
@@ -73,40 +117,50 @@ public class ForkIdManager {
    * @return boolean (peer valid (true) or invalid (false))
    */
   boolean peerCheck(final ForkId forkId) {
-    if (forkId == null) {
-      return true; // Another method must be used to validate (i.e. genesis hash)
-    }
-    // Run the fork checksum validation rule set:
-    //   1. If local and remote FORK_CSUM matches, connect.
-    //        The two nodes are in the same fork state currently. They might know
-    //        of differing future forks, but that's not relevant until the fork
-    //        triggers (might be postponed, nodes might be updated to match).
-    //   2. If the remote FORK_CSUM is a subset of the local past forks and the
-    //      remote FORK_NEXT matches with the locally following fork block number,
-    //      connect.
-    //        Remote node is currently syncing. It might eventually diverge from
-    //        us, but at this current point in time we don't have enough information.
-    //   3. If the remote FORK_CSUM is a superset of the local past forks and can
-    //      be completed with locally known future forks, connect.
-    //        Local node is currently syncing. It might eventually diverge from
-    //        the remote, but at this current point in time we don't have enough
-    //        information.
-    //   4. Reject in all other cases.
-    if (isHashKnown(forkId.getHash())) {
-      if (blockchain.getChainHeadBlockNumber() < forkNext) {
-        return true;
-      } else {
-        if (isForkKnown(forkId.getNext())) {
-          return isRemoteAwareOfPresent(forkId.getHash(), forkId.getNext());
-        } else {
-          return false;
-        }
-      }
-    } else {
-      return false;
-    }
+    return forkIDCheckers.stream().anyMatch(checker -> checker.test(forkId));
   }
 
+  private static Predicate<ForkId> eip2124(
+      final Blockchain blockchain,
+      final long forkNext,
+      final List<ForkId> forkAndHashList,
+      final long highestKnownFork) {
+    return forkId -> {
+      if (forkId == null) {
+        return true; // Another method must be used to validate (i.e. genesis hash)
+      }
+      // Run the fork checksum validation rule set:
+      //   1. If local and remote FORK_CSUM matches, connect.
+      //        The two nodes are in the same fork state currently. They might know
+      //        of differing future forks, but that's not relevant until the fork
+      //        triggers (might be postponed, nodes might be updated to match).
+      //   2. If the remote FORK_CSUM is a subset of the local past forks and the
+      //      remote FORK_NEXT matches with the locally following fork block number,
+      //      connect.
+      //        Remote node is currently syncing. It might eventually diverge from
+      //        us, but at this current point in time we don't have enough information.
+      //   3. If the remote FORK_CSUM is a superset of the local past forks and can
+      //      be completed with locally known future forks, connect.
+      //        Local node is currently syncing. It might eventually diverge from
+      //        the remote, but at this current point in time we don't have enough
+      //        information.
+      //   4. Reject in all other cases.
+      if (isHashKnown(forkId.getHash(), forkAndHashList)) {
+        if (blockchain.getChainHeadBlockNumber() < forkNext) {
+          return true;
+        } else {
+          if (isForkKnown(forkId.getNext(), highestKnownFork, forkAndHashList)) {
+            return isRemoteAwareOfPresent(
+                forkId.getHash(), forkId.getNext(), highestKnownFork, forkAndHashList);
+          } else {
+            return false;
+          }
+        }
+      } else {
+        return false;
+      }
+    };
+  }
   /**
    * Non EIP-2124 behaviour
    *
@@ -117,16 +171,21 @@ public class ForkIdManager {
     return !peerGenesisHash.equals(genesisHash);
   }
 
-  private boolean isHashKnown(final Bytes forkHash) {
+  private static boolean isHashKnown(final Bytes forkHash, final List<ForkId> forkAndHashList) {
     return forkAndHashList.stream().map(ForkId::getHash).anyMatch(hash -> hash.equals(forkHash));
   }
 
-  private boolean isForkKnown(final Long nextFork) {
+  private static boolean isForkKnown(
+      final Long nextFork, final long highestKnownFork, final List<ForkId> forkAndHashList) {
     return highestKnownFork < nextFork
         || forkAndHashList.stream().map(ForkId::getNext).anyMatch(fork -> fork.equals(nextFork));
   }
 
-  private boolean isRemoteAwareOfPresent(final Bytes forkHash, final Long nextFork) {
+  private static boolean isRemoteAwareOfPresent(
+      final Bytes forkHash,
+      final Long nextFork,
+      final long highestKnownFork,
+      final List<ForkId> forkAndHashList) {
     for (final ForkId j : forkAndHashList) {
       if (forkHash.equals(j.getHash())) {
         if (nextFork.equals(j.getNext())) {
@@ -141,7 +200,8 @@ public class ForkIdManager {
     return false;
   }
 
-  private void createForkIds() {
+  private static long createForkIds(
+      final Hash genesisHash, final List<Long> forks, final List<ForkId> forkIds) {
     final CRC32 crc = new CRC32();
     crc.update(genesisHash.toArray());
     final List<Bytes> forkHashes = new ArrayList<>(List.of(getCurrentCrcHash(crc)));
@@ -149,24 +209,24 @@ public class ForkIdManager {
       updateCrc(crc, fork);
       forkHashes.add(getCurrentCrcHash(crc));
     }
-    final List<ForkId> forkIds = new ArrayList<>();
     // This loop is for all the fork hashes that have an associated "next fork"
     for (int i = 0; i < forks.size(); i++) {
       forkIds.add(new ForkId(forkHashes.get(i), forks.get(i)));
     }
+    long forkNext = 0;
     if (!forks.isEmpty()) {
       forkNext = forkIds.get(forkIds.size() - 1).getNext();
       forkIds.add(new ForkId(forkHashes.get(forkHashes.size() - 1), 0));
     }
-    this.forkAndHashList = forkIds;
+    return forkNext;
   }
 
-  private void updateCrc(final CRC32 crc, final Long block) {
+  private static void updateCrc(final CRC32 crc, final Long block) {
     final byte[] byteRepresentationFork = longToBigEndian(block);
     crc.update(byteRepresentationFork, 0, byteRepresentationFork.length);
   }
 
-  private Bytes getCurrentCrcHash(final CRC32 crc) {
+  private static Bytes getCurrentCrcHash(final CRC32 crc) {
     return Bytes.ofUnsignedInt(crc.getValue());
   }
 
