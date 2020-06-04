df8ae42a0b87263aca5775098955ef08e40b7dbe
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-06-04 14:10:51 +0200

    Reset default timeout value.
    Signed-off-by: Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/handlers/TimeoutOptions.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/handlers/TimeoutOptions.java
@@ -18,7 +18,7 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class TimeoutOptions {
-  private static final long DEFAULT_TIMEOUT_SECONDS = Duration.ofSeconds(5).toSeconds();
+  private static final long DEFAULT_TIMEOUT_SECONDS = Duration.ofMinutes(5).toSeconds();
   private static final int DEFAULT_ERROR_CODE = 504;
   private final long timeoutSec;
 
