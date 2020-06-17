98ac6671934275c404d146db76358a980ba1dacc
Sally MacFarlane <sally.macfarlane@consensys.net>
2020-06-17 11:35:26 +1000

    deprecate whitelist rpc
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/permissioning/PermGetNodesWhitelist.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/permissioning/PermGetNodesWhitelist.java
@@ -27,6 +27,7 @@ import org.hyperledger.besu.ethereum.permissioning.NodeLocalConfigPermissioningC
 import java.util.List;
 import java.util.Optional;
 
+@Deprecated
 public class PermGetNodesWhitelist implements JsonRpcMethod {
 
   private final Optional<NodeLocalConfigPermissioningController>
