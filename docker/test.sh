8067ff4f82d9b8b7a8a7b88a54b967c884d3e75c
Danno Ferrin <danno.ferrin@gmail.com>
2020-08-06 08:32:53 -0600

    Multiple Docker Images with different JVMs (#1277)
    Create different Docker images with different base JVMs.  Initially
    OpenJDK 11.0.7, whatever OpenJDK latest is (14.0.1 at the moment), and
    Graal 20.1.0 .
    
    We still maintain a "Default" which will remain Java 11.0.x
    
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- docker/test.sh
+++ docker/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export GOSS_PATH=tests/goss-linux-amd64
+export TEST_PATH=../tests
+export GOSS_PATH=$TEST_PATH/goss-linux-amd64
 export GOSS_OPTS="$GOSS_OPTS --format junit"
 export GOSS_FILES_STRATEGY=cp
 DOCKER_IMAGE=$1
@@ -9,21 +10,21 @@ DOCKER_FILE="${2:-$PWD/Dockerfile}"
 i=0
 
 ## Checks on the Dockerfile
-GOSS_FILES_PATH=tests/00 \
-bash tests/dgoss dockerfile $DOCKER_IMAGE $DOCKER_FILE \
-> ./reports/00.xml || i=`expr $i + 1`
+GOSS_FILES_PATH=$TEST_PATH/00 \
+bash $TEST_PATH/dgoss dockerfile $DOCKER_IMAGE $DOCKER_FILE \
+> ../reports/00.xml || i=`expr $i + 1`
 # fail fast if we dont pass static checks
 if [[ $i != 0 ]]; then exit $i; fi
 
 # Test for normal startup with ports opened
 # we test that things listen on the right interface/port, not what interface the advertise
 # hence we dont set p2p-host=0.0.0.0 because this sets what its advertising to devp2p; the important piece is that it defaults to listening on all interfaces
-GOSS_FILES_PATH=tests/01 \
-bash tests/dgoss run $DOCKER_IMAGE \
+GOSS_FILES_PATH=$TEST_PATH/01 \
+bash $TEST_PATH/dgoss run $DOCKER_IMAGE \
 --network=dev \
 --rpc-http-enabled \
 --rpc-ws-enabled \
 --graphql-http-enabled \
-> ./reports/01.xml || i=`expr $i + 1`
+> ../reports/01.xml || i=`expr $i + 1`
 
 exit $i
