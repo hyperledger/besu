baa395cf36dcadfb3eb5660ce29bad2d4a619001
Danno Ferrin <danno.ferrin@gmail.com>
2020-10-29 17:41:58 -0600

    spotless
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/PrecompiledContract.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/PrecompiledContract.java
@@ -17,8 +17,9 @@ package org.hyperledger.besu.ethereum.mainnet;
 import org.hyperledger.besu.ethereum.core.Gas;
 import org.hyperledger.besu.ethereum.vm.MessageFrame;
 
-import org.apache.tuweni.bytes.Bytes;
 import javax.annotation.Nonnull;
+
+import org.apache.tuweni.bytes.Bytes;
 
 /**
  * A pre-compiled contract.
