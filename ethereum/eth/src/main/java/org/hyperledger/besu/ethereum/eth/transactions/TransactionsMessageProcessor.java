b4db75b52e20a2d132ae855d4a61214c75ff1d7d
Karim TAAM <karim.t2am@gmail.com>
2020-11-26 20:18:27 +0100

    update transaction processor
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/transactions/TransactionsMessageProcessor.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/transactions/TransactionsMessageProcessor.java
@@ -78,16 +78,8 @@ class TransactionsMessageProcessor {
       final Iterator<Transaction> readTransactions =
           transactionsMessage.transactions(Transaction::readFrom);
       final Set<Transaction> transactions = Sets.newHashSet(readTransactions);
-      // transactionTracker.markTransactionsAsSeen(peer, transactions);
-      // transactionPool.addRemoteTransactions(transactions);
-      LOG.trace(
-          transactions.getClass()
-              + " "
-              + peer.getClass()
-              + " "
-              + transactionTracker
-              + " "
-              + transactionPool);
+      transactionTracker.markTransactionsAsSeen(peer, transactions);
+      transactionPool.addRemoteTransactions(transactions);
     } catch (final RLPException ex) {
       if (peer != null) {
         LOG.debug("Malformed transaction message received, disconnecting: {}", peer, ex);
