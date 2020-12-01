0b21e5ac2bc2eb38d71476afcba4258fd6bf69be
Sally MacFarlane <sally.macfarlane@consensys.net>
2020-12-01 12:47:58 +1000

    string format
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/Transaction.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/Transaction.java
@@ -133,7 +133,7 @@ public class Transaction implements org.hyperledger.besu.plugin.data.Transaction
       final Optional<BigInteger> v) {
     if (v.isPresent() && chainId.isPresent()) {
       throw new IllegalStateException(
-          String.format("chainId {} and v {} cannot both be provided", chainId.get(), v.get()));
+          String.format("chainId '%s' and v '%s' cannot both be provided", chainId.get(), v.get()));
     }
     this.nonce = nonce;
     this.gasPrice = gasPrice;
