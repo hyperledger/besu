28b341b70cd172a0229fda340542cf0224aeb042
Stefan Pingel <stefan.pingel@consensys.net>
2020-12-16 14:59:56 +1000

    add goquorum privacy configuration
    Signed-off-by: Stefan Pingel <stefan.pingel@consensys.net>

---
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/GoQuorumPrivacyParameters.java
@@ -0,0 +1,12 @@
+package org.hyperledger.besu.ethereum.core;
+
+import org.hyperledger.besu.enclave.GoQuorumEnclave;
+
+public class GoQuorumPrivacyParameters {
+
+  public static boolean isEnabled = false;
+
+  public static GoQuorumEnclave goQuorumEnclave;
+
+  public static String enclaveKey;
+}
