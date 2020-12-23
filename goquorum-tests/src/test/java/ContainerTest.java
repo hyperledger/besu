e5c1daa6c7c3c1c4f23f703e3ddfb0239863b1ff
Lucas Saldanha <lucascrsaldanha@gmail.com>
2020-12-23 15:49:26 +1300

    Added GoQuorum integration tests prototype
    Signed-off-by: Lucas Saldanha <lucascrsaldanha@gmail.com>

---
+++ goquorum-tests/src/test/java/ContainerTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class ContainerTest {
+
+  @ClassRule
+  public static GenericContainer besuContainer =
+      new GenericContainer("hyperledger/besu:20.10.3")
+          .withExposedPorts(8545, 30303)
+          .withEnv(
+              Map.of(
+                  "NETWORK", "dev",
+                  "BESU_RPC_HTTP_ENABLED", "true"));
+
+  @Test
+  public void testBesuGetVersion() {
+    Web3j web3 =
+        Web3j.build(
+            new HttpService(
+                "http://"
+                    + besuContainer.getContainerIpAddress()
+                    + ":"
+                    + besuContainer.getMappedPort(8545)));
+
+    web3.web3ClientVersion()
+        .flowable()
+        .subscribe(
+            x -> {
+              assertThat(x.getWeb3ClientVersion()).contains("20.10.3");
+            })
+        .dispose();
+  }
+}
