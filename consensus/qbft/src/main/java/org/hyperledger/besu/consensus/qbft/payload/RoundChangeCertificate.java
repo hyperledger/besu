a938078c2c3dd9fa6f82ee48b374f183fae2d8d1
Trent Mohay <trent.mohay@consensys.net>
2021-01-06 08:51:29 +1100

    Added proposal and roundChange QBFT msgs
    Signed-off-by: Trent Mohay <trent.mohay@consensys.net>

---
+++ consensus/qbft/src/main/java/org/hyperledger/besu/consensus/qbft/payload/RoundChangeCertificate.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.consensus.qbft.payload;
+
+import org.hyperledger.besu.consensus.common.bft.payload.SignedData;
+
+import java.util.Collection;
+import java.util.StringJoiner;
+
+public class RoundChangeCertificate {
+
+  private final Collection<SignedData<RoundChangePayload>> roundChangePayloads;
+  private final Collection<SignedData<PreparePayload>> prepares;
+
+  public RoundChangeCertificate(
+      final Collection<SignedData<RoundChangePayload>> roundChangePayloads,
+      final Collection<SignedData<PreparePayload>> prepares) {
+    this.roundChangePayloads = roundChangePayloads;
+    this.prepares = prepares;
+  }
+
+  public Collection<SignedData<PreparePayload>> getPrepares() {
+    return prepares;
+  }
+
+  public Collection<SignedData<RoundChangePayload>> getRoundChangePayloads() {
+    return roundChangePayloads;
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", RoundChangeCertificate.class.getSimpleName() + "[", "]")
+        .add("roundChangePayloads=" + roundChangePayloads)
+        .toString();
+  }
+}
