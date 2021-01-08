4381a14e4cec837dd41ccaf4e2a05a27ffd40b16
Trent Mohay <trent.mohay@consensys.net>
2021-01-08 15:53:52 +1100

    remove RoundChangeMetadata chagnes
    Signed-off-by: Trent Mohay <trent.mohay@consensys.net>

--- consensus/qbft/src/main/java/org/hyperledger/besu/consensus/qbft/payload/RoundChangeMetadata.java
+++ consensus/qbft/src/main/java/org/hyperledger/besu/consensus/qbft/payload/RoundChangeMetadata.java
@@ -15,74 +15,28 @@
 package org.hyperledger.besu.consensus.qbft.payload;
 
 import org.hyperledger.besu.consensus.common.bft.payload.SignedData;
-import org.hyperledger.besu.consensus.qbft.messagewrappers.RoundChange;
-import org.hyperledger.besu.ethereum.core.Block;
 
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
-
-import com.google.common.collect.Lists;
 
 public class RoundChangeMetadata {
 
-  private final Optional<Block> block;
-  private final List<SignedData<RoundChangePayload>> roundChangePayloads;
-  private final List<SignedData<PreparePayload>> prepares;
+  private final Collection<SignedData<RoundChangePayload>> roundChangePayloads;
+  private final Collection<SignedData<PreparePayload>> prepares;
 
   public RoundChangeMetadata(
-      final Optional<Block> block,
-      final List<SignedData<RoundChangePayload>> roundChangePayloads,
-      final List<SignedData<PreparePayload>> prepares) {
-    this.block = block;
+      final Collection<SignedData<RoundChangePayload>> roundChangePayloads,
+      final Collection<SignedData<PreparePayload>> prepares) {
     this.roundChangePayloads = roundChangePayloads;
     this.prepares = prepares;
   }
 
-  public List<SignedData<PreparePayload>> getPrepares() {
+  public Collection<SignedData<PreparePayload>> getPrepares() {
     return prepares;
   }
 
-  public List<SignedData<RoundChangePayload>> getRoundChangePayloads() {
+  public Collection<SignedData<RoundChangePayload>> getRoundChangePayloads() {
     return roundChangePayloads;
-  }
-
-  public Optional<Block> getBlock() {
-    return block;
-  }
-
-  public static RoundChangeMetadata create(final Collection<RoundChange> roundChanges) {
-
-    final Comparator<RoundChange> preparedRoundComparator =
-        (o1, o2) -> {
-          if (o1.getPreparedRound().isEmpty()) {
-            return -1;
-          }
-          if (o2.getPreparedRound().isEmpty()) {
-            return 1;
-          }
-          return o1.getPreparedRound().get().compareTo(o2.getPreparedRound().get());
-        };
-
-    final Optional<RoundChange> roundChangeWithNewestPrepare =
-        roundChanges.stream().max(preparedRoundComparator);
-
-    final Optional<Block> proposedBlock;
-    final List<SignedData<PreparePayload>> prepares = Lists.newArrayList();
-    if (roundChangeWithNewestPrepare.isPresent()) {
-      proposedBlock = roundChangeWithNewestPrepare.get().getProposedBlock();
-      prepares.addAll(roundChangeWithNewestPrepare.get().getPrepares());
-    } else {
-      proposedBlock = Optional.empty();
-    }
-
-    final List<SignedData<RoundChangePayload>> payloads =
-        roundChanges.stream().map(RoundChange::getSignedPayload).collect(Collectors.toList());
-
-    return new RoundChangeMetadata(proposedBlock, payloads, prepares);
   }
 
   @Override
