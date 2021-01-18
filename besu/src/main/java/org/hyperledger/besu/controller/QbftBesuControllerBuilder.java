f22bb0a4c27cba12c5efa8120b887f5e68fa890a
Jason Frame <jasonwframe@gmail.com>
2021-01-18 15:45:52 +1000

    remove IbftQueryPluginServiceFactory and bft forks
    Signed-off-by: Jason Frame <jasonwframe@gmail.com>

--- besu/src/main/java/org/hyperledger/besu/controller/QbftBesuControllerBuilder.java
+++ besu/src/main/java/org/hyperledger/besu/controller/QbftBesuControllerBuilder.java
@@ -15,7 +15,6 @@
 package org.hyperledger.besu.controller;
 
 import org.hyperledger.besu.config.BftConfigOptions;
-import org.hyperledger.besu.config.BftFork;
 import org.hyperledger.besu.config.GenesisConfigOptions;
 import org.hyperledger.besu.consensus.common.BftValidatorOverrides;
 import org.hyperledger.besu.consensus.common.BlockInterface;
@@ -71,10 +70,7 @@ import org.hyperledger.besu.ethereum.p2p.config.SubProtocolConfiguration;
 import org.hyperledger.besu.ethereum.worldstate.WorldStateArchive;
 import org.hyperledger.besu.util.Subscribers;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Collections;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -214,7 +210,7 @@ public class QbftBesuControllerBuilder extends BesuControllerBuilder {
 
   @Override
   protected PluginServiceFactory createAdditionalPluginServices(final Blockchain blockchain) {
-    return new IbftQueryPluginServiceFactory(blockchain, nodeKey);
+    return new NoopPluginServiceFactory();
   }
 
   @Override
@@ -242,36 +238,16 @@ public class QbftBesuControllerBuilder extends BesuControllerBuilder {
     final BftConfigOptions bftConfig = configOptions.getBftConfigOptions();
     final EpochManager epochManager = new EpochManager(bftConfig.getEpochLength());
 
-    final Map<Long, List<Address>> bftValidatorForkMap =
-        convertBftForks(configOptions.getTransitions().getIbftForks());
-
     return new BftContext(
         new ForkingVoteTallyCache(
             blockchain,
             new VoteTallyUpdater(epochManager, new BftBlockInterface()),
             epochManager,
             new BftBlockInterface(),
-            new BftValidatorOverrides(bftValidatorForkMap)),
+            new BftValidatorOverrides(Collections.emptyMap())),
         new VoteProposer(),
         epochManager,
         blockInterface);
-  }
-
-  private Map<Long, List<Address>> convertBftForks(final List<BftFork> bftForks) {
-    final Map<Long, List<Address>> result = new HashMap<>();
-
-    for (final BftFork fork : bftForks) {
-      fork.getValidators()
-          .map(
-              validators ->
-                  result.put(
-                      fork.getForkBlock(),
-                      validators.stream()
-                          .map(Address::fromHexString)
-                          .collect(Collectors.toList())));
-    }
-
-    return result;
   }
 
   private static MinedBlockObserver blockLogger(
