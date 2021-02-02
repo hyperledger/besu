85333cd928a0fa655b60dc93bac4b7e4ab5914ac
Usman Saleem <usman@usmans.info>
2021-02-02 15:19:04 +1000

    QBFT jsonrpc methods (#1865)
    Add QBFT rpc methods and namespace. At the moment, they are copied verbatim from IBFT namespace and may change in future once specifications are finalized.
    
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- besu/src/main/java/org/hyperledger/besu/controller/QbftBesuControllerBuilder.java
+++ besu/src/main/java/org/hyperledger/besu/controller/QbftBesuControllerBuilder.java
@@ -44,8 +44,8 @@ import org.hyperledger.besu.consensus.common.bft.protocol.BftProtocolManager;
 import org.hyperledger.besu.consensus.common.bft.statemachine.BftEventHandler;
 import org.hyperledger.besu.consensus.common.bft.statemachine.BftFinalState;
 import org.hyperledger.besu.consensus.common.bft.statemachine.FutureMessageBuffer;
-import org.hyperledger.besu.consensus.ibft.jsonrpc.IbftJsonRpcMethods;
 import org.hyperledger.besu.consensus.qbft.QbftGossip;
+import org.hyperledger.besu.consensus.qbft.jsonrpc.QbftJsonRpcMethods;
 import org.hyperledger.besu.consensus.qbft.payload.MessageFactory;
 import org.hyperledger.besu.consensus.qbft.protocol.QbftSubProtocol;
 import org.hyperledger.besu.consensus.qbft.statemachine.QbftBlockHeightManagerFactory;
@@ -96,7 +96,7 @@ public class QbftBesuControllerBuilder extends BesuControllerBuilder {
   @Override
   protected JsonRpcMethods createAdditionalJsonRpcMethodFactory(
       final ProtocolContext protocolContext) {
-    return new IbftJsonRpcMethods(protocolContext);
+    return new QbftJsonRpcMethods(protocolContext);
   }
 
   @Override
---
+++ consensus/qbft/src/main/java/org/hyperledger/besu/consensus/qbft/jsonrpc/methods/QbftGetValidatorsByBlockNumber.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.consensus.qbft.jsonrpc.methods;
+
+import org.hyperledger.besu.consensus.common.BlockInterface;
+import org.hyperledger.besu.ethereum.api.jsonrpc.RpcMethod;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.JsonRpcRequestContext;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.AbstractBlockParameterMethod;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.JsonRpcMethod;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.parameters.BlockParameter;
+import org.hyperledger.besu.ethereum.api.query.BlockchainQueries;
+import org.hyperledger.besu.ethereum.core.BlockHeader;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class QbftGetValidatorsByBlockNumber extends AbstractBlockParameterMethod
+    implements JsonRpcMethod {
+  private static final Logger LOG = LogManager.getLogger();
+
+  private final BlockInterface blockInterface;
+
+  public QbftGetValidatorsByBlockNumber(
+      final BlockchainQueries blockchainQueries, final BlockInterface blockInterface) {
+    super(blockchainQueries);
+    this.blockInterface = blockInterface;
+  }
+
+  @Override
+  protected BlockParameter blockParameter(final JsonRpcRequestContext request) {
+    return request.getRequiredParameter(0, BlockParameter.class);
+  }
+
+  @Override
+  protected Object resultByBlockNumber(
+      final JsonRpcRequestContext request, final long blockNumber) {
+    final Optional<BlockHeader> blockHeader =
+        getBlockchainQueries().getBlockHeaderByNumber(blockNumber);
+    LOG.trace("Received RPC rpcName={} block={}", getName(), blockNumber);
+    return blockHeader
+        .map(
+            header ->
+                blockInterface.validatorsInBlock(header).stream()
+                    .map(validator -> validator.toString())
+                    .collect(Collectors.toList()))
+        .orElse(null);
+  }
+
+  @Override
+  public String getName() {
+    return RpcMethod.QBFT_GET_VALIDATORS_BY_BLOCK_NUMBER.getMethodName();
+  }
+}
