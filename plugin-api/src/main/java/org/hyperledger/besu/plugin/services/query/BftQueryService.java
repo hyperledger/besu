9f5c6acc304a9089c4dc896e8830f36189f881a5
Trent Mohay <37158202+rain-on@users.noreply.github.com>
2021-02-02 14:41:13 +1100

    Create a QbftMetricsService (#1861)
    Enables QBFT metrics to be exported to an API for reading via a plugin.
    
    Signed-off-by: Trent Mohay <trent.mohay@consensys.net>
    (cherry picked from commit 08615f2b060729db81423a49669dfd2f25d671c2)
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- plugin-api/src/main/java/org/hyperledger/besu/plugin/services/query/BftQueryService.java
+++ plugin-api/src/main/java/org/hyperledger/besu/plugin/services/query/BftQueryService.java
@@ -19,14 +19,8 @@ import org.hyperledger.besu.plugin.data.BlockHeader;
 
 import java.util.Collection;
 
-/**
- * Allows for the IBFT 2.0 specific aspects of the block chain to be queried.
- *
- * @deprecated This interface has been replaced by {@link
- *     org.hyperledger.besu.plugin.services.query.BftQueryService}
- */
-@Deprecated
-public interface IbftQueryService extends PoaQueryService {
+/** Allows for the BFT specific aspects of the block chain to be queried. */
+public interface BftQueryService extends PoaQueryService {
 
   /**
    * Extracts the round number from the supplied header and returns it to the caller.
@@ -44,4 +38,12 @@ public interface IbftQueryService extends PoaQueryService {
    * @return The addresses of
    */
   Collection<Address> getSignersFrom(final BlockHeader header);
+
+  /**
+   * Returns the literal name of the BFT consensus mechanism is use (eg ibft or qbft), which forms
+   * the prefix for all BFT metrics.
+   *
+   * @return The name of the consensus mechanism being used by Besu
+   */
+  String getConsensusMechanismName();
 }
