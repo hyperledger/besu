635f5c9f380123300cef99579ce91357943e078f
Ratan Rai Sur <ratan.r.sur@gmail.com>
2021-02-09 16:57:08 -0600

    transaction pending result stuff
    Signed-off-by: Ratan Rai Sur <ratan.r.sur@gmail.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/results/TransactionPendingResult.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/results/TransactionPendingResult.java
@@ -14,12 +14,15 @@
  */
 package org.hyperledger.besu.ethereum.api.jsonrpc.internal.results;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.hyperledger.besu.ethereum.core.AccessList;
 import org.hyperledger.besu.ethereum.core.Address;
 import org.hyperledger.besu.ethereum.core.Transaction;
 import org.hyperledger.besu.ethereum.rlp.BytesValueRLPOutput;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.hyperledger.besu.plugin.data.TransactionType;
 
 @JsonPropertyOrder({
   "blockHash",
@@ -37,8 +40,10 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
   "r",
   "s"
 })
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TransactionPendingResult implements TransactionResult {
 
+  private final AccessList accessList;
   private final String chainId;
   private final String from;
   private final String gas;
@@ -49,12 +54,16 @@ public class TransactionPendingResult implements TransactionResult {
   private final String publicKey;
   private final String raw;
   private final String to;
+  private final String type;
   private final String value;
   private final String v;
   private final String r;
   private final String s;
 
   public TransactionPendingResult(final Transaction transaction) {
+    final TransactionType transactionType = transaction.getType();
+    this.accessList =
+        transactionType.equals(TransactionType.ACCESS_LIST) ? transaction.getAccessList() : null;
     this.chainId = transaction.getChainId().map(Quantity::create).orElse(null);
     this.from = transaction.getSender().toString();
     this.gas = Quantity.create(transaction.getGasLimit());
@@ -67,10 +76,19 @@ public class TransactionPendingResult implements TransactionResult {
     transaction.writeTo(out);
     this.raw = out.encoded().toString();
     this.to = transaction.getTo().map(Address::toHexString).orElse(null);
+    this.type =
+        transactionType.equals(TransactionType.FRONTIER)
+            ? null
+            : Quantity.create(transactionType.getSerializedType());
     this.value = Quantity.create(transaction.getValue());
     this.v = Quantity.create(transaction.getV());
     this.r = Quantity.create(transaction.getR());
     this.s = Quantity.create(transaction.getS());
+  }
+
+  @JsonGetter(value = "accessList")
+  public AccessList getAccessList() {
+    return accessList;
   }
 
   @JsonGetter(value = "chainId")
@@ -121,6 +139,11 @@ public class TransactionPendingResult implements TransactionResult {
   @JsonGetter(value = "to")
   public String getTo() {
     return to;
+  }
+
+  @JsonGetter(value = "type")
+  public String getType() {
+    return type;
   }
 
   @JsonGetter(value = "value")
