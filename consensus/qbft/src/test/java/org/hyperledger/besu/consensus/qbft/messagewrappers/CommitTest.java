a3f59e1e722a00897f2d43e1b8c6f7ce69c72137
Trent Mohay <trent.mohay@consensys.net>
2021-03-03 21:42:19 +1100

    add interop test for commit msg
    Signed-off-by: Trent Mohay <trent.mohay@consensys.net>

--- consensus/qbft/src/test/java/org/hyperledger/besu/consensus/qbft/messagewrappers/CommitTest.java
+++ consensus/qbft/src/test/java/org/hyperledger/besu/consensus/qbft/messagewrappers/CommitTest.java
@@ -18,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hyperledger.besu.consensus.common.bft.payload.PayloadHelpers.qbftHashForSignature;
 
 import java.math.BigInteger;
+import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.consensus.common.bft.payload.SignedData;
 import org.hyperledger.besu.consensus.qbft.QbftConsensusRoundIdentifier;
 import org.hyperledger.besu.consensus.qbft.messagedata.QbftV1;
@@ -55,5 +56,19 @@ public class CommitTest {
     assertThat(decodedPrepare.getAuthor()).isEqualTo(addr);
     assertThat(decodedPrepare.getSignedPayload())
         .isEqualToComparingFieldByField(signedCommitPayload);
+  }
+
+  @Test
+  public void testInterop() {
+    final String input = "f8abf8660c80a0c0ad22d202fd84209d95dabf073162cd470370baf384814e63caaaf2fcbaf053b8415a161d0cb9380c9cff739190666408c3fc4a7a84f930482288762c524a66209b13a30876a1c9c56284eefc95096dc07580f24b58e22a045faddc825867627b7300b84145d73ddfb07c38ea484801060fe8207ea2ddab6e4fc0b80497b8681ce94df2110eebcf015e902d9526e746c414a6df458a0f25489e446551f68beacfa354154f00";
+
+    final Bytes inBytes = Bytes.fromHexString(input);
+
+    final Commit commit = Commit.decode(inBytes);
+
+    assertThat(commit.getAuthor()).isEqualTo(Address.fromHexString("0x9F66F8a0f0A6537e4A36AA1799673ea7AE97a166"));
+    assertThat(commit.getDigest()).isEqualTo(Hash.fromHexString("0xc0ad22d202fd84209d95dabf073162cd470370baf384814e63caaaf2fcbaf053"));
+    assertThat(commit.getRoundIdentifier().getSequenceNumber()).isEqualTo(12);
+    assertThat(commit.getRoundIdentifier().getRoundNumber()).isEqualTo(0);
   }
 }
