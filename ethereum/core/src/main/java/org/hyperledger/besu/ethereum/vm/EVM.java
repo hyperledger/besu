28b46c7977c0554b63b2fec7877d1dd67ac6d1b3
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-03-12 17:06:08 +0100

    debug
    Signed-off-by: Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/vm/EVM.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/vm/EVM.java
@@ -111,20 +111,18 @@ public class EVM {
   }
 
   private static void logState(final MessageFrame frame, final Gas currentGasCost) {
-    if (LOG.isTraceEnabled()) {
-      final StringBuilder builder = new StringBuilder();
-      builder.append("Depth: ").append(frame.getMessageStackDepth()).append("\n");
-      builder.append("Operation: ").append(frame.getCurrentOperation().getName()).append("\n");
-      builder.append("PC: ").append(frame.getPC()).append("\n");
-      builder.append("Gas cost: ").append(currentGasCost).append("\n");
-      builder.append("Gas Remaining: ").append(frame.getRemainingGas()).append("\n");
-      builder.append("Depth: ").append(frame.getMessageStackDepth()).append("\n");
-      builder.append("Stack:");
-      for (int i = 0; i < frame.stackSize(); ++i) {
-        builder.append("\n\t").append(i).append(" ").append(frame.getStackItem(i));
-      }
-      LOG.trace(builder.toString());
+    final StringBuilder builder = new StringBuilder();
+    builder.append("Depth: ").append(frame.getMessageStackDepth()).append("\n");
+    builder.append("Operation: ").append(frame.getCurrentOperation().getName()).append("\n");
+    builder.append("PC: ").append(frame.getPC()).append("\n");
+    builder.append("Gas cost: ").append(currentGasCost).append("\n");
+    builder.append("Gas Remaining: ").append(frame.getRemainingGas()).append("\n");
+    builder.append("Depth: ").append(frame.getMessageStackDepth()).append("\n");
+    builder.append("Stack:");
+    for (int i = 0; i < frame.stackSize(); ++i) {
+      builder.append("\n\t").append(i).append(" ").append(frame.getStackItem(i));
     }
+    System.out.println(builder.toString());
   }
 
   @VisibleForTesting
