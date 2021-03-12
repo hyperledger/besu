09188d9b1c3cfb1bb0546d576e13f10e1f5b7548
Karim TAAM <karim.t2am@gmail.com>
2021-03-12 21:51:26 +0100

    change accesslist
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/BerlinGasCalculator.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/BerlinGasCalculator.java
@@ -53,6 +53,11 @@ public class BerlinGasCalculator extends IstanbulGasCalculator {
 
   // unchanged from Istanbul
   private static final Gas SSTORE_SET_GAS = Gas.of(20_000);
+  private static final Gas SSTORE_CLEARS_SCHEDULE = Gas.of(15_000);
+
+  private static final Gas SSTORE_SET_GAS_LESS_SLOAD_GAS = SSTORE_SET_GAS.minus(SLOAD_GAS);
+  private static final Gas SSTORE_RESET_GAS_LESS_SLOAD_GAS = SSTORE_RESET_GAS.minus(SLOAD_GAS);
+  private static final Gas NEGATIVE_SSTORE_CLEARS_SCHEDULE = Gas.ZERO.minus(SSTORE_CLEARS_SCHEDULE);
 
   // unchanged from Frontier
   private static final Gas COPY_WORD_GAS_COST = Gas.of(3L);
@@ -88,9 +93,8 @@ public class BerlinGasCalculator extends IstanbulGasCalculator {
     }
 
     return new GasAndAccessedState(
-        super.transactionIntrinsicGasCostAndAccessedState(transaction)
-            .getGas()
-            .plus(ACCESS_LIST_ADDRESS_COST.times(accessList.size()))
+        ACCESS_LIST_ADDRESS_COST
+            .times(accessList.size())
             .plus(ACCESS_LIST_STORAGE_COST.times(accessedStorageCount)),
         accessedAddresses,
         accessedStorage);
@@ -199,6 +203,47 @@ public class BerlinGasCalculator extends IstanbulGasCalculator {
         return originalValue.isZero() ? SSTORE_SET_GAS : SSTORE_RESET_GAS;
       } else {
         return SLOAD_GAS;
+      }
+    }
+  }
+
+  // defined in Istanbul, but re-implemented with new constants
+  @Override
+  // As per https://eips.ethereum.org/EIPS/eip-2200
+  public Gas calculateStorageRefundAmount(
+      final Account account, final UInt256 key, final UInt256 newValue) {
+
+    final UInt256 currentValue = account.getStorageValue(key);
+    if (currentValue.equals(newValue)) {
+      return Gas.ZERO;
+    } else {
+      final UInt256 originalValue = account.getOriginalStorageValue(key);
+      if (originalValue.equals(currentValue)) {
+        if (originalValue.isZero()) {
+          return Gas.ZERO;
+        } else if (newValue.isZero()) {
+          return SSTORE_CLEARS_SCHEDULE;
+        } else {
+          return Gas.ZERO;
+        }
+      } else {
+        Gas refund = Gas.ZERO;
+        if (!originalValue.isZero()) {
+          if (currentValue.isZero()) {
+            refund = NEGATIVE_SSTORE_CLEARS_SCHEDULE;
+          } else if (newValue.isZero()) {
+            refund = SSTORE_CLEARS_SCHEDULE;
+          }
+        }
+
+        if (originalValue.equals(newValue)) {
+          refund =
+              refund.plus(
+                  originalValue.isZero()
+                      ? SSTORE_SET_GAS_LESS_SLOAD_GAS
+                      : SSTORE_RESET_GAS_LESS_SLOAD_GAS);
+        }
+        return refund;
       }
     }
   }
