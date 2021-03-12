779f0c926d10128b05c563ea4adaa235c712457e
Karim TAAM <karim.t2am@gmail.com>
2021-03-12 21:27:44 +0100

    clean
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/BerlinGasCalculator.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/BerlinGasCalculator.java
@@ -53,11 +53,6 @@ public class BerlinGasCalculator extends IstanbulGasCalculator {
 
   // unchanged from Istanbul
   private static final Gas SSTORE_SET_GAS = Gas.of(20_000);
-  private static final Gas SSTORE_CLEARS_SCHEDULE = Gas.of(15_000);
-
-  private static final Gas SSTORE_SET_GAS_LESS_SLOAD_GAS = SSTORE_SET_GAS.minus(SLOAD_GAS);
-  private static final Gas SSTORE_RESET_GAS_LESS_SLOAD_GAS = SSTORE_RESET_GAS.minus(SLOAD_GAS);
-  private static final Gas NEGATIVE_SSTORE_CLEARS_SCHEDULE = Gas.ZERO.minus(SSTORE_CLEARS_SCHEDULE);
 
   // unchanged from Frontier
   private static final Gas COPY_WORD_GAS_COST = Gas.of(3L);
