ee278e67b73de7e62b538a6a8d1b4a95dbc42ad9
garyschulte <garyschulte@gmail.com>
2021-03-31 16:24:32 -0700

    fix broken tests
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/util/DomainObjectDecodeUtilsTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/util/DomainObjectDecodeUtilsTest.java
@@ -1,17 +1,20 @@
 package org.hyperledger.besu.ethereum.api.util;
 
+import org.apache.tuweni.bytes.Bytes;
+import org.assertj.core.api.Assertions;
 import org.hyperledger.besu.crypto.SECPSignature;
 import org.hyperledger.besu.crypto.SignatureAlgorithmFactory;
-import org.hyperledger.besu.ethereum.core.*;
+import org.hyperledger.besu.ethereum.core.AccessListEntry;
+import org.hyperledger.besu.ethereum.core.Address;
+import org.hyperledger.besu.ethereum.core.BlockDataGenerator;
+import org.hyperledger.besu.ethereum.core.Transaction;
+import org.hyperledger.besu.ethereum.core.Wei;
 import org.hyperledger.besu.ethereum.core.encoding.TransactionEncoder;
 import org.hyperledger.besu.ethereum.rlp.BytesValueRLPOutput;
+import org.junit.Test;
 
 import java.math.BigInteger;
 import java.util.List;
-
-import org.apache.tuweni.bytes.Bytes;
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
 
 public class DomainObjectDecodeUtilsTest {
 
@@ -24,6 +27,7 @@ public class DomainObjectDecodeUtilsTest {
 
   private static final Transaction accessListTxn =
       Transaction.builder()
+          .chainId(BigInteger.valueOf(2018))
           .accessList(List.of(new AccessListEntry(gen.address(), List.of(gen.bytes32()))))
           .nonce(1)
           .gasPrice(Wei.of(12))
@@ -39,16 +43,16 @@ public class DomainObjectDecodeUtilsTest {
   public void testAccessListRLPSerDes() {
     final BytesValueRLPOutput encoded = new BytesValueRLPOutput();
     TransactionEncoder.encodeForWire(accessListTxn, encoded);
-    Transaction decoded = DomainObjectDecodeUtils.decodeRawTransaction(encoded.toString());
-    Assertions.assertThat(decoded.getAccessList().isPresent());
-    Assertions.assertThat(decoded.getAccessList().map(l -> l.size() == 1));
+    Transaction decoded = DomainObjectDecodeUtils.decodeRawTransaction(encoded.encoded().toHexString());
+    Assertions.assertThat(decoded.getAccessList().isPresent()).isTrue();
+    Assertions.assertThat(decoded.getAccessList().map(List::size).get()).isEqualTo(1);
   }
 
   @Test
   public void testAccessList2718OpaqueSerDes() {
     final Bytes encoded = TransactionEncoder.encodeOpaqueBytes(accessListTxn);
     Transaction decoded = DomainObjectDecodeUtils.decodeRawTransaction(encoded.toString());
-    Assertions.assertThat(decoded.getAccessList().isPresent());
-    Assertions.assertThat(decoded.getAccessList().map(l -> l.size() == 1));
+    Assertions.assertThat(decoded.getAccessList().isPresent()).isTrue();
+    Assertions.assertThat(decoded.getAccessList().map(List::size).get()).isEqualTo(1);
   }
 }
