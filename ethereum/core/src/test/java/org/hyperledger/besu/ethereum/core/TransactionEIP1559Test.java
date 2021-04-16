d247d96283373d77d9cac320a19a4c09530f6f43
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-04-16 10:24:12 +0200

    Init faucet test class
    Signed-off-by: Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/core/TransactionEIP1559Test.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/core/TransactionEIP1559Test.java
@@ -35,6 +35,8 @@ public class TransactionEIP1559Test {
   private static final Supplier<SignatureAlgorithm> SIGNATURE_ALGORITHM =
       Suppliers.memoize(SignatureAlgorithmFactory::getInstance);
 
+  private static final String PRIVATE_KEY = "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
+
   @After
   public void reset() {
     ExperimentalEIPs.eip1559Enabled = ExperimentalEIPs.EIP1559_ENABLED_DEFAULT_VALUE;
@@ -42,37 +44,35 @@ public class TransactionEIP1559Test {
 
   @Test
   public void buildEip1559Transaction() {
-    final List<AccessListEntry> accessListEntries =
+    /*final List<AccessListEntry> accessListEntries =
         List.of(
             new AccessListEntry(
                 Address.fromHexString("0x000000000000000000000000000000000000aaaa"),
-                List.of(Bytes32.ZERO)));
+                List.of(Bytes32.ZERO)));*/
     final Transaction tx =
         Transaction.builder()
-            .chainId(new BigInteger("1559", 10))
-            .nonce(0)
-            .value(Wei.ZERO)
+            .chainId(new BigInteger("7822", 10))
+            .nonce(1)
+            .value(Wei.fromEth(2))
             .gasLimit(30000)
-            .gasPremium(Wei.of(2))
+            .gasPremium(Wei.of(1000000000))
             .payload(Bytes.EMPTY.trimLeadingZeros())
             .feeCap(Wei.of(new BigInteger("5000000000", 10)))
-            .gasPrice(null)
-            .to(Address.fromHexString("0x000000000000000000000000000000000000aaaa"))
-            .accessList(accessListEntries)
+            //.gasPrice(Wei.of(1000000000))
+            .to(Address.fromHexString("0xC3298C6341f82468309302611e24D3003Bc79B46"))
             .guessType()
             .signAndBuild(
-                keyPair("0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"));
+                keyPair(PRIVATE_KEY));
     final BytesValueRLPOutput out = new BytesValueRLPOutput();
     tx.writeTo(out);
     System.out.println(out.encoded().toHexString());
-    System.out.println(tx.getUpfrontCost());
     // final String raw =
     // "b8a902f8a686796f6c6f7632800285012a05f20082753094000000000000000000000000000000000000aaaa8080f838f794000000000000000000000000000000000000aaaae1a0000000000000000000000000000000000000000000000000000000000000000001a00c1d69648e348fe26155b45de45004f0e4195f6352d8f0935bc93e98a3e2a862a060064e5b9765c0ac74223b0cf49635c59ae0faf82044fd17bcc68a549ade6f95";
     final String raw = out.encoded().toHexString();
     final Transaction decoded = Transaction.readFrom(RLP.input(Bytes.fromHexString(raw)));
     System.out.println(decoded);
-    System.out.println(decoded.getAccessList().orElseThrow().get(0).getAddress().toHexString());
-    System.out.println(decoded.getAccessList().orElseThrow().get(0).getStorageKeys());
+    /*System.out.println(decoded.getAccessList().orElseThrow().get(0).getAddress().toHexString());
+    System.out.println(decoded.getAccessList().orElseThrow().get(0).getStorageKeys());*/
   }
 
   private static KeyPair keyPair(final String privateKey) {
