541633cb97d71c806c289dbaa5403196f22c4270
garyschulte <garyschulte@gmail.com>
2021-04-21 23:40:13 -0700

    add SynJsonRpcMethod
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/ConsensusJsonRpcMethods.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/ConsensusJsonRpcMethods.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.hyperledger.besu.ethereum.api.jsonrpc.methods;
 
 import org.hyperledger.besu.ethereum.api.jsonrpc.RpcApi;
@@ -10,6 +24,8 @@ import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.JsonRpcMethod;
 
 import java.util.Map;
 
+import io.vertx.core.Vertx;
+
 public class ConsensusJsonRpcMethods extends ApiGroupJsonRpcMethods {
   @Override
   protected RpcApi getApiGroup() {
@@ -18,10 +34,11 @@ public class ConsensusJsonRpcMethods extends ApiGroupJsonRpcMethods {
 
   @Override
   protected Map<String, JsonRpcMethod> create() {
+    Vertx syncVertx = Vertx.vertx();
     return mapOf(
-        new ConsensusAssembleBlock(),
-        new ConsensusFinalizeBlock(),
-        new ConsensusNewBlock(),
-        new ConsensusSetHead());
+        new ConsensusAssembleBlock(syncVertx),
+        new ConsensusFinalizeBlock(syncVertx),
+        new ConsensusNewBlock(syncVertx),
+        new ConsensusSetHead(syncVertx));
   }
 }
