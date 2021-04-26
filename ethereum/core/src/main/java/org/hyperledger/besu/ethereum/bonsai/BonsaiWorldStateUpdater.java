46d4952a70d32c0bc6b3895bf49d43612f8dc7e6
matkt <karim.t2am@gmail.com>
2021-04-26 12:18:23 +0200

    Bonsai trie : fix consensus issue on ropsten (#2164)
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/BonsaiWorldStateUpdater.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/BonsaiWorldStateUpdater.java
@@ -267,6 +267,10 @@ public class BonsaiWorldStateUpdater extends AbstractWorldUpdater<BonsaiWorldVie
         storageToUpdate.remove(updatedAddress);
       }
 
+      if (tracked.getStorageWasCleared()) {
+        tracked.setStorageWasCleared(false); // storage already cleared for this transaction
+      }
+
       // TODO maybe add address preimage?
     }
   }
@@ -313,13 +317,13 @@ public class BonsaiWorldStateUpdater extends AbstractWorldUpdater<BonsaiWorldVie
   @Override
   public UInt256 getPriorStorageValue(final Address address, final UInt256 storageKey) {
     // TODO maybe log the read into the trie layer?
-    if (storageToClear.contains(address)) {
-      return UInt256.ZERO;
-    }
     final Map<Hash, BonsaiValue<UInt256>> localAccountStorage = storageToUpdate.get(address);
     final Hash slotHash = Hash.hash(storageKey.toBytes());
     if (localAccountStorage != null && localAccountStorage.containsKey(slotHash)) {
       final BonsaiValue<UInt256> value = localAccountStorage.get(slotHash);
+      if (value.isCleared()) {
+        return UInt256.ZERO;
+      }
       final UInt256 updated = value.getUpdated();
       if (updated != null) {
         return updated;
@@ -328,6 +332,9 @@ public class BonsaiWorldStateUpdater extends AbstractWorldUpdater<BonsaiWorldVie
       if (original != null) {
         return original;
       }
+    }
+    if (storageToClear.contains(address)) {
+      return UInt256.ZERO;
     }
     return getStorageValue(address, storageKey);
   }
