0dcc085627d691d2221198f672bdf7ecfcb8d5ed
Ratan Rai Sur <ratan.r.sur@gmail.com>
2021-05-18 17:41:53 -0500

    delete PrioritizedTransactions
    Signed-off-by: Ratan Rai Sur <ratan.r.sur@gmail.com>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/transactions/PrioritizedTransactions.java
+++
@@ -1,8 +0,0 @@
-package org.hyperledger.besu.ethereum.eth.transactions;
-
-import java.util.NavigableSet;
-import java.util.TreeSet;
-
-import static java.util.Comparator.comparing;
-
-public class PrioritizedTransactions {}
