031c443fc36473dcddab79136f21346fb9b4bf41
taccatisid <88524035+taccatisid@users.noreply.github.com>
2021-08-31 12:41:54 +1200

    DRY onchain management proxies (#2674)
    * move onchain privacy group contract read access to OnchainPrivacyGroupContract
    
    Signed-off-by: Taccat Isid <taccatisid@protonmail.com>
    
    * fix crash when onchain group managment contract returns invalid member list
    
    Signed-off-by: Taccat Isid <taccatisid@protonmail.com>
    
    * simplify PrivacyPrecompiledContract.isMining
    
    Signed-off-by: Taccat Isid <taccatisid@protonmail.com>
    
    * Declare some variables final.
    
    Signed-off-by: Taccat Isid <taccatisid@protonmail.com>
    
    Co-authored-by: Stefan Pingel <16143240+pinges@users.noreply.github.com>
    Co-authored-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/mainnet/precompiles/privacy/OnChainPrivacyPrecompiledContractTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/mainnet/precompiles/privacy/OnChainPrivacyPrecompiledContractTest.java
@@ -57,7 +57,6 @@ import org.hyperledger.besu.ethereum.vm.OperationTracer;
 import org.hyperledger.besu.ethereum.worldstate.WorldStateArchive;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -168,7 +167,7 @@ public class OnChainPrivacyPrecompiledContractTest {
     final OnChainPrivacyPrecompiledContract contractSpy = spy(contract);
     Mockito.doReturn(true)
         .when(contractSpy)
-        .canExecute(any(), any(), any(), any(), any(), any(), any(), any(), any(), any());
+        .canExecute(any(), any(), any(), any(), any(), any(), any(), any(), any());
 
     final Bytes actual = contractSpy.compute(privateTransactionLookupId, messageFrame);
 
@@ -234,22 +233,46 @@ public class OnChainPrivacyPrecompiledContractTest {
 
   @Test
   public void testPrivateFromNotMemberOfGroup() {
+    // array length too big
+    assertThatComputeReturnsEmptyGivenContractMembershipQueryReturns(
+        Bytes.concatenate(
+            Bytes32.fromHexStringLenient("0x0"),
+            // array length
+            Bytes32.fromHexStringLenient("0x1"),
+            // first array content
+            Bytes32.fromHexStringLenient("0x1")));
+  }
+
+  @Test
+  public void testInvalidResponseToMembershipQuery() {
+    // response shorter than empty array response
+    assertThatComputeReturnsEmptyGivenContractMembershipQueryReturns(
+        Bytes32.fromHexStringLenient("0x0"));
+
+    // array length too big
+    assertThatComputeReturnsEmptyGivenContractMembershipQueryReturns(
+        Bytes.concatenate(
+            // offset to start of array
+            Bytes32.fromHexStringLenient("0x0"),
+            // array length
+            Bytes32.fromHexStringLenient("0x2"),
+            // first array content
+            Bytes32.fromHexStringLenient("0x1")));
+  }
+
+  private void assertThatComputeReturnsEmptyGivenContractMembershipQueryReturns(
+      final Bytes memberList) {
     final Enclave enclave = mock(Enclave.class);
     final OnChainPrivacyPrecompiledContract contract = buildPrivacyPrecompiledContract(enclave);
+
+    final List<Log> logs = new ArrayList<>();
+    contract.setPrivateTransactionProcessor(
+        mockPrivateTxProcessor(
+            TransactionProcessingResult.successful(logs, 0, 0, memberList, null)));
+
     final OnChainPrivacyPrecompiledContract contractSpy = spy(contract);
-    Mockito.doReturn(false)
-        .when(contractSpy)
-        .isContractLocked(any(), any(), any(), any(), any(), any(), any());
-    Mockito.doReturn(true)
-        .when(contractSpy)
-        .onChainPrivacyGroupVersionMatches(any(), any(), any(), any(), any(), any(), any(), any());
-    final TransactionProcessingResult mockResult = mock(TransactionProcessingResult.class);
-    Mockito.doReturn(mockResult)
-        .when(contractSpy)
-        .simulateTransaction(any(), any(), any(), any(), any(), any(), any(), any());
-    Mockito.doReturn(Arrays.asList(Bytes.ofUnsignedInt(1L)))
-        .when(contractSpy)
-        .getMembersFromResult(any());
+    Mockito.doReturn(false).when(contractSpy).isContractLocked(any(), any());
+    Mockito.doReturn(true).when(contractSpy).onChainPrivacyGroupVersionMatches(any(), any(), any());
 
     final VersionedPrivateTransaction privateTransaction = versionedPrivateTransactionBesu();
     final byte[] payload = convertVersionedPrivateTransactionToBytes(privateTransaction);
@@ -285,7 +308,7 @@ public class OnChainPrivacyPrecompiledContractTest {
     final OnChainPrivacyPrecompiledContract contractSpy = spy(contract);
     Mockito.doReturn(true)
         .when(contractSpy)
-        .canExecute(any(), any(), any(), any(), any(), any(), any(), any(), any(), any());
+        .canExecute(any(), any(), any(), any(), any(), any(), any(), any(), any());
 
     final VersionedPrivateTransaction privateTransaction = versionedPrivateTransactionBesu();
     final byte[] payload = convertVersionedPrivateTransactionToBytes(privateTransaction);
