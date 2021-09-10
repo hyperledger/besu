5cc172d78c9f39b1a8670bfaed2ce2763798b3dc
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2021-09-10 20:01:43 +0200

    Metrics idle timeout is now configurable
    Signed-off-by: Giuseppe Bertone <bertone.giuseppe@gmail.com>

--- besu/src/main/java/org/hyperledger/besu/cli/options/unstable/MetricsCLIOptions.java
+++ besu/src/main/java/org/hyperledger/besu/cli/options/unstable/MetricsCLIOptions.java
@@ -24,13 +24,22 @@ import picocli.CommandLine;
 
 public class MetricsCLIOptions implements CLIOptions<MetricsConfiguration.Builder> {
   private static final String TIMERS_ENABLED_FLAG = "--Xmetrics-timers-enabled";
+  private static final String IDLE_TIMEOUT_FLAG = "--Xmetrics-idle-timeout";
 
   @CommandLine.Option(
       names = TIMERS_ENABLED_FLAG,
       hidden = true,
       defaultValue = "true",
       description = "Whether to enable timer metrics (default: ${DEFAULT-VALUE}).")
-  private Boolean timersEnabled = MetricsConfiguration.DEFAULT_TIMERS_ENABLED;
+  private Boolean timersEnabled = MetricsConfiguration.DEFAULT_METRICS_TIMERS_ENABLED;
+
+  @CommandLine.Option(
+      hidden = true,
+      names = {IDLE_TIMEOUT_FLAG},
+      paramLabel = "<INTEGER>",
+      description = "Timeout for metrics TCP connections, in seconds (default: ${DEFAULT-VALUE})",
+      arity = "1")
+  private int idleTimeout = MetricsConfiguration.DEFAULT_METRICS_IDLE_TIMEOUT_SECONDS;
 
   private MetricsCLIOptions() {}
 
@@ -41,16 +50,19 @@ public class MetricsCLIOptions implements CLIOptions<MetricsConfiguration.Builde
   public static MetricsCLIOptions fromConfiguration(final MetricsConfiguration config) {
     final MetricsCLIOptions metricsOptions = create();
     metricsOptions.timersEnabled = config.isTimersEnabled();
+    metricsOptions.idleTimeout = config.getIdleTimeout();
     return metricsOptions;
   }
 
   @Override
   public MetricsConfiguration.Builder toDomainObject() {
-    return MetricsConfiguration.builder().timersEnabled(timersEnabled);
+    return MetricsConfiguration.builder().timersEnabled(timersEnabled).idleTimeout(idleTimeout);
   }
 
   @Override
   public List<String> getCLIOptions() {
-    return Arrays.asList(TIMERS_ENABLED_FLAG + "=" + timersEnabled.toString());
+    return Arrays.asList(
+        TIMERS_ENABLED_FLAG + "=" + timersEnabled.toString(),
+        IDLE_TIMEOUT_FLAG + "=" + idleTimeout);
   }
 }
