d8465971b1ff829ba15d37eff9a6765e57d55734
Lucas Saldanha <lucascrsaldanha@gmail.com>
2021-10-11 11:55:33 +1300

    Fix license header
    Signed-off-by: Lucas Saldanha <lucascrsaldanha@gmail.com>

--- consensus/qbft/src/main/java/org/hyperledger/besu/consensus/qbft/pki/DefaultKeyStoreWrapperProvider.java
+++ consensus/qbft/src/main/java/org/hyperledger/besu/consensus/qbft/pki/DefaultKeyStoreWrapperProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright ConsenSys AG.
+ * Copyright Copyright contributors to Hyperledger Besu.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with
