ad6803628b7a5c58caf72380624dd4ede8d77b85
Diego López León <dieguitoll@gmail.com>
2021-11-04 20:36:55 -0300

    Only add --bootnodes if there are some nodes set (#1632)
    Signed-off-by: Diego López León <dieguitoll@gmail.com>

--- acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/node/ProcessBesuNodeRunner.java
+++ acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/node/ProcessBesuNodeRunner.java
@@ -144,9 +144,8 @@ public class ProcessBesuNodeRunner implements BesuNodeRunner {
       }
     }
 
-    params.add("--bootnodes");
-
     if (!node.getBootnodes().isEmpty()) {
+      params.add("--bootnodes");
       params.add(node.getBootnodes().stream().map(URI::toString).collect(Collectors.joining(",")));
     }
 
