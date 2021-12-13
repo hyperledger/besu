aba368316172790713c793d59f935ec62ca20b3a
Justin Florentine <justin+github@florentine.us>
2021-12-13 17:37:39 -0500

    new validation rules and support tests
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

---
+++ consensus/merge/src/main/java/org/hyperledger/besu/consensus/merge/headervalidationrules/ConstantOmmersHashRule.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright Hyperledger Besu Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.hyperledger.besu.consensus.merge.headervalidationrules;
+
+import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.ethereum.ProtocolContext;
+import org.hyperledger.besu.ethereum.core.BlockHeader;
+import org.hyperledger.besu.ethereum.mainnet.AttachedBlockHeaderValidationRule;
+
+public class ConstantOmmersHashRule implements AttachedBlockHeaderValidationRule {
+  @Override
+  public boolean validate(
+      final BlockHeader header, final BlockHeader parent, final ProtocolContext protocolContext) {
+    if (header
+        .getOmmersHash()
+        .equals(
+            Hash.fromHexString(
+                "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347"))) {
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public boolean includeInLightValidation() {
+    return AttachedBlockHeaderValidationRule.super.includeInLightValidation();
+  }
+}
---
+++ consensus/merge/src/test/java/org/hyperledger/besu/consensus/merge/headervalidationrules/NoDifficultyValidationTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Hyperledger Besu Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.hyperledger.besu.consensus.merge.headervalidationrules;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.hyperledger.besu.ethereum.ProtocolContext;
+import org.hyperledger.besu.ethereum.core.BlockHeader;
+import org.hyperledger.besu.ethereum.core.Difficulty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NoDifficultyValidationTest {
+
+  @Mock private ProtocolContext protocolContext;
+
+  @Test
+  public void difficultyMustBeZero() {
+    final NoDifficultyRule rule = new NoDifficultyRule();
+    final BlockHeader parentHeader = mock(BlockHeader.class);
+
+    final BlockHeader invalidHeader = mock(BlockHeader.class);
+    when(invalidHeader.getDifficulty()).thenReturn(Difficulty.MAX_VALUE);
+    assertThat(rule.validate(invalidHeader, parentHeader, protocolContext)).isFalse();
+
+    final BlockHeader validHeader = mock(BlockHeader.class);
+    when(validHeader.getDifficulty()).thenReturn(Difficulty.ZERO);
+
+    assertThat(rule.validate(validHeader, parentHeader, protocolContext)).isTrue();
+  }
+}
