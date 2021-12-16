83d872f7b44058ddb5e124b35bc2839ecb8aaa90
Sally MacFarlane <sally.macfarlane@consensys.net>
2021-12-16 18:46:56 +1000

    uprev pragma to include 0.8
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- acceptance-tests/tests/contracts/EventEmitter.sol
+++ acceptance-tests/tests/contracts/EventEmitter.sol
@@ -13,7 +13,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-pragma solidity >=0.4.0 <0.6.0;
+pragma solidity >=0.4.0 <0.9.0;
 
 // compile with:
 // solc EventEmitter.sol --bin --abi --optimize --overwrite -o .
@@ -25,7 +25,7 @@ contract EventEmitter {
     address _sender;
     uint _value;
 
-    constructor() public {
+    constructor() {
         owner = msg.sender;
     }
 
--- acceptance-tests/tests/contracts/RevertReason.sol
+++ acceptance-tests/tests/contracts/RevertReason.sol
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-pragma solidity >=0.4.0 <0.6.0;
+pragma solidity >=0.4.0 <0.9.0;
 
 // compile with:
 // solc RevertReason.sol --bin --abi --optimize --overwrite -o .
