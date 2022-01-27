0f548dee22314cd404378d5908f3f19268529be7
Frank Li <b439988l@gmail.com>
2022-01-27 11:53:43 +1000

    (fix) Failing test in java 17 - jackson optional parsing (#3325)
    * add jdk8 module for optional parsing
    
    Signed-off-by: Frank Li <b439988l@gmail.com>
    
    * register modle at mapper initiation
    
    Signed-off-by: Frank Li <b439988l@gmail.com>
    Signed-off-by: Daniel Lehrner <daniel.lehrner@consensys.net>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/parameters/JsonRpcParameter.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/parameters/JsonRpcParameter.java
@@ -20,10 +20,13 @@ import java.util.Optional;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 public class JsonRpcParameter {
 
-  private static final ObjectMapper mapper = new ObjectMapper();
+  private static final ObjectMapper mapper =
+      new ObjectMapper()
+          .registerModule(new Jdk8Module()); // Handle JDK8 Optionals (de)serialization
 
   /**
    * Retrieves a required parameter at the given index interpreted as the given class. Throws
