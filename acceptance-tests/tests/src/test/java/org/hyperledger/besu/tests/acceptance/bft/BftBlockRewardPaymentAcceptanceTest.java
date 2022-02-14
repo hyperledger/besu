ba59f2317a9f67b086e59bc4e1a4784e68fe9f69
mbaxter <meredith.baxter@palm.io>
2022-02-14 10:58:56 -0500

    [Issue-3115] Fix mining beneficiary acceptance tests (#3415)
    * Fix bft acceptance tests
    
    Only set genesis config if it is not already set.  Throw if user attempts to verify an empty cluster.
    Configure genesis file with appropriate keys for consensus type.
    
    Signed-off-by: Meredith Baxter <meredith.baxter@palm.io>
    
    * Fix SECP256R1AcceptanceTest
    
    Signed-off-by: Meredith Baxter <meredith.baxter@palm.io>

--- acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/bft/BftBlockRewardPaymentAcceptanceTest.java
+++ acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/bft/BftBlockRewardPaymentAcceptanceTest.java
@@ -67,8 +67,12 @@ public class BftBlockRewardPaymentAcceptanceTest extends ParameterizedBftTestBas
         initialConfig
             .get()
             .replace(
-                "\"ibft2\": {",
-                "\"ibft2\": { \"miningbeneficiary\": \"" + miningBeneficiaryAddress + "\",");
+                "\"" + bftType + "\": {",
+                "\""
+                    + bftType
+                    + "\": { \"miningbeneficiary\": \""
+                    + miningBeneficiaryAddress
+                    + "\",");
 
     validator1.setGenesisConfig(configWithMiningBeneficiary);
 
@@ -76,7 +80,7 @@ public class BftBlockRewardPaymentAcceptanceTest extends ParameterizedBftTestBas
         Account.create(ethTransactions, Address.fromHexString(miningBeneficiaryAddress));
 
     // This starts a node, without executing its configGenerator
-    cluster.runNodeStart(validator1);
+    cluster.start(validator1);
     final int blockRewardEth = 5;
     final int blockToCheck = 2;
 
