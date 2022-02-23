66330f7d6cb422657d5d2270bfdaf4f8935037fe
Daniel Lehrner <daniel.lehrner@consensys.net>
2022-02-23 15:45:40 +0100

    added missing tests
    Signed-off-by: Daniel Lehrner <daniel.lehrner@consensys.net>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineExchangeTransitionConfigurationTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineExchangeTransitionConfigurationTest.java
@@ -18,7 +18,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import org.hyperledger.besu.consensus.merge.MergeContext;
+import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.ethereum.ProtocolContext;
 import org.hyperledger.besu.ethereum.api.jsonrpc.RpcMethod;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.JsonRpcRequest;
@@ -28,8 +30,19 @@ import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcError;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcErrorResponse;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcResponse;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcResponseType;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcSuccessResponse;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.results.EngineExchangeTransitionConfigurationResult;
+import org.hyperledger.besu.ethereum.core.BlockHeader;
+import org.hyperledger.besu.ethereum.core.BlockHeaderFunctions;
+import org.hyperledger.besu.ethereum.core.Difficulty;
+import org.hyperledger.besu.ethereum.core.ParsedExtraData;
+import org.hyperledger.besu.evm.log.LogsBloomFilter;
+
+import java.util.Optional;
 
 import io.vertx.core.Vertx;
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -68,6 +81,52 @@ public class EngineExchangeTransitionConfigurationTest {
     assertThat(res.getError()).isEqualTo(JsonRpcError.INVALID_PARAMS);
   }
 
+  @Test
+  public void shouldReturnInternalErrorOnFinalizedBlockHeaderEmpty() {
+    when(mergeContext.getFinalized()).thenReturn(Optional.empty());
+
+    var response =
+        resp(new EngineExchangeTransitionConfigurationParameter("0", Hash.ZERO.toHexString(), 0));
+
+    assertThat(response.getType()).isEqualTo(JsonRpcResponseType.ERROR);
+    JsonRpcErrorResponse res = ((JsonRpcErrorResponse) response);
+    assertThat(res.getError()).isEqualTo(JsonRpcError.INTERNAL_ERROR);
+  }
+
+  @Test
+  public void shouldReturnConfigurationOnConfigurationMisMatch() {
+    final BlockHeader fakeBlockHeader = createBlockHeader(Hash.fromHexStringLenient("0x01"), 42);
+    when(mergeContext.getFinalized()).thenReturn(Optional.of(fakeBlockHeader));
+    when(mergeContext.getTerminalTotalDifficulty()).thenReturn(Difficulty.of(24));
+
+    var response =
+        resp(
+            new EngineExchangeTransitionConfigurationParameter(
+                "1", Hash.fromHexStringLenient("0xff").toHexString(), 0));
+
+    var result = fromSuccessResp(response);
+    assertThat(result.getTerminalTotalDifficulty()).isEqualTo(Difficulty.of(24));
+    assertThat(result.getTerminalBlockHash()).isEqualTo(Hash.fromHexStringLenient("0x01"));
+    assertThat(result.getTerminalBlockNumber()).isEqualTo(42);
+  }
+
+  @Test
+  public void shouldReturnConfigurationOnConfigurationMatch() {
+    final BlockHeader fakeBlockHeader = createBlockHeader(Hash.fromHexStringLenient("0x01"), 42);
+    when(mergeContext.getFinalized()).thenReturn(Optional.of(fakeBlockHeader));
+    when(mergeContext.getTerminalTotalDifficulty()).thenReturn(Difficulty.of(24));
+
+    var response =
+        resp(
+            new EngineExchangeTransitionConfigurationParameter(
+                "24", Hash.fromHexStringLenient("0x01").toHexString(), 0));
+
+    var result = fromSuccessResp(response);
+    assertThat(result.getTerminalTotalDifficulty()).isEqualTo(Difficulty.of(24));
+    assertThat(result.getTerminalBlockHash()).isEqualTo(Hash.fromHexStringLenient("0x01"));
+    assertThat(result.getTerminalBlockNumber()).isEqualTo(42);
+  }
+
   private JsonRpcResponse resp(final EngineExchangeTransitionConfigurationParameter param) {
     return method.response(
         new JsonRpcRequestContext(
@@ -77,13 +136,43 @@ public class EngineExchangeTransitionConfigurationTest {
                 new Object[] {param})));
   }
 
-  //    private EngineExchangeTransitionConfigurationResult fromSuccessResp(final JsonRpcResponse
-  // resp) {
-  //        assertThat(resp.getType()).isEqualTo(JsonRpcResponseType.SUCCESS);
-  //        return Optional.of(resp)
-  //                .map(JsonRpcSuccessResponse.class::cast)
-  //                .map(JsonRpcSuccessResponse::getResult)
-  //                .map(EngineExchangeTransitionConfigurationResult.class::cast)
-  //                .get();
-  //    }
+  private EngineExchangeTransitionConfigurationResult fromSuccessResp(final JsonRpcResponse resp) {
+    assertThat(resp.getType()).isEqualTo(JsonRpcResponseType.SUCCESS);
+    return Optional.of(resp)
+        .map(JsonRpcSuccessResponse.class::cast)
+        .map(JsonRpcSuccessResponse::getResult)
+        .map(EngineExchangeTransitionConfigurationResult.class::cast)
+        .get();
+  }
+
+  private BlockHeader createBlockHeader(final Hash blockHash, final long blockNumber) {
+    return new BlockHeader(
+        Hash.EMPTY,
+        Hash.EMPTY,
+        Address.ZERO,
+        Hash.EMPTY,
+        Hash.EMPTY,
+        Hash.EMPTY,
+        LogsBloomFilter.empty(),
+        Difficulty.ZERO,
+        blockNumber,
+        0,
+        0,
+        0,
+        Bytes.EMPTY,
+        Wei.ZERO,
+        Bytes32.ZERO,
+        0,
+        new BlockHeaderFunctions() {
+          @Override
+          public Hash hash(final BlockHeader header) {
+            return blockHash;
+          }
+
+          @Override
+          public ParsedExtraData parseExtraData(final BlockHeader header) {
+            return null;
+          }
+        });
+  }
 }
