777540c7e7cfa8b8fda60272767e563309bc5902
Frank Li <b439988l@gmail.com>
2022-03-02 20:29:23 +1000

    Refactor trace api methods (#3510)
    * abstract trace classes
    
    Signed-off-by: Frank Li <b439988l@gmail.com>
    
    * add spdx headers
    
    Signed-off-by: Frank Li <b439988l@gmail.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/TraceTransaction.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/TraceTransaction.java
@@ -17,39 +17,20 @@ package org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.ethereum.api.jsonrpc.RpcMethod;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.JsonRpcRequestContext;
-import org.hyperledger.besu.ethereum.api.jsonrpc.internal.processor.BlockTrace;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.processor.BlockTracer;
-import org.hyperledger.besu.ethereum.api.jsonrpc.internal.processor.TransactionTrace;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcResponse;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcSuccessResponse;
-import org.hyperledger.besu.ethereum.api.jsonrpc.internal.results.tracing.flat.FlatTraceGenerator;
 import org.hyperledger.besu.ethereum.api.query.BlockchainQueries;
-import org.hyperledger.besu.ethereum.api.query.TransactionWithMetadata;
-import org.hyperledger.besu.ethereum.core.Block;
-import org.hyperledger.besu.ethereum.debug.TraceOptions;
 import org.hyperledger.besu.ethereum.mainnet.ProtocolSchedule;
-import org.hyperledger.besu.ethereum.vm.DebugOperationTracer;
 
-import java.util.Collections;
 import java.util.function.Supplier;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-
-public class TraceTransaction implements JsonRpcMethod {
-  private final Supplier<BlockTracer> blockTracerSupplier;
-
-  private final BlockchainQueries blockchainQueries;
-  private final ProtocolSchedule protocolSchedule;
-
+public class TraceTransaction extends AbstractTraceByHash implements JsonRpcMethod {
   public TraceTransaction(
       final Supplier<BlockTracer> blockTracerSupplier,
       final ProtocolSchedule protocolSchedule,
       final BlockchainQueries blockchainQueries) {
-    this.blockTracerSupplier = blockTracerSupplier;
-    this.blockchainQueries = blockchainQueries;
-    this.protocolSchedule = protocolSchedule;
+    super(blockTracerSupplier, blockchainQueries, protocolSchedule);
   }
 
   @Override
@@ -61,52 +42,7 @@ public class TraceTransaction implements JsonRpcMethod {
   public JsonRpcResponse response(final JsonRpcRequestContext requestContext) {
     final Hash transactionHash = requestContext.getRequiredParameter(0, Hash.class);
     return new JsonRpcSuccessResponse(
-        requestContext.getRequest().getId(), resultByTransactionHash(transactionHash));
-  }
-
-  private Object resultByTransactionHash(final Hash transactionHash) {
-    return blockchainQueries
-        .transactionByHash(transactionHash)
-        .flatMap(TransactionWithMetadata::getBlockNumber)
-        .flatMap(blockNumber -> blockchainQueries.getBlockchain().getBlockByNumber(blockNumber))
-        .map((block) -> traceBlock(block, transactionHash))
-        .orElse(emptyResult());
-  }
-
-  private Object traceBlock(final Block block, final Hash transactionHash) {
-    if (block == null || block.getBody().getTransactions().isEmpty()) {
-      return emptyResult();
-    }
-    final TransactionTrace transactionTrace =
-        blockTracerSupplier
-            .get()
-            .trace(block, new DebugOperationTracer(new TraceOptions(false, false, true)))
-            .map(BlockTrace::getTransactionTraces)
-            .orElse(Collections.emptyList())
-            .stream()
-            .filter(trxTrace -> trxTrace.getTransaction().getHash().equals(transactionHash))
-            .findFirst()
-            .orElseThrow();
-    return generateTracesFromTransactionTraceAndBlock(protocolSchedule, transactionTrace, block);
-  }
-
-  private JsonNode generateTracesFromTransactionTraceAndBlock(
-      final ProtocolSchedule protocolSchedule,
-      final TransactionTrace transactionTrace,
-      final Block block) {
-    final ObjectMapper mapper = new ObjectMapper();
-
-    final ArrayNode resultArrayNode = mapper.createArrayNode();
-
-    FlatTraceGenerator.generateFromTransactionTraceAndBlock(
-            protocolSchedule, transactionTrace, block)
-        .forEachOrdered(resultArrayNode::addPOJO);
-
-    return resultArrayNode;
-  }
-
-  private Object emptyResult() {
-    final ObjectMapper mapper = new ObjectMapper();
-    return mapper.createArrayNode();
+        requestContext.getRequest().getId(),
+        arrayNodeFromTraceStream(resultByTransactionHash(transactionHash)));
   }
 }
