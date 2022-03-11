4ca21650f7b8d84a2fdaf2889810090399be2295
Karim TAAM <karim.t2am@gmail.com>
2022-03-11 09:48:32 +0100

    add range proof validation
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

---
+++ ethereum/trie/src/main/java/org/hyperledger/besu/ethereum/trie/InnerNodeDiscoveryManager.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright contributors to Hyperledger Besu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.ethereum.trie;
+
+import org.hyperledger.besu.ethereum.rlp.RLPInput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
+import org.apache.tuweni.bytes.MutableBytes;
+import org.immutables.value.Value;
+
+public class InnerNodeDiscoveryManager<V> extends StoredNodeFactory<V> {
+
+  public List<InnerNode> innerNodes = new ArrayList<>();
+  public Set<Bytes> incompleteLocation = new HashSet<>();
+
+  private final Bytes startKeyHash, endKeyHash;
+
+  private final boolean allowMissingElementInRange;
+
+  public InnerNodeDiscoveryManager(
+      final NodeLoader nodeLoader,
+      final Function<V, Bytes> valueSerializer,
+      final Function<Bytes, V> valueDeserializer,
+      final Bytes32 startKeyHash,
+      final Bytes32 endKeyHash,
+      final boolean allowMissingElementInRange) {
+    super(nodeLoader, valueSerializer, valueDeserializer);
+    this.startKeyHash = createPath(startKeyHash);
+    this.endKeyHash = createPath(endKeyHash);
+    this.allowMissingElementInRange = allowMissingElementInRange;
+  }
+
+  @Override
+  protected Node<V> decodeExtension(
+      final Bytes location,
+      final Bytes path,
+      final RLPInput valueRlp,
+      final Supplier<String> errMessage) {
+    final ExtensionNode<V> vNode =
+        (ExtensionNode<V>) super.decodeExtension(location, path, valueRlp, errMessage);
+    if (isInRange(Bytes.concatenate(location, Bytes.of(0)))) {
+      innerNodes.add(
+          ImmutableInnerNode.builder()
+              .location(location)
+              .path(Bytes.of(0, CompactEncoding.LEAF_TERMINATOR))
+              .build());
+    }
+    return vNode;
+  }
+
+  @Override
+  protected BranchNode<V> decodeBranch(
+      final Bytes location, final RLPInput nodeRLPs, final Supplier<String> errMessage) {
+    final BranchNode<V> vBranchNode = super.decodeBranch(location, nodeRLPs, errMessage);
+    final List<Node<V>> children = vBranchNode.getChildren();
+    for (int i = 0; i < children.size(); i++) {
+      if (isInRange(Bytes.concatenate(location, Bytes.of(i)))) {
+        innerNodes.add(
+            ImmutableInnerNode.builder()
+                .location(location)
+                .path(Bytes.of(i, CompactEncoding.LEAF_TERMINATOR))
+                .build());
+      }
+    }
+    return vBranchNode;
+  }
+
+  @Override
+  protected LeafNode<V> decodeLeaf(
+      final Bytes location,
+      final Bytes path,
+      final RLPInput valueRlp,
+      final Supplier<String> errMessage) {
+    final LeafNode<V> vLeafNode = super.decodeLeaf(location, path, valueRlp, errMessage);
+    final Bytes concatenatePath = Bytes.concatenate(location, path);
+    if (isInRange(concatenatePath.slice(0, concatenatePath.size() - 1))) {
+      innerNodes.add(ImmutableInnerNode.builder().location(location).path(path).build());
+    }
+    return vLeafNode;
+  }
+
+  @Override
+  public Optional<Node<V>> retrieve(final Bytes location, final Bytes32 hash)
+      throws MerkleTrieException {
+    return super.retrieve(location, hash)
+        .or(
+            () -> {
+              if (!allowMissingElementInRange && isInRange(location)) {
+                return Optional.empty();
+              }
+              final MissingNode<V> missingNode = new MissingNode<>(hash, location);
+              if (!hash.equals(MerklePatriciaTrie.EMPTY_TRIE_NODE_HASH)) {
+                incompleteLocation.add(location.slice(0, Math.max(0, location.size() - 1)));
+              }
+              return Optional.of(missingNode);
+            });
+  }
+
+  public List<InnerNode> getInnerNodes() {
+    return List.copyOf(innerNodes);
+  }
+
+  private boolean isInRange(final Bytes location) {
+    return !location.isEmpty()
+        && Arrays.compare(location.toArrayUnsafe(), startKeyHash.toArrayUnsafe()) >= 0
+        && Arrays.compare(location.toArrayUnsafe(), endKeyHash.toArrayUnsafe()) <= 0;
+  }
+
+  private Bytes createPath(final Bytes bytes) {
+    final MutableBytes path = MutableBytes.create(bytes.size() * 2);
+    int j = 0;
+    for (int i = 0; i < bytes.size(); i += 1, j += 2) {
+      final byte b = bytes.get(i);
+      path.set(j, (byte) ((b >>> 4) & 0x0f));
+      path.set(j + 1, (byte) (b & 0x0f));
+    }
+    return path;
+  }
+
+  @Value.Immutable
+  public interface InnerNode {
+    Bytes location();
+
+    Bytes path();
+  }
+}
