a889c7e79932cb12715b9ff9a5cefdad71b3fbbf
Frank Li <b439988l@gmail.com>
2022-04-29 15:29:25 +1000

    spotless
    Signed-off-by: Frank Li <b439988l@gmail.com>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/TxPoolBesuPendingTransactionsTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/TxPoolBesuPendingTransactionsTest.java
@@ -29,7 +29,6 @@ import org.hyperledger.besu.ethereum.eth.transactions.sorter.AbstractPendingTran
 import org.hyperledger.besu.ethereum.eth.transactions.sorter.GasPricePendingTransactionsSorter;
 
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -87,12 +86,18 @@ public class TxPoolBesuPendingTransactionsTest {
     final Set<TransactionPendingResult> result =
         (Set<TransactionPendingResult>) actualResponse.getResult();
 
-    assertThat(result).extracting(TransactionPendingResult::getGasPrice).filteredOn(
-        Objects::nonNull).allSatisfy(p -> assertThat(p).doesNotContain("0x0"));
-    assertThat(result).extracting(TransactionPendingResult::getMaxFeePerGas).filteredOn(
-        Objects::nonNull).allSatisfy(p -> assertThat(p).doesNotContain("0x0"));
-    assertThat(result).extracting(TransactionPendingResult::getMaxPriorityFeePerGas).filteredOn(
-        Objects::nonNull).allSatisfy(p -> assertThat(p).doesNotContain("0x0"));
+    assertThat(result)
+        .extracting(TransactionPendingResult::getGasPrice)
+        .filteredOn(Objects::nonNull)
+        .allSatisfy(p -> assertThat(p).doesNotContain("0x0"));
+    assertThat(result)
+        .extracting(TransactionPendingResult::getMaxFeePerGas)
+        .filteredOn(Objects::nonNull)
+        .allSatisfy(p -> assertThat(p).doesNotContain("0x0"));
+    assertThat(result)
+        .extracting(TransactionPendingResult::getMaxPriorityFeePerGas)
+        .filteredOn(Objects::nonNull)
+        .allSatisfy(p -> assertThat(p).doesNotContain("0x0"));
   }
 
   @Test
