ffea820b671f1963d4828c4be815733f29f6cf27
Sally MacFarlane <sally.macfarlane@consensys.net>
2022-05-05 11:02:30 +1000

    Revert "added clone method"
    This reverts commit 1372fb187ec904cd0bacb6c4c99dce8586fb5826.
    
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- consensus/common/src/main/java/org/hyperledger/besu/consensus/common/bft/SizeLimitedMap.java
+++ consensus/common/src/main/java/org/hyperledger/besu/consensus/common/bft/SizeLimitedMap.java
@@ -32,9 +32,4 @@ public class SizeLimitedMap<K, V> extends LinkedHashMap<K, V> {
   protected boolean removeEldestEntry(final Map.Entry<K, V> ignored) {
     return size() > maxEntries;
   }
-
-  @Override
-  public Object clone() {
-    return super.clone();
-  }
 }
