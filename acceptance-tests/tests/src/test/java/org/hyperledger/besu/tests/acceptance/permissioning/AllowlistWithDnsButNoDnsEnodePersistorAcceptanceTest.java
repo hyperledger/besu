d4daead1b737dd5b679653c03f8c94cf8d3f3140
Sally MacFarlane <sally.macfarlane@consensys.net>
2022-05-09 13:53:13 +1000

    differentiate enodes
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/permissioning/AllowlistWithDnsButNoDnsEnodePersistorAcceptanceTest.java
+++ acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/permissioning/AllowlistWithDnsButNoDnsEnodePersistorAcceptanceTest.java
@@ -49,7 +49,7 @@ public class AllowlistWithDnsButNoDnsEnodePersistorAcceptanceTest extends Accept
             + InetAddress.getLocalHost().getHostName()
             + ":4567";
     ENODE_TWO_IP =
-        "enode://5f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@192.168.0.10:4567";
+        "enode://5f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@192.168.0.10:1234";
 
     senderA = accounts.getPrimaryBenefactor();
     tempFile = Files.createTempFile("test", "perm-dns-test1");
