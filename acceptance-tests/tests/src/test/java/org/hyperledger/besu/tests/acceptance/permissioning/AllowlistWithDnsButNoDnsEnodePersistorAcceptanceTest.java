510dc44440b720a991ad7468a4cc4abf6948772d
Sally MacFarlane <sally.macfarlane@consensys.net>
2022-05-09 11:03:14 +1000

    added a separate test for the IP enode when DNS is enabled
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/permissioning/AllowlistWithDnsButNoDnsEnodePersistorAcceptanceTest.java
+++ acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/permissioning/AllowlistWithDnsButNoDnsEnodePersistorAcceptanceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright ConsenSys AG.
+ * Copyright contributors to Hyperledger Besu.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -20,25 +20,21 @@ import org.hyperledger.besu.tests.acceptance.dsl.AcceptanceTestBase;
 import org.hyperledger.besu.tests.acceptance.dsl.account.Account;
 import org.hyperledger.besu.tests.acceptance.dsl.node.Node;
 
-import java.net.InetAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AllowlistWithDnsPersistorAcceptanceTest extends AcceptanceTestBase {
+public class AllowlistWithDnsButNoDnsEnodePersistorAcceptanceTest extends AcceptanceTestBase {
   private static final Logger LOG =
-      LoggerFactory.getLogger(AllowlistWithDnsPersistorAcceptanceTest.class);
+      LoggerFactory.getLogger(AllowlistWithDnsButNoDnsEnodePersistorAcceptanceTest.class);
 
-  private String ENODE_ONE_DNS;
   private String ENODE_TWO_IP;
-  private String ENODE_THREE_IP;
 
   private Node node;
   private Account senderA;
@@ -46,17 +42,11 @@ public class AllowlistWithDnsPersistorAcceptanceTest extends AcceptanceTestBase 
 
   @Before
   public void setUp() throws Exception {
-    ENODE_ONE_DNS =
-        "enode://6f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@"
-            + InetAddress.getLocalHost().getHostName()
-            + ":4567";
     ENODE_TWO_IP =
         "enode://5f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@192.168.0.10:4567";
-    ENODE_THREE_IP =
-        "enode://4f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@192.168.0.11:4567";
 
     senderA = accounts.getPrimaryBenefactor();
-    tempFile = Files.createTempFile("test", "perm-dns-test0");
+    tempFile = Files.createTempFile("test", "perm-dns-test1");
 
     this.node =
         permissionedNodeBuilder
@@ -71,37 +61,14 @@ public class AllowlistWithDnsPersistorAcceptanceTest extends AcceptanceTestBase 
     cluster.start(this.node);
   }
 
-  @Ignore("test is failing in CI")
   @Test
-  public void manipulatedNodesAllowlistWithHostnameShouldWorkWhenDnsEnabled() {
+  public void singleNodeAllowlistWithIpShouldWorkWhenDnsEnabled() {
 
-    node.verify(perm.addNodesToAllowlist(ENODE_ONE_DNS, ENODE_TWO_IP));
-    LOG.info("enode one " + ENODE_ONE_DNS);
+    node.verify(perm.addNodesToAllowlist(ENODE_TWO_IP));
     LOG.info("enode two " + ENODE_TWO_IP);
     LOG.info("temp file " + tempFile.toAbsolutePath());
     node.verify(
         perm.expectPermissioningAllowlistFileKeyValue(
-            ALLOWLIST_TYPE.NODES, tempFile, ENODE_ONE_DNS, ENODE_TWO_IP));
-
-    node.verify(perm.removeNodesFromAllowlist(ENODE_ONE_DNS));
-    node.verify(
-        perm.expectPermissioningAllowlistFileKeyValue(
             ALLOWLIST_TYPE.NODES, tempFile, ENODE_TWO_IP));
-
-    node.verify(perm.addNodesToAllowlist(ENODE_ONE_DNS, ENODE_THREE_IP));
-    node.verify(
-        perm.expectPermissioningAllowlistFileKeyValue(
-            ALLOWLIST_TYPE.NODES, tempFile, ENODE_TWO_IP, ENODE_ONE_DNS, ENODE_THREE_IP));
-  }
-
-  @Test
-  public void singleNodeAllowlistWithHostnameShouldWorkWhenDnsEnabled() {
-
-    node.verify(perm.addNodesToAllowlist(ENODE_ONE_DNS));
-    LOG.info("enode one " + ENODE_ONE_DNS);
-    LOG.info("temp file " + tempFile.toAbsolutePath());
-    node.verify(
-        perm.expectPermissioningAllowlistFileKeyValue(
-            ALLOWLIST_TYPE.NODES, tempFile, ENODE_ONE_DNS));
   }
 }
