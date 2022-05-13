551976e798056a191ad395d345f7347474a3c98d
Ameziane H <ameziane.hamlat@consensys.net>
2022-05-13 10:33:51 +0200

    Performance test : add the name of the Pipeline's step in Tracing Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/SynchronizerConfiguration.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/SynchronizerConfiguration.java
@@ -39,7 +39,7 @@ public class SynchronizerConfiguration {
   public static final long DEFAULT_DOWNLOADER_CHANGE_TARGET_THRESHOLD_BY_HEIGHT = 200L;
   public static final UInt256 DEFAULT_DOWNLOADER_CHANGE_TARGET_THRESHOLD_BY_TD =
       UInt256.valueOf(1_000_000_000_000_000_000L);
-  public static final int DEFAULT_DOWNLOADER_HEADER_REQUEST_SIZE = 200;
+  public static final int DEFAULT_DOWNLOADER_HEADER_REQUEST_SIZE = 50;
   public static final int DEFAULT_DOWNLOADER_CHECKPOINT_TIMEOUTS_PERMITTED = 5;
   public static final int DEFAULT_DOWNLOADER_CHAIN_SEGMENT_SIZE = 200;
   public static final int DEFAULT_DOWNLOADER_PARALLELISM = 4;
