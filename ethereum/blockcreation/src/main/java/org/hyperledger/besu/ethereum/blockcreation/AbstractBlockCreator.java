cc72bd089e2ba5da8bccdd806939da05a0d7db7c
Justin Florentine <justin+github@florentine.us>
2022-05-16 16:02:31 -0400

    don't wrap and re-throw, pollutes logs and causing exception is handled correctly
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreator.java
+++ ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreator.java
@@ -208,8 +208,6 @@ public abstract class AbstractBlockCreator implements AsyncBlockCreator {
       return new Block(blockHeader, new BlockBody(transactionResults.getTransactions(), ommers));
     } catch (final SecurityModuleException ex) {
       throw new IllegalStateException("Failed to create block signature", ex);
-    } catch (final CancellationException ex) {
-      throw new IllegalStateException("Attempt to create block was interrupted", ex);
     } catch (final Exception ex) {
       // TODO(tmm): How are we going to know this has exploded, and thus restart it?
       throw new IllegalStateException(
