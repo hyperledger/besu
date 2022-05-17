bb6db5a7a69564a5ceacc47ffac9045e13f696a9
Justin Florentine <justin+github@florentine.us>
2022-05-17 11:17:19 -0400

    rethrow to make sure it isn't caught by Exception catch and re-wrapped
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreator.java
+++ ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreator.java
@@ -208,6 +208,8 @@ public abstract class AbstractBlockCreator implements AsyncBlockCreator {
       return new Block(blockHeader, new BlockBody(transactionResults.getTransactions(), ommers));
     } catch (final SecurityModuleException ex) {
       throw new IllegalStateException("Failed to create block signature", ex);
+    } catch (final CancellationException ex) {
+      throw ex;
     } catch (final Exception ex) {
       // TODO(tmm): How are we going to know this has exploded, and thus restart it?
       throw new IllegalStateException(
