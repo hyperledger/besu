9a4284c7c056b58d93dc59caa6fd4769dd7c626e
Karim TAAM <karim.t2am@gmail.com>
2022-05-30 14:06:54 +0200

    add log
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/BonsaiWorldStateKeyValueStorage.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/BonsaiWorldStateKeyValueStorage.java
@@ -88,6 +88,7 @@ public class BonsaiWorldStateKeyValueStorage implements WorldStateStorage {
     Optional<Bytes> response = Optional.empty();
     final Optional<Bytes> worldStateRootHash = getWorldStateRootHash();
     LOG.info("read worldtstate from root " + worldStateRootHash);
+    LOG.info("block hash key " + getWorldStateBlockHash());
     LOG.info(
         "read root node "
             + trieBranchStorage.get(Bytes.EMPTY.toArrayUnsafe()).map(Bytes::wrap).map(Hash::hash));
