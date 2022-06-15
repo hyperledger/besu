9bfd97af2ebaaf051f6f11405b17220f273d0c90
Ameziane H <ameziane.hamlat@consensys.net>
2022-06-15 20:59:51 +0200

    Apply some RocksDB documentation suggestions to reduce memory usage
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>

--- plugins/rocksdb/src/main/java/org/hyperledger/besu/plugin/services/storage/rocksdb/segmented/RocksDBColumnarKeyValueStorage.java
+++ plugins/rocksdb/src/main/java/org/hyperledger/besu/plugin/services/storage/rocksdb/segmented/RocksDBColumnarKeyValueStorage.java
@@ -48,6 +48,7 @@ import org.rocksdb.BloomFilter;
 import org.rocksdb.ColumnFamilyDescriptor;
 import org.rocksdb.ColumnFamilyHandle;
 import org.rocksdb.ColumnFamilyOptions;
+import org.rocksdb.CompressionType;
 import org.rocksdb.DBOptions;
 import org.rocksdb.Env;
 import org.rocksdb.LRUCache;
@@ -98,6 +99,7 @@ public class RocksDBColumnarKeyValueStorage
                           new ColumnFamilyOptions()
                               .setTtl(0)
                               .setOptimizeFiltersForHits(true)
+                              .setCompressionType(CompressionType.LZ4_COMPRESSION)
                               .setTableFormatConfig(createBlockBasedTableConfig(configuration))))
               .collect(Collectors.toList());
       columnDescriptors.add(
@@ -105,6 +107,7 @@ public class RocksDBColumnarKeyValueStorage
               DEFAULT_COLUMN.getBytes(StandardCharsets.UTF_8),
               columnFamilyOptions
                   .setTtl(0)
+                  .setCompressionType(CompressionType.LZ4_COMPRESSION)
                   .setOptimizeFiltersForHits(true)
                   .setTableFormatConfig(createBlockBasedTableConfig(configuration))));
 
