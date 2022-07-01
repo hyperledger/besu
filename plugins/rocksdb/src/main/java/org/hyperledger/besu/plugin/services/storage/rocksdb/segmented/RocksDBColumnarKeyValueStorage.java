d3aaca8f1b35c1893269b8e4c71e014e512e1dce
Ameziane H <ameziane.hamlat@consensys.net>
2022-07-01 11:16:21 +0200

    Enforce RocksDB to respect block cache size Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>

--- plugins/rocksdb/src/main/java/org/hyperledger/besu/plugin/services/storage/rocksdb/segmented/RocksDBColumnarKeyValueStorage.java
+++ plugins/rocksdb/src/main/java/org/hyperledger/besu/plugin/services/storage/rocksdb/segmented/RocksDBColumnarKeyValueStorage.java
@@ -148,7 +148,7 @@ public class RocksDBColumnarKeyValueStorage
   }
 
   private BlockBasedTableConfig createBlockBasedTableConfig(final RocksDBConfiguration config) {
-    final LRUCache cache = new LRUCache(config.getCacheCapacity());
+    final LRUCache cache = new LRUCache(config.getCacheCapacity(), -1, true);
     return new BlockBasedTableConfig()
         .setBlockCache(cache)
         .setFormatVersion(5)
