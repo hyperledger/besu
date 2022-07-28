65ce39ecb91eac6a91190cc89958c58f9346f1ab
Fabio Di Fabio <fabio.difabio@consensys.net>
2022-07-28 09:59:05 +0200

    Revert change in unit test
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/mainnet/feemarket/LondonFeeMarketTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/mainnet/feemarket/LondonFeeMarketTest.java
@@ -49,7 +49,7 @@ public class LondonFeeMarketTest {
     final Transaction transaction =
         new TransactionTestFixture()
             .type(TransactionType.EIP1559)
-            .maxFeePerGas(Optional.of(Wei.of(0)))
+            .maxFeePerGas(Optional.of(Wei.of(6)))
             .maxPriorityFeePerGas(Optional.of(Wei.of(0)))
             .gasPrice(null)
             .createTransaction(KEY_PAIR1);
