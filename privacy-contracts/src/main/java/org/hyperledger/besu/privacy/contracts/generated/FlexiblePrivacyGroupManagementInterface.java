45c3329652a8f304f1cdddff6289019bfcdd73a0
Miguel Rojo <miguelangel.rojofernandez@mastercard.com>
2022-08-23 09:55:47 +0100

    spotless
    Signed-off-by: Miguel Rojo <miguelangel.rojofernandez@mastercard.com>

--- privacy-contracts/src/main/java/org/hyperledger/besu/privacy/contracts/generated/FlexiblePrivacyGroupManagementInterface.java
+++ privacy-contracts/src/main/java/org/hyperledger/besu/privacy/contracts/generated/FlexiblePrivacyGroupManagementInterface.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
+
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Bool;
 import org.web3j.abi.datatypes.DynamicArray;
@@ -36,156 +37,237 @@ import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.ContractGasProvider;
 
 /**
- * <p>Auto generated code.
+ * Auto generated code.
+ *
  * <p><strong>Do not modify!</strong>
+ *
  * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
- * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the
- * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the <a
+ * href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
  *
  * <p>Generated with web3j version 1.4.1.
  */
 @SuppressWarnings("rawtypes")
 public class FlexiblePrivacyGroupManagementInterface extends Contract {
-    public static final String BINARY = "";
+  public static final String BINARY = "";
 
-    public static final String FUNC_ADDPARTICIPANTS = "addParticipants";
+  public static final String FUNC_ADDPARTICIPANTS = "addParticipants";
 
-    public static final String FUNC_CANEXECUTE = "canExecute";
+  public static final String FUNC_CANEXECUTE = "canExecute";
 
-    public static final String FUNC_CANUPGRADE = "canUpgrade";
+  public static final String FUNC_CANUPGRADE = "canUpgrade";
 
-    public static final String FUNC_GETPARTICIPANTS = "getParticipants";
+  public static final String FUNC_GETPARTICIPANTS = "getParticipants";
 
-    public static final String FUNC_GETVERSION = "getVersion";
+  public static final String FUNC_GETVERSION = "getVersion";
 
-    public static final String FUNC_LOCK = "lock";
+  public static final String FUNC_LOCK = "lock";
 
-    public static final String FUNC_REMOVEPARTICIPANT = "removeParticipant";
+  public static final String FUNC_REMOVEPARTICIPANT = "removeParticipant";
 
-    public static final String FUNC_UNLOCK = "unlock";
+  public static final String FUNC_UNLOCK = "unlock";
 
-    @Deprecated
-    protected FlexiblePrivacyGroupManagementInterface(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
-        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
-    }
+  @Deprecated
+  protected FlexiblePrivacyGroupManagementInterface(
+      String contractAddress,
+      Web3j web3j,
+      Credentials credentials,
+      BigInteger gasPrice,
+      BigInteger gasLimit) {
+    super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+  }
 
-    protected FlexiblePrivacyGroupManagementInterface(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
-        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
-    }
+  protected FlexiblePrivacyGroupManagementInterface(
+      String contractAddress,
+      Web3j web3j,
+      Credentials credentials,
+      ContractGasProvider contractGasProvider) {
+    super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+  }
 
-    @Deprecated
-    protected FlexiblePrivacyGroupManagementInterface(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
-        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
-    }
+  @Deprecated
+  protected FlexiblePrivacyGroupManagementInterface(
+      String contractAddress,
+      Web3j web3j,
+      TransactionManager transactionManager,
+      BigInteger gasPrice,
+      BigInteger gasLimit) {
+    super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+  }
 
-    protected FlexiblePrivacyGroupManagementInterface(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
-        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
-    }
+  protected FlexiblePrivacyGroupManagementInterface(
+      String contractAddress,
+      Web3j web3j,
+      TransactionManager transactionManager,
+      ContractGasProvider contractGasProvider) {
+    super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+  }
 
-    public RemoteFunctionCall<TransactionReceipt> addParticipants(List<byte[]> publicEnclaveKeys) {
-        final Function function = new Function(
-                FUNC_ADDPARTICIPANTS,
-                Arrays.<Type>asList(new org.web3j.abi.datatypes.DynamicArray<org.web3j.abi.datatypes.DynamicBytes>(
-                        org.web3j.abi.datatypes.DynamicBytes.class,
-                        org.web3j.abi.Utils.typeMap(publicEnclaveKeys, org.web3j.abi.datatypes.DynamicBytes.class))),
-                Collections.<TypeReference<?>>emptyList());
-        return executeRemoteCallTransaction(function);
-    }
+  public RemoteFunctionCall<TransactionReceipt> addParticipants(List<byte[]> publicEnclaveKeys) {
+    final Function function =
+        new Function(
+            FUNC_ADDPARTICIPANTS,
+            Arrays.<Type>asList(
+                new org.web3j.abi.datatypes.DynamicArray<org.web3j.abi.datatypes.DynamicBytes>(
+                    org.web3j.abi.datatypes.DynamicBytes.class,
+                    org.web3j.abi.Utils.typeMap(
+                        publicEnclaveKeys, org.web3j.abi.datatypes.DynamicBytes.class))),
+            Collections.<TypeReference<?>>emptyList());
+    return executeRemoteCallTransaction(function);
+  }
 
-    public RemoteFunctionCall<Boolean> canExecute() {
-        final Function function = new Function(FUNC_CANEXECUTE,
-                Arrays.<Type>asList(),
-                Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {}));
-        return executeRemoteCallSingleValueReturn(function, Boolean.class);
-    }
+  public RemoteFunctionCall<Boolean> canExecute() {
+    final Function function =
+        new Function(
+            FUNC_CANEXECUTE,
+            Arrays.<Type>asList(),
+            Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {}));
+    return executeRemoteCallSingleValueReturn(function, Boolean.class);
+  }
 
-    public RemoteFunctionCall<TransactionReceipt> canUpgrade() {
-        final Function function = new Function(
-                FUNC_CANUPGRADE,
-                Arrays.<Type>asList(),
-                Collections.<TypeReference<?>>emptyList());
-        return executeRemoteCallTransaction(function);
-    }
+  public RemoteFunctionCall<TransactionReceipt> canUpgrade() {
+    final Function function =
+        new Function(
+            FUNC_CANUPGRADE, Arrays.<Type>asList(), Collections.<TypeReference<?>>emptyList());
+    return executeRemoteCallTransaction(function);
+  }
 
-    public RemoteFunctionCall<List> getParticipants() {
-        final Function function = new Function(FUNC_GETPARTICIPANTS,
-                Arrays.<Type>asList(),
-                Arrays.<TypeReference<?>>asList(new TypeReference<DynamicArray<DynamicBytes>>() {}));
-        return new RemoteFunctionCall<List>(function,
-                new Callable<List>() {
-                    @Override
-                    @SuppressWarnings("unchecked")
-                    public List call() throws Exception {
-                        List<Type> result = (List<Type>) executeCallSingleValueReturn(function, List.class);
-                        return convertToNative(result);
-                    }
-                });
-    }
+  public RemoteFunctionCall<List> getParticipants() {
+    final Function function =
+        new Function(
+            FUNC_GETPARTICIPANTS,
+            Arrays.<Type>asList(),
+            Arrays.<TypeReference<?>>asList(new TypeReference<DynamicArray<DynamicBytes>>() {}));
+    return new RemoteFunctionCall<List>(
+        function,
+        new Callable<List>() {
+          @Override
+          @SuppressWarnings("unchecked")
+          public List call() throws Exception {
+            List<Type> result = (List<Type>) executeCallSingleValueReturn(function, List.class);
+            return convertToNative(result);
+          }
+        });
+  }
 
-    public RemoteFunctionCall<byte[]> getVersion() {
-        final Function function = new Function(FUNC_GETVERSION,
-                Arrays.<Type>asList(),
-                Arrays.<TypeReference<?>>asList(new TypeReference<Bytes32>() {}));
-        return executeRemoteCallSingleValueReturn(function, byte[].class);
-    }
+  public RemoteFunctionCall<byte[]> getVersion() {
+    final Function function =
+        new Function(
+            FUNC_GETVERSION,
+            Arrays.<Type>asList(),
+            Arrays.<TypeReference<?>>asList(new TypeReference<Bytes32>() {}));
+    return executeRemoteCallSingleValueReturn(function, byte[].class);
+  }
 
-    public RemoteFunctionCall<TransactionReceipt> lock() {
-        final Function function = new Function(
-                FUNC_LOCK,
-                Arrays.<Type>asList(),
-                Collections.<TypeReference<?>>emptyList());
-        return executeRemoteCallTransaction(function);
-    }
+  public RemoteFunctionCall<TransactionReceipt> lock() {
+    final Function function =
+        new Function(FUNC_LOCK, Arrays.<Type>asList(), Collections.<TypeReference<?>>emptyList());
+    return executeRemoteCallTransaction(function);
+  }
 
-    public RemoteFunctionCall<TransactionReceipt> removeParticipant(byte[] participant) {
-        final Function function = new Function(
-                FUNC_REMOVEPARTICIPANT,
-                Arrays.<Type>asList(new org.web3j.abi.datatypes.DynamicBytes(participant)),
-                Collections.<TypeReference<?>>emptyList());
-        return executeRemoteCallTransaction(function);
-    }
+  public RemoteFunctionCall<TransactionReceipt> removeParticipant(byte[] participant) {
+    final Function function =
+        new Function(
+            FUNC_REMOVEPARTICIPANT,
+            Arrays.<Type>asList(new org.web3j.abi.datatypes.DynamicBytes(participant)),
+            Collections.<TypeReference<?>>emptyList());
+    return executeRemoteCallTransaction(function);
+  }
 
-    public RemoteFunctionCall<TransactionReceipt> unlock() {
-        final Function function = new Function(
-                FUNC_UNLOCK,
-                Arrays.<Type>asList(),
-                Collections.<TypeReference<?>>emptyList());
-        return executeRemoteCallTransaction(function);
-    }
+  public RemoteFunctionCall<TransactionReceipt> unlock() {
+    final Function function =
+        new Function(FUNC_UNLOCK, Arrays.<Type>asList(), Collections.<TypeReference<?>>emptyList());
+    return executeRemoteCallTransaction(function);
+  }
 
-    @Deprecated
-    public static FlexiblePrivacyGroupManagementInterface load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
-        return new FlexiblePrivacyGroupManagementInterface(contractAddress, web3j, credentials, gasPrice, gasLimit);
-    }
+  @Deprecated
+  public static FlexiblePrivacyGroupManagementInterface load(
+      String contractAddress,
+      Web3j web3j,
+      Credentials credentials,
+      BigInteger gasPrice,
+      BigInteger gasLimit) {
+    return new FlexiblePrivacyGroupManagementInterface(
+        contractAddress, web3j, credentials, gasPrice, gasLimit);
+  }
 
-    @Deprecated
-    public static FlexiblePrivacyGroupManagementInterface load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
-        return new FlexiblePrivacyGroupManagementInterface(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
-    }
+  @Deprecated
+  public static FlexiblePrivacyGroupManagementInterface load(
+      String contractAddress,
+      Web3j web3j,
+      TransactionManager transactionManager,
+      BigInteger gasPrice,
+      BigInteger gasLimit) {
+    return new FlexiblePrivacyGroupManagementInterface(
+        contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+  }
 
-    public static FlexiblePrivacyGroupManagementInterface load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
-        return new FlexiblePrivacyGroupManagementInterface(contractAddress, web3j, credentials, contractGasProvider);
-    }
+  public static FlexiblePrivacyGroupManagementInterface load(
+      String contractAddress,
+      Web3j web3j,
+      Credentials credentials,
+      ContractGasProvider contractGasProvider) {
+    return new FlexiblePrivacyGroupManagementInterface(
+        contractAddress, web3j, credentials, contractGasProvider);
+  }
 
-    public static FlexiblePrivacyGroupManagementInterface load(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
-        return new FlexiblePrivacyGroupManagementInterface(contractAddress, web3j, transactionManager, contractGasProvider);
-    }
+  public static FlexiblePrivacyGroupManagementInterface load(
+      String contractAddress,
+      Web3j web3j,
+      TransactionManager transactionManager,
+      ContractGasProvider contractGasProvider) {
+    return new FlexiblePrivacyGroupManagementInterface(
+        contractAddress, web3j, transactionManager, contractGasProvider);
+  }
 
-    public static RemoteCall<FlexiblePrivacyGroupManagementInterface> deploy(Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
-        return deployRemoteCall(FlexiblePrivacyGroupManagementInterface.class, web3j, credentials, contractGasProvider, BINARY, "");
-    }
+  public static RemoteCall<FlexiblePrivacyGroupManagementInterface> deploy(
+      Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+    return deployRemoteCall(
+        FlexiblePrivacyGroupManagementInterface.class,
+        web3j,
+        credentials,
+        contractGasProvider,
+        BINARY,
+        "");
+  }
 
-    @Deprecated
-    public static RemoteCall<FlexiblePrivacyGroupManagementInterface> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
-        return deployRemoteCall(FlexiblePrivacyGroupManagementInterface.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
-    }
+  @Deprecated
+  public static RemoteCall<FlexiblePrivacyGroupManagementInterface> deploy(
+      Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+    return deployRemoteCall(
+        FlexiblePrivacyGroupManagementInterface.class,
+        web3j,
+        credentials,
+        gasPrice,
+        gasLimit,
+        BINARY,
+        "");
+  }
 
-    public static RemoteCall<FlexiblePrivacyGroupManagementInterface> deploy(Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
-        return deployRemoteCall(FlexiblePrivacyGroupManagementInterface.class, web3j, transactionManager, contractGasProvider, BINARY, "");
-    }
+  public static RemoteCall<FlexiblePrivacyGroupManagementInterface> deploy(
+      Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+    return deployRemoteCall(
+        FlexiblePrivacyGroupManagementInterface.class,
+        web3j,
+        transactionManager,
+        contractGasProvider,
+        BINARY,
+        "");
+  }
 
-    @Deprecated
-    public static RemoteCall<FlexiblePrivacyGroupManagementInterface> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
-        return deployRemoteCall(FlexiblePrivacyGroupManagementInterface.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
-    }
+  @Deprecated
+  public static RemoteCall<FlexiblePrivacyGroupManagementInterface> deploy(
+      Web3j web3j,
+      TransactionManager transactionManager,
+      BigInteger gasPrice,
+      BigInteger gasLimit) {
+    return deployRemoteCall(
+        FlexiblePrivacyGroupManagementInterface.class,
+        web3j,
+        transactionManager,
+        gasPrice,
+        gasLimit,
+        BINARY,
+        "");
+  }
 }
