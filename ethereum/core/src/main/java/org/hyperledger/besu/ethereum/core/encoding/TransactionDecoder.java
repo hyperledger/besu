e5d975d4397b9c86d08eaad884ebd85768f81573
Ameziane H <ameziane.hamlat@consensys.net>
2022-10-12 15:50:00 +0200

    Delete some debug code committed by error
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/encoding/TransactionDecoder.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/encoding/TransactionDecoder.java
@@ -42,11 +42,8 @@ import java.util.function.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableMap;
 import org.apache.tuweni.bytes.Bytes;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TransactionDecoder {
-  private static final Logger LOG = LoggerFactory.getLogger(TransactionDecoder.class);
 
   @FunctionalInterface
   interface Decoder {
