41036f2dd054e9c7cf0c3a8bbaf58c7f3f58327c
Danno Ferrin <danno.ferrin@gmail.com>
2022-10-16 20:01:41 -0600

    spotless
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/vm/operations/BaseFeeOperationTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/vm/operations/BaseFeeOperationTest.java
@@ -15,7 +15,6 @@
 package org.hyperledger.besu.ethereum.vm.operations;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
