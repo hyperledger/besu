9742c40ee5fa91a0b2837f02e0be7b42585f621f
garyschulte <garyschulte@gmail.com>
2022-11-15 13:03:28 -0800

    only copy the mutable worldstate if it is not persistable in abstract block creator
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreator.java
+++ ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreator.java
@@ -253,13 +253,19 @@ public abstract class AbstractBlockCreator implements AsyncBlockCreator {
         .getMutable(parentStateRoot, parentHeader.getHash(), false)
         .map(
             ws -> {
-              var wsCopy = ws.copy();
-              try {
-                ws.close();
-              } catch (Exception e) {
-                throw new RuntimeException(e);
+              if (!ws.isPersistable()) {
+                var wsCopy = ws.copy();
+                try {
+                  ws.close();
+                } catch (Exception ex) {
+                  LOG.error(
+                      "unexpected error closing non-peristable worldstate + "
+                          + parentHeader.toLogString(),
+                      ex);
+                }
+                return wsCopy;
               }
-              return wsCopy;
+              return ws;
             })
         .orElseThrow(
             () -> {
