c905eaa940371cc5f9aebf8b3067bb4be51d5e78
Danno Ferrin <danno.ferrin@swirldslabs.com>
2022-11-16 22:59:59 -0700

    big EOF parses
    Signed-off-by: Danno Ferrin <danno.ferrin@swirldslabs.com>

--- evm/src/main/java/org/hyperledger/besu/evm/code/EOFLayout.java
+++ evm/src/main/java/org/hyperledger/besu/evm/code/EOFLayout.java
@@ -17,8 +17,6 @@
 package org.hyperledger.besu.evm.code;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,93 +24,179 @@ import org.apache.tuweni.bytes.Bytes;
 
 public class EOFLayout {
 
+  static final int SECTION_TERMINATOR = 0x00;
   static final int SECTION_CODE = 0x01;
   static final int SECTION_DATA = 0x02;
+  static final int SECTION_TYPES = 0x03;
 
-  static final int MAX_SUPPORTED_SECTION = SECTION_DATA;
   static final int MAX_SUPPORTED_VERSION = 1;
 
   private final Bytes container;
   private final int version;
-  private final Bytes[] sections;
+  private final CodeSection[] codeSections;
   private final String invalidReason;
 
-  private EOFLayout(final Bytes container, final int version, final Bytes[] sections) {
+  private EOFLayout(final Bytes container, final int version, final CodeSection[] codeSections) {
     this.container = container;
     this.version = version;
-    this.sections = sections;
+    this.codeSections = codeSections;
     this.invalidReason = null;
   }
 
   private EOFLayout(final Bytes container, final String invalidReason) {
     this.container = container;
     this.version = -1;
-    this.sections = null;
+    this.codeSections = null;
     this.invalidReason = invalidReason;
+  }
+
+  private static EOFLayout invalidLayout(final Bytes container, final String invalidReason) {
+    return new EOFLayout(container, invalidReason);
   }
 
   public static EOFLayout parseEOF(final Bytes container) {
     final ByteArrayInputStream inputStream = new ByteArrayInputStream(container.toArrayUnsafe());
+    List<Integer> codeSectionSizes = new ArrayList<>();
+    int dataSize = 0;
+    int typeSize = 0;
+
     if (inputStream.available() < 3) {
-      return new EOFLayout(container, "EOF Container too small");
+      return invalidLayout(container, "EOF Container too small");
     }
     if (inputStream.read() != 0xEF) {
-      return new EOFLayout(container, "EOF header byte 0 incorrect");
+      return invalidLayout(container, "EOF header byte 0 incorrect");
     }
     if (inputStream.read() != 0x0) {
-      return new EOFLayout(container, "EOF header byte 1 incorrect");
+      return invalidLayout(container, "EOF header byte 1 incorrect");
     }
 
     final int version = inputStream.read();
     if (version > MAX_SUPPORTED_VERSION || version < 1) {
-      return new EOFLayout(container, "Unsupported EOF Version " + version);
-    }
-    final List<EOFSectionInfo> sectionInfos = new ArrayList<>(3);
-    EOFSectionInfo sectionInfo = EOFSectionInfo.getSectionInfo(inputStream);
-    while (sectionInfo != null && sectionInfo.kind() != 0) {
-      sectionInfos.add(sectionInfo);
-      sectionInfo = EOFSectionInfo.getSectionInfo(inputStream);
-    }
-    if (sectionInfo == null) {
-      return new EOFLayout(container, "Improper section headers");
+      return invalidLayout(container, "Unsupported EOF Version " + version);
     }
 
-    int remaining = inputStream.available();
-    int pos = container.size() - remaining;
+    // parse section headers
+    SECTION_HEADER_LOOP:
+    while (true) {
+      int kind = inputStream.read();
+      switch (kind) {
+        case -1:
+          return invalidLayout(container, "Improper section headers");
+        case SECTION_TERMINATOR:
+          break SECTION_HEADER_LOOP;
+        case SECTION_CODE:
+          if (dataSize > 0) {
+            return invalidLayout(container, "Code section cannot follow data section");
+          }
+          int codeSectionSize = readUnsignedShort(inputStream);
+          if (codeSectionSize < 0) {
+            return invalidLayout(container, "Improper section headers");
+          } else if (codeSectionSize == 0) {
+            return invalidLayout(container, "Empty section contents");
+          }
+          codeSectionSizes.add(codeSectionSize);
+          break;
+        case SECTION_DATA:
+          if (dataSize != 0) {
+            return invalidLayout(container, "Duplicate section number 2");
+          }
+          dataSize = readUnsignedShort(inputStream);
+          if (dataSize < 0) {
+            return invalidLayout(container, "Improper section headers");
+          } else if (dataSize == 0) {
+            return invalidLayout(container, "Empty section contents");
+          }
+          break;
+        case SECTION_TYPES:
+          if (typeSize != 0) {
+            return invalidLayout(container, "Duplicate section number 3");
+          }
+          if (!codeSectionSizes.isEmpty()) {
+            return invalidLayout(container, "Code section cannot precede Type Section");
+          }
+          typeSize = readUnsignedShort(inputStream);
+          if (typeSize == 0) {
+            return invalidLayout(container, "Type section cannot be zero length");
+          }
+          if (typeSize % 2 == 1) {
+            return invalidLayout(container, "Type section cannot be odd length");
+          }
+          break;
+        default:
+          return invalidLayout(container, "EOF Section kind " + kind + " not supported");
+      }
+    }
 
-    final Bytes[] sections = new Bytes[MAX_SUPPORTED_SECTION + 1];
-    for (final var info : sectionInfos) {
-      final int kind = info.kind();
-      final int size = info.size();
-      if (kind > MAX_SUPPORTED_SECTION) {
-        return new EOFLayout(container, "EOF Section kind " + kind + " not supported");
-      }
-      if (sections[kind] != null) {
-        return new EOFLayout(container, "Duplicate section number " + kind);
-      }
-      if (size == 0) {
-        return new EOFLayout(container, "Empty section contents");
-      }
-      if (size > remaining) {
-        return new EOFLayout(container, "Missing or incomplete section data");
-      }
-      if (kind == 1 && sections[2] != null) {
-        return new EOFLayout(container, "Code section cannot follow data section");
+    if (codeSectionSizes.isEmpty()) {
+      return invalidLayout(container, "Missing code (kind=1) section");
+    }
+
+    // parse input/output table
+    int[] inputs;
+    int[] outputs;
+    int codeSectionCount = codeSectionSizes.size();
+    if (typeSize == 0) {
+      if (codeSectionCount > 1) {
+        return invalidLayout(container, "Multiple code sections but not enough type entries");
       }
 
-      sections[kind] = container.slice(pos, size);
-      pos += size;
-      remaining -= size;
+      inputs = new int[] {0};
+      outputs = new int[] {0};
+    } else if (typeSize > 2048) {
+      return invalidLayout(container, "Too many code sections");
+    } else {
+      if (codeSectionCount != typeSize / 2) {
+        return new EOFLayout(
+            container,
+            "Type data length ("
+                + typeSize
+                + ") does not match code size count ("
+                + codeSectionSizes.size()
+                + " * 2)");
+      }
+
+      inputs = new int[codeSectionCount];
+      outputs = new int[codeSectionCount];
+      inputs[0] = inputStream.read();
+      outputs[0] = inputStream.read();
+      if (inputs[0] != 0 || outputs[0] != 0) {
+        return invalidLayout(container, "First section input and output must be zero");
+      }
+      for (int i = 1; i < codeSectionCount; i++) {
+        inputs[i] = inputStream.read();
+        outputs[i] = inputStream.read();
+      }
     }
-    if (pos < container.size()) {
-      return new EOFLayout(container, "Dangling data at end of container");
+
+    // assemble code sections
+    CodeSection[] codeSections = new CodeSection[codeSectionCount];
+    for (int i = 0; i < codeSectionSizes.size(); i++) {
+      int thisSectionSize = codeSectionSizes.get(i);
+      byte[] codeBytes = new byte[thisSectionSize];
+      if (thisSectionSize != inputStream.read(codeBytes, 0, thisSectionSize)) {
+        return invalidLayout(container, "Missing or incomplete section data");
+      }
+      Bytes code = Bytes.wrap(codeBytes);
+      codeSections[i] = new CodeSection(code, inputs[i], outputs[i]);
     }
-    if (sections[1] == null) {
-      return new EOFLayout(container, "Missing code (kind=1) section");
-    } else if (sections[1].size() < 1) {
-      return new EOFLayout(container, "Code section empty");
+
+    // check remaining container validity
+    if (dataSize != inputStream.skip(dataSize)) {
+      return invalidLayout(container, "Missing or incomplete section data");
     }
-    return new EOFLayout(container, version, sections);
+    if (inputStream.available() > 0) {
+      return invalidLayout(container, "Dangling data at end of container");
+    }
+
+    return new EOFLayout(container, version, codeSections);
+  }
+
+  static int readUnsignedShort(final ByteArrayInputStream inputStream) {
+    if (inputStream.available() < 2) {
+      return -1;
+    } else {
+      return inputStream.read() << 8 | inputStream.read();
+    }
   }
 
   public Bytes getContainer() {
@@ -123,8 +207,8 @@ public class EOFLayout {
     return version;
   }
 
-  public Bytes[] getSections() {
-    return sections;
+  public CodeSection[] getCodeSections() {
+    return codeSections;
   }
 
   public String getInvalidReason() {
@@ -136,38 +220,28 @@ public class EOFLayout {
   }
 }
 
-// TODO should be a record
-final class EOFSectionInfo {
-  private final int kind;
-  private final int size;
+//// java17 convert to record
 
-  private EOFSectionInfo(final int kind, final int size) {
-    this.kind = kind;
-    this.size = size;
+final class CodeSection {
+  final Bytes code;
+  final int inputs;
+  final int outputs;
+
+  public CodeSection(final Bytes code, final int inputs, final int outputs) {
+    this.code = code;
+    this.inputs = inputs;
+    this.outputs = outputs;
   }
 
-  static EOFSectionInfo getSectionInfo(final InputStream in) {
-    try {
-      final int kind = in.read();
-      if (kind < 0) {
-        return null;
-      } else if (kind == 0) {
-        return new EOFSectionInfo(kind, 0);
-      } else {
-        final int msb = in.read() << 8;
-        final int lsb = in.read();
-        return new EOFSectionInfo(kind, msb + lsb);
-      }
-    } catch (final IOException ioe) {
-      return null;
-    }
+  public Bytes getCode() {
+    return code;
   }
 
-  public int kind() {
-    return kind;
+  public int getInputs() {
+    return inputs;
   }
 
-  public int size() {
-    return size;
+  public int getOutputs() {
+    return outputs;
   }
 }
--- evm/src/test/java/org/hyperledger/besu/evm/code/EOFLayoutTest.java
+++ evm/src/test/java/org/hyperledger/besu/evm/code/EOFLayoutTest.java
@@ -30,118 +30,188 @@ public class EOFLayoutTest {
   public static Collection<Object[]> testCasesFromEIP3540() {
     return Arrays.asList(
         new Object[][] {
-          {"EF", "No magic", "EOF Container too small", null},
-          {
-            "EFFF01010002020004006000AABBCCDD", "Invalid magic", "EOF header byte 1 incorrect", null
-          },
-          {"EF00", "No version", "EOF Container too small", null},
-          {
-            "EF0000010002020004006000AABBCCDD", "Invalid version", "Unsupported EOF Version 0", null
-          },
-          {
-            "EF0002010002020004006000AABBCCDD", "Invalid version", "Unsupported EOF Version 2", null
-          },
-          {
-            "EF00FF010002020004006000AABBCCDD",
-            "Invalid version",
-            "Unsupported EOF Version 255",
-            null
-          },
-          {"EF0001", "No header", "Improper section headers", null},
-          {"EF000100", "No code section", "Missing code (kind=1) section", null},
-          {"EF000101", "No code section size", "Improper section headers", null},
-          {"EF00010100", "Code section size incomplete", "Improper section headers", null},
-          {"EF0001010002", "No section terminator", "Improper section headers", null},
-          {
-            "EF000101000200", "No code section contents", "Missing or incomplete section data", null
-          },
+          {"EF", "No magic", "EOF Container too small"},
+          {"EFFF01010002020004006000AABBCCDD", "Invalid magic", "EOF header byte 1 incorrect"},
+          {"EF00", "No version", "EOF Container too small"},
+          {"EF0000010002020004006000AABBCCDD", "Invalid version", "Unsupported EOF Version 0"},
+          {"EF0002010002020004006000AABBCCDD", "Invalid version", "Unsupported EOF Version 2"},
+          {"EF00FF010002020004006000AABBCCDD", "Invalid version", "Unsupported EOF Version 255"},
+          {"EF0001", "No header", "Improper section headers"},
+          {"EF000100", "No code section", "Missing code (kind=1) section"},
+          {"EF000101", "No code section size", "Improper section headers"},
+          {"EF00010100", "Code section size incomplete", "Improper section headers"},
+          {"EF0001010002", "No section terminator", "Improper section headers"},
+          {"EF000101000200", "No code section contents", "Missing or incomplete section data"},
           {
             "EF00010100020060",
             "Code section contents incomplete",
-            "Missing or incomplete section data",
-            null
+            "Missing or incomplete section data"
           },
           {
             "EF0001010002006000DEADBEEF",
             "Trailing bytes after code section",
-            "Dangling data at end of container",
-            null
+            "Dangling data at end of container"
           },
-          {
-            "EF00010100020100020060006000",
-            "Multiple code sections",
-            "Duplicate section number 1",
-            null
-          },
-          {"EF000101000000", "Empty code section", "Empty section contents", null},
-          {"EF000101000002000200AABB", "Empty code section", "Empty section contents", null},
+          // {"EF00010100020100020060006000", "Multiple code sections",
+          //     "Duplicate section number 1"},
+          {"EF000101000000", "Empty code section", "Empty section contents"},
+          {"EF000101000002000200AABB", "Empty code section", "Empty section contents"},
           {
             "EF000102000401000200AABBCCDD6000",
             "Data section preceding code section",
-            "Code section cannot follow data section",
-            null
+            "Code section cannot follow data section"
           },
           {
             "EF000102000400AABBCCDD",
             "Data section without code section",
-            "Missing code (kind=1) section",
-            null
+            "Missing code (kind=1) section"
           },
-          {"EF000101000202", "No data section size", "Improper section headers", null},
-          {"EF00010100020200", "Data section size incomplete", "Improper section headers", null},
-          {"EF0001010002020004", "No section terminator", "Improper section headers", null},
+          {"EF000101000202", "No data section size", "Improper section headers"},
+          {"EF00010100020200", "Data section size incomplete", "Improper section headers"},
+          {"EF0001010002020004", "No section terminator", "Improper section headers"},
           {
             "EF0001010002020004006000",
             "No data section contents",
-            "Missing or incomplete section data",
-            null
+            "Missing or incomplete section data"
           },
           {
             "EF0001010002020004006000AABBCC",
             "Data section contents incomplete",
-            "Missing or incomplete section data",
-            null
+            "Missing or incomplete section data"
           },
           {
             "EF0001010002020004006000AABBCCDDEE",
             "Trailing bytes after data section",
-            "Dangling data at end of container",
-            null
+            "Dangling data at end of container"
           },
           {
             "EF0001010002020004020004006000AABBCCDDAABBCCDD",
             "Multiple data sections",
-            "Duplicate section number 2",
-            null
+            "Duplicate section number 2"
           },
-          {"EF0001010002020000006000", "Empty data section", "Empty section contents", null},
+          {"EF0001010002020000006000", "Empty data section", "Empty section contents"},
+          // {
+          //   "EF0001010002030004006000AABBCCDD",
+          //   "Unknown section (id = 3)",
+          //   "EOF Section kind 3 not supported"
+          // },
+          {"EF0001010002006000", "Valid", null},
+        });
+  }
+
+  public static Collection<Object[]> testCasesFromEIP4750() {
+    return Arrays.asList(
+        new Object[][] {
+          {"EF000101000100FE", "implicit type section, no data section", null},
+          {"EF000101000102000100FEDA", "implicit type section, data section", null},
+          {"EF0001030002010001000000FE", "type section, no data section", null},
+          {"EF0001030002010001020001000000FEDA", "type section, data section", null},
           {
-            "EF0001010002030004006000AABBCCDD",
-            "Unknown section (id = 3)",
-            "EOF Section kind 3 not supported",
+            "EF00010300040100010100010000000000FEFE",
+            "multiple code sections, no data section",
             null
           },
-          {"EF0001010002006000", "Valid", null, "0x6000"},
+          {
+            "EF00010300040100010100010200010000000000FEFEDA",
+            "multiple code sections, data section",
+            null
+          },
+          {
+            "EF0001030008010001010002010002010002000000010000010203FE500030008000",
+            "non-void input and output types",
+            null
+          },
+          {
+            "EF0001030800" + "010001".repeat(1024) + "00" + "0000".repeat(1024) + "FE".repeat(1024),
+            "max number of code sections",
+            null
+          },
+          {"ef000101000100fc", "RETF is terminating instruction", null},
+          {"EF000104000100FE", "unknown_section_id", "EOF Section kind 4 not supported"},
+          {
+            "EF000101000101000100FEFE",
+            "mandatory_type_section_missing",
+            "Multiple code sections but not enough type entries"
+          },
+          {
+            "EF00010300020300020100010100010000000000FEFE",
+            "multiple_type_sections",
+            "Duplicate section number 3"
+          },
+          {
+            "EF0001030002010001010001030002000000FEFE0000",
+            "multiple_type_sections",
+            "Duplicate section number 3"
+          },
+          {
+            "EF000101000103000200FE0000",
+            "code_section_before_type_section",
+            "Code section cannot precede Type Section"
+          },
+          {
+            "EF000101000103000201000100FE0000FE",
+            "code_section_before_type_section",
+            "Code section cannot precede Type Section"
+          },
+          {
+            "EF000101000103000202000300FE0000AABBCC",
+            "code_section_before_type_section",
+            "Code section cannot precede Type Section"
+          },
+          {
+            "EF000101000102000303000200FEAABBCC0000",
+            "code_section_before_type_section",
+            "Code section cannot precede Type Section"
+          },
+          {
+            "EF00010300010100010000FE",
+            "invalid_type_section_size",
+            "Type section cannot be odd length"
+          },
+          {
+            "EF00010300040100010000000000FE",
+            "invalid_type_section_size",
+            "Type data length (4) does not match code size count (1 * 2)"
+          },
+          {
+            "EF00010300040100010100010100010000000000FEFEFE",
+            "invalid_type_section_size",
+            "Type data length (4) does not match code size count (3 * 2)"
+          },
+          {
+            "EF0001030008010001010001010001000000000000000000FEFEFE",
+            "invalid_type_section_size",
+            "Type data length (8) does not match code size count (3 * 2)"
+          },
+          {
+            "EF000103000201000300000160005C",
+            "invalid_first_section_type",
+            "First section input and output must be zero"
+          },
+          {
+            "EF00010300020100020001005000",
+            "invalid_first_section_type",
+            "First section input and output must be zero"
+          },
+          {
+            "EF000103000201000300020360005C",
+            "invalid_first_section_type",
+            "First section input and output must be zero"
+          },
+          {
+            "EF0001030802" + "010001".repeat(1025) + "00" + "0000".repeat(1025) + "FE".repeat(1025),
+            "too_many_code_sections",
+            "Too many code sections"
+          }
         });
   }
 
   @ParameterizedTest(name = "{1}")
-  @MethodSource("testCasesFromEIP3540")
-  void test(
-      final String containerString,
-      final String description,
-      final String failureReason,
-      final String code) {
+  @MethodSource({"testCasesFromEIP3540", "testCasesFromEIP4750"})
+  void test(final String containerString, final String description, final String failureReason) {
     final Bytes container = Bytes.fromHexString(containerString);
     final EOFLayout layout = EOFLayout.parseEOF(container);
     System.out.println(description);
     assertThat(layout.getInvalidReason()).isEqualTo(failureReason);
-    if (code != null) {
-      assertThat(layout.getSections()).hasSize(3);
-      assertThat(layout.getSections()[1]).isNotNull();
-      assertThat(layout.getSections()[1].toHexString()).isEqualTo(code);
-    } else {
-      assertThat(layout.getSections()).isNull();
-    }
   }
 }
