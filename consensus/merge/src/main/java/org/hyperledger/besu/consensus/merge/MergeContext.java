c00c0c46c56272688f2eeff354fc284f9095f76f
Gabriel Fukushima <gabrielfukushima@gmail.com>
2022-11-25 12:38:17 +1100

    Add isNearHeadCheckpointSync method to interface
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- consensus/merge/src/main/java/org/hyperledger/besu/consensus/merge/MergeContext.java
+++ consensus/merge/src/main/java/org/hyperledger/besu/consensus/merge/MergeContext.java
@@ -65,4 +65,6 @@ public interface MergeContext extends ConsensusContext {
   void putPayloadById(final PayloadIdentifier payloadId, final Block block);
 
   Optional<Block> retrieveBlockById(final PayloadIdentifier payloadId);
+
+  boolean isNearHeadCheckpointSync();
 }
