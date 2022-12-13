2260eaedf981597a8784164790115bd3d4154768
Gabriel Trintinalia <gabriel.trintinalia@gmail.com>
2022-12-13 12:19:15 +1100

    Make EthCreateAccessList extend EthEstimateGas
    Signed-off-by: Gabriel Trintinalia <gabriel.trintinalia@gmail.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/EthCreateAccessList.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/EthCreateAccessList.java
@@ -14,14 +14,10 @@
  */
 package org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods;
 
-import org.hyperledger.besu.datatypes.Wei;
-import org.hyperledger.besu.ethereum.api.jsonrpc.JsonRpcErrorConverter;
 import org.hyperledger.besu.ethereum.api.jsonrpc.RpcMethod;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.JsonRpcRequestContext;
-import org.hyperledger.besu.ethereum.api.jsonrpc.internal.exception.InvalidJsonRpcParameters;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.parameters.JsonCallParameter;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcError;
-import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcErrorResponse;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcResponse;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcSuccessResponse;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.results.CreateAccessListResult;
@@ -29,28 +25,22 @@ import org.hyperledger.besu.ethereum.api.query.BlockchainQueries;
 import org.hyperledger.besu.ethereum.core.BlockHeader;
 import org.hyperledger.besu.ethereum.mainnet.ImmutableTransactionValidationParams;
 import org.hyperledger.besu.ethereum.mainnet.TransactionValidationParams;
-import org.hyperledger.besu.ethereum.mainnet.ValidationResult;
-import org.hyperledger.besu.ethereum.processing.TransactionProcessingResult;
 import org.hyperledger.besu.ethereum.transaction.CallParameter;
-import org.hyperledger.besu.ethereum.transaction.TransactionInvalidReason;
 import org.hyperledger.besu.ethereum.transaction.TransactionSimulator;
 import org.hyperledger.besu.ethereum.transaction.TransactionSimulatorResult;
 import org.hyperledger.besu.evm.AccessListEntry;
 import org.hyperledger.besu.evm.tracing.AccessListOperationTracer;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
-public class EthCreateAccessList implements JsonRpcMethod {
-
-  private final BlockchainQueries blockchainQueries;
-  private final TransactionSimulator transactionSimulator;
+public class EthCreateAccessList extends EthEstimateGas {
 
   public EthCreateAccessList(
       final BlockchainQueries blockchainQueries, final TransactionSimulator transactionSimulator) {
-    this.blockchainQueries = blockchainQueries;
-    this.transactionSimulator = transactionSimulator;
+    super(blockchainQueries, transactionSimulator);
   }
 
   @Override
@@ -67,10 +57,8 @@ public class EthCreateAccessList implements JsonRpcMethod {
     if (jsonRpcError.isPresent()) {
       return errorResponse(requestContext, jsonRpcError.get());
     }
-
     final TransactionValidationParams transactionValidationParams =
         transactionValidationParams(!callParams.isMaybeStrict().orElse(Boolean.FALSE));
-
     final CallParameter modifiedCallParams =
         overrideGasLimitAndPrice(callParams, blockHeader.getGasLimit());
 
@@ -82,19 +70,32 @@ public class EthCreateAccessList implements JsonRpcMethod {
             accessListOperationTracer,
             blockHeader.getNumber());
 
-    if (accessListOperationTracer.getAccessList().isEmpty()) {
-      return maybeResult
-          .map(createAccessListResponse(requestContext, accessListOperationTracer))
-          .orElse(errorResponse(requestContext, JsonRpcError.INTERNAL_ERROR));
-    } else {
-      final AccessListOperationTracer tracer = new AccessListOperationTracer();
+    // verify if provided accessList == calculated accessList
+    if (mustCalculateUsingCalculatedAccessList(modifiedCallParams, accessListOperationTracer)) {
+      // process gas with new accessList parameter
+      final AccessListOperationTracer estimateGasWithAccessListTracer =
+          new AccessListOperationTracer();
       final CallParameter callParameter =
           overrideAccessList(modifiedCallParams, accessListOperationTracer.getAccessList());
       return transactionSimulator
-          .process(callParameter, transactionValidationParams, tracer, blockHeader.getNumber())
-          .map(createAccessListResponse(requestContext, tracer))
+          .process(
+              callParameter,
+              transactionValidationParams,
+              estimateGasWithAccessListTracer,
+              blockHeader.getNumber())
+          .map(createAccessListResponse(requestContext, estimateGasWithAccessListTracer))
+          .orElse(errorResponse(requestContext, JsonRpcError.INTERNAL_ERROR));
+    } else {
+      // Do not need to reprocess, calculate gas using maybeResult
+      return maybeResult
+          .map(createAccessListResponse(requestContext, accessListOperationTracer))
           .orElse(errorResponse(requestContext, JsonRpcError.INTERNAL_ERROR));
     }
+  }
+
+  private boolean mustCalculateUsingCalculatedAccessList(
+      final CallParameter parameters, final AccessListOperationTracer tracer) {
+    return Objects.equals(parameters.getAccessList(), Optional.of(tracer.getAccessList()));
   }
 
   private Optional<JsonRpcError> validateBlockHeader(final BlockHeader blockHeader) {
@@ -109,30 +110,11 @@ public class EthCreateAccessList implements JsonRpcMethod {
     return Optional.empty();
   }
 
-  private BlockHeader blockHeader() {
-    final long headBlockNumber = blockchainQueries.headBlockNumber();
-    return blockchainQueries.getBlockchain().getBlockHeader(headBlockNumber).orElse(null);
-  }
-
   private TransactionValidationParams transactionValidationParams(final boolean strict) {
     return ImmutableTransactionValidationParams.builder()
         .from(TransactionValidationParams.transactionSimulator())
         .isAllowExceedingBalance(strict)
         .build();
-  }
-
-  private CallParameter overrideGasLimitAndPrice(
-      final JsonCallParameter callParams, final long gasLimit) {
-    return new CallParameter(
-        callParams.getFrom(),
-        callParams.getTo(),
-        gasLimit,
-        Optional.ofNullable(callParams.getGasPrice()).orElse(Wei.ZERO),
-        callParams.getMaxPriorityFeePerGas(),
-        callParams.getMaxFeePerGas(),
-        callParams.getValue(),
-        callParams.getPayload(),
-        callParams.getAccessList());
   }
 
   private CallParameter overrideAccessList(
@@ -160,42 +142,5 @@ public class EthCreateAccessList implements JsonRpcMethod {
                     operationTracer.calculateEstimateGas(
                         result.getResult().getEstimateGasUsedByTransaction())))
             : errorResponse(request, result);
-  }
-
-  private JsonCallParameter validateAndGetCallParams(final JsonRpcRequestContext request) {
-    final JsonCallParameter callParams = request.getRequiredParameter(0, JsonCallParameter.class);
-    if (callParams.getGasPrice() != null
-        && (callParams.getMaxFeePerGas().isPresent()
-            || callParams.getMaxPriorityFeePerGas().isPresent())) {
-      throw new InvalidJsonRpcParameters("gasPrice cannot be used with baseFee or maxFeePerGas");
-    }
-    return callParams;
-  }
-
-  private JsonRpcErrorResponse errorResponse(
-      final JsonRpcRequestContext request, final JsonRpcError jsonRpcError) {
-    return new JsonRpcErrorResponse(request.getRequest().getId(), jsonRpcError);
-  }
-
-  private JsonRpcErrorResponse errorResponse(
-      final JsonRpcRequestContext request, final TransactionSimulatorResult result) {
-    final JsonRpcError jsonRpcError;
-
-    final ValidationResult<TransactionInvalidReason> validationResult =
-        result.getValidationResult();
-    if (validationResult != null && !validationResult.isValid()) {
-      jsonRpcError =
-          JsonRpcErrorConverter.convertTransactionInvalidReason(
-              validationResult.getInvalidReason());
-    } else {
-      final TransactionProcessingResult resultTrx = result.getResult();
-      if (resultTrx != null && resultTrx.getRevertReason().isPresent()) {
-        jsonRpcError = JsonRpcError.REVERT_ERROR;
-        jsonRpcError.setData(resultTrx.getRevertReason().get().toHexString());
-      } else {
-        jsonRpcError = JsonRpcError.INTERNAL_ERROR;
-      }
-    }
-    return errorResponse(request, jsonRpcError);
   }
 }
