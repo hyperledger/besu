db2321b90a8b13e00328a542d735e6ea749cc9dc
Usman Saleem <usman@usmans.info>
2022-12-14 15:33:26 +1000

    Generate javadoc
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- besu/src/main/java/org/hyperledger/besu/controller/TransitionBesuControllerBuilder.java
+++ besu/src/main/java/org/hyperledger/besu/controller/TransitionBesuControllerBuilder.java
@@ -70,12 +70,21 @@ import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The Transition besu controller builder.
+ */
 public class TransitionBesuControllerBuilder extends BesuControllerBuilder {
   private final BesuControllerBuilder preMergeBesuControllerBuilder;
   private final MergeBesuControllerBuilder mergeBesuControllerBuilder;
 
   private static final Logger LOG = LoggerFactory.getLogger(TransitionBesuControllerBuilder.class);
 
+  /**
+   * Instantiates a new Transition besu controller builder.
+   *
+   * @param preMergeBesuControllerBuilder the pre merge besu controller builder
+   * @param mergeBesuControllerBuilder    the merge besu controller builder
+   */
   public TransitionBesuControllerBuilder(
       final BesuControllerBuilder preMergeBesuControllerBuilder,
       final MergeBesuControllerBuilder mergeBesuControllerBuilder) {
