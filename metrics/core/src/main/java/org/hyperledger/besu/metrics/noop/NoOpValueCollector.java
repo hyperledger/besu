c7e6805c473dd6943dc01d8ec3fdcb77ff397f86
Usman Saleem <usman@usmans.info>
2022-12-16 16:48:46 +1000

    generate javadocs for metrics module
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- metrics/core/src/main/java/org/hyperledger/besu/metrics/noop/NoOpValueCollector.java
+++ metrics/core/src/main/java/org/hyperledger/besu/metrics/noop/NoOpValueCollector.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.DoubleSupplier;
 
+/** The NoOp value collector. */
 public class NoOpValueCollector implements LabelledGauge {
   private final List<String> labelValuesCreated = new ArrayList<>();
 
