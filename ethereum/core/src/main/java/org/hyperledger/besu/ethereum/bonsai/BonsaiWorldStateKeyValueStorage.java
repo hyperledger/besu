394d56a2c62db215b06594d366d3a962f9e1ede0
Ameziane H <ameziane.hamlat@consensys.net>
2023-01-10 15:19:47 +0100

    add a debug log (to delete later)
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/BonsaiWorldStateKeyValueStorage.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/BonsaiWorldStateKeyValueStorage.java
@@ -114,6 +114,7 @@ public class BonsaiWorldStateKeyValueStorage implements WorldStateStorage, AutoC
                 .get(accountHash);
       }
     }
+    System.out.println("Get Account "+ accountHash);
     return response;
   }
 
