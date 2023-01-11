c705e53c37e92d45e6d7e4c70c20fc1ac4fb84e0
ahamlat <ameziane.hamlat@consensys.net>
2023-01-11 22:49:03 +0100

    Attempt to fix CPU spikes issue (#4867)
    * Modify reattemptPendingPeerRequests implementation to not iterate on pending request. This should avoid a lot of unnecessary computation
    
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>
    
    * Fix failed unit tests.
    
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>
    
    * Fix failed unit tests
    
    Iterate over pending requests only if there is a peer with available request capacity
    
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>
    
    * Fix stream implementation (use the same stream several times)
    
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>
    
    * Spotless
    
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>
    
    * Undo reducing method visibility as it is no longer used
    
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>
    
    * use one buffered for each peer and wait to have capacity before sending request
    
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>
    
    * fix tests
    
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>
    
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>
    Co-authored-by: Karim TAAM <karim.t2am@gmail.com>
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/transactions/NewPooledTransactionHashesMessageProcessorTest.java
+++ ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/transactions/NewPooledTransactionHashesMessageProcessorTest.java
@@ -170,7 +170,8 @@ public class NewPooledTransactionHashesMessageProcessorTest {
   }
 
   @Test
-  public void shouldScheduleGetPooledTransactionsTaskWhenNewTransactionAdded() {
+  public void
+      shouldScheduleGetPooledTransactionsTaskWhenNewTransactionAddedFromPeerForTheFirstTime() {
 
     final EthScheduler ethScheduler = mock(EthScheduler.class);
     when(ethContext.getScheduler()).thenReturn(ethScheduler);
@@ -183,7 +184,8 @@ public class NewPooledTransactionHashesMessageProcessorTest {
         ofMinutes(1));
 
     verify(ethScheduler, times(1))
-        .scheduleFutureTask(any(FetcherCreatorTask.class), any(Duration.class));
+        .scheduleFutureTaskWithFixedDelay(
+            any(FetcherCreatorTask.class), any(Duration.class), any(Duration.class));
   }
 
   @Test
@@ -204,7 +206,8 @@ public class NewPooledTransactionHashesMessageProcessorTest {
         ofMinutes(1));
 
     verify(ethScheduler, times(1))
-        .scheduleFutureTask(any(FetcherCreatorTask.class), any(Duration.class));
+        .scheduleFutureTaskWithFixedDelay(
+            any(FetcherCreatorTask.class), any(Duration.class), any(Duration.class));
   }
 
   @Test
