f532ac0bfe932c9268a778269d861c6b9db81796
garyschulte <garyschulte@gmail.com>
2023-01-22 11:02:38 +0100

    fix javadoc regressions in main
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- evm/src/main/java/org/hyperledger/besu/evm/frame/MessageFrame.java
+++ evm/src/main/java/org/hyperledger/besu/evm/frame/MessageFrame.java
@@ -1297,10 +1297,16 @@ public class MessageFrame {
     return maybeUpdatedStorage;
   }
 
+  /**
+   * Accessor for versionedHashes, if present.
+   *
+   * @return optional list of hashes
+   */
   public Optional<List<Hash>> getVersionedHashes() {
     return versionedHashes;
   }
 
+  /** Reset. */
   public void reset() {
     maybeUpdatedMemory = Optional.empty();
     maybeUpdatedStorage = Optional.empty();
@@ -1335,7 +1341,12 @@ public class MessageFrame {
     private Multimap<Address, Bytes32> accessListWarmStorage = HashMultimap.create();
 
     private Optional<List<Hash>> versionedHashes;
-
+    /**
+     * Sets Type.
+     *
+     * @param type the type
+     * @return the builder
+     */
     public Builder type(final Type type) {
       this.type = type;
       return this;
