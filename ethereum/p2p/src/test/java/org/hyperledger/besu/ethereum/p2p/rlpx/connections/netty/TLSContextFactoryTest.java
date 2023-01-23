f76fbbfb70c06855c68481f7642f15246e2605af
Usman Saleem <usman@usmans.info>
2023-01-23 19:59:52 +1000

    Fixing TLSContextFactoryTest for Java 17
     increase server and client latch timeout
    
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- ethereum/p2p/src/test/java/org/hyperledger/besu/ethereum/p2p/rlpx/connections/netty/TLSContextFactoryTest.java
+++ ethereum/p2p/src/test/java/org/hyperledger/besu/ethereum/p2p/rlpx/connections/netty/TLSContextFactoryTest.java
@@ -271,9 +271,11 @@ class TLSContextFactoryTest {
 
     if (testSuccess) {
       client.getChannelFuture().channel().writeAndFlush(Unpooled.copyInt(0));
-      final boolean allMessagesServerExchanged = serverLatch.await(10, TimeUnit.SECONDS);
-      final boolean allMessagesClientExchanged = clientLatch.await(10, TimeUnit.SECONDS);
-      assertThat(allMessagesClientExchanged && allMessagesServerExchanged).isTrue();
+      final boolean allMessagesServerExchanged = serverLatch.await(15, TimeUnit.SECONDS);
+      final boolean allMessagesClientExchanged = clientLatch.await(15, TimeUnit.SECONDS);
+
+      assertThat(allMessagesClientExchanged).isTrue();
+      assertThat(allMessagesServerExchanged).isTrue();
     } else {
       try {
         client.getChannelFuture().channel().writeAndFlush(Unpooled.copyInt(0)).sync();
@@ -281,6 +283,7 @@ class TLSContextFactoryTest {
         assertThat(client.getChannelFuture().channel().isActive()).isFalse();
       } catch (final Exception e) {
         // NOOP
+        LOG.info("Failure test, unexpected exception - ignoring. " + e.getMessage());
       }
     }
   }
