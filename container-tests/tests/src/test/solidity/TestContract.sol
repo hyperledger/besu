d6c2c449100afd4b9911f981645b6a30980829b7
Sally MacFarlane <macfarla.github@gmail.com>
2023-01-25 06:40:25 +1000

    remove container tests
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- container-tests/tests/src/test/solidity/TestContract.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.6.0 < 0.8.0;
-
-contract TestContract {
-    event TestEvent(address indexed _from, int val);
-
-    function logEvent(int randomNum) public {
-        emit TestEvent(msg.sender, randomNum);
-    }
-}
