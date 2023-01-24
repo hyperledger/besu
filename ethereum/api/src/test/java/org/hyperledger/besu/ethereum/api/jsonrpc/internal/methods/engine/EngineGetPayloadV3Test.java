746e5a9a6184e6bca8911aef3650330d92e3dc0d
Justin Florentine <justin+github@florentine.us>
2023-01-24 10:54:16 -0500

    4844 engine api (#4991)
    * json container for getting blob bundles
    * spdx headers, formatting, npe fix
    
    Signed-off-by: Justin Florentine <justin+github@florentine.us>
    Co-authored-by: Jason Frame <jason.frame@consensys.net>
    Co-authored-by: garyschulte <garyschulte@gmail.com>
    (cherry picked from commit 5fa943380176a18c9b5c394756fca6d3aabfe66f)
    (cherry picked from commit af8466f1561b4d5995bfa85782acee0644c3f879)

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineGetPayloadV3Test.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineGetPayloadV3Test.java
@@ -15,6 +15,7 @@
 package org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.engine;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -32,6 +33,8 @@ import org.hyperledger.besu.ethereum.core.BlockBody;
 import org.hyperledger.besu.ethereum.core.BlockHeader;
 import org.hyperledger.besu.ethereum.core.BlockHeaderTestFixture;
 import org.hyperledger.besu.ethereum.core.BlockWithReceipts;
+import org.hyperledger.besu.ethereum.mainnet.DefaultTimestampSchedule;
+import org.hyperledger.besu.ethereum.mainnet.TimestampSchedule;
 
 import java.util.Collections;
 import java.util.Optional;
@@ -47,10 +50,19 @@ import org.mockito.junit.MockitoJUnitRunner;
         .class) // mocks in parent class may not be used, throwing unnecessary stubbing
 public class EngineGetPayloadV3Test extends AbstractEngineGetPayloadTest {
 
+  private static final long SHANGHAI_AT = 1337L;
   private static final long CANCUN_AT = 31337L;
 
   public EngineGetPayloadV3Test() {
     super(EngineGetPayloadV3::new);
+  }
+
+  @Override
+  public TimestampSchedule getTimestampSchedule() {
+    DefaultTimestampSchedule mockSchedule = mock(DefaultTimestampSchedule.class);
+    when(mockSchedule.scheduledAt("Cancun")).thenReturn(CANCUN_AT);
+    when(mockSchedule.scheduledAt("Shanghai")).thenReturn(SHANGHAI_AT);
+    return mockSchedule;
   }
 
   @Override
@@ -72,11 +84,7 @@ public class EngineGetPayloadV3Test extends AbstractEngineGetPayloadTest {
     // should return withdrawals and excessGas for a post-cancun block
     PayloadIdentifier postCancunPid =
         PayloadIdentifier.forPayloadParams(
-            Hash.ZERO,
-            CANCUN_AT,
-            Bytes32.random(),
-            Address.fromHexString("0x42"),
-            Optional.empty());
+            Hash.ZERO, CANCUN_AT, Bytes32.random(), Address.fromHexString("0x42"));
 
     BlockWithReceipts postCancunBlock =
         new BlockWithReceipts(
@@ -85,8 +93,7 @@ public class EngineGetPayloadV3Test extends AbstractEngineGetPayloadTest {
                 new BlockBody(
                     Collections.emptyList(),
                     Collections.emptyList(),
-                    Optional.of(Collections.emptyList()),
-                    Optional.empty())),
+                    Optional.of(Collections.emptyList()))),
             Collections.emptyList());
 
     when(mergeContext.retrieveBlockById(postCancunPid)).thenReturn(Optional.of(postCancunBlock));
