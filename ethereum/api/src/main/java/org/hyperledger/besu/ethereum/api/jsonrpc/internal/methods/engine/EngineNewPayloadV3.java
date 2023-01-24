36577b68016db8b1a08fca367512d950f7a533d8
Justin Florentine <justin+github@florentine.us>
2023-01-24 10:54:16 -0500

    4844 engine api (#4991)
    * json container for getting blob bundles
    * spdx headers, formatting, npe fix
    
    Signed-off-by: Justin Florentine <justin+github@florentine.us>
    Co-authored-by: Jason Frame <jason.frame@consensys.net>
    Co-authored-by: garyschulte <garyschulte@gmail.com>
    (cherry picked from commit 5fa943380176a18c9b5c394756fca6d3aabfe66f)
    (cherry picked from commit af8466f1561b4d5995bfa85782acee0644c3f879)

---
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineNewPayloadV3.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright Hyperledger Besu Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.engine;
+
+import org.hyperledger.besu.consensus.merge.blockcreation.MergeMiningCoordinator;
+import org.hyperledger.besu.ethereum.ProtocolContext;
+import org.hyperledger.besu.ethereum.api.jsonrpc.RpcMethod;
+import org.hyperledger.besu.ethereum.eth.manager.EthPeers;
+import org.hyperledger.besu.ethereum.mainnet.TimestampSchedule;
+
+import io.vertx.core.Vertx;
+
+public class EngineNewPayloadV3 extends AbstractEngineNewPayload {
+
+  public EngineNewPayloadV3(
+      final Vertx vertx,
+      final TimestampSchedule timestampSchedule,
+      final ProtocolContext protocolContext,
+      final MergeMiningCoordinator mergeCoordinator,
+      final EthPeers ethPeers,
+      final EngineCallListener engineCallListener) {
+    super(
+        vertx, timestampSchedule, protocolContext, mergeCoordinator, ethPeers, engineCallListener);
+  }
+
+  @Override
+  public String getName() {
+    return RpcMethod.ENGINE_NEW_PAYLOAD_V3.getMethodName();
+  }
+}
