1b255103d669d56149e22753686451fd0e2d7ce7
Justin Florentine <justin+github@florentine.us>
2023-01-24 10:54:16 -0500

    4844 engine api (#4991)
    * json container for getting blob bundles
    * spdx headers, formatting, npe fix
    
    Signed-off-by: Justin Florentine <justin+github@florentine.us>
    Co-authored-by: Jason Frame <jason.frame@consensys.net>
    Co-authored-by: garyschulte <garyschulte@gmail.com>
    (cherry picked from commit 5fa943380176a18c9b5c394756fca6d3aabfe66f)
    (cherry picked from commit af8466f1561b4d5995bfa85782acee0644c3f879)

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/AbstractEngineNewPayload.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/AbstractEngineNewPayload.java
@@ -164,8 +164,7 @@ public abstract class AbstractEngineNewPayload extends ExecutionEngineJsonRpcMet
             blockParam.getExcessDataGas() == null
                 ? null
                 : DataGas.fromHexString(blockParam.getExcessDataGas()),
-            null,
-            headerFunctions);
+            null, headerFunctions);
 
     // ensure the block hash matches the blockParam hash
     // this must be done before any other check
