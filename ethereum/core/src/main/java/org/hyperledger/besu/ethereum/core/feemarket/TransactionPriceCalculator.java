ec6fe6335eece27e3440b42b479dddb347ee4ffa
Fabio Di Fabio <fabio.difabio@consensys.net>
2023-01-25 11:16:40 +0100

    [Interop-4844] Data gas accounting (#4998)
    merge of #4992 into interop feature branch
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    (cherry picked from commit 949e3fe49183312c3cb03953a7377d5cd5efeef9)
    (cherry picked from commit 9734c983ce00bda161434506688f12ee07bbd820)

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/feemarket/TransactionPriceCalculator.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/feemarket/TransactionPriceCalculator.java
@@ -14,7 +14,8 @@
  */
 package org.hyperledger.besu.ethereum.core.feemarket;
 
-import org.hyperledger.besu.datatypes.DataGas;
+import static org.hyperledger.besu.util.Slf4jLambdaHelper.traceLambda;
+
 import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.ethereum.core.ProcessableBlockHeader;
 import org.hyperledger.besu.ethereum.core.Transaction;
@@ -22,17 +23,32 @@ import org.hyperledger.besu.ethereum.core.Transaction;
 import java.math.BigInteger;
 import java.util.Optional;
 
-@FunctionalInterface
-public interface TransactionPriceCalculator {
-  Wei price(Transaction transaction, Optional<Wei> baseFee);
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-  static TransactionPriceCalculator frontier() {
-    return (transaction, baseFee) -> transaction.getGasPrice().orElse(Wei.ZERO);
+public interface TransactionPriceCalculator {
+  Wei price(Transaction transaction, Optional<Wei> maybeFee);
+
+  default Wei dataPrice(final Transaction transaction, final ProcessableBlockHeader blockHeader) {
+    return Wei.ZERO;
   }
 
-  static TransactionPriceCalculator eip1559() {
-    return (transaction, maybeBaseFee) -> {
-      final Wei baseFee = maybeBaseFee.orElseThrow();
+  class Frontier implements TransactionPriceCalculator {
+    @Override
+    public Wei price(final Transaction transaction, final Optional<Wei> maybeFee) {
+      return transaction.getGasPrice().orElse(Wei.ZERO);
+    }
+
+    @Override
+    public Wei dataPrice(final Transaction transaction, final ProcessableBlockHeader blockHeader) {
+      return Wei.ZERO;
+    }
+  }
+
+  class EIP1559 implements TransactionPriceCalculator {
+    @Override
+    public Wei price(final Transaction transaction, final Optional<Wei> maybeFee) {
+      final Wei baseFee = maybeFee.orElseThrow();
       if (!transaction.getType().supports1559FeeMarket()) {
         return transaction.getGasPrice().orElse(Wei.ZERO);
       }
@@ -43,7 +59,50 @@ public interface TransactionPriceCalculator {
         price = maxFeePerGas;
       }
       return price;
-    };
+    }
+  }
+
+  class DataBlob extends EIP1559 {
+    private static final Logger LOG = LoggerFactory.getLogger(DataBlob.class);
+    private final BigInteger minDataGasPrice;
+    private final BigInteger dataGasPriceUpdateFraction;
+
+    public DataBlob(final int minDataGasPrice, final int dataGasPriceUpdateFraction) {
+      this.minDataGasPrice = BigInteger.valueOf(minDataGasPrice);
+      this.dataGasPriceUpdateFraction = BigInteger.valueOf(dataGasPriceUpdateFraction);
+    }
+
+    @Override
+    public Wei dataPrice(final Transaction transaction, final ProcessableBlockHeader blockHeader) {
+      final var excessDataGas = blockHeader.getExcessDataGas().orElseThrow();
+
+      final var dataGasPrice =
+          Wei.of(
+              fakeExponential(
+                  minDataGasPrice, excessDataGas.toBigInteger(), dataGasPriceUpdateFraction));
+      traceLambda(
+          LOG,
+          "block #{} parentExcessDataGas: {} dataGasPrice: {}",
+          blockHeader::getNumber,
+          excessDataGas::toShortHexString,
+          dataGasPrice::toHexString);
+
+      return dataGasPrice;
+    }
+
+    private BigInteger fakeExponential(
+        final BigInteger factor, final BigInteger numerator, final BigInteger denominator) {
+      BigInteger i = BigInteger.ONE;
+      BigInteger output = BigInteger.ZERO;
+      BigInteger numeratorAccumulator = factor.multiply(denominator);
+      while (numeratorAccumulator.compareTo(BigInteger.ZERO) > 0) {
+        output = output.add(numeratorAccumulator);
+        numeratorAccumulator =
+            (numeratorAccumulator.multiply(numerator)).divide(denominator.multiply(i));
+        i = i.add(BigInteger.ONE);
+      }
+      return output.divide(denominator);
+    }
   }
 
   static TransactionPriceCalculator dataGas(
