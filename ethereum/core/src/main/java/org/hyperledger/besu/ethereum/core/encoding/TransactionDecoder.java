c9337cfcc9d2fbfa7d5a415e47feeb8d55092e7a
Jiri Peinlich <jiri.peinlich@gmail.com>
2023-01-26 09:18:01 +0100

    Adding trace logs to mention ssz transaction encoding/deconding in logs
    Signed-off-by: Jiri Peinlich <jiri.peinlich@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/encoding/TransactionDecoder.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/encoding/TransactionDecoder.java
@@ -21,6 +21,7 @@ import static org.hyperledger.besu.ethereum.core.Transaction.REPLAY_PROTECTED_V_
 import static org.hyperledger.besu.ethereum.core.Transaction.REPLAY_UNPROTECTED_V_BASE;
 import static org.hyperledger.besu.ethereum.core.Transaction.REPLAY_UNPROTECTED_V_BASE_PLUS_1;
 import static org.hyperledger.besu.ethereum.core.Transaction.TWO;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import org.hyperledger.besu.config.GoQuorumOptions;
 import org.hyperledger.besu.crypto.SECPSignature;
@@ -47,10 +48,13 @@ import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.ssz.SSZ;
 import org.apache.tuweni.ssz.SSZReader;
 import org.apache.tuweni.units.bigints.UInt32;
+import org.slf4j.Logger;
 
 public class TransactionDecoder {
 
   private static final UInt32 BLOB_TRANSACTION_OFFSET = UInt32.fromHexString("0x3c000000");
+
+  private static final Logger LOG = getLogger(TransactionDecoder.class);
 
   @FunctionalInterface
   interface Decoder {
@@ -90,6 +94,7 @@ public class TransactionDecoder {
     TransactionNetworkPayload.SingedBlobTransaction signedBlobTransaction;
 
     if (firstOffset.equals(BLOB_TRANSACTION_OFFSET)) {
+      LOG.trace("Decoding TransactionNetworkPayload");
 
       TransactionNetworkPayload payload = new TransactionNetworkPayload();
       payload.populateFromReader(input);
@@ -97,6 +102,7 @@ public class TransactionDecoder {
 
       builder.kzgBlobs(payload.getKzgCommitments(), payload.getBlobs(), payload.getKzgProof());
     } else {
+      LOG.trace("Decoding TransactionNetworkPayload.SingedBlobTransaction");
       signedBlobTransaction = new TransactionNetworkPayload.SingedBlobTransaction();
       signedBlobTransaction.populateFromReader(input);
     }
