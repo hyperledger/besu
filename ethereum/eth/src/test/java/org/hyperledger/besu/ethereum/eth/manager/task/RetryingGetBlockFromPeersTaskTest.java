2a2f39ae1aa729427e02b17203c096687b496343
Sally MacFarlane <macfarla.github@gmail.com>
2023-01-30 14:57:40 +1000

    formatting
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/manager/task/RetryingGetBlockFromPeersTaskTest.java
+++ ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/manager/task/RetryingGetBlockFromPeersTaskTest.java
@@ -15,7 +15,6 @@
 package org.hyperledger.besu.ethereum.eth.manager.task;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 import static org.hyperledger.besu.ethereum.core.BlockHeader.GENESIS_BLOCK_NUMBER;
 import static org.mockito.Mockito.mock;
 
