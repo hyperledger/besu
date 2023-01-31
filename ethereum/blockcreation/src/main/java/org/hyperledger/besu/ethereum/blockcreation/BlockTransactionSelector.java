fdd8c25bc1ccb1c0036e8fba882156f7d3008d2b
Fabio Di Fabio <fabio.difabio@consensys.net>
2023-01-31 19:09:04 +0100

    Fix data gas calculation during block import (#5023)
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    (cherry picked from commit f20d064b18a9e57a910cda69ee3123e29de56b50)
    (cherry picked from commit bcd9735d1605f4fffef57bec4af95bfae818ac7e)

--- ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/BlockTransactionSelector.java
+++ ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/BlockTransactionSelector.java
@@ -15,6 +15,7 @@
 package org.hyperledger.besu.ethereum.blockcreation;
 
 import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.datatypes.DataGas;
 import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.ethereum.GasLimitCalculator;
 import org.hyperledger.besu.ethereum.chain.Blockchain;
@@ -267,7 +268,7 @@ public class BlockTransactionSelector {
           .lessThan(
               feeMarket
                   .getTransactionPriceCalculator()
-                  .dataPrice(transaction, processableBlockHeader))) {
+                  .dataPrice(processableBlockHeader.getExcessDataGas().orElse(DataGas.ZERO)))) {
         return true;
       }
     }
