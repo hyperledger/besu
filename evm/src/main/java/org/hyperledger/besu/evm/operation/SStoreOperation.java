532797fc1725d3264ca51d083cb2b8a89123f034
Danno Ferrin <danno.ferrin@swirldslabs.com>
2023-02-20 22:10:41 -0700

    Add needed tooling support for execution-spec-tests (#5030)
    Adding the t8n and b11r tool allows Hyperledger besu to produce reference 
    tests from the https://github.com/ethereum/execution-spec-tests repository.
    
    * Add t8n tool, or transition tool. Calculates the state changes from a set 
      of transactions against a given parent context
    * Add b11r tool, or block-builder tool. Given block parts it can generate a 
      RLP block.
    * Change some tracing CLI flags to work better with retesteth
    
    Signed-off-by: Danno Ferrin <danno.ferrin@swirldslabs.com>

--- evm/src/main/java/org/hyperledger/besu/evm/operation/SStoreOperation.java
+++ evm/src/main/java/org/hyperledger/besu/evm/operation/SStoreOperation.java
@@ -21,7 +21,8 @@ import org.hyperledger.besu.evm.frame.ExceptionalHaltReason;
 import org.hyperledger.besu.evm.frame.MessageFrame;
 import org.hyperledger.besu.evm.gascalculator.GasCalculator;
 
-import com.google.common.base.Supplier;
+import java.util.function.Supplier;
+
 import com.google.common.base.Suppliers;
 import org.apache.tuweni.units.bigints.UInt256;
 
