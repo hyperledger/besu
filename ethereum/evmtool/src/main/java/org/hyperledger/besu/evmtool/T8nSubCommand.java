f426a272dbc4bb3aefdf24cee974dbc41a72b0f2
Danno Ferrin <danno.ferrin@swirldslabs.com>
2023-02-21 11:04:58 -0700

    t8n changes
    Signed-off-by: Danno Ferrin <danno.ferrin@swirldslabs.com>

--- ethereum/evmtool/src/main/java/org/hyperledger/besu/evmtool/T8nSubCommand.java
+++ ethereum/evmtool/src/main/java/org/hyperledger/besu/evmtool/T8nSubCommand.java
@@ -54,7 +54,6 @@ import org.hyperledger.besu.evm.tracing.StandardJsonTracer;
 import org.hyperledger.besu.evm.worldstate.WorldUpdater;
 import org.hyperledger.besu.evmtool.exception.UnsupportedForkException;
 import org.hyperledger.besu.plugin.data.TransactionType;
-import org.hyperledger.besu.util.Log4j2ConfiguratorUtil;
 
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -83,7 +82,6 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 import com.google.common.base.Stopwatch;
-import org.apache.logging.log4j.Level;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.units.bigints.UInt256;
@@ -245,13 +243,9 @@ public class T8nSubCommand implements Runnable {
       return;
     }
 
-    Log4j2ConfiguratorUtil.setLevel(
-        "org.hyperledger.besu.ethereum.mainnet.AbstractProtocolScheduleBuilder", Level.OFF);
     final ReferenceTestProtocolSchedules referenceTestProtocolSchedules =
         ReferenceTestProtocolSchedules.create(
             new StubGenesisConfigOptions().chainId(BigInteger.valueOf(chainId)));
-    Log4j2ConfiguratorUtil.setLevel(
-        "org.hyperledger.besu.ethereum.mainnet.AbstractProtocolScheduleBuilder", null);
 
     final MutableWorldState worldState = new DefaultMutableWorldState(initialWorldState);
 
