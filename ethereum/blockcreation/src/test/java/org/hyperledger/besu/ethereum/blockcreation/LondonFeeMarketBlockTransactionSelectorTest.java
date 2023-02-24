hyperledger/besu

commit 28f2bd4a1ad618130763ea1163e0c72f3bf30330

Gas accounting for EIP-4844 (#4992)
Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/BlockTransactionSelector.java
@@ -18,6 +18,7 @@ import static org.hyperledger.besu.util.Slf4jLambdaHelper.traceLambda;
 
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Wei;
+import org.hyperledger.besu.ethereum.GasLimitCalculator;
 import org.hyperledger.besu.ethereum.chain.Blockchain;
 import org.hyperledger.besu.ethereum.core.MutableWorldState;
 import org.hyperledger.besu.ethereum.core.ProcessableBlockHeader;
@@ -35,14 +36,20 @@ import org.hyperledger.besu.ethereum.processing.TransactionProcessingResult;
 import org.hyperledger.besu.ethereum.transaction.TransactionInvalidReason;
 import org.hyperledger.besu.ethereum.vm.BlockHashLookup;
 import org.hyperledger.besu.evm.account.EvmAccount;
+import org.hyperledger.besu.evm.gascalculator.GasCalculator;
 import org.hyperledger.besu.evm.worldstate.WorldUpdater;
+import org.hyperledger.besu.plugin.data.TransactionType;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CancellationException;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.google.common.collect.Lists;
 import org.apache.tuweni.bytes.Bytes;
@@ -70,11 +77,6 @@ import org.slf4j.LoggerFactory;
  * not cleared between executions of buildTransactionListForBlock().
  */
 public class BlockTransactionSelector {
-  private static final Logger LOG = LoggerFactory.getLogger(BlockTransactionSelector.class);
-
-  private final Wei minTransactionGasPrice;
-  private final Double minBlockOccupancyRatio;
-
   public static class TransactionValidationResult {
     private final Transaction transaction;
     private final ValidationResult<TransactionInvalidReason> validationResult;
@@ -115,22 +117,30 @@ public class BlockTransactionSelector {
 
   public static class TransactionSelectionResults {
 
-    private final List<Transaction> transactions = Lists.newArrayList();
+    private final Map<TransactionType, List<Transaction>> transactionsByType = new HashMap<>();
     private final List<TransactionReceipt> receipts = Lists.newArrayList();
     private final List<TransactionValidationResult> invalidTransactions = Lists.newArrayList();
     private long cumulativeGasUsed = 0;
+    private long cumulativeDataGasUsed = 0;
 
     private void update(
-        final Transaction transaction, final TransactionReceipt receipt, final long gasUsed) {
-      transactions.add(transaction);
+        final Transaction transaction,
+        final TransactionReceipt receipt,
+        final long gasUsed,
+        final long dataGasUsed) {
+      transactionsByType
+          .computeIfAbsent(transaction.getType(), type -> new ArrayList<>())
+          .add(transaction);
       receipts.add(receipt);
       cumulativeGasUsed += gasUsed;
+      cumulativeDataGasUsed += dataGasUsed;
       traceLambda(
           LOG,
-          "New selected transaction {}, total transactions {}, cumulative gas used {}",
+          "New selected transaction {}, total transactions {}, cumulative gas used {}, cumulative data gas used {}",
           transaction::toTraceLog,
-          transactions::size,
-          () -> cumulativeGasUsed);
+          () -> transactionsByType.values().stream().mapToInt(List::size).sum(),
+          () -> cumulativeGasUsed,
+          () -> cumulativeDataGasUsed);
     }
 
     private void updateWithInvalidTransaction(
@@ -140,7 +150,11 @@ public class BlockTransactionSelector {
     }
 
     public List<Transaction> getTransactions() {
-      return transactions;
+      return streamAllTransactions().collect(Collectors.toList());
+    }
+
+    public List<Transaction> getTransactionsByType(final TransactionType type) {
+      return transactionsByType.getOrDefault(type, List.of());
     }
 
     public List<TransactionReceipt> getReceipts() {
@@ -151,8 +165,16 @@ public class BlockTransactionSelector {
       return cumulativeGasUsed;
     }
 
+    public long getCumulativeDataGasUsed() {
+      return cumulativeDataGasUsed;
+    }
+
     public List<TransactionValidationResult> getInvalidTransactions() {
       return invalidTransactions;
+    }
+
+    private Stream<Transaction> streamAllTransactions() {
+      return transactionsByType.values().stream().flatMap(List::stream);
     }
 
     @Override
@@ -165,24 +187,36 @@ public class BlockTransactionSelector {
       }
       TransactionSelectionResults that = (TransactionSelectionResults) o;
       return cumulativeGasUsed == that.cumulativeGasUsed
-          && transactions.equals(that.transactions)
+          && cumulativeDataGasUsed == that.cumulativeDataGasUsed
+          && transactionsByType.equals(that.transactionsByType)
           && receipts.equals(that.receipts)
           && invalidTransactions.equals(that.invalidTransactions);
     }
 
     @Override
     public int hashCode() {
-      return Objects.hash(transactions, receipts, invalidTransactions, cumulativeGasUsed);
+      return Objects.hash(
+          transactionsByType,
+          receipts,
+          invalidTransactions,
+          cumulativeGasUsed,
+          cumulativeDataGasUsed);
     }
 
     public String toTraceLog() {
       return "cumulativeGasUsed="
           + cumulativeGasUsed
+          + ", cumulativeDataGasUsed="
+          + cumulativeDataGasUsed
           + ", transactions="
-          + transactions.stream().map(Transaction::toTraceLog).collect(Collectors.joining("; "));
+          + streamAllTransactions().map(Transaction::toTraceLog).collect(Collectors.joining("; "));
     }
   }
 
+  private static final Logger LOG = LoggerFactory.getLogger(BlockTransactionSelector.class);
+
+  private final Wei minTransactionGasPrice;
+  private final Double minBlockOccupancyRatio;
   private final Supplier<Boolean> isCancelled;
   private final MainnetTransactionProcessor transactionProcessor;
   private final ProcessableBlockHeader processableBlockHeader;
@@ -191,7 +225,10 @@ public class BlockTransactionSelector {
   private final PendingTransactions pendingTransactions;
   private final AbstractBlockProcessor.TransactionReceiptFactory transactionReceiptFactory;
   private final Address miningBeneficiary;
+  private final Wei dataGasPrice;
   private final FeeMarket feeMarket;
+  private final GasCalculator gasCalculator;
+  private final GasLimitCalculator gasLimitCalculator;
 
   private final TransactionSelectionResults transactionSelectionResult =
       new TransactionSelectionResults();
@@ -207,7 +244,10 @@ public class BlockTransactionSelector {
       final Double minBlockOccupancyRatio,
       final Supplier<Boolean> isCancelled,
       final Address miningBeneficiary,
-      final FeeMarket feeMarket) {
+      final Wei dataGasPrice,
+      final FeeMarket feeMarket,
+      final GasCalculator gasCalculator,
+      final GasLimitCalculator gasLimitCalculator) {
     this.transactionProcessor = transactionProcessor;
     this.blockchain = blockchain;
     this.worldState = worldState;
@@ -218,7 +258,10 @@ public class BlockTransactionSelector {
     this.minTransactionGasPrice = minTransactionGasPrice;
     this.minBlockOccupancyRatio = minBlockOccupancyRatio;
     this.miningBeneficiary = miningBeneficiary;
+    this.dataGasPrice = dataGasPrice;
     this.feeMarket = feeMarket;
+    this.gasCalculator = gasCalculator;
+    this.gasLimitCalculator = gasLimitCalculator;
   }
 
   /*
@@ -278,6 +321,9 @@ public class BlockTransactionSelector {
     if (transactionCurrentPriceBelowMin(transaction)) {
       return TransactionSelectionResult.CONTINUE;
     }
+    if (transactionDataPriceBelowMin(transaction)) {
+      return TransactionSelectionResult.CONTINUE;
+    }
 
     final WorldUpdater worldStateUpdater = worldState.updater();
     final BlockHashLookup blockHashLookup = new BlockHashLookup(processableBlockHeader, blockchain);
@@ -298,7 +344,7 @@ public class BlockTransactionSelector {
             transaction.getHash().toHexString());
         return transactionSelectionResultForInvalidResult(transaction, validationResult);
       } else {
-        // valid GoQuorum private tx, we need to hand craft the receipt and increment the nonce
+        // valid GoQuorum private tx, we need to handcraft the receipt and increment the nonce
         effectiveResult = publicResultForWhenWeHaveAPrivateTransaction(transaction);
         worldStateUpdater.getOrCreate(transaction.getSender()).getMutable().incrementNonce();
       }
@@ -312,7 +358,8 @@ public class BlockTransactionSelector {
               miningBeneficiary,
               blockHashLookup,
               false,
-              TransactionValidationParams.mining());
+              TransactionValidationParams.mining(),
+              dataGasPrice);
     }
 
     if (!effectiveResult.isInvalid()) {
@@ -329,6 +376,15 @@ public class BlockTransactionSelector {
           transaction, effectiveResult.getValidationResult());
     }
     return TransactionSelectionResult.CONTINUE;
+  }
+
+  private boolean transactionDataPriceBelowMin(final Transaction transaction) {
+    if (transaction.getType().supportsBlob()) {
+      if (transaction.getMaxFeePerDataGas().orElseThrow().lessThan(dataGasPrice)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   private boolean transactionCurrentPriceBelowMin(final Transaction transaction) {
@@ -426,11 +482,14 @@ public class BlockTransactionSelector {
     final long cumulativeGasUsed =
         transactionSelectionResult.getCumulativeGasUsed() + gasUsedByTransaction;
 
+    final long dataGasUsed = gasCalculator.dataGasCost(transaction.getBlobCount());
+
     transactionSelectionResult.update(
         transaction,
         transactionReceiptFactory.create(
             transaction.getType(), result, worldState, cumulativeGasUsed),
-        gasUsedByTransaction);
+        gasUsedByTransaction,
+        dataGasUsed);
   }
 
   private boolean isIncorrectNonce(final ValidationResult<TransactionInvalidReason> result) {
@@ -448,7 +507,15 @@ public class BlockTransactionSelector {
   }
 
   private boolean transactionTooLargeForBlock(final Transaction transaction) {
-    return transaction.getGasLimit()
+    final long dataGasUsed = gasCalculator.dataGasCost(transaction.getBlobCount());
+
+    if (dataGasUsed
+        > gasLimitCalculator.currentDataGasLimit()
+            - transactionSelectionResult.getCumulativeDataGasUsed()) {
+      return true;
+    }
+
+    return transaction.getGasLimit() + dataGasUsed
         > processableBlockHeader.getGasLimit() - transactionSelectionResult.getCumulativeGasUsed();
   }
 

ethereum/blockcreation/src/test/java/org/hyperledger/besu/ethereum/blockcreation/LondonFeeMarketBlockTransactionSelectorTest.java
@@ -70,7 +70,8 @@ public class LondonFeeMarketBlockTransactionSelectorTest
 
     final Address miningBeneficiary = AddressHelpers.ofValue(1);
     final BlockTransactionSelector selector =
-        createBlockSelector(transactionProcessor, blockHeader, Wei.of(6), miningBeneficiary);
+        createBlockSelector(
+            transactionProcessor, blockHeader, Wei.of(6), miningBeneficiary, Wei.ZERO);
 
     // tx is willing to pay max 6 wei for gas, but current network condition (baseFee == 1)
     // result in it paying 2 wei, that is below the minimum accepted by the node, so it is skipped
@@ -90,7 +91,8 @@ public class LondonFeeMarketBlockTransactionSelectorTest
 
     final Address miningBeneficiary = AddressHelpers.ofValue(1);
     final BlockTransactionSelector selector =
-        createBlockSelector(transactionProcessor, blockHeader, Wei.of(6), miningBeneficiary);
+        createBlockSelector(
+            transactionProcessor, blockHeader, Wei.of(6), miningBeneficiary, Wei.ZERO);
 
     // tx is willing to pay max 6 wei for gas, and current network condition (baseFee == 5)
     // result in it paying the max, that is >= the minimum accepted by the node, so it is selected
@@ -112,7 +114,8 @@ public class LondonFeeMarketBlockTransactionSelectorTest
 
     final Address miningBeneficiary = AddressHelpers.ofValue(1);
     final BlockTransactionSelector selector =
-        createBlockSelector(transactionProcessor, blockHeader, Wei.of(6), miningBeneficiary);
+        createBlockSelector(
+            transactionProcessor, blockHeader, Wei.of(6), miningBeneficiary, Wei.ZERO);
 
     // tx is willing to pay max 6 wei for gas, but current network condition (baseFee == 1)
     // result in it paying 2 wei, that is below the minimum accepted by the node, but since it is

consensus/merge/src/test/java/org/hyperledger/besu/consensus/merge/blockcreation/MergeCoordinatorTest.java
@@ -26,7 +26,6 @@ import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
@@ -437,7 +436,8 @@ public class MergeCoordinatorTest implements MergeGenesisConfigHelper {
                   .getTransactions()
                   .isEmpty()) {
                 // this is called by the first empty block
-                doThrow(new MerkleTrieException("lock")) // first fail
+                doCallRealMethod() // first work
+                    .doThrow(new MerkleTrieException("lock")) // second fail
                     .doCallRealMethod() // then work
                     .when(blockchain)
                     .getBlockHeader(any());

ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/JsonRpcErrorConverter.java
@@ -67,6 +67,8 @@ public class JsonRpcErrorConverter {
         return JsonRpcError.NONCE_TOO_FAR_IN_FUTURE_FOR_SENDER;
       case LOWER_NONCE_INVALID_TRANSACTION_EXISTS:
         return JsonRpcError.LOWER_NONCE_INVALID_TRANSACTION_EXISTS;
+      case TOTAL_DATA_GAS_TOO_HIGH:
+        return JsonRpcError.TOTAL_DATA_GAS_TOO_HIGH;
       default:
         return JsonRpcError.INVALID_PARAMS;
     }

ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/DebugStorageRangeAt.java
@@ -96,7 +96,12 @@ public class DebugStorageRangeAt implements JsonRpcMethod {
                     .afterTransactionInBlock(
                         blockHash,
                         transactionWithMetadata.getTransaction().getHash(),
-                        (transaction, blockHeader, blockchain, worldState, transactionProcessor) ->
+                        (transaction,
+                            blockHeader,
+                            blockchain,
+                            worldState,
+                            transactionProcessor,
+                            protocolSpec) ->
                             extractStorageAt(
                                 requestContext, accountAddress, startKey, limit, worldState))
                     .orElseGet(() -> emptyResponse(requestContext))))

ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/processor/BlockReplay.java
@@ -14,7 +14,9 @@
  */
 package org.hyperledger.besu.ethereum.api.jsonrpc.internal.processor;
 
+import org.hyperledger.besu.datatypes.DataGas;
 import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.ethereum.chain.Blockchain;
 import org.hyperledger.besu.ethereum.core.Block;
 import org.hyperledger.besu.ethereum.core.BlockBody;
@@ -52,8 +54,17 @@ public class BlockReplay {
     return performActionWithBlock(
         block.getHeader(),
         block.getBody(),
-        (body, header, blockchain, mutableWorldState, transactionProcessor) -> {
-          List<TransactionTrace> transactionTraces =
+        (body, header, blockchain, mutableWorldState, transactionProcessor, protocolSpec) -> {
+          final Wei dataGasPrice =
+              protocolSpec
+                  .getFeeMarket()
+                  .dataPrice(
+                      blockchain
+                          .getBlockHeader(header.getParentHash())
+                          .flatMap(BlockHeader::getExcessDataGas)
+                          .orElse(DataGas.ZERO));
+
+          final List<TransactionTrace> transactionTraces =
               body.getTransactions().stream()
                   .map(
                       transaction ->
@@ -62,7 +73,8 @@ public class BlockReplay {
                               header,
                               blockchain,
                               mutableWorldState,
-                              transactionProcessor))
+                              transactionProcessor,
+                              dataGasPrice))
                   .collect(Collectors.toList());
           return Optional.of(new BlockTrace(transactionTraces));
         });
@@ -77,24 +89,38 @@ public class BlockReplay {
       final Hash blockHash, final Hash transactionHash, final TransactionAction<T> action) {
     return performActionWithBlock(
         blockHash,
-        (body, header, blockchain, mutableWorldState, transactionProcessor) -> {
+        (body, header, blockchain, mutableWorldState, transactionProcessor, protocolSpec) -> {
           final BlockHashLookup blockHashLookup = new BlockHashLookup(header, blockchain);
+          final Wei dataGasPrice =
+              protocolSpec
+                  .getFeeMarket()
+                  .dataPrice(
+                      blockchain
+                          .getBlockHeader(header.getParentHash())
+                          .flatMap(BlockHeader::getExcessDataGas)
+                          .orElse(DataGas.ZERO));
+
           for (final Transaction transaction : body.getTransactions()) {
             if (transaction.getHash().equals(transactionHash)) {
               return Optional.of(
                   action.performAction(
-                      transaction, header, blockchain, mutableWorldState, transactionProcessor));
+                      transaction,
+                      header,
+                      blockchain,
+                      mutableWorldState,
+                      transactionProcessor,
+                      dataGasPrice));
             } else {
-              final ProtocolSpec spec = protocolSchedule.getByBlockHeader(header);
               transactionProcessor.processTransaction(
                   blockchain,
                   mutableWorldState.updater(),
                   header,
                   transaction,
-                  spec.getMiningBeneficiaryCalculator().calculateBeneficiary(header),
+                  protocolSpec.getMiningBeneficiaryCalculator().calculateBeneficiary(header),
                   blockHashLookup,
                   false,
-                  TransactionValidationParams.blockReplay());
+                  TransactionValidationParams.blockReplay(),
+                  dataGasPrice);
             }
           }
           return Optional.empty();
@@ -106,7 +132,7 @@ public class BlockReplay {
     return beforeTransactionInBlock(
         blockHash,
         transactionHash,
-        (transaction, blockHeader, blockchain, worldState, transactionProcessor) -> {
+        (transaction, blockHeader, blockchain, worldState, transactionProcessor, dataGasPrice) -> {
           final ProtocolSpec spec = protocolSchedule.getByBlockHeader(blockHeader);
           transactionProcessor.processTransaction(
               blockchain,
@@ -116,9 +142,10 @@ public class BlockReplay {
               spec.getMiningBeneficiaryCalculator().calculateBeneficiary(blockHeader),
               new BlockHashLookup(blockHeader, blockchain),
               false,
-              TransactionValidationParams.blockReplay());
+              TransactionValidationParams.blockReplay(),
+              dataGasPrice);
           return action.performAction(
-              transaction, blockHeader, blockchain, worldState, transactionProcessor);
+              transaction, blockHeader, blockchain, worldState, transactionProcessor, dataGasPrice);
         });
   }
 
@@ -160,7 +187,8 @@ public class BlockReplay {
                     new IllegalArgumentException(
                         "Missing worldstate for stateroot "
                             + previous.getStateRoot().toShortHexString()))) {
-      return action.perform(body, header, blockchain, worldState, transactionProcessor);
+      return action.perform(
+          body, header, blockchain, worldState, transactionProcessor, protocolSpec);
     } catch (Exception ex) {
       return Optional.empty();
     }
@@ -190,7 +218,8 @@ public class BlockReplay {
         BlockHeader blockHeader,
         Blockchain blockchain,
         MutableWorldState worldState,
-        MainnetTransactionProcessor transactionProcessor);
+        MainnetTransactionProcessor transactionProcessor,
+        ProtocolSpec protocolSpec);
   }
 
   @FunctionalInterface
@@ -200,6 +229,7 @@ public class BlockReplay {
         BlockHeader blockHeader,
         Blockchain blockchain,
         MutableWorldState worldState,
-        MainnetTransactionProcessor transactionProcessor);
+        MainnetTransactionProcessor transactionProcessor,
+        Wei dataGasPrice);
   }
 }

ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/processor/BlockTracer.java
@@ -47,7 +47,12 @@ public class BlockTracer {
 
   private BlockReplay.TransactionAction<TransactionTrace> prepareReplayAction(
       final DebugOperationTracer tracer) {
-    return (transaction, header, blockchain, mutableWorldState, transactionProcessor) -> {
+    return (transaction,
+        header,
+        blockchain,
+        mutableWorldState,
+        transactionProcessor,
+        dataGasPrice) -> {
       // if we have no prior updater, it must be the first TX, so use the block's initial state
       if (chainedUpdater == null) {
         chainedUpdater = mutableWorldState.updater();
@@ -65,7 +70,8 @@ public class BlockTracer {
               header.getCoinbase(),
               tracer,
               new BlockHashLookup(header, blockchain),
-              false);
+              false,
+              dataGasPrice);
       final List<TraceFrame> traceFrames = tracer.copyTraceFrames();
       tracer.reset();
       return new TransactionTrace(transaction, result, traceFrames);

commit 2f29f4b65686cee51fb8f55b93418b2486c4b098

Fix transaction selection result regression
The regression was introduced in commit 28f2bd4a1ad618130763ea1163e0c72f3bf30330
Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/BlockTransactionSelector.java
@@ -42,14 +42,13 @@ import org.hyperledger.besu.plugin.data.TransactionType;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CancellationException;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.google.common.collect.Lists;
 import org.apache.tuweni.bytes.Bytes;
@@ -116,8 +115,9 @@ public class BlockTransactionSelector {
   }
 
   public static class TransactionSelectionResults {
-
-    private final Map<TransactionType, List<Transaction>> transactionsByType = new HashMap<>();
+    private final List<Transaction> transactions = Lists.newArrayList();
+    private final Map<TransactionType, List<Transaction>> transactionsByType =
+        new EnumMap<>(TransactionType.class);
     private final List<TransactionReceipt> receipts = Lists.newArrayList();
     private final List<TransactionValidationResult> invalidTransactions = Lists.newArrayList();
     private long cumulativeGasUsed = 0;
@@ -128,6 +128,7 @@ public class BlockTransactionSelector {
         final TransactionReceipt receipt,
         final long gasUsed,
         final long dataGasUsed) {
+      transactions.add(transaction);
       transactionsByType
           .computeIfAbsent(transaction.getType(), type -> new ArrayList<>())
           .add(transaction);
@@ -138,7 +139,7 @@ public class BlockTransactionSelector {
           LOG,
           "New selected transaction {}, total transactions {}, cumulative gas used {}, cumulative data gas used {}",
           transaction::toTraceLog,
-          () -> transactionsByType.values().stream().mapToInt(List::size).sum(),
+          transactions::size,
           () -> cumulativeGasUsed,
           () -> cumulativeDataGasUsed);
     }
@@ -150,7 +151,7 @@ public class BlockTransactionSelector {
     }
 
     public List<Transaction> getTransactions() {
-      return streamAllTransactions().collect(Collectors.toList());
+      return transactions;
     }
 
     public List<Transaction> getTransactionsByType(final TransactionType type) {
@@ -173,10 +174,6 @@ public class BlockTransactionSelector {
       return invalidTransactions;
     }
 
-    private Stream<Transaction> streamAllTransactions() {
-      return transactionsByType.values().stream().flatMap(List::stream);
-    }
-
     @Override
     public boolean equals(final Object o) {
       if (this == o) {
@@ -188,7 +185,7 @@ public class BlockTransactionSelector {
       TransactionSelectionResults that = (TransactionSelectionResults) o;
       return cumulativeGasUsed == that.cumulativeGasUsed
           && cumulativeDataGasUsed == that.cumulativeDataGasUsed
-          && transactionsByType.equals(that.transactionsByType)
+          && transactions.equals(that.transactions)
           && receipts.equals(that.receipts)
           && invalidTransactions.equals(that.invalidTransactions);
     }
@@ -196,11 +193,7 @@ public class BlockTransactionSelector {
     @Override
     public int hashCode() {
       return Objects.hash(
-          transactionsByType,
-          receipts,
-          invalidTransactions,
-          cumulativeGasUsed,
-          cumulativeDataGasUsed);
+          transactions, receipts, invalidTransactions, cumulativeGasUsed, cumulativeDataGasUsed);
     }
 
     public String toTraceLog() {
@@ -209,7 +202,7 @@ public class BlockTransactionSelector {
           + ", cumulativeDataGasUsed="
           + cumulativeDataGasUsed
           + ", transactions="
-          + streamAllTransactions().map(Transaction::toTraceLog).collect(Collectors.joining("; "));
+          + transactions.stream().map(Transaction::toTraceLog).collect(Collectors.joining("; "));
     }
   }
 

ethereum/blockcreation/src/test/java/org/hyperledger/besu/ethereum/blockcreation/LondonFeeMarketBlockTransactionSelectorTest.java
@@ -24,6 +24,7 @@ import org.hyperledger.besu.ethereum.core.AddressHelpers;
 import org.hyperledger.besu.ethereum.core.BlockHeader;
 import org.hyperledger.besu.ethereum.core.ProcessableBlockHeader;
 import org.hyperledger.besu.ethereum.core.Transaction;
+import org.hyperledger.besu.ethereum.core.TransactionTestFixture;
 import org.hyperledger.besu.ethereum.eth.transactions.ImmutableTransactionPoolConfiguration;
 import org.hyperledger.besu.ethereum.eth.transactions.PendingTransactions;
 import org.hyperledger.besu.ethereum.eth.transactions.sorter.BaseFeePendingTransactionsSorter;
@@ -130,6 +131,50 @@ public class LondonFeeMarketBlockTransactionSelectorTest
 
     assertThat(results.getTransactions().size()).isEqualTo(1);
     assertThat(pendingTransactions.size()).isEqualTo(1);
+  }
+
+  @Test
+  public void transactionFromSameSenderWithMixedTypes() {
+    final ProcessableBlockHeader blockHeader = createBlock(5000);
+
+    final TransactionTestFixture txTestFixture = new TransactionTestFixture();
+    final Transaction txFrontier1 =
+        txTestFixture
+            .type(TransactionType.FRONTIER)
+            .nonce(0)
+            .gasLimit(1)
+            .gasPrice(Wei.ONE)
+            .createTransaction(keyPair);
+    final Transaction txLondon1 = createEIP1559Transaction(1, Wei.ONE, Wei.ONE);
+    final Transaction txFrontier2 =
+        txTestFixture
+            .type(TransactionType.FRONTIER)
+            .nonce(2)
+            .gasLimit(1)
+            .gasPrice(Wei.ONE)
+            .createTransaction(keyPair);
+    final Transaction txLondon2 = createEIP1559Transaction(3, Wei.ONE, Wei.ONE);
+
+    pendingTransactions.addRemoteTransaction(txFrontier1, Optional.empty());
+    pendingTransactions.addRemoteTransaction(txLondon1, Optional.empty());
+    pendingTransactions.addRemoteTransaction(txFrontier2, Optional.empty());
+    pendingTransactions.addRemoteTransaction(txLondon2, Optional.empty());
+
+    ensureTransactionIsValid(txFrontier1);
+    ensureTransactionIsValid(txLondon1);
+    ensureTransactionIsValid(txFrontier2);
+    ensureTransactionIsValid(txLondon2);
+
+    final Address miningBeneficiary = AddressHelpers.ofValue(1);
+    final BlockTransactionSelector selector =
+        createBlockSelector(
+            transactionProcessor, blockHeader, Wei.ZERO, miningBeneficiary, Wei.ZERO);
+
+    final BlockTransactionSelector.TransactionSelectionResults results =
+        selector.buildTransactionListForBlock();
+
+    assertThat(results.getTransactions())
+        .containsExactly(txFrontier1, txLondon1, txFrontier2, txLondon2);
   }
 
   private Transaction createEIP1559Transaction(