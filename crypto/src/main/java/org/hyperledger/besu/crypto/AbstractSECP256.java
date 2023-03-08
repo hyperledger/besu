80a9886717df7f710ea7a738f1ba9da6232dcda3
Danno Ferrin <danno.ferrin@swirldslabs.com>
2023-03-08 14:58:18 -0700

    Option to use GraalVM AOT for EvmTool
    Final changes and config to support using GraalVM AOT to compile and
    execute evmTool.  While not as long-term performant the startup time
    makes filling reference tests with Besu reasonable.
    
    Signed-off-by: Danno Ferrin <danno.ferrin@swirldslabs.com>

--- crypto/src/main/java/org/hyperledger/besu/crypto/AbstractSECP256.java
+++ crypto/src/main/java/org/hyperledger/besu/crypto/AbstractSECP256.java
@@ -15,6 +15,7 @@
 package org.hyperledger.besu.crypto;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static org.bouncycastle.jcajce.provider.asymmetric.ec.KeyPairGeneratorSpi.ECDSA;
 
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
@@ -43,13 +44,6 @@ import org.bouncycastle.math.ec.ECPoint;
 
 /** The Abstract secp256. */
 public abstract class AbstractSECP256 implements SignatureAlgorithm {
-
-  /** The constant PRIVATE_KEY_BYTE_LENGTH. */
-  protected static final int PRIVATE_KEY_BYTE_LENGTH = 32;
-  /** The constant PUBLIC_KEY_BYTE_LENGTH. */
-  protected static final int PUBLIC_KEY_BYTE_LENGTH = 64;
-  /** The constant SIGNATURE_BYTE_LENGTH. */
-  protected static final int SIGNATURE_BYTE_LENGTH = 65;
 
   /** The constant PROVIDER. */
   public static final String PROVIDER = "BC";
@@ -82,7 +76,7 @@ public abstract class AbstractSECP256 implements SignatureAlgorithm {
     curveOrder = curve.getN();
     halfCurveOrder = curveOrder.shiftRight(1);
     try {
-      keyPairGenerator = KeyPairGenerator.getInstance(ALGORITHM, PROVIDER);
+      keyPairGenerator = new ECDSA();
     } catch (final Exception e) {
       throw new RuntimeException(e);
     }
