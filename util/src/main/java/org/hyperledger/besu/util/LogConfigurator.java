a1797d76f5e5c6d9fa09f8b16946657f272565ef
Danno Ferrin <danno.ferrin@swirldslabs.com>
2023-03-08 21:33:28 -0700

    spotless
    Signed-off-by: Danno Ferrin <danno.ferrin@swirldslabs.com>

--- util/src/main/java/org/hyperledger/besu/util/LogConfigurator.java
+++ util/src/main/java/org/hyperledger/besu/util/LogConfigurator.java
@@ -15,6 +15,7 @@
 package org.hyperledger.besu.util;
 
 import java.util.NoSuchElementException;
+
 /** The library independent logger configurator util. */
 @SuppressWarnings("CatchAndPrintStackTrace")
 public interface LogConfigurator {
