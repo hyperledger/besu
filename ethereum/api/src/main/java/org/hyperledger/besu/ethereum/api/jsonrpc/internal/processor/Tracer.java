7bf623b2d628d728533a90ae15303f299a5aeab6
garyschulte <garyschulte@gmail.com>
2023-03-09 13:17:10 -0800

    tracer
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/processor/Tracer.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/processor/Tracer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright Hyperledger Besu Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 package org.hyperledger.besu.ethereum.api.jsonrpc.internal.processor;
 
 import org.hyperledger.besu.datatypes.Address;
@@ -49,6 +64,12 @@ public class Tracer {
     }
 
     @Override
+    public MutableWorldState copy() {
+      throw new UnsupportedOperationException(
+          "This method is not supported and will not exist in the future version of MutableWorldState interface");
+    }
+
+    @Override
     public void persist(final BlockHeader blockHeader) {
       mutableWorldState.persist(blockHeader);
     }
@@ -76,11 +97,6 @@ public class Tracer {
     @Override
     public Account get(final Address address) {
       return mutableWorldState.get(address);
-    }
-
-    @Override
-    public MutableWorldState freeze() {
-      return mutableWorldState.freeze();
     }
 
     @Override
