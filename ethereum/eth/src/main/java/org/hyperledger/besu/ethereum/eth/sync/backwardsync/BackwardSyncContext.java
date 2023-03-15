c0c329fecb58833b3106ff97f22ade4c7fda4940
Fabio Di Fabio <fabio.difabio@consensys.net>
2023-03-15 12:11:18 +0100

    Fix for backward sync wrongly thinking it is done after a restart (#5182)
    <!-- Thanks for sending a pull request! Please check out our
    contribution guidelines: -->
    <!-- https://github.com/hyperledger/besu/blob/main/CONTRIBUTING.md -->
    
    ## PR description
    
    There is an issue when restarting Besu when a backward sync session is
    running, since after the restart it is possible that the Consensus
    client sends a FcU or a NewPayload for a block that is present in the
    backward sync storage, but not yet imported, so not on the main chain,
    but still the backward sync thinks it should not do anything with that
    block, so it returns like it has completed the sync, but since the sync
    is not done actually then the internal error that the finalize block is
    not present.
    
    The solution is to persist the backward sync status, so in case of a
    restart, it can resume from where it was interrupted.
    
    ## Fixed Issue(s)
    <!-- Please link to fixed issue(s) here using format: fixes #<issue
    number> -->
    <!-- Example: "fixes #2" -->
    
    fixes #5053 
    
    ## Documentation
    
    - [x] I thought about documentation and added the `doc-change-required`
    label to this PR if
    [updates are
    required](https://wiki.hyperledger.org/display/BESU/Documentation).
    
    ## Acceptance Tests (Non Mainnet)
    
    - [x] I have considered running `./gradlew acceptanceTestNonMainnet`
    locally if my PR affects non-mainnet modules.
    
    ## Changelog
    
    - [x] I thought about the changelog and included a [changelog update if
    required](https://wiki.hyperledger.org/display/BESU/Changelog).
    
    ---------
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/backwardsync/BackwardSyncContext.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/backwardsync/BackwardSyncContext.java
@@ -153,7 +153,7 @@ public class BackwardSyncContext {
     if (backwardChain.isTrusted(hash)) {
       LOG.atDebug()
           .setMessage(
-              "not fetching or appending hash {} to backwards sync since it is present in successors")
+              "Not fetching or appending hash {} to backward sync since it is present in successors")
           .addArgument(hash::toHexString)
           .log();
       return true;
@@ -237,7 +237,7 @@ public class BackwardSyncContext {
   @VisibleForTesting
   CompletableFuture<Void> prepareBackwardSyncFuture() {
     final MutableBlockchain blockchain = getProtocolContext().getBlockchain();
-    return new BackwardsSyncAlgorithm(
+    return new BackwardSyncAlgorithm(
             this,
             FinalBlockConfirmation.confirmationChain(
                 FinalBlockConfirmation.genesisConfirmation(blockchain),
