0d66e08932e72413b2edf42418e923212bc996b9
Ameziane H <ameziane.hamlat@consensys.net>
2023-03-20 10:51:41 +0100

    Code cleaning and some refactoring, following Sally's review
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/TraceReplayBlockTransactions.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/TraceReplayBlockTransactions.java
@@ -122,7 +122,7 @@ public class TraceReplayBlockTransactions extends AbstractBlockParameterMethod {
                   new PrometheusMetricsSystem(BesuMetricCategory.DEFAULT_METRIC_CATEGORIES, false)
                       .createLabelledCounter(
                           BesuMetricCategory.BLOCKCHAIN,
-                          "transactions_traceblock_pipeline_processed_total",
+                          "transactions_traceReplayblock_pipeline_processed_total",
                           "Number of transactions processed for each block",
                           "step",
                           "action");
