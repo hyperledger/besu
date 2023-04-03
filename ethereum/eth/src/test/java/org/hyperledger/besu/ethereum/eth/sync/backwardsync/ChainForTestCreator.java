731ee42f157dd1b4496f0b50f77f068ed91a8a15
Navie Chan <naviechan@gmail.com>
2023-04-03 11:47:08 +0800

    Fix compilation error
    Signed-off-by: Navie Chan <naviechan@gmail.com>

--- ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/sync/backwardsync/ChainForTestCreator.java
+++ ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/sync/backwardsync/ChainForTestCreator.java
@@ -83,7 +83,6 @@ public class ChainForTestCreator {
         blockHeader.getMixHash(),
         blockHeader.getNonce(),
         blockHeader.getWithdrawalsRoot().orElse(null),
-        blockHeader.getDepositsRoot().orElse(null),
         blockHeader.getExcessDataGas().orElse(null),
         blockHeader.getDepositsRoot().orElse(null),
         new MainnetBlockHeaderFunctions());
