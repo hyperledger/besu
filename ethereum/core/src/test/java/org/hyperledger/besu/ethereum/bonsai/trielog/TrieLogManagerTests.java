8977a54e0a64ef0bfd593b63e4489458d909a3e8
garyschulte <garyschulte@gmail.com>
2023-04-06 15:30:14 -0700

    add account zero reads, mark self-destructed accounts, code and storage as cleared
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/bonsai/trielog/TrieLogManagerTests.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/bonsai/trielog/TrieLogManagerTests.java
@@ -31,6 +31,8 @@ import org.hyperledger.besu.metrics.noop.NoOpMetricsSystem;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
