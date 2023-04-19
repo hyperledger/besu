6bdf7b8720e50f01baae01406eba4650d743a367
garyschulte <garyschulte@gmail.com>
2023-04-19 15:41:56 -0700

    spdx header and fixture fix for addedCode test
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/bonsai/trielog/TrieLogLayerTests.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/bonsai/trielog/TrieLogLayerTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright contributors to Hyperledger Besu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 package org.hyperledger.besu.ethereum.bonsai.trielog;
 
 import org.hyperledger.besu.datatypes.Address;
@@ -58,8 +73,8 @@ public class TrieLogLayerTests {
     Hash blockHash = Hash.fromHexStringLenient("0xfeedbeef02dead");
 
     Address otherAddress = Address.fromHexString("0x0000deadbeef");
-    Bytes otherOldValue = Bytes.fromHexString("0x0000");
-    Bytes otherNewValue = Bytes.fromHexString("0x0001030307");
+    Bytes otherOldValue = Bytes.fromHexString("0x00");
+    Bytes otherNewValue = Bytes.fromHexString("0x01030307");
     Hash otherBlockHash = Hash.fromHexStringLenient("0x00feedbeef02dead");
 
     trieLogLayer.addCodeChange(address, oldValue, newValue, blockHash);
