a30f7cdea48d674d3459597b5ef0263e47cfb8f4
Antony Denyer <git@antonydenyer.co.uk>
2023-04-21 10:10:47 +0100

    Use binary search to eth_estimateGas more accurately (#5142)
    * Use binary search to eth_estimateGas more accurately
    
    eth_estimateGas currently conservatively estimates gas this change
    will take that estimate and use it as a hi bound and will use
    gasUsed as lo bound to hone in on the most true gas limit required
    
    
    Signed-off-by: Antony Denyer <git@antonydenyer.co.uk>

---
+++ acceptance-tests/dsl/src/main/java/org/hyperledger/besu/tests/acceptance/dsl/transaction/eth/EthEstimateGasTransaction.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright Hyperledger Besu Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.tests.acceptance.dsl.transaction.eth;
+
+import org.hyperledger.besu.tests.acceptance.dsl.transaction.NodeRequests;
+import org.hyperledger.besu.tests.acceptance.dsl.transaction.Transaction;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+import org.web3j.protocol.core.methods.response.EthEstimateGas;
+
+public class EthEstimateGasTransaction implements Transaction<EthEstimateGas> {
+  private final String contractAddress;
+  private final String functionCall;
+  private final String from = "";
+
+  public EthEstimateGasTransaction(final String contractAddress, final String functionCall) {
+    this.contractAddress = contractAddress;
+    this.functionCall = functionCall;
+  }
+
+  @Override
+  public EthEstimateGas execute(final NodeRequests node) {
+    try {
+
+      return node.eth()
+          .ethEstimateGas(
+              new org.web3j.protocol.core.methods.request.Transaction(
+                  from,
+                  BigInteger.ONE,
+                  BigInteger.ZERO,
+                  BigInteger.ZERO,
+                  contractAddress,
+                  BigInteger.ZERO,
+                  functionCall))
+          .send();
+    } catch (final IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
