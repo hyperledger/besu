e7ad166af790b0abcad6386e13c4fe9ab8a68a7b
garyschulte <garyschulte@gmail.com>
2023-05-08 16:49:37 -0700

    add plugin observer subscription
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/cache/CachedWorldStorageManager.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/cache/CachedWorldStorageManager.java
@@ -216,15 +216,20 @@ public class CachedWorldStorageManager extends AbstractTrieLogManager
   @Override
   protected TrieLogFactory setupTrieLogFactory(final BesuContext pluginContext) {
     // if we have a TrieLogService from pluginContext, use it.
-    var trieLogService =
+    var trieLogServicez =
         Optional.ofNullable(pluginContext)
             .flatMap(context -> context.getService(TrieLogService.class));
 
-    if (trieLogService.isPresent()) {
+    if (trieLogServicez.isPresent()) {
+      var trieLogService = trieLogServicez.get();
       // push the TrieLogProvider into the TrieLogService
-      trieLogService.get().configureTrieLogProvider(getTrieLogProvider());
+      trieLogService.configureTrieLogProvider(getTrieLogProvider());
+
+      // configure plugin observers:
+      trieLogService.getObservers().forEach(trieLogObservers::subscribe);
+
       // return the TrieLogFactory implementation from the TrieLogService
-      return trieLogService.get().getTrieLogFactory();
+      return trieLogService.getTrieLogFactory();
     } else {
       // Otherwise default to TrieLogFactoryImpl
       return new TrieLogFactoryImpl();
