ea0eeaff65af54dc63b90229c4282657fc89654c
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2023-05-15 15:22:46 +1000

    Stream response when batching json rpc calls
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

---
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/handlers/JsonRpcObjectExecutor.java
@@ -0,0 +1,92 @@
+package org.hyperledger.besu.ethereum.api.handlers;
+
+import static io.netty.handler.codec.http.HttpHeaderValues.APPLICATION_JSON;
+
+import org.hyperledger.besu.ethereum.api.jsonrpc.JsonResponseStreamer;
+import org.hyperledger.besu.ethereum.api.jsonrpc.JsonRpcConfiguration;
+import org.hyperledger.besu.ethereum.api.jsonrpc.context.ContextKey;
+import org.hyperledger.besu.ethereum.api.jsonrpc.execution.JsonRpcExecutor;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcError;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcErrorResponse;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcResponse;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcResponseType;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.opentelemetry.api.trace.Tracer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
+
+public class JsonRpcObjectExecutor extends AbstractJsonRpcExecutor {
+  private static final ObjectWriter jsonObjectWriter = createObjectWriter();
+
+  public JsonRpcObjectExecutor(
+      final JsonRpcExecutor jsonRpcExecutor,
+      final Tracer tracer,
+      final RoutingContext ctx,
+      final JsonRpcConfiguration jsonRpcConfiguration) {
+    super(jsonRpcExecutor, tracer, ctx, jsonRpcConfiguration);
+  }
+
+  @Override
+  void execute() throws IOException {
+    HttpServerResponse response = ctx.response();
+    response = response.putHeader("Content-Type", APPLICATION_JSON);
+
+    final JsonObject jsonRequest = ctx.get(ContextKey.REQUEST_BODY_AS_JSON_OBJECT.name());
+    lazyTraceLogger(jsonRequest::toString);
+    final JsonRpcResponse jsonRpcResponse =
+        executeRequest(jsonRpcExecutor, tracer, jsonRequest, ctx);
+    handleJsonObjectResponse(response, jsonRpcResponse, ctx);
+  }
+
+  @Override
+  String getRpcMethodName(final RoutingContext ctx) {
+    final JsonObject jsonObject = ctx.get(ContextKey.REQUEST_BODY_AS_JSON_OBJECT.name());
+    return jsonObject.getString("method");
+  }
+
+  private static void handleJsonObjectResponse(
+      final HttpServerResponse response,
+      final JsonRpcResponse jsonRpcResponse,
+      final RoutingContext ctx)
+      throws IOException {
+    response.setStatusCode(status(jsonRpcResponse).code());
+    if (jsonRpcResponse.getType() == JsonRpcResponseType.NONE) {
+      response.end();
+    } else {
+      try (final JsonResponseStreamer streamer =
+          new JsonResponseStreamer(response, ctx.request().remoteAddress())) {
+        // underlying output stream lifecycle is managed by the json object writer
+        lazyTraceLogger(() -> getJsonObjectMapper().writeValueAsString(jsonRpcResponse));
+        jsonObjectWriter.writeValue(streamer, jsonRpcResponse);
+      }
+    }
+  }
+
+  private static HttpResponseStatus status(final JsonRpcResponse response) {
+    return switch (response.getType()) {
+      case UNAUTHORIZED -> HttpResponseStatus.UNAUTHORIZED;
+      case ERROR -> statusCodeFromError(((JsonRpcErrorResponse) response).getError());
+      default -> HttpResponseStatus.OK;
+    };
+  }
+
+  private static ObjectWriter createObjectWriter() {
+    return getJsonObjectMapper()
+        .writerWithDefaultPrettyPrinter()
+        .without(JsonGenerator.Feature.FLUSH_PASSED_TO_STREAM)
+        .with(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+  }
+
+  private static HttpResponseStatus statusCodeFromError(final JsonRpcError error) {
+    return switch (error) {
+      case INVALID_REQUEST, PARSE_ERROR -> HttpResponseStatus.BAD_REQUEST;
+      default -> HttpResponseStatus.OK;
+    };
+  }
+}
