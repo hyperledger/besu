969043c42d8a115fee04670223a64ed33e744ccb
Justin Florentine <justin+github@florentine.us>
2023-05-17 12:07:43 -0400

    spotless
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- ethereum/retesteth/src/main/java/org/hyperledger/besu/ethereum/retesteth/NoRewardProtocolScheduleWrapper.java
+++ ethereum/retesteth/src/main/java/org/hyperledger/besu/ethereum/retesteth/NoRewardProtocolScheduleWrapper.java
@@ -116,7 +116,8 @@ public class NoRewardProtocolScheduleWrapper implements ProtocolSchedule {
   }
 
   @Override
-  public Optional<ScheduledProtocolSpec.Hardfork> hardforkFor(final Predicate<ScheduledProtocolSpec> predicate) {
+  public Optional<ScheduledProtocolSpec.Hardfork> hardforkFor(
+      final Predicate<ScheduledProtocolSpec> predicate) {
     return delegate.hardforkFor(predicate);
   }
 
