eced98732eaf54570a0a14a16a52eee8555ff037
Simon Dudley <simon.dudley@consensys.net>
2023-05-26 17:32:51 +1000

    Fix spotless Signed-off-by: Simon Dudley <simon.dudley@consensys.net>

--- ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreator.java
+++ ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreator.java
@@ -203,15 +203,12 @@ public abstract class AbstractBlockCreator implements AsyncBlockCreator {
       final DepositsValidator depositsValidator = newProtocolSpec.getDepositsValidator();
       Optional<List<Deposit>> maybeDeposits = Optional.empty();
 
-      if (depositsValidator instanceof DepositsValidator.AllowedDeposits && depositContractAddress.isPresent()) {
+      if (depositsValidator instanceof DepositsValidator.AllowedDeposits
+          && depositContractAddress.isPresent()) {
         final List<Deposit> depositsFromReceipts =
             transactionResults.getReceipts().stream()
                 .flatMap(receipt -> receipt.getLogsList().stream())
-                .filter(
-                    log ->
-                        depositContractAddress
-                            .get()
-                            .equals(log.getLogger()))
+                .filter(log -> depositContractAddress.get().equals(log.getLogger()))
                 .map(DepositDecoder::decodeFromLog)
                 .toList();
         maybeDeposits = Optional.of(depositsFromReceipts);
