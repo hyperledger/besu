f809c6827107a02c5d52aca91db9ee3e195e6cb4
Gabriel Fukushima <gabrielfukushima@gmail.com>
2023-06-01 11:18:24 +1000

    Load code when WS snapshot still available
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/graphql/internal/pojoadapter/BlockAdapterBase.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/graphql/internal/pojoadapter/BlockAdapterBase.java
@@ -32,6 +32,7 @@ import org.hyperledger.besu.ethereum.rlp.BytesValueRLPOutput;
 import org.hyperledger.besu.ethereum.transaction.CallParameter;
 import org.hyperledger.besu.ethereum.transaction.TransactionSimulator;
 import org.hyperledger.besu.ethereum.transaction.TransactionSimulatorResult;
+import org.hyperledger.besu.evm.account.Account;
 import org.hyperledger.besu.evm.log.LogTopic;
 import org.hyperledger.besu.evm.tracing.OperationTracer;
 
@@ -145,13 +146,15 @@ public class BlockAdapterBase extends AdapterBase {
   public AccountAdapter getAccount(final DataFetchingEnvironment environment) {
 
     final BlockchainQueries query = getBlockchainQueries(environment);
+    final Address address = environment.getArgument("address");
     final long bn = header.getNumber();
     return query
         .getAndMapWorldState(
             bn,
             ws -> {
-              final Address address = environment.getArgument("address");
-              return Optional.of(new AccountAdapter(ws.get(address)));
+              Account account = ws.get(address);
+              account.getCode();
+              return Optional.of(new AccountAdapter(account));
             })
         .get();
   }
