db7fd1da1f57ba64c584003f41e3dadb00e6bc93
Justin Florentine <justin+github@florentine.us>
2023-06-05 10:17:48 -0400

    disabled KZG point eval test temporarily
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- evm/src/test/java/org/hyperledger/besu/evm/precompile/KZGPointEvalPrecompileContractTest.java
+++ evm/src/test/java/org/hyperledger/besu/evm/precompile/KZGPointEvalPrecompileContractTest.java
@@ -31,9 +31,11 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@Disabled
 public class KZGPointEvalPrecompileContractTest {
   private static KZGPointEvalPrecompiledContract contract;
   private final MessageFrame toRun = mock(MessageFrame.class);
