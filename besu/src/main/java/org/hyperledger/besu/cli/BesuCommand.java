cbb3545ce45b1e29949e5d78a56d27946bd2dfb7
Justin Florentine <justin+github@florentine.us>
2023-06-07 11:24:28 -0400

    KZGPrecompile test coverage and versionedhash checks, fixes bug on adding v3 engine api at startup
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- besu/src/main/java/org/hyperledger/besu/cli/BesuCommand.java
+++ besu/src/main/java/org/hyperledger/besu/cli/BesuCommand.java
@@ -1569,6 +1569,7 @@ public class BesuCommand implements DefaultCommandValues, Runnable {
       runner.awaitStop();
 
     } catch (final Exception e) {
+      logger.warn("Failed to start Besu", e);
       throw new ParameterException(this.commandLine, e.getMessage(), e);
     }
   }
