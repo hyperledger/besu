4367506989784aa812f2b08b46dd48b615381c10
George Tebrean <george@web3labs.com>
2023-06-29 12:40:01 +0300

    move enum value
    Signed-off-by: George Tebrean <george@web3labs.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/response/RpcErrorType.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/response/RpcErrorType.java
@@ -25,6 +25,9 @@ public enum RpcErrorType {
 
   METHOD_NOT_ENABLED(-32604, "Method not enabled"),
 
+  // Resource unavailable error
+  TX_POOL_DISABLED(-32002, "Transaction pool not enabled"),
+
   // eth_getBlockByNumber specific error message
   UNKNOWN_BLOCK(-39001, "Unknown block"),
 
