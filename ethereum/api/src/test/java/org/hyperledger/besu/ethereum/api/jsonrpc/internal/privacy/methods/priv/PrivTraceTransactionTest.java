b245c5d7748fce11d27658ce1d4c7bbe97e3eec7
Nischal Sharma <nischal@web3labs.com>
2023-06-30 14:31:34 +0530

    unit tests first commit
    Signed-off-by: Nischal Sharma <nischal@web3labs.com>

---
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/privacy/methods/priv/PrivTraceTransactionTest.java
@@ -0,0 +1,23 @@
+package org.hyperledger.besu.ethereum.api.jsonrpc.internal.privacy.methods.priv;
+
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.privacy.methods.PrivacyIdProvider;
+import org.hyperledger.besu.ethereum.api.query.BlockchainQueries;
+import org.hyperledger.besu.ethereum.api.query.PrivacyQueries;
+import org.hyperledger.besu.ethereum.privacy.PrivacyController;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PrivTraceTransactionTest {
+
+    private final String PRIVACY_GROUP_ID = "B1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo=";
+
+    @Mock
+    private BlockchainQueries blockchainQueries;
+    @Mock private PrivacyQueries privacyQueries;
+    @Mock private PrivacyController privacyController;
+    @Mock private PrivacyIdProvider privacyIdProvider;
+
+    private PrivTraceTransaction method;
+}
