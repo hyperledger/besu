69067c595cd30f69b5d90d2fcdd6f85f95c644cb
spencer-tb <spencer@spencertaylorbrown.uk>
2023-06-30 18:29:04 -0600

    fix for genesis mismatch
    Signed-off-by: spencer-tb <spencer@spencertaylorbrown.uk>

--- config/src/main/java/org/hyperledger/besu/config/GenesisConfigFile.java
+++ config/src/main/java/org/hyperledger/besu/config/GenesisConfigFile.java
@@ -246,6 +246,25 @@ public class GenesisConfigFile {
   public String getNonce() {
     return JsonUtil.getValueAsString(configRoot, "nonce", "0x0");
   }
+  
+    /**
+   * Gets excess data gas.
+   *
+   * @return the excess data gas
+   */
+  public String getExcessDataGas() {
+      return JsonUtil.getValueAsString(configRoot, "excessdatagas", "0x0");
+  }
+
+  /**
+   * Gets data gas used.
+   *
+   * @return the data gas used
+   */
+  public String getDataGasUsed() {
+      return JsonUtil.getValueAsString(configRoot, "datagasused", "0x0");
+  }
+
 
   /**
    * Gets coinbase.
