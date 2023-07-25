28105612b6825c7d229f8c8422e87c5ccf6f0a06
Nischal Sharma <nischal@web3labs.com>
2023-07-25 23:39:41 +0530

    ethereum/part5 - migrated tests from Junit4 to Junit5 (#5717)
    Signed-off-by: Nischal Sharma <nischal@web3labs.com>
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/ethstats/src/test/java/org/hyperledger/besu/ethstats/util/EthStatsConnectOptionsTest.java
+++ ethereum/ethstats/src/test/java/org/hyperledger/besu/ethstats/util/EthStatsConnectOptionsTest.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.nio.file.Path;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
