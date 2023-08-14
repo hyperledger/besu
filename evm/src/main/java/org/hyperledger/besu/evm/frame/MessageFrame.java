1d9b4471486c0ae4d57d2b4d25b8a418a30ffa00
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-08-15 01:31:53 +0200

    Make the `MessageFrame.isWarm(...)` method family public
    Signed-off-by: Franklin Delehelle <franklin.delehelle@odena.eu>

--- evm/src/main/java/org/hyperledger/besu/evm/frame/MessageFrame.java
+++ evm/src/main/java/org/hyperledger/besu/evm/frame/MessageFrame.java
@@ -993,6 +993,18 @@ public class MessageFrame {
     return !txValues.warmedUpAddresses().add(address);
   }
 
+   /**
+   * Returns whether an address has been warmed up. Is deliberately publicly exposed for access from
+   * tracers.
+   *
+   * @param address the address context
+   * @return whether the address has been warmed up
+   */
+  public boolean isWarm(final Address address) {
+    return txValues.warmedUpAddresses().contains(address);
+  }
+
+
   /**
    * "Warms up" the storage slot as per EIP-2929
    *
@@ -1002,6 +1014,18 @@ public class MessageFrame {
    */
   public boolean warmUpStorage(final Address address, final Bytes32 slot) {
     return txValues.warmedUpStorage().put(address, slot, Boolean.TRUE) != null;
+  }
+
+    /**
+   * Returns whether an address' slot is warmed up. Is deliberately publicly exposed for access from
+   * tracers.
+   *
+   * @param address the address context
+   * @param slot the slot to query
+   * @return whether the address/slot couple is warmed up
+   */
+  public boolean isWarm(final Address address, final Bytes32 slot) {
+    return this.txValues.warmedUpStorage().contains(address, slot);
   }
 
   /**
