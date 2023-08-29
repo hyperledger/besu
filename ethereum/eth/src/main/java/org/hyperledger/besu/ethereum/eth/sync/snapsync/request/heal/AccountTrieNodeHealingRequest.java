3aba96da174ab777ca5035a2fc4a8443bc6daea3
Karim TAAM <karim.t2am@gmail.com>
2023-08-29 20:05:02 +0200

    change trie healing
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/snapsync/request/heal/AccountTrieNodeHealingRequest.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/snapsync/request/heal/AccountTrieNodeHealingRequest.java
@@ -17,14 +17,15 @@ package org.hyperledger.besu.ethereum.eth.sync.snapsync.request.heal;
 import static org.hyperledger.besu.ethereum.eth.sync.snapsync.request.SnapDataRequest.createAccountTrieNodeDataRequest;
 
 import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.ethereum.bonsai.storage.BonsaiWorldStateKeyValueStorage;
 import org.hyperledger.besu.ethereum.eth.sync.snapsync.SnapSyncConfiguration;
 import org.hyperledger.besu.ethereum.eth.sync.snapsync.SnapSyncProcessState;
 import org.hyperledger.besu.ethereum.eth.sync.snapsync.SnapWorldDownloadState;
 import org.hyperledger.besu.ethereum.eth.sync.snapsync.request.SnapDataRequest;
 import org.hyperledger.besu.ethereum.rlp.RLP;
 import org.hyperledger.besu.ethereum.trie.CompactEncoding;
-import org.hyperledger.besu.ethereum.trie.MerkleTrie;
 import org.hyperledger.besu.ethereum.trie.patricia.StoredMerklePatriciaTrie;
+import org.hyperledger.besu.ethereum.worldstate.FlatDbMode;
 import org.hyperledger.besu.ethereum.worldstate.StateTrieAccountValue;
 import org.hyperledger.besu.ethereum.worldstate.WorldStateStorage;
 
@@ -139,18 +140,23 @@ public class AccountTrieNodeHealingRequest extends TrieNodeHealingRequest {
         Hash.wrap(
             Bytes32.wrap(CompactEncoding.pathToBytes(Bytes.concatenate(getLocation(), path))));
 
+    // update the flat db only for bonsai
+    if (!worldStateStorage.getFlatDbMode().equals(FlatDbMode.NO_FLATTENED)) {
+      ((BonsaiWorldStateKeyValueStorage.Updater) worldStateStorage.updater())
+          .putAccountInfoState(accountHash, value)
+          .commit();
+    }
+
     // Add code, if appropriate
     if (!accountValue.getCodeHash().equals(Hash.EMPTY)) {
       builder.add(createBytecodeRequest(accountHash, getRootHash(), accountValue.getCodeHash()));
     }
     // Add storage, if appropriate
-    if (!accountValue.getStorageRoot().equals(MerkleTrie.EMPTY_TRIE_NODE_HASH)) {
-      // If we detect an account storage we fill it with snapsync before completing with a heal
-      final SnapDataRequest storageTrieRequest =
-          createStorageTrieNodeDataRequest(
-              accountValue.getStorageRoot(), accountHash, getRootHash(), Bytes.EMPTY);
-      builder.add(storageTrieRequest);
-    }
+    // If we detect an account storage we fill it with snapsync before completing with a heal
+    final SnapDataRequest storageTrieRequest =
+        createStorageTrieNodeDataRequest(
+            accountValue.getStorageRoot(), accountHash, getRootHash(), Bytes.EMPTY);
+    builder.add(storageTrieRequest);
 
     return builder.build();
   }
