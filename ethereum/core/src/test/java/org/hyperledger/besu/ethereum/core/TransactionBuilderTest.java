d69adda941f57a70474f936f9d31fcb614e99060
Stefan Pingel <16143240+pinges@users.noreply.github.com>
2023-08-31 09:42:32 +1000

    enforce that BlobTransactions have at least one blob (#5826)
    * enforce that BlobTransactions have at least one blob
    
    Signed-off-by: Stefan <stefan.pingel@consensys.net>
    Signed-off-by: Stefan Pingel <16143240+pinges@users.noreply.github.com>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/core/TransactionBuilderTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/core/TransactionBuilderTest.java
@@ -73,7 +73,7 @@ public class TransactionBuilderTest {
           .createTransaction(senderKeys);
       fail();
     } catch (IllegalArgumentException iea) {
-      assertThat(iea).hasMessage("Blob transaction must have at least one blob");
+      assertThat(iea).hasMessage("Blob transaction must have at least one versioned hash");
     }
   }
 }
