4b01973f904d2deaf4d291b88c0b0a8f04ab4979
Navie Chan <naviechan@gmail.com>
2023-10-07 19:00:28 +0800

    Address comment
    Signed-off-by: Navie Chan <naviechan@gmail.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineNewPayloadV6110.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineNewPayloadV6110.java
@@ -17,17 +17,19 @@ package org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.engine;
 import org.hyperledger.besu.consensus.merge.blockcreation.MergeMiningCoordinator;
 import org.hyperledger.besu.ethereum.ProtocolContext;
 import org.hyperledger.besu.ethereum.api.jsonrpc.RpcMethod;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.parameters.EnginePayloadParameter;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.RpcErrorType;
 import org.hyperledger.besu.ethereum.eth.manager.EthPeers;
 import org.hyperledger.besu.ethereum.mainnet.ProtocolSchedule;
 import org.hyperledger.besu.ethereum.mainnet.ScheduledProtocolSpec;
 import org.hyperledger.besu.ethereum.mainnet.ValidationResult;
 
+import java.util.List;
 import java.util.Optional;
 
 import io.vertx.core.Vertx;
 
-public class EngineNewPayloadV6110 extends EngineNewPayloadV3 {
+public class EngineNewPayloadV6110 extends AbstractEngineNewPayload {
 
   private final Optional<ScheduledProtocolSpec.Hardfork> eip6110;
 
@@ -47,6 +49,27 @@ public class EngineNewPayloadV6110 extends EngineNewPayloadV3 {
   @Override
   public String getName() {
     return RpcMethod.ENGINE_NEW_PAYLOAD_V6110.getMethodName();
+  }
+
+  @Override
+  protected ValidationResult<RpcErrorType> validateParameters(
+      final EnginePayloadParameter payloadParameter,
+      final Optional<List<String>> maybeVersionedHashParam,
+      final Optional<String> maybeBeaconBlockRootParam) {
+    if (payloadParameter.getBlobGasUsed() == null || payloadParameter.getExcessBlobGas() == null) {
+      return ValidationResult.invalid(RpcErrorType.INVALID_PARAMS, "Missing blob gas fields");
+    } else if (maybeVersionedHashParam == null) {
+      return ValidationResult.invalid(
+          RpcErrorType.INVALID_PARAMS, "Missing versioned hashes field");
+    } else if (maybeBeaconBlockRootParam.isEmpty()) {
+      return ValidationResult.invalid(
+          RpcErrorType.INVALID_PARAMS, "Missing parent beacon block root field");
+    } else if (payloadParameter.getDeposits() == null) {
+      return ValidationResult.invalid(
+          RpcErrorType.INVALID_PARAMS, "Missing deposit field");
+    } else {
+      return ValidationResult.valid();
+    }
   }
 
   @Override
