be7147e945918b4ecbb3e1f7b10d7771c44290ab
Sally MacFarlane <macfarla.github@gmail.com>
2023-10-20 10:08:36 +1000

    formatting
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/ParentBeaconBlockRootHelper.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/ParentBeaconBlockRootHelper.java
@@ -19,8 +19,8 @@ import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.evm.account.MutableAccount;
 import org.hyperledger.besu.evm.worldstate.WorldUpdater;
 
-import org.apache.tuweni.bytes.Bytes;
 import com.google.common.primitives.Longs;
+import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.units.bigints.UInt256;
 
@@ -49,7 +49,8 @@ public interface ParentBeaconBlockRootHelper {
     final UInt256 timestampIndex = UInt256.valueOf(timestampReduced);
     final UInt256 rootIndex = UInt256.valueOf(timestampExtended);
 
-    account.setStorageValue(timestampIndex, UInt256.fromBytes(Bytes.of(Longs.toByteArray(timestamp))));
+    account.setStorageValue(
+        timestampIndex, UInt256.fromBytes(Bytes.of(Longs.toByteArray(timestamp))));
     account.setStorageValue(rootIndex, UInt256.fromBytes(root));
     worldUpdater.commit();
   }
