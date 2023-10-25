3310c416bd0892512b42c82e37cc458efe97f662
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2023-10-25 15:37:15 +1100

    Implement miner_setMinPriorityFee and miner_getMinPriorityFee (#6080)
    * Refactor mining options
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Fix null pointer exception
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * fix another null pointer exception
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * uncomment code
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Move miner options tests
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Unit test fixes
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Removed the commented code
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * WIP
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * WIP
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * New miner option: min-priority-fee
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Remove not relevant for this feature
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Remove not relevant for this feature
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Fix javadoc
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Remove code not belonging to this PR
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * coinbase is an updatable parameter
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Move MiningOptions to upper package
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Fix coinbase for *bft
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Implement methods to get and set min priority fee
    
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
    
    * Fix spotless
    
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
    
    * Apply suggestions from code review
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Update besu/src/main/java/org/hyperledger/besu/cli/BesuCommand.java
    
    [skip-ci]
    
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    
    * Add new config option to everything config
    
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
    
    * Fix unit test
    
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
    
    * Accept PR suggestions
    
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
    
    ---------
    
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
    Signed-off-by: Stefan Pingel <16143240+pinges@users.noreply.github.com>
    Co-authored-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>
    Co-authored-by: Stefan Pingel <16143240+pinges@users.noreply.github.com>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/JsonRpcHttpServiceLoginTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/JsonRpcHttpServiceLoginTest.java
@@ -35,6 +35,7 @@ import org.hyperledger.besu.ethereum.api.jsonrpc.methods.JsonRpcMethodsFactory;
 import org.hyperledger.besu.ethereum.api.jsonrpc.websocket.WebSocketConfiguration;
 import org.hyperledger.besu.ethereum.api.query.BlockchainQueries;
 import org.hyperledger.besu.ethereum.blockcreation.PoWMiningCoordinator;
+import org.hyperledger.besu.ethereum.core.MiningParameters;
 import org.hyperledger.besu.ethereum.core.PrivacyParameters;
 import org.hyperledger.besu.ethereum.core.Synchronizer;
 import org.hyperledger.besu.ethereum.eth.EthProtocol;
@@ -139,6 +140,7 @@ public class JsonRpcHttpServiceLoginTest {
                     mock(ProtocolContext.class),
                     mock(FilterManager.class),
                     mock(TransactionPool.class),
+                    mock(MiningParameters.class),
                     mock(PoWMiningCoordinator.class),
                     new NoOpMetricsSystem(),
                     supportedCapabilities,
