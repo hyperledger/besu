af011d2a0faa1876c43728b21e695e501b6f0af7
Danno Ferrin <danno.ferrin@swirldslabs.com>
2023-10-24 23:11:15 -0600

    Use Bytes Trie to track warm addresses (#6069)
    * Use Bytes Trie to track warm addresses
    
    Move from a java HashSet to a custom Trie based on bytes to store the
    warm addresses, creates, and self-destructs.
    
    This avoids needing to calculate java hashes or engage in using custom
    Comparators.
    
    Signed-off-by: Danno Ferrin <danno.ferrin@swirldslabs.com>
    
    * codeql scan
    
    Signed-off-by: Danno Ferrin <danno.ferrin@swirldslabs.com>
    
    ---------
    
    Signed-off-by: Danno Ferrin <danno.ferrin@swirldslabs.com>
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>

--- evm/src/main/java/org/hyperledger/besu/evm/fluent/EVMExecutor.java
+++ evm/src/main/java/org/hyperledger/besu/evm/fluent/EVMExecutor.java
@@ -16,6 +16,7 @@ package org.hyperledger.besu.evm.fluent;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import org.hyperledger.besu.collections.trie.BytesTrieSet;
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.datatypes.VersionedHash;
@@ -41,7 +42,6 @@ import org.hyperledger.besu.evm.worldstate.WorldUpdater;
 import java.math.BigInteger;
 import java.util.Collection;
 import java.util.Deque;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -80,7 +80,7 @@ public class EVMExecutor {
       List.of(MaxCodeSizeRule.of(0x6000), PrefixCodeRule.of());
   private long initialNonce = 1;
   private Collection<Address> forceCommitAddresses = List.of(Address.fromHexString("0x03"));
-  private Set<Address> accessListWarmAddresses = new HashSet<>();
+  private Set<Address> accessListWarmAddresses = new BytesTrieSet<>(Address.SIZE);
   private Multimap<Address, Bytes32> accessListWarmStorage = HashMultimap.create();
   private MessageCallProcessor messageCallProcessor = null;
   private ContractCreationProcessor contractCreationProcessor = null;
