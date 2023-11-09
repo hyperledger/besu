a3711f3edb224255148b28b7444e7a7b0db95283
MAKHONIN Aleksey M <Aleksey.MAKHONIN@raiffeisen.ru>
2023-11-09 10:32:55 +0300

    Fix tests
    Signed-off-by: MAKHONIN Aleksey M <Aleksey.MAKHONIN@raiffeisen.ru>

--- besu/src/test/java/org/hyperledger/besu/cli/BesuCommandTest.java
+++ besu/src/test/java/org/hyperledger/besu/cli/BesuCommandTest.java
@@ -1969,7 +1969,7 @@ public class BesuCommandTest extends CommandTestAbstract {
     assertThat(commandOutput.toString(UTF_8)).isEmpty();
     assertThat(commandErrorOutput.toString(UTF_8))
         .contains(
-            "The `--Xnat-kube-service-name` parameter is only used in kubernetes mode. Either remove --Xnat-kube-service-name or select the KUBERNETES mode (via --nat--method=KUBERNETES)");
+            "The `--Xnat-kube-service-name` parameter is only used in kubernetes mode. Either remove --Xnat-kube-service-name or select the KUBERNETES mode (via --nat-method=KUBERNETES)");
   }
 
   @Test
@@ -1979,7 +1979,7 @@ public class BesuCommandTest extends CommandTestAbstract {
     assertThat(commandOutput.toString(UTF_8)).isEmpty();
     assertThat(commandErrorOutput.toString(UTF_8))
         .contains(
-            "The `--Xnat-kube-service-name` parameter is only used in kubernetes mode. Either remove --Xnat-kube-service-name or select the KUBERNETES mode (via --nat--method=KUBERNETES)");
+            "The `--Xnat-kube-service-name` parameter is only used in kubernetes mode. Either remove --Xnat-kube-service-name or select the KUBERNETES mode (via --nat-method=KUBERNETES)");
   }
 
   @Test
