101dc5fe422e8182c4b436943b1325b957892cbd
Fabio Di Fabio <fabio.difabio@consensys.net>
2023-11-30 09:53:19 +0100

    Update ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/transactions/NewPooledTransactionHashesMessageProcessor.java
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/transactions/NewPooledTransactionHashesMessageProcessor.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/transactions/NewPooledTransactionHashesMessageProcessor.java
@@ -87,7 +87,7 @@ public class NewPooledTransactionHashesMessageProcessor {
       LOG.atTrace()
           .setMessage(
               "Received pooled transaction hashes message from {}... incoming hashes {}, incoming list {}")
-          .addArgument(peer == null ? null : peer.getShortNodeId())
+          .addArgument(() -> peer == null ? null : peer.getShortNodeId())
           .addArgument(incomingTransactionHashes::size)
           .addArgument(incomingTransactionHashes)
           .log();
