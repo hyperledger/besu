9d579275726225835278ca51666c84e1c25205ca
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2023-12-04 15:12:51 +1100

    New cli options to limit rewards return by eth_feeHistory  (#6202)
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- consensus/common/src/main/java/org/hyperledger/besu/consensus/common/bft/blockcreation/BftBlockCreatorFactory.java
+++ consensus/common/src/main/java/org/hyperledger/besu/consensus/common/bft/blockcreation/BftBlockCreatorFactory.java
@@ -148,6 +148,15 @@ public class BftBlockCreatorFactory<T extends BftConfigOptions> {
   }
 
   /**
+   * Gets min priority fee per gas
+   *
+   * @return min priority fee per gas
+   */
+  public Wei getMinPriorityFeePerGas() {
+    return miningParameters.getMinPriorityFeePerGas();
+  }
+
+  /**
    * Create extra data bytes.
    *
    * @param round the round
