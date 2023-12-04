c9ffc961143d389527312139cf06fd24c6fa5a6e
David Lutzardo <jdlutzardo@izertis.com>
2023-12-04 12:24:54 +0100

    Add test to check empty login and check response in body is not empty
    Signed-off-by: David Lutzardo <jdlutzardo@izertis.com>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/JsonRpcHttpServiceLoginTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/JsonRpcHttpServiceLoginTest.java
@@ -204,6 +204,20 @@ public class JsonRpcHttpServiceLoginTest {
   }
 
   @Test
+  public void loginWithEmptyCredentials() throws IOException {
+    final RequestBody body =
+            RequestBody.create("{}", JSON);
+    final Request request = new Request.Builder().post(body).url(baseUrl + "/login").build();
+    try (final Response resp = client.newCall(request).execute()) {
+      assertThat(resp.code()).isEqualTo(400);
+      assertThat(resp.message()).isEqualTo("Bad Request");
+      final String bodyString = resp.body().string();
+      assertThat(bodyString).isNotNull();
+      assertThat(bodyString).isNotBlank();
+    }
+  }
+
+  @Test
   public void loginWithBadCredentials() throws IOException {
     final RequestBody body =
         RequestBody.create("{\"username\":\"user\",\"password\":\"badpass\"}", JSON);
@@ -211,6 +225,9 @@ public class JsonRpcHttpServiceLoginTest {
     try (final Response resp = client.newCall(request).execute()) {
       assertThat(resp.code()).isEqualTo(401);
       assertThat(resp.message()).isEqualTo("Unauthorized");
+      final String bodyString = resp.body().string();
+      assertThat(bodyString).isNotNull();
+      assertThat(bodyString).isNotBlank();
     }
   }
 
