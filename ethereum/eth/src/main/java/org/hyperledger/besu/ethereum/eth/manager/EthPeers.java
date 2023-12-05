495ff7dc17478752381bc8a9188ca58a2d4c9a62
Stefan Pingel <16143240+pinges@users.noreply.github.com>
2023-12-05 16:17:54 +1000

    revert (#6232)
    Signed-off-by: stefan.pingel@consensys.net <stefan.pingel@consensys.net>
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthPeers.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthPeers.java
@@ -197,7 +197,7 @@ public class EthPeers {
         disconnectCallbacks.forEach(callback -> callback.onDisconnect(peer));
         peer.handleDisconnect();
         abortPendingRequestsAssignedToDisconnectedPeers();
-        LOG.debug("Disconnected EthPeer {}...", peer.getShortNodeId());
+        LOG.debug("Disconnected EthPeer {}", peer.getShortNodeId());
         LOG.trace("Disconnected EthPeer {}", peer);
       }
     }
@@ -391,7 +391,7 @@ public class EthPeers {
             peer -> {
               LOG.atDebug()
                   .setMessage(
-                      "disconnecting peer {}... Waiting for better peers. Current {} of max {}")
+                      "disconnecting peer {}. Waiting for better peers. Current {} of max {}")
                   .addArgument(peer::getShortNodeId)
                   .addArgument(this::peerCount)
                   .addArgument(this::getMaxPeers)
