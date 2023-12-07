bd2cede4361dc1967de7e5fa0e409f00bf2933db
Sally MacFarlane <macfarla.github@gmail.com>
2023-12-07 11:07:07 +1000

    imports
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- consensus/common/src/test/java/org/hyperledger/besu/consensus/common/MigratingMiningCoordinatorTest.java
+++ consensus/common/src/test/java/org/hyperledger/besu/consensus/common/MigratingMiningCoordinatorTest.java
@@ -46,8 +46,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 
 @ExtendWith(MockitoExtension.class)
 public class MigratingMiningCoordinatorTest {
