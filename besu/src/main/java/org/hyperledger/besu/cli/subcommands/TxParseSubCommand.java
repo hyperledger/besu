a243c82a11c3e33808a843b4eddb08ce7094681d
garyschulte <garyschulte@gmail.com>
2023-12-08 22:44:01 -0800

    add chain id and signature checks
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- besu/src/main/java/org/hyperledger/besu/cli/subcommands/TxParseSubCommand.java
+++ besu/src/main/java/org/hyperledger/besu/cli/subcommands/TxParseSubCommand.java
@@ -18,12 +18,14 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.hyperledger.besu.cli.subcommands.TxParseSubCommand.COMMAND_NAME;
 
 import org.hyperledger.besu.cli.util.VersionProvider;
+import org.hyperledger.besu.crypto.SignatureAlgorithmFactory;
 import org.hyperledger.besu.ethereum.core.encoding.EncodingContext;
 import org.hyperledger.besu.ethereum.core.encoding.TransactionDecoder;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
@@ -37,10 +39,9 @@ import picocli.CommandLine;
  */
 @CommandLine.Command(
     name = COMMAND_NAME,
-    description = "Execute an Eth ereum State Test.",
+    description = "Execute an Ethereum State Test.",
     mixinStandardHelpOptions = true,
-    versionProvider = VersionProvider.class,
-    subcommands = {})
+    versionProvider = VersionProvider.class)
 public class TxParseSubCommand implements Runnable {
 
   /** The constant COMMAND_NAME. */
@@ -52,6 +53,8 @@ public class TxParseSubCommand implements Runnable {
       arity = "1..1",
       description = "file to read transaction data lines from, otherwise defaults to stdin")
   private String corpusFile = null;
+  static final BigInteger halfCurveOrder = SignatureAlgorithmFactory.getInstance().getHalfCurveOrder();
+  static final BigInteger chainId = new BigInteger("1", 10);
 
   private final PrintWriter out;
 
@@ -100,7 +103,19 @@ public class TxParseSubCommand implements Runnable {
 
   void dump(final Bytes tx) {
     try {
-      out.println(TransactionDecoder.decodeOpaqueBytes(tx, EncodingContext.BLOCK_BODY).getSender());
+      var transaction = TransactionDecoder.decodeOpaqueBytes(tx, EncodingContext.BLOCK_BODY);
+
+      // https://github.com/hyperledger/besu/blob/5fe49c60b30fe2954c7967e8475c3b3e9afecf35/ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/MainnetTransactionValidator.java#L252
+      if (transaction.getChainId().isPresent() && !transaction.getChainId().get().equals(chainId) ){
+        throw new Exception("wrong chain id");
+      }
+
+      // https://github.com/hyperledger/besu/blob/5fe49c60b30fe2954c7967e8475c3b3e9afecf35/ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/MainnetTransactionValidator.java#L270
+      if (transaction.getS().compareTo(halfCurveOrder) > 0) {
+        throw new Exception("signature s out of range");
+      }
+      out.println(transaction.getSender());
+
     } catch (Exception ex) {
       err(ex.getMessage());
     }
