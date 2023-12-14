c8bcf49df7270cd1a66fa0d5c13d605d8df76b2f
Sally MacFarlane <macfarla.github@gmail.com>
2023-12-14 22:23:56 +1000

    [MINOR] CLI Subcommands migrate to junit 5 (#6282)
    * updated cli and chainimport/export tests to junit 5
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    * subcommands and besucommand test to junit5
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    * removed duplicate line
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    * removed vintage junit dep
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    ---------
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    Co-authored-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    Signed-off-by: jflo <justin+github@florentine.us>

--- besu/src/test/java/org/hyperledger/besu/cli/options/NetworkingOptionsTest.java
+++ besu/src/test/java/org/hyperledger/besu/cli/options/NetworkingOptionsTest.java
@@ -23,11 +23,11 @@ import org.hyperledger.besu.ethereum.p2p.config.NetworkingConfiguration;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class NetworkingOptionsTest
     extends AbstractCLIOptionsTest<NetworkingConfiguration, NetworkingOptions> {
 
