df2d89a9f92f0d014e82fed971e754917fe2f282
garyschulte <garyschulte@gmail.com>
2024-01-03 13:01:18 -0800

    use hyperledger main behavior for accumulator get account (#28)
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/bonsai/worldview/BonsaiWorldStateUpdateAccumulator.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/bonsai/worldview/BonsaiWorldStateUpdateAccumulator.java
@@ -109,7 +109,7 @@ public class BonsaiWorldStateUpdateAccumulator
 
   @Override
   public Account get(final Address address) {
-    return super.getAccount(address);
+    return super.get(address);
   }
 
   @Override
