09077ad97176e9661788bfc62c4f4301809917f3
Karim Taam <karim.t2am@gmail.com>
2024-01-10 14:10:45 +0100

    add missing file
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

---
+++ ethereum/blockcreation/src/main/java/org/hyperledger/besu/ethereum/blockcreation/NoopMiningCoordinator.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.hyperledger.besu.ethereum.blockcreation;
+
+import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.datatypes.Wei;
+import org.hyperledger.besu.ethereum.core.Block;
+import org.hyperledger.besu.ethereum.core.BlockHeader;
+import org.hyperledger.besu.ethereum.core.MiningParameters;
+import org.hyperledger.besu.ethereum.core.Transaction;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.tuweni.bytes.Bytes;
+
+public class NoopMiningCoordinator implements MiningCoordinator {
+
+  private final MiningParameters miningParameters;
+
+  public NoopMiningCoordinator(final MiningParameters miningParameters) {
+    this.miningParameters = miningParameters;
+  }
+
+  @Override
+  public void start() {}
+
+  @Override
+  public void stop() {}
+
+  @Override
+  public void awaitStop() {}
+
+  @Override
+  public boolean enable() {
+    return false;
+  }
+
+  @Override
+  public boolean disable() {
+    return true;
+  }
+
+  @Override
+  public boolean isMining() {
+    return false;
+  }
+
+  @Override
+  public Wei getMinTransactionGasPrice() {
+    return miningParameters.getMinTransactionGasPrice();
+  }
+
+  @Override
+  public Wei getMinPriorityFeePerGas() {
+    return miningParameters.getMinPriorityFeePerGas();
+  }
+
+  @Override
+  public void setExtraData(final Bytes extraData) {}
+
+  @Override
+  public Optional<Address> getCoinbase() {
+    return miningParameters.getCoinbase();
+  }
+
+  @Override
+  public Optional<Block> createBlock(
+      final BlockHeader parentHeader,
+      final List<Transaction> transactions,
+      final List<BlockHeader> ommers) {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<Block> createBlock(final BlockHeader parentHeader, final long timestamp) {
+    return Optional.empty();
+  }
+
+  @Override
+  public void changeTargetGasLimit(final Long targetGasLimit) {}
+}
