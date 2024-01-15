8fde4746608540619d0e22de0c0ca4caeeb52e4e
Sally MacFarlane <macfarla.github@gmail.com>
2024-01-15 12:26:21 +1000

    rename SnapsyncMetricsManager
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/sync/snapsync/PersistDataStepTest.java
+++ ethereum/eth/src/test/java/org/hyperledger/besu/ethereum/eth/sync/snapsync/PersistDataStepTest.java
@@ -50,7 +50,7 @@ public class PersistDataStepTest {
 
   @BeforeEach
   public void setUp() {
-    when(downloadState.getMetricsManager()).thenReturn(mock(SnapsyncMetricsManager.class));
+    when(downloadState.getMetricsManager()).thenReturn(mock(SnapSyncMetricsManager.class));
   }
 
   @Test
