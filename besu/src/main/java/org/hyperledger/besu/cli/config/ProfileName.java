5d1c57eb734a8e470a7f26dbb083b2162c436d66
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2024-01-23 15:08:54 +1100

    PR requests
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- besu/src/main/java/org/hyperledger/besu/cli/config/ProfileName.java
+++ besu/src/main/java/org/hyperledger/besu/cli/config/ProfileName.java
@@ -16,17 +16,11 @@ package org.hyperledger.besu.cli.config;
 
 /** Enum for profile names. Each profile corresponds to a configuration file. */
 public enum ProfileName {
-  /**
-   * The 'ENTERPRISE' profile. Corresponds to the 'profiles/enterprise-private.toml' configuration
-   * file.
-   */
+  /** The 'ENTERPRISE' profile */
   ENTERPRISE("profiles/enterprise-private.toml"),
-  /**
-   * The 'PRIVATE' profile. Corresponds to the 'profiles/enterprise-private.toml' configuration
-   * file.
-   */
+  /** The 'PRIVATE' profile */
   PRIVATE("profiles/enterprise-private.toml"),
-  /** The 'DEV' profile. Corresponds to the 'profiles/dev.toml' configuration file. */
+  /** The 'DEV' profile. */
   DEV("profiles/dev.toml");
 
   private final String configFile;
