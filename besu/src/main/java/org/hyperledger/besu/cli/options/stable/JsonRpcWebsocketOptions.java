c4963da11e8bbcb89dc969401c6cbc4feb51d513
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2024-01-25 09:42:01 +1100

    Move websocket options to own class
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

---
+++ besu/src/main/java/org/hyperledger/besu/cli/options/stable/JsonRpcWebsocketOptions.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright Hyperledger Besu Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.cli.options.stable;
+
+import static java.util.Arrays.asList;
+import static org.hyperledger.besu.ethereum.api.jsonrpc.RpcApis.DEFAULT_RPC_APIS;
+import static org.hyperledger.besu.ethereum.api.jsonrpc.RpcApis.VALID_APIS;
+import static org.hyperledger.besu.ethereum.api.jsonrpc.websocket.WebSocketConfiguration.DEFAULT_WEBSOCKET_PORT;
+
+import org.hyperledger.besu.cli.DefaultCommandValues;
+import org.hyperledger.besu.cli.custom.RpcAuthFileValidator;
+import org.hyperledger.besu.cli.util.CommandLineUtils;
+import org.hyperledger.besu.ethereum.api.jsonrpc.RpcMethod;
+import org.hyperledger.besu.ethereum.api.jsonrpc.authentication.JwtAlgorithm;
+import org.hyperledger.besu.ethereum.api.jsonrpc.websocket.WebSocketConfiguration;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import com.google.common.base.Strings;
+import org.slf4j.Logger;
+import picocli.CommandLine;
+
+public class JsonRpcWebsocketOptions {
+  @CommandLine.Option(
+      names = {"--rpc-ws-authentication-jwt-algorithm"},
+      description =
+          "Encryption algorithm used for Websockets JWT public key. Possible values are ${COMPLETION-CANDIDATES}"
+              + " (default: ${DEFAULT-VALUE})",
+      arity = "1")
+  private final JwtAlgorithm rpcWebsocketsAuthenticationAlgorithm =
+      DefaultCommandValues.DEFAULT_JWT_ALGORITHM;
+
+  @CommandLine.Option(
+      names = {"--rpc-ws-enabled"},
+      description = "Set to start the JSON-RPC WebSocket service (default: ${DEFAULT-VALUE})")
+  private final Boolean isRpcWsEnabled = false;
+
+  @SuppressWarnings({"FieldCanBeFinal", "FieldMayBeFinal"}) // PicoCLI requires non-final Strings.
+  @CommandLine.Option(
+      names = {"--rpc-ws-host"},
+      paramLabel = DefaultCommandValues.MANDATORY_HOST_FORMAT_HELP,
+      description = "Host for JSON-RPC WebSocket service to listen on (default: ${DEFAULT-VALUE})",
+      arity = "1")
+  private String rpcWsHost;
+
+  @CommandLine.Option(
+      names = {"--rpc-ws-port"},
+      paramLabel = DefaultCommandValues.MANDATORY_PORT_FORMAT_HELP,
+      description = "Port for JSON-RPC WebSocket service to listen on (default: ${DEFAULT-VALUE})",
+      arity = "1")
+  private final Integer rpcWsPort = DEFAULT_WEBSOCKET_PORT;
+
+  @CommandLine.Option(
+      names = {"--rpc-ws-max-frame-size"},
+      description =
+          "Maximum size in bytes for JSON-RPC WebSocket frames (default: ${DEFAULT-VALUE}). If this limit is exceeded, the websocket will be disconnected.",
+      arity = "1")
+  private final Integer rpcWsMaxFrameSize = DefaultCommandValues.DEFAULT_WS_MAX_FRAME_SIZE;
+
+  @CommandLine.Option(
+      names = {"--rpc-ws-max-active-connections"},
+      description =
+          "Maximum number of WebSocket connections allowed for JSON-RPC (default: ${DEFAULT-VALUE}). Once this limit is reached, incoming connections will be rejected.",
+      arity = "1")
+  private final Integer rpcWsMaxConnections = DefaultCommandValues.DEFAULT_WS_MAX_CONNECTIONS;
+
+  @CommandLine.Option(
+      names = {"--rpc-ws-api", "--rpc-ws-apis"},
+      paramLabel = "<api name>",
+      split = " {0,1}, {0,1}",
+      arity = "1..*",
+      description =
+          "Comma separated list of APIs to enable on JSON-RPC WebSocket service (default: ${DEFAULT-VALUE})")
+  private final List<String> rpcWsApis = DEFAULT_RPC_APIS;
+
+  @CommandLine.Option(
+      names = {"--rpc-ws-api-methods-no-auth", "--rpc-ws-api-method-no-auth"},
+      paramLabel = "<api name>",
+      split = " {0,1}, {0,1}",
+      arity = "1..*",
+      description =
+          "Comma separated list of RPC methods to exclude from RPC authentication services, RPC WebSocket authentication must be enabled")
+  private final List<String> rpcWsApiMethodsNoAuth = new ArrayList<String>();
+
+  @CommandLine.Option(
+      names = {"--rpc-ws-authentication-enabled"},
+      description =
+          "Require authentication for the JSON-RPC WebSocket service (default: ${DEFAULT-VALUE})")
+  private final Boolean isRpcWsAuthenticationEnabled = false;
+
+  @SuppressWarnings({"FieldCanBeFinal", "FieldMayBeFinal"}) // PicoCLI requires non-final Strings.
+  @CommandLine.Option(
+      names = {"--rpc-ws-authentication-credentials-file"},
+      paramLabel = DefaultCommandValues.MANDATORY_FILE_FORMAT_HELP,
+      description =
+          "Storage file for JSON-RPC WebSocket authentication credentials (default: ${DEFAULT-VALUE})",
+      arity = "1")
+  private String rpcWsAuthenticationCredentialsFile = null;
+
+  @CommandLine.Option(
+      names = {"--rpc-ws-authentication-jwt-public-key-file"},
+      paramLabel = DefaultCommandValues.MANDATORY_FILE_FORMAT_HELP,
+      description = "JWT public key file for JSON-RPC WebSocket authentication",
+      arity = "1")
+  private final File rpcWsAuthenticationPublicKeyFile = null;
+
+  public void validateRpcOptionsParams(
+      final Logger logger, final CommandLine commandLine, final Predicate<String> configuredApis) {
+    if (!rpcWsApis.stream().allMatch(configuredApis)) {
+      final List<String> invalidWsApis = new ArrayList<String>(rpcWsApis);
+      invalidWsApis.removeAll(VALID_APIS);
+      throw new CommandLine.ParameterException(
+          commandLine,
+          "Invalid value for option '--rpc-ws-api': invalid entries found " + invalidWsApis);
+    }
+
+    final boolean validWsApiMethods =
+        rpcWsApiMethodsNoAuth.stream().allMatch(RpcMethod::rpcMethodExists);
+
+    if (!validWsApiMethods) {
+      throw new CommandLine.ParameterException(
+          commandLine,
+          "Invalid value for option '--rpc-ws-api-methods-no-auth', options must be valid RPC methods");
+    }
+
+    CommandLineUtils.checkOptionDependencies(
+        logger,
+        commandLine,
+        "--rpc-ws-enabled",
+        !isRpcWsEnabled,
+        asList(
+            "--rpc-ws-api",
+            "--rpc-ws-apis",
+            "--rpc-ws-api-method-no-auth",
+            "--rpc-ws-api-methods-no-auth",
+            "--rpc-ws-host",
+            "--rpc-ws-port",
+            "--rpc-ws-max-frame-size",
+            "--rpc-ws-max-active-connections",
+            "--rpc-ws-authentication-enabled",
+            "--rpc-ws-authentication-credentials-file",
+            "--rpc-ws-authentication-public-key-file",
+            "--rpc-ws-authentication-jwt-algorithm"));
+
+    if (isRpcWsAuthenticationEnabled) {
+      CommandLineUtils.checkOptionDependencies(
+          logger,
+          commandLine,
+          "--rpc-ws-authentication-public-key-file",
+          rpcWsAuthenticationPublicKeyFile == null,
+          asList("--rpc-ws-authentication-jwt-algorithm"));
+    }
+
+    if (isRpcWsAuthenticationEnabled
+        && rpcWsAuthenticationCredentialsFile(commandLine) == null
+        && rpcWsAuthenticationPublicKeyFile == null) {
+      throw new CommandLine.ParameterException(
+          commandLine,
+          "Unable to authenticate JSON-RPC WebSocket endpoint without a supplied credentials file or authentication public key file");
+    }
+  }
+
+  private String rpcWsAuthenticationCredentialsFile(final CommandLine commandLine) {
+    final String filename = rpcWsAuthenticationCredentialsFile;
+
+    if (filename != null) {
+      RpcAuthFileValidator.validate(commandLine, filename, "WS");
+    }
+    return filename;
+  }
+
+  public WebSocketConfiguration webSocketConfiguration(
+      final List<String> allowCallsFrom, final String hostAddress, final Long wsTimoutSec) {
+    final WebSocketConfiguration webSocketConfiguration = WebSocketConfiguration.createDefault();
+    webSocketConfiguration.setEnabled(isRpcWsEnabled);
+    webSocketConfiguration.setHost(Strings.isNullOrEmpty(rpcWsHost) ? hostAddress : rpcWsHost);
+    webSocketConfiguration.setPort(rpcWsPort);
+    webSocketConfiguration.setMaxFrameSize(rpcWsMaxFrameSize);
+    webSocketConfiguration.setMaxActiveConnections(rpcWsMaxConnections);
+    webSocketConfiguration.setRpcApis(rpcWsApis);
+    webSocketConfiguration.setRpcApisNoAuth(
+        rpcWsApiMethodsNoAuth.stream().distinct().collect(Collectors.toList()));
+    webSocketConfiguration.setAuthenticationEnabled(isRpcWsAuthenticationEnabled);
+    webSocketConfiguration.setAuthenticationCredentialsFile(rpcWsAuthenticationCredentialsFile);
+    webSocketConfiguration.setHostsAllowlist(allowCallsFrom);
+    webSocketConfiguration.setAuthenticationPublicKeyFile(rpcWsAuthenticationPublicKeyFile);
+    webSocketConfiguration.setAuthenticationAlgorithm(rpcWebsocketsAuthenticationAlgorithm);
+    webSocketConfiguration.setTimeoutSec(wsTimoutSec);
+    return webSocketConfiguration;
+  }
+
+  public List<String> getRpcWsApis() {
+    return rpcWsApis;
+  }
+
+  public Boolean isRpcWsEnabled() {
+    return isRpcWsEnabled;
+  }
+
+  public Integer getRpcWsPort() {
+    return rpcWsPort;
+  }
+}
--- besu/src/test/java/org/hyperledger/besu/cli/options/AbstractCLIOptionsTest.java
+++ besu/src/test/java/org/hyperledger/besu/cli/options/AbstractCLIOptionsTest.java
@@ -16,8 +16,6 @@ package org.hyperledger.besu.cli.options;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hyperledger.besu.cli.util.CommandLineUtils.DEPENDENCY_WARNING_MSG;
-import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.verify;
 
 import org.hyperledger.besu.cli.CommandTestAbstract;
@@ -124,30 +122,5 @@ public abstract class AbstractCLIOptionsTest<D, T extends CLIOptions<D>>
 
     assertThat(commandOutput.toString(UTF_8)).isEmpty();
     assertThat(commandErrorOutput.toString(UTF_8)).contains(errorMsg);
-  }
-
-  /**
-   * Check logger calls
-   *
-   * <p>Here we check the calls to logger and not the result of the log line as we don't test the
-   * logger itself but the fact that we call it.
-   *
-   * @param dependentOptions the string representing the list of dependent options names
-   * @param mainOption the main option name
-   */
-  protected void verifyOptionsConstraintLoggerCall(
-      final String mainOption, final String... dependentOptions) {
-    verify(mockLogger, atLeast(1))
-        .warn(
-            stringArgumentCaptor.capture(),
-            stringArgumentCaptor.capture(),
-            stringArgumentCaptor.capture());
-    assertThat(stringArgumentCaptor.getAllValues().get(0)).isEqualTo(DEPENDENCY_WARNING_MSG);
-
-    for (final String option : dependentOptions) {
-      assertThat(stringArgumentCaptor.getAllValues().get(1)).contains(option);
-    }
-
-    assertThat(stringArgumentCaptor.getAllValues().get(2)).isEqualTo(mainOption);
   }
 }
