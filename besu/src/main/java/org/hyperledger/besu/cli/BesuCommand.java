09947d9c6b60ad05b67caa5d47ed26e5355fe388
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2024-01-30 11:41:08 +1100

    Remove deprecated option
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- besu/src/main/java/org/hyperledger/besu/cli/BesuCommand.java
+++ besu/src/main/java/org/hyperledger/besu/cli/BesuCommand.java
@@ -610,14 +610,6 @@ public class BesuCommand implements DefaultCommandValues, Runnable {
         paramLabel = MANDATORY_FILE_FORMAT_HELP,
         description = "Path to file containing shared secret key for JWT signature verification")
     private final Path engineJwtKeyFile = null;
-
-    @Option(
-        names = {"--engine-jwt-enabled"},
-        description = "deprecated option, engine jwt auth is enabled by default",
-        hidden = true)
-    @SuppressWarnings({"FieldCanBeFinal", "UnusedVariable"})
-    private final Boolean deprecatedIsEngineAuthEnabled = true;
-
     @Option(
         names = {"--engine-jwt-disabled"},
         description = "Disable authentication for Engine APIs (default: ${DEFAULT-VALUE})")
