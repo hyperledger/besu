586ae9f6a5b27ec48c828da61599b5334dbc069f
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2024-03-07 15:16:16 +1100

    Simplify
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/plugins/PluginInfo.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/plugins/PluginInfo.java
@@ -1,46 +1,23 @@
 package org.hyperledger.besu.ethereum.core.plugins;
+
 /** Represents information about a plugin, including its name. */
-public record PluginInfo(String name) {
+public final class PluginInfo {
+  private final String name;
+
   /**
    * Constructs a new PluginInfo instance with the specified name.
    *
    * @param name The name of the plugin. Cannot be null or empty.
    * @throws IllegalArgumentException if the name is null or empty.
    */
-  public PluginInfo {
+  public PluginInfo(final String name) {
     if (name == null || name.trim().isEmpty()) {
       throw new IllegalArgumentException("Plugin name cannot be null or empty.");
     }
+    this.name = name;
   }
 
-  /**
-   * Returns the name of the plugin.
-   *
-   * @return The name of the plugin.
-   */
-  @Override
   public String name() {
     return name;
-  }
-
-  // Optionally, override toString, equals, and hashCode methods
-
-  @Override
-  public String toString() {
-    return "PluginInfo{name='" + name + "'}";
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-
-    PluginInfo that = (PluginInfo) o;
-
-    return name.equals(that.name);
   }
 }
