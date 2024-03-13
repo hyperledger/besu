cca7490ff4ba7f3f6c3b2401485364fc6e59a382
Usman Saleem <usman@usmans.info>
2024-03-14 09:48:18 +1000

    Refactor AbstractGasLimitSpecification
    Simplify isValidTargetGasLimit bound condition
    
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/AbstractGasLimitSpecification.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/AbstractGasLimitSpecification.java
@@ -40,7 +40,14 @@ public abstract class AbstractGasLimitSpecification {
     return Long.divideUnsigned(currentGasLimit, GAS_LIMIT_BOUND_DIVISOR);
   }
 
+  /**
+   * Verify that the target gas limit is within the allowed bounds.
+   *
+   * @param targetGasLimit the target gas limit to validate
+   * @return true if within bounds
+   */
   public static boolean isValidTargetGasLimit(final long targetGasLimit) {
-    return DEFAULT_MIN_GAS_LIMIT <= targetGasLimit && DEFAULT_MAX_GAS_LIMIT >= targetGasLimit;
+    // only check the lower bound as the upper bound is Long.MAX_VALUE
+    return targetGasLimit >= DEFAULT_MIN_GAS_LIMIT;
   }
 }
