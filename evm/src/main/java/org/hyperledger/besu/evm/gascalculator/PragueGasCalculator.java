95cf47a4afb3d83143c2e060a8f65f1aea44d9cd
Karim Taam <karim.t2am@gmail.com>
2024-03-14 11:30:35 +0100

    remove cold gas cost
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- evm/src/main/java/org/hyperledger/besu/evm/gascalculator/PragueGasCalculator.java
+++ evm/src/main/java/org/hyperledger/besu/evm/gascalculator/PragueGasCalculator.java
@@ -41,4 +41,14 @@ public class PragueGasCalculator extends CancunGasCalculator {
   protected PragueGasCalculator(final int maxPrecompile) {
     super(maxPrecompile);
   }
+
+  @Override
+  public long getColdSloadCost() {
+    return 0; // no cold gas cost after verkle
+  }
+
+  @Override
+  public long getColdAccountAccessCost() {
+    return 0; // no cold gas cost after verkle
+  }
 }
