5db31e2dfefdd7c1d86370a50cc8466f8f7b5e3b
Matthew Whitehead <matthew1001@gmail.com>
2024-03-19 19:48:23 +0000

    Unit tests missing mock
    Signed-off-by: Matthew Whitehead <matthew1001@gmail.com>

--- consensus/ibft/src/test/java/org/hyperledger/besu/consensus/ibft/statemachine/IbftRoundTest.java
+++ consensus/ibft/src/test/java/org/hyperledger/besu/consensus/ibft/statemachine/IbftRoundTest.java
@@ -139,6 +139,10 @@ public class IbftRoundTest {
         .when(blockImporter.importBlock(any(), any(), any()))
         .thenReturn(new BlockImportResult(BlockImportResult.BlockImportStatus.IMPORTED));
 
+    lenient()
+        .when(protocolSchedule.getByBlockNumberOrTimestamp(anyLong(), anyLong()))
+        .thenReturn(protocolSpec);
+
     subscribers.subscribe(minedBlockObserver);
   }
 
