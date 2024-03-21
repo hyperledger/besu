8c298b484d603ecd20b5f14cd380349e343cb143
Justin Florentine <justin+github@florentine.us>
2024-03-21 09:53:38 -0400

    new engine api methods
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

---
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineGetInclusionListV1.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright Hyperledger Besu contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.engine;
+
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.JsonRpcRequestContext;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.JsonRpcMethod;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcResponse;
+
+public class EngineGetInclusionListV1 implements JsonRpcMethod {
+    @Override
+    public String getName() {
+        return "engine_getInclusionListV1";
+    }
+
+    @Override
+    public JsonRpcResponse response(final JsonRpcRequestContext request) {
+        return null;
+    }
+}
