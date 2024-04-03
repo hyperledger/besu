f74f0ffa84c0f7e2f91240e29b1db71547bcac0b
Sally MacFarlane <macfarla.github@gmail.com>
2024-04-03 18:17:44 +1000

    tostring
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- ethereum/p2p/src/main/java/org/hyperledger/besu/ethereum/p2p/rlpx/handshake/ecies/InitiatorHandshakeMessageV4.java
+++ ethereum/p2p/src/main/java/org/hyperledger/besu/ethereum/p2p/rlpx/handshake/ecies/InitiatorHandshakeMessageV4.java
@@ -120,4 +120,9 @@ public final class InitiatorHandshakeMessageV4 implements InitiatorHandshakeMess
   public Bytes32 getEphPubKeyHash() {
     return ephPubKeyHash;
   }
+
+  @Override
+  public String toString() {
+    return "InitiatorHandshakeMessageV4{" + "pubKey=" + pubKey + ", nonce=" + nonce + '}';
+  }
 }
