06cf9a49ee897eba43c894d185cc2b0f4ff0bb1d
Usman Saleem <usman@usmans.info>
2024-04-30 20:46:01 +1000

    Update License headers
     -- Update license header to pass spotlessCheck task
    
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/diffbased/common/trielog/TrieLogLayer.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/diffbased/common/trielog/TrieLogLayer.java
@@ -11,9 +11,7 @@
  * specific language governing permissions and limitations under the License.
  *
  * SPDX-License-Identifier: Apache-2.0
- *
  */
-
 package org.hyperledger.besu.ethereum.trie.diffbased.common.trielog;
 
 import static com.google.common.base.Preconditions.checkState;
