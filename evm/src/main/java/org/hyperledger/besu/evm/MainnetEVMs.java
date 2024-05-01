5c10263b783baf5bc9127f6fe4819b5de85b92bf
garyschulte <garyschulte@gmail.com>
2024-05-01 15:44:53 -0700

    use non-EOF evm spec version for eip4762 fork
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- evm/src/main/java/org/hyperledger/besu/evm/MainnetEVMs.java
+++ evm/src/main/java/org/hyperledger/besu/evm/MainnetEVMs.java
@@ -1103,7 +1103,7 @@ public class MainnetEVMs {
         eip4762Operations(gasCalculator, chainId),
         gasCalculator,
         evmConfiguration,
-        EvmSpecVersion.FUTURE_EIPS);
+        EvmSpecVersion.PRAGUE);
   }
 
   /**
