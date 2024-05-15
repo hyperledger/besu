0cc8a0905cf0e2f067a32bf58bfa308899d0fb12
Matt Nelson <85905982+non-fungible-nelson@users.noreply.github.com>
2024-05-15 11:39:55 -0400

    Spotless
    Signed-off-by: Matt Nelson <85905982+non-fungible-nelson@users.noreply.github.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/response/RpcErrorType.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/response/RpcErrorType.java
@@ -33,7 +33,9 @@ public enum RpcErrorType implements RpcMethodError {
   METHOD_NOT_ENABLED(-32604, "Method not enabled"),
 
   // Resource unavailable error
-  TX_POOL_DISABLED(-32002, "Transaction pool not enabled. Node may not be in sync or it is explicitly disabled."),
+  TX_POOL_DISABLED(
+      -32002,
+      "Transaction pool not enabled. Node may not be in sync or it is explicitly disabled."),
 
   // eth_getBlockByNumber specific error message
   UNKNOWN_BLOCK(-39001, "Unknown block"),
