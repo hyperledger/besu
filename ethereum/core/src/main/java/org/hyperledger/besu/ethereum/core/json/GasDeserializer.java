2f473b71992f7db34aa9e53e48c0e323b93138c4
Fabio Di Fabio <fabio.difabio@consensys.net>
2024-05-17 01:33:26 +0300

    Fix parsing gasLimit parameter when its value is > Long.MAX_VALUE (#7116)
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/json/GasDeserializer.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/json/GasDeserializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright ConsenSys AG.
+ * Copyright contributors to Hyperledger Besu.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -19,19 +19,27 @@ import java.io.IOException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import org.apache.tuweni.units.bigints.UInt64;
+import org.apache.tuweni.units.bigints.UInt64s;
 
-public class HexLongDeserializer extends StdDeserializer<Long> {
-  public HexLongDeserializer() {
+public class GasDeserializer extends StdDeserializer<Long> {
+  private static final UInt64 GAS_MAX_VALUE = UInt64.valueOf(Long.MAX_VALUE);
+
+  public GasDeserializer() {
     this(null);
   }
 
-  public HexLongDeserializer(final Class<?> vc) {
+  public GasDeserializer(final Class<?> vc) {
     super(vc);
   }
 
   @Override
   public Long deserialize(final JsonParser jsonparser, final DeserializationContext context)
       throws IOException {
-    return Long.decode(jsonparser.getCodec().readValue(jsonparser, String.class));
+    final var uint64 =
+        UInt64.fromHexString(jsonparser.getCodec().readValue(jsonparser, String.class));
+    // we can safely cap the value to Long.MAX_VALUE since gas is not expected to reach these value
+    // anytime soon if ever
+    return UInt64s.min(uint64, GAS_MAX_VALUE).toLong();
   }
 }
