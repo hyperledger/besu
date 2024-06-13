2c83f08062963f4973dc7ed279615e96085e9ecb
Matthew Whitehead <matthew1001@gmail.com>
2024-06-13 08:17:10 +0100

    Add braces to log message
    Signed-off-by: Matthew Whitehead <matthew1001@gmail.com>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthPeer.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthPeer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright ConsenSys AG.
+ * Copyright Hyperledger Besu contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -425,7 +425,7 @@ public class EthPeer implements Comparable<EthPeer> {
         localRequestManager -> localRequestManager.dispatchResponse(ethMessage),
         () -> {
           LOG.trace(
-              "Request message has just been received for protocol {}, peer {} ",
+              "Request message {} has just been received for protocol {}, peer {} ",
               messageCode,
               protocolName,
               this);
