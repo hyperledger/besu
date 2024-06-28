0261b1588e3275ffb7cae4842755d8a14b2be5a3
Karim Taam <karim.t2am@gmail.com>
2024-06-28 09:58:04 +0200

    add tests for key comparator
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- services/kvstore/src/main/java/org/hyperledger/besu/services/kvstore/KeyComparator.java
+++ services/kvstore/src/main/java/org/hyperledger/besu/services/kvstore/KeyComparator.java
@@ -16,6 +16,13 @@ package org.hyperledger.besu.services.kvstore;
 
 import org.apache.tuweni.bytes.Bytes;
 
+/**
+ * This class is a comparator that allows comparing two byte arrays from left to right.
+ *
+ * <p>For example:
+ *
+ * <p>>0x01 is smaller than 0x0101 or 0x01 is smaller than 0x02.
+ */
 public class KeyComparator {
 
   /**
@@ -26,8 +33,8 @@ public class KeyComparator {
    * hierarchical or sequential order.
    *
    * <p>The method returns: - A negative integer if {@code key1} is lexicographically less than
-   * {@code key2}. - Zero if {@code key1} and {@code key2} are equal. - A positive integer if {@code
-   * key1} is lexicographically greater than {@code key2}.
+   * key2. - Zero if key1 and key2 are equal. - A positive integer if key1 is lexicographically
+   * greater than key2.
    *
    * <p>If the keys are of unequal length but identical for the length of the shorter key (prefix),
    * the shorter key is considered to be lexicographically less than the longer key. This is
@@ -35,9 +42,8 @@ public class KeyComparator {
    *
    * @param key1 the first key compare.
    * @param key2 the second key to compare with.
-   * @return the value {@code 0} if {@code key1} is equal to {@code key2}; a value less than {@code
-   *     0} if {@code key1} is lexicographically less than {@code key2}; and a value greater than
-   *     {@code 0} if {@code key1} is lexicographically greater than {@code key2}.
+   * @return the value 0 if key1 is equal to key2; a value less than 0 if key1 is lexicographically
+   *     less than key2; and a value greater than 0 if key1 is lexicographically greater than key2.
    */
   public static int compareKeyLeftToRight(final Bytes key1, final Bytes key2) {
     int minLength = Math.min(key1.size(), key2.size());
