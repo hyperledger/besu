261c48bc5979ded202b35a9f006beec958d0db26
Sally MacFarlane <macfarla.github@gmail.com>
2024-07-09 20:11:54 +1000

    add bytecode for consolidation contract
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/requests/ConsolidationRequestProcessor.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/requests/ConsolidationRequestProcessor.java
@@ -14,21 +14,56 @@
  */
 package org.hyperledger.besu.ethereum.mainnet.requests;
 
-import org.hyperledger.besu.ethereum.core.MutableWorldState;
-import org.hyperledger.besu.ethereum.core.Request;
-import org.hyperledger.besu.ethereum.core.TransactionReceipt;
+import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.datatypes.BLSPublicKey;
+import org.hyperledger.besu.ethereum.core.ConsolidationRequest;
 
-import java.util.List;
-import java.util.Optional;
+import org.apache.tuweni.bytes.Bytes;
 
-public class ConsolidationRequestProcessor implements RequestProcessor {
+public class ConsolidationRequestProcessor
+    extends AbstractSystemCallRequestProcessor<ConsolidationRequest> {
+  // TODO this is not the final value
+  public static final Address CONSOLIDATION_REQUEST_PREDEPLOY_ADDRESS =
+      Address.fromHexString("0x00b42dbF2194e931E80326D950320f7d9Dbeac02");
+
+  private static final int ADDRESS_BYTES = 20;
+  private static final int PUBLIC_KEY_BYTES = 48;
+  private static final int CONSOLIDATION_REQUEST_BYTES_SIZE =
+      ADDRESS_BYTES + PUBLIC_KEY_BYTES + PUBLIC_KEY_BYTES;
+
+  /**
+   * Gets the call address for consolidation requests.
+   *
+   * @return The call address.
+   */
   @Override
-  public Optional<List<? extends Request>> process(
-      final MutableWorldState mutableWorldState,
-      final List<TransactionReceipt> transactionReceipts) {
+  protected Address getCallAddress() {
+    return CONSOLIDATION_REQUEST_PREDEPLOY_ADDRESS;
+  }
 
-    // TODO follow system call approach https://github.com/hyperledger/besu/pull/7263
+  /**
+   * Gets the size of the bytes representing a single consolidation request.
+   *
+   * @return The size of the bytes representing a single consolidation request.
+   */
+  @Override
+  protected int getRequestBytesSize() {
+    return CONSOLIDATION_REQUEST_BYTES_SIZE;
+  }
 
-    return Optional.empty();
+  /**
+   * Parses a single consolidation request from the provided bytes.
+   *
+   * @param requestBytes The bytes representing a single consolidation request.
+   * @return A parsed {@link ConsolidationRequest} object.
+   */
+  @Override
+  protected ConsolidationRequest parseRequest(final Bytes requestBytes) {
+    final Address sourceAddress = Address.wrap(requestBytes.slice(0, ADDRESS_BYTES));
+    final BLSPublicKey sourcePublicKey =
+        BLSPublicKey.wrap(requestBytes.slice(ADDRESS_BYTES, PUBLIC_KEY_BYTES));
+    final BLSPublicKey targetPublicKey =
+        BLSPublicKey.wrap(requestBytes.slice(ADDRESS_BYTES + PUBLIC_KEY_BYTES, PUBLIC_KEY_BYTES));
+    return new ConsolidationRequest(sourceAddress, sourcePublicKey, targetPublicKey);
   }
 }
