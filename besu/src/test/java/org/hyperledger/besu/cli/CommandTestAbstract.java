e3ed95edf3bb7c9d64298dd53e70d2f54de70a09
Matt Whitehead <matthew.whitehead@kaleido.io>
2024-07-23 10:25:07 +0100

    Add new PoA network option to use bootnodes during any peer table refresh, not just the first one (#7314)
    * Add new config option to use bootnodes during any peer table refresh, not just the first one
    
    Signed-off-by: Matthew Whitehead <matthew1001@gmail.com>
    
    * Update everything-config list
    
    Signed-off-by: Matthew Whitehead <matthew1001@gmail.com>
    
    * Revert debug setting
    
    Signed-off-by: Matthew Whitehead <matthew1001@gmail.com>
    
    * Update ethereum/p2p/src/main/java/org/hyperledger/besu/ethereum/p2p/discovery/internal/PeerDiscoveryController.java
    
    Co-authored-by: Stefan Pingel <16143240+pinges@users.noreply.github.com>
    Signed-off-by: Matt Whitehead <matthew1001@hotmail.com>
    
    ---------
    
    Signed-off-by: Matthew Whitehead <matthew1001@gmail.com>
    Signed-off-by: Matt Whitehead <matthew1001@hotmail.com>
    Co-authored-by: Stefan Pingel <16143240+pinges@users.noreply.github.com>
    Signed-off-by: gconnect <agatevureglory@gmail.com>

--- besu/src/test/java/org/hyperledger/besu/cli/CommandTestAbstract.java
+++ besu/src/test/java/org/hyperledger/besu/cli/CommandTestAbstract.java
@@ -350,6 +350,7 @@ public abstract class CommandTestAbstract {
     when(mockRunnerBuilder.apiConfiguration(any())).thenReturn(mockRunnerBuilder);
     when(mockRunnerBuilder.enodeDnsConfiguration(any())).thenReturn(mockRunnerBuilder);
     when(mockRunnerBuilder.allowedSubnets(any())).thenReturn(mockRunnerBuilder);
+    when(mockRunnerBuilder.poaDiscoveryRetryBootnodes(anyBoolean())).thenReturn(mockRunnerBuilder);
     when(mockRunnerBuilder.build()).thenReturn(mockRunner);
 
     final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithmFactory.getInstance();
