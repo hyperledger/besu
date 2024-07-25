0cd7f5bb671fdea053d666cd60bf8e6d2b32a537
garyschulte <garyschulte@gmail.com>
2024-07-25 13:05:37 -0700

    interim commit to allow for branch-off point for execution-spec-tests
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/diffbased/transition/VerkleTransitionWorldStateProvider.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/diffbased/transition/VerkleTransitionWorldStateProvider.java
@@ -2,10 +2,16 @@ package org.hyperledger.besu.ethereum.trie.diffbased.transition;
 
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.ethereum.chain.Blockchain;
 import org.hyperledger.besu.ethereum.core.BlockHeader;
 import org.hyperledger.besu.ethereum.core.MutableWorldState;
 import org.hyperledger.besu.ethereum.proof.WorldStateProof;
+import org.hyperledger.besu.ethereum.trie.diffbased.bonsai.cache.BonsaiCachedMerkleTrieLoader;
+import org.hyperledger.besu.ethereum.trie.diffbased.bonsai.storage.BonsaiWorldStateKeyValueStorage;
+import org.hyperledger.besu.ethereum.trie.diffbased.common.storage.DiffBasedWorldStateKeyValueStorage;
 import org.hyperledger.besu.ethereum.worldstate.WorldStateArchive;
+import org.hyperledger.besu.ethereum.worldstate.WorldStateKeyValueStorage;
+import org.hyperledger.besu.evm.internal.EvmConfiguration;
 import org.hyperledger.besu.evm.worldstate.WorldState;
 
 import java.io.IOException;
@@ -15,40 +21,63 @@ import java.util.function.Function;
 
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.units.bigints.UInt256;
+import org.hyperledger.besu.plugin.BesuContext;
 
 public class VerkleTransitionWorldStateProvider
     implements WorldStateArchive, VerkleTransitionContext.VerkleTransitionSubscriber {
+
+  private final DiffBasedWorldStateKeyValueStorage worldStateKeyValueStorage;
+
+  public VerkleTransitionWorldStateProvider(
+      final DiffBasedWorldStateKeyValueStorage worldStateKeyValueStorage,
+      final Blockchain blockchain,
+      final Optional<Long> maxLayersToLoad,
+      final BonsaiCachedMerkleTrieLoader bonsaiCachedMerkleTrieLoader,
+      final BesuContext pluginContext,
+      final EvmConfiguration evmConfiguration  ) {
+    this.worldStateKeyValueStorage = worldStateKeyValueStorage;
+    // TODO: finish constructor
+  }
+
   @Override
   public Optional<WorldState> get(final Hash rootHash, final Hash blockHash) {
+    // TODO: write me
     return Optional.empty();
   }
 
   @Override
   public boolean isWorldStateAvailable(final Hash rootHash, final Hash blockHash) {
+    // TODO: write me
     return false;
   }
 
   @Override
   public Optional<MutableWorldState> getMutable(
       final BlockHeader blockHeader, final boolean isPersistingState) {
+    // TODO: write me
     return Optional.empty();
   }
 
   @Override
   public Optional<MutableWorldState> getMutable(final Hash rootHash, final Hash blockHash) {
+    // TODO: write me
     return Optional.empty();
   }
 
   @Override
   public MutableWorldState getMutable() {
+    // TODO: write me
     return null;
   }
 
   @Override
-  public void resetArchiveStateTo(final BlockHeader blockHeader) {}
+  public void resetArchiveStateTo(final BlockHeader blockHeader) {
+    // TODO: write me
+  }
 
   @Override
   public Optional<Bytes> getNodeData(final Hash hash) {
+    // TODO: write me
     return Optional.empty();
   }
 
@@ -58,11 +87,18 @@ public class VerkleTransitionWorldStateProvider
       final Address accountAddress,
       final List<UInt256> accountStorageKeys,
       final Function<Optional<WorldStateProof>, ? extends Optional<U>> mapper) {
+    // TODO: write me
     return Optional.empty();
   }
 
   @Override
-  public void close() throws IOException {}
+  public void close() throws IOException {
+    try {
+      worldStateKeyValueStorage.close();
+    } catch (Exception e) {
+      // no op
+    }
+  }
 
   @Override
   public void onTransitionStarted() {
