343c66a16d42e949b83d94ca0f2f74c1cef98c7d
Danno Ferrin <danno@numisight.com>
2024-08-13 17:36:58 -0600

    fix section depth measuring
    Signed-off-by: Danno Ferrin <danno@numisight.com>

--- evm/src/main/java/org/hyperledger/besu/evm/tracing/StandardJsonTracer.java
+++ evm/src/main/java/org/hyperledger/besu/evm/tracing/StandardJsonTracer.java
@@ -215,8 +215,8 @@ public class StandardJsonTracer implements OperationTracer {
       sb.append("\"returnData\":\"").append(returnData.toHexString()).append("\",");
     }
     sb.append("\"depth\":").append(depth).append(",");
-    if (subdepth > 1) {
-      sb.append("\"subdepth\":").append(subdepth).append(",");
+    if (subdepth >= 1) {
+      sb.append("\"fdepth\":").append(subdepth).append(",");
     }
     sb.append("\"refund\":").append(messageFrame.getGasRefund()).append(",");
     sb.append("\"opName\":\"").append(currentOp.getName()).append("\"");
