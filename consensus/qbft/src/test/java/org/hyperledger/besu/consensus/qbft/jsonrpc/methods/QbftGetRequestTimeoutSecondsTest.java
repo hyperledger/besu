09223a2e8f4f37126ccb5ebacec33547e39e7647
7suyash7 <suyashnyn1@gmail.com>
2024-08-29 10:11:03 +0530

    fix spotless error
    Signed-off-by: 7suyash7 <suyashnyn1@gmail.com>

--- consensus/qbft/src/test/java/org/hyperledger/besu/consensus/qbft/jsonrpc/methods/QbftGetRequestTimeoutSecondsTest.java
+++ consensus/qbft/src/test/java/org/hyperledger/besu/consensus/qbft/jsonrpc/methods/QbftGetRequestTimeoutSecondsTest.java
@@ -28,8 +28,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @ExtendWith(MockitoExtension.class)
 public class QbftGetRequestTimeoutSecondsTest {
