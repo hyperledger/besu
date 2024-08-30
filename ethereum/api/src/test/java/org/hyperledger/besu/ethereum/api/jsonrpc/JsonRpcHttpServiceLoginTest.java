2541a66ed004bc6aa89fb734159b8f4afea9ed78
Matilda Clerke <matilda.clerke@consensys.net>
2024-08-30 16:41:05 +1000

    Revert "Revert engine_getClientVersionV1 changes"
    This reverts commit f855091f
    
    Signed-off-by: Matilda Clerke <matilda.clerke@consensys.net>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/JsonRpcHttpServiceLoginTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/JsonRpcHttpServiceLoginTest.java
@@ -100,7 +100,9 @@ public class JsonRpcHttpServiceLoginTest {
   protected static OkHttpClient client;
   protected static String baseUrl;
   protected static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-  protected static final String CLIENT_VERSION = "TestClientVersion/0.1.0";
+  protected static final String CLIENT_NODE_NAME = "TestClientVersion/0.1.0";
+  protected static final String CLIENT_VERSION = "0.1.0";
+  protected static final String CLIENT_COMMIT = "12345678";
   protected static final BigInteger CHAIN_ID = BigInteger.valueOf(123);
   protected static P2PNetwork peerDiscoveryMock;
   protected static BlockchainQueries blockchainQueries;
@@ -131,7 +133,9 @@ public class JsonRpcHttpServiceLoginTest {
     rpcMethods =
         new JsonRpcMethodsFactory()
             .methods(
+                CLIENT_NODE_NAME,
                 CLIENT_VERSION,
+                CLIENT_COMMIT,
                 CHAIN_ID,
                 genesisConfigOptions,
                 peerDiscoveryMock,
