fa731020971f1a728b4b0e6667ddee9fe6caf239
Danno Ferrin <danno@numisight.com>
2024-09-01 17:01:04 -0600

    Performance improvements to EOF layout fuzzing (#7545)
    * Performance improvements to fuzzing
    
    Turning off guidance speeds the rate of testing up by 10%.
    Also, add other options to store new guided-discovered tests.
    
    Signed-off-by: Danno Ferrin <danno@numisight.com>
    
    * bring in the whole javafuzz lib so we can tweak it.
    
    Signed-off-by: Danno Ferrin <danno@numisight.com>
    
    ---------
    
    Signed-off-by: Danno Ferrin <danno@numisight.com>
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>

--- testfuzz/src/main/java/org/hyperledger/besu/testfuzz/javafuzz/Fuzzer.java
+++ testfuzz/src/main/java/org/hyperledger/besu/testfuzz/javafuzz/Fuzzer.java
@@ -12,14 +12,20 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-package org.hyperledger.besu.testfuzz;
+package org.hyperledger.besu.testfuzz.javafuzz;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 import org.hyperledger.besu.crypto.Hash;
 import org.hyperledger.besu.crypto.MessageDigestFactory;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.math.BigInteger;
@@ -28,9 +34,9 @@ import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import com.gitlab.javafuzz.core.AbstractFuzzTarget;
-import com.gitlab.javafuzz.core.Corpus;
 import org.apache.tuweni.bytes.Bytes;
 import org.jacoco.core.data.ExecutionData;
 import org.jacoco.core.data.ExecutionDataReader;
@@ -38,13 +44,19 @@ import org.jacoco.core.data.IExecutionDataVisitor;
 import org.jacoco.core.data.ISessionInfoVisitor;
 import org.jacoco.core.data.SessionInfo;
 
-/** Ported from javafuzz because JaCoCo APIs changed. */
+/**
+ * Ported from <a
+ * href="https://gitlab.com/gitlab-org/security-products/analyzers/fuzzers/javafuzz">...</a> because
+ * JaCoCo APIs changed.
+ */
 @SuppressWarnings({"java:S106", "CallToPrintStackTrace"}) // we use lots the console, on purpose
 public class Fuzzer {
-  private final AbstractFuzzTarget target;
+  private final FuzzTarget target;
   private final Corpus corpus;
   private final Object agent;
   private final Method getExecutionDataMethod;
+  private final Pattern guidanceRegexp;
+  private final File newCorpusDir;
   private long executionsInSample;
   private long lastSampleTime;
   private long totalExecutions;
@@ -58,6 +70,8 @@ public class Fuzzer {
    * @param target The target to fuzz
    * @param dirs the list of corpus dirs and files, comma separated.
    * @param fuzzStats additional fuzzing data from the client
+   * @param guidanceRegexp Regexp of (slash delimited) class names to check for guidance.
+   * @param newCorpusDir Direcroty to dump hex encoded versions of guidance discovered tests.
    * @throws ClassNotFoundException If Jacoco RT is not found (because jacocoagent.jar is not
    *     loaded)
    * @throws NoSuchMethodException If the wrong version of Jacoco is loaded
@@ -66,7 +80,11 @@ public class Fuzzer {
    * @throws NoSuchAlgorithmException If the SHA-256 crypto algo cannot be loaded.
    */
   public Fuzzer(
-      final AbstractFuzzTarget target, final String dirs, final Supplier<String> fuzzStats)
+      final FuzzTarget target,
+      final String dirs,
+      final Supplier<String> fuzzStats,
+      final String guidanceRegexp,
+      final File newCorpusDir)
       throws ClassNotFoundException,
           NoSuchMethodException,
           InvocationTargetException,
@@ -75,6 +93,15 @@ public class Fuzzer {
     this.target = target;
     this.corpus = new Corpus(dirs);
     this.fuzzStats = fuzzStats;
+    this.newCorpusDir = newCorpusDir;
+    if (newCorpusDir != null) {
+      if (newCorpusDir.exists() && newCorpusDir.isFile()) {
+        throw new IllegalArgumentException("New corpus directory already exists as a file");
+      }
+      newCorpusDir.mkdirs();
+    }
+    this.guidanceRegexp =
+        guidanceRegexp == null || guidanceRegexp.isBlank() ? null : Pattern.compile(guidanceRegexp);
     Class<?> c = Class.forName("org.jacoco.agent.rt.RT");
     Method getAgentMethod = c.getMethod("getAgent");
     this.agent = getAgentMethod.invoke(null);
@@ -132,6 +159,11 @@ public class Fuzzer {
     this.lastSampleTime = System.currentTimeMillis();
 
     Map<String, Integer> hitMap = new HashMap<>();
+    // preload some values so we don't get false hits in coverage we don't care about.
+    hitMap.put("org/hyperledger/besu/testfuzz/EofContainerSubCommand", 100);
+    hitMap.put("org/hyperledger/besu/testfuzz/Fuzzer", 100);
+    hitMap.put("org/hyperledger/besu/testfuzz/Fuzzer$HitCounter", 100);
+    hitMap.put("org/hyperledger/besu/testfuzz/InternalClient", 100);
 
     while (true) {
       byte[] buf = this.corpus.generateInput();
@@ -152,19 +184,22 @@ public class Fuzzer {
       if (newCoverage > this.totalCoverage) {
         this.totalCoverage = newCoverage;
         this.corpus.putBuffer(buf);
-        this.logStats("NEW");
+        if (totalExecutions > corpus.getLength()) {
+          this.logStats("NEW");
+          if (newCorpusDir != null) {
 
-        // If you want hex strings of new hits, uncomment the following.
-        // String filename = fileNameForBuffer(buf);
-        // try (var pw =
-        //     new PrintWriter(
-        //         new BufferedWriter(
-        //             new OutputStreamWriter(new FileOutputStream(filename), UTF_8)))) {
-        //   pw.println(Bytes.wrap(buf).toHexString());
-        //   System.out.println(filename);
-        // } catch (IOException e) {
-        //   e.printStackTrace(System.out);
-        // }
+            String filename = fileNameForBuffer(buf);
+            try (var pw =
+                new PrintWriter(
+                    new BufferedWriter(
+                        new OutputStreamWriter(
+                            new FileOutputStream(new File(newCorpusDir, filename)), UTF_8)))) {
+              pw.println(Bytes.wrap(buf).toHexString());
+            } catch (IOException e) {
+              e.printStackTrace(System.out);
+            }
+          }
+        }
       } else if ((System.currentTimeMillis() - this.lastSampleTime) > 30000) {
         this.logStats("PULSE");
       }
@@ -175,14 +210,17 @@ public class Fuzzer {
     MessageDigest md = MessageDigestFactory.create(MessageDigestFactory.SHA256_ALG);
     md.update(buf);
     byte[] digest = md.digest();
-    return String.format("./new-%064x.hex", new BigInteger(1, digest));
+    return String.format("new-%064x.hex", new BigInteger(1, digest));
   }
 
   private long getHitCount(final Map<String, Integer> hitMap)
       throws IllegalAccessException, InvocationTargetException {
+    if (guidanceRegexp == null) {
+      return 1;
+    }
     byte[] dumpData = (byte[]) this.getExecutionDataMethod.invoke(this.agent, false);
     ExecutionDataReader edr = new ExecutionDataReader(new ByteArrayInputStream(dumpData));
-    HitCounter hc = new HitCounter(hitMap);
+    HitCounter hc = new HitCounter(hitMap, guidanceRegexp);
     edr.setExecutionDataVisitor(hc);
     edr.setSessionInfoVisitor(hc);
     try {
@@ -198,28 +236,35 @@ public class Fuzzer {
   static class HitCounter implements IExecutionDataVisitor, ISessionInfoVisitor {
     long hits = 0;
     Map<String, Integer> hitMap;
+    Pattern guidanceRegexp;
 
-    public HitCounter(final Map<String, Integer> hitMap) {
+    public HitCounter(final Map<String, Integer> hitMap, final Pattern guidanceRegexp) {
       this.hitMap = hitMap;
+      this.guidanceRegexp = guidanceRegexp;
     }
 
     @Override
     public void visitClassExecution(final ExecutionData executionData) {
+      String name = executionData.getName();
+      Matcher matcher = guidanceRegexp.matcher(name);
       int hit = 0;
+      if (!matcher.find()) {
+        return;
+      }
+      if (matcher.start() != 0) {
+        return;
+      }
       for (boolean b : executionData.getProbes()) {
-        if (executionData.getName().startsWith("org/hyperledger/besu/testfuzz/")
-            || executionData.getName().startsWith("org/bouncycastle/")
-            || executionData.getName().startsWith("com/gitlab/javafuzz/")) {
-          continue;
-        }
         if (b) {
           hit++;
         }
       }
-      String name = executionData.getName();
       if (hitMap.containsKey(name)) {
-        if (hitMap.get(name) < hit) {
+        int theHits = hitMap.get(name);
+        if (theHits < hit) {
           hitMap.put(name, hit);
+        } else {
+          hit = theHits;
         }
       } else {
         hitMap.put(name, hit);
