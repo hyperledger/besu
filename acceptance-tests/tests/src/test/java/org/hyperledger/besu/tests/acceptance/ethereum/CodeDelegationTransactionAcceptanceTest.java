130387eb84d74cdb73874e09d565ad959e5813c0
Daniel Lehrner <daniel.lehrner@consensys.net>
2024-09-06 11:13:05 +0200

    spotless
    Signed-off-by: Daniel Lehrner <daniel.lehrner@consensys.net>

--- acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/ethereum/CodeDelegationTransactionAcceptanceTest.java
+++ acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/ethereum/CodeDelegationTransactionAcceptanceTest.java
@@ -36,7 +36,6 @@ import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
