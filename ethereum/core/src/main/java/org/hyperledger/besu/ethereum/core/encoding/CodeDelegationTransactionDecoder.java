1751a77f98cc6cdf218cd80f429e15ccd8e01e67
daniellehrner <daniel.lehrner@consensys.net>
2024-09-21 13:28:01 +0200

    7702 validation checks v2 (#7653)
    * yParity is valid  up to 2**256 as well
    
    Signed-off-by: Daniel Lehrner <daniel.lehrner@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/encoding/CodeDelegationTransactionDecoder.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/encoding/CodeDelegationTransactionDecoder.java
@@ -81,7 +81,7 @@ public class CodeDelegationTransactionDecoder {
     final Address address = Address.wrap(input.readBytes());
     final long nonce = input.readLongScalar();
 
-    final long yParity = input.readUnsignedIntScalar();
+    final BigInteger yParity = input.readUInt256Scalar().toUnsignedBigInteger();
     final BigInteger r = input.readUInt256Scalar().toUnsignedBigInteger();
     final BigInteger s = input.readUInt256Scalar().toUnsignedBigInteger();
 
