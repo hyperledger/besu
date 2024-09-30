9c80c9bf42fea402f9bfeace7069ab76b2dc982f
Stefan Pingel <16143240+pinges@users.noreply.github.com>
2024-10-01 08:47:47 +1000

    Improve finding peers (#7626)
    * add check before adding peer to peer table
    
    Signed-off-by: stefan.pingel@consensys.net <stefan.pingel@consensys.net>

--- ethereum/p2p/src/main/java/org/hyperledger/besu/ethereum/p2p/discovery/DiscoveryPeer.java
+++ ethereum/p2p/src/main/java/org/hyperledger/besu/ethereum/p2p/discovery/DiscoveryPeer.java
@@ -17,12 +17,12 @@ package org.hyperledger.besu.ethereum.p2p.discovery;
 import org.hyperledger.besu.ethereum.forkid.ForkId;
 import org.hyperledger.besu.ethereum.p2p.peers.DefaultPeer;
 import org.hyperledger.besu.ethereum.p2p.peers.Peer;
-import org.hyperledger.besu.ethereum.p2p.peers.PeerId;
 import org.hyperledger.besu.ethereum.rlp.RLPInput;
 import org.hyperledger.besu.ethereum.rlp.RLPOutput;
 import org.hyperledger.besu.plugin.data.EnodeURL;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.tuweni.bytes.Bytes;
 import org.ethereum.beacon.discovery.schema.NodeRecord;
@@ -37,9 +37,7 @@ public class DiscoveryPeer extends DefaultPeer {
   private final Endpoint endpoint;
 
   // Timestamps.
-  private long firstDiscovered = 0;
-  private long lastContacted = 0;
-  private long lastSeen = 0;
+  private final AtomicLong firstDiscovered = new AtomicLong(0L);
   private long lastAttemptedConnection = 0;
 
   private NodeRecord nodeRecord;
@@ -96,20 +94,11 @@ public class DiscoveryPeer extends DefaultPeer {
   }
 
   public long getFirstDiscovered() {
-    return firstDiscovered;
+    return firstDiscovered.get();
   }
 
-  public PeerId setFirstDiscovered(final long firstDiscovered) {
-    this.firstDiscovered = firstDiscovered;
-    return this;
-  }
-
-  public long getLastContacted() {
-    return lastContacted;
-  }
-
-  public void setLastContacted(final long lastContacted) {
-    this.lastContacted = lastContacted;
+  public void setFirstDiscovered(final long firstDiscovered) {
+    this.firstDiscovered.compareAndExchange(0L, firstDiscovered);
   }
 
   public long getLastAttemptedConnection() {
@@ -118,14 +107,6 @@ public class DiscoveryPeer extends DefaultPeer {
 
   public void setLastAttemptedConnection(final long lastAttemptedConnection) {
     this.lastAttemptedConnection = lastAttemptedConnection;
-  }
-
-  public long getLastSeen() {
-    return lastSeen;
-  }
-
-  public void setLastSeen(final long lastSeen) {
-    this.lastSeen = lastSeen;
   }
 
   public Endpoint getEndpoint() {
@@ -163,8 +144,6 @@ public class DiscoveryPeer extends DefaultPeer {
     sb.append("status=").append(status);
     sb.append(", enode=").append(this.getEnodeURL());
     sb.append(", firstDiscovered=").append(firstDiscovered);
-    sb.append(", lastContacted=").append(lastContacted);
-    sb.append(", lastSeen=").append(lastSeen);
     sb.append('}');
     return sb.toString();
   }
