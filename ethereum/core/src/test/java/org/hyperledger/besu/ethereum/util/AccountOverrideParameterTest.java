f63bea63f09e4cd4372f355c4d3336d0be2806da
Sally MacFarlane <macfarla.github@gmail.com>
2024-10-25 10:42:51 +1000

    added extra tests
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/util/AccountOverrideParameterTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/util/AccountOverrideParameterTest.java
@@ -30,11 +30,14 @@ import org.junit.jupiter.api.Test;
 
 public class AccountOverrideParameterTest {
 
-  private static final String ADDRESS_HEX = "0xd9c9cd5f6779558b6e0ed4e6acf6b1947e7fa1f3";
+  private static final String ADDRESS_HEX1 = "0xd9c9cd5f6779558b6e0ed4e6acf6b1947e7fa1f3";
+  private static final String ADDRESS_HEX2 = "0xd5E23607D5d73ff2293152f464C3caB005f87696";
   private static final String STORAGE_KEY =
       "0x1cf7945003fc5b59d2f6736f0704557aa805c4f2844084ccd1173b8d56946962";
   private static final String STORAGE_VALUE =
       "0x000000000000000000000000000000000000000000000000000000110ed03bf7";
+  private static final String CODE_STRING =
+      "0xdbf4257000000000000000000000000000000000000000000000000000000000";
 
   @Test
   public void jsonDeserializesCorrectly() throws Exception {
@@ -43,7 +46,7 @@ public class AccountOverrideParameterTest {
             + "\"from\":\"0x0\", \"to\": \"0x0\"}, "
             + "\"latest\","
             + "{\""
-            + ADDRESS_HEX
+            + ADDRESS_HEX1
             + "\":"
             + "{"
             + "\"balance\": \"0x01\","
@@ -55,10 +58,39 @@ public class AccountOverrideParameterTest {
         request.getRequiredParameter(2, AccountOverrideMap.class);
 
     final AccountOverride accountOverride =
-        accountOverrideParam.get(Address.fromHexString(ADDRESS_HEX));
+        accountOverrideParam.get(Address.fromHexString(ADDRESS_HEX1));
 
     assertThat(accountOverride.getNonce()).isEqualTo(Optional.of(88L));
     assertThat(accountOverride.getBalance()).isEqualTo(Optional.of(Wei.of(1)));
+    assertFalse(accountOverride.getStateDiff().isPresent());
+  }
+
+  @Test
+  public void jsonWithCodeDeserializesCorrectly() throws Exception {
+    final String json =
+        "{\"jsonrpc\":\"2.0\",\"method\":\"eth_call\",\"params\":[{"
+            + "\"from\":\"0x0\", \"to\": \"0x0\"}, "
+            + "\"latest\","
+            + "{\""
+            + ADDRESS_HEX1
+            + "\":"
+            + "{"
+            + "\"balance\": \"0x01\","
+            + "\"code\": \""
+            + CODE_STRING
+            + "\""
+            + "}}],\"id\":1}";
+
+    final JsonRpcRequestContext request = new JsonRpcRequestContext(readJsonAsJsonRpcRequest(json));
+    final AccountOverrideMap accountOverrideParam =
+        request.getRequiredParameter(2, AccountOverrideMap.class);
+
+    final AccountOverride accountOverride =
+        accountOverrideParam.get(Address.fromHexString(ADDRESS_HEX1));
+
+    assertFalse(accountOverride.getNonce().isPresent());
+    assertThat(accountOverride.getBalance()).isEqualTo(Optional.of(Wei.of(1)));
+    assertThat(accountOverride.getCode()).isEqualTo(Optional.of(CODE_STRING));
     assertFalse(accountOverride.getStateDiff().isPresent());
   }
 
@@ -69,7 +101,7 @@ public class AccountOverrideParameterTest {
             + "\"from\":\"0x0\", \"to\": \"0x0\"}, "
             + "\"latest\","
             + "{\""
-            + ADDRESS_HEX
+            + ADDRESS_HEX1
             + "\":"
             + "{"
             + "\"balance\": \"0x01\","
@@ -86,14 +118,68 @@ public class AccountOverrideParameterTest {
 
     final AccountOverrideMap accountOverrideParam =
         request.getRequiredParameter(2, AccountOverrideMap.class);
-    System.out.println(accountOverrideParam);
+    assertThat(accountOverrideParam.size()).isEqualTo(1);
 
     final AccountOverride accountOverride =
-        accountOverrideParam.get(Address.fromHexString(ADDRESS_HEX));
+        accountOverrideParam.get(Address.fromHexString(ADDRESS_HEX1));
     assertThat(accountOverride.getNonce()).isEqualTo(Optional.of(88L));
 
     assertTrue(accountOverride.getStateDiff().isPresent());
     assertThat(accountOverride.getStateDiff().get().get(STORAGE_KEY)).isEqualTo(STORAGE_VALUE);
+  }
+
+  @Test
+  public void jsonWithMultipleAccountOverridesDeserializesCorrectly() throws Exception {
+    final String json =
+        "{\"jsonrpc\":\"2.0\",\"method\":\"eth_call\",\"params\":[{"
+            + "\"from\":\"0x0\", \"to\": \"0x0\"}, "
+            + "\"latest\","
+            + "{\""
+            + ADDRESS_HEX1
+            + "\":"
+            + "{"
+            + "\"balance\": \"0x01\","
+            + "\"nonce\": 88,"
+            + "\"stateDiff\": {"
+            + "\""
+            + STORAGE_KEY
+            + "\": \""
+            + STORAGE_VALUE
+            + "\""
+            + "}},"
+            + "\""
+            + ADDRESS_HEX2
+            + "\":"
+            + "{"
+            + "\"balance\": \"0xFF\","
+            + "\"nonce\": 99,"
+            + "\"stateDiff\": {"
+            + "\""
+            + STORAGE_KEY
+            + "\": \""
+            + STORAGE_VALUE
+            + "\""
+            + "}}}],\"id\":1}";
+
+    final JsonRpcRequestContext request = new JsonRpcRequestContext(readJsonAsJsonRpcRequest(json));
+
+    final AccountOverrideMap accountOverrideParam =
+        request.getRequiredParameter(2, AccountOverrideMap.class);
+    assertThat(accountOverrideParam.size()).isEqualTo(2);
+
+    final AccountOverride accountOverride1 =
+        accountOverrideParam.get(Address.fromHexString(ADDRESS_HEX1));
+    assertThat(accountOverride1.getNonce()).isEqualTo(Optional.of(88L));
+    assertThat(accountOverride1.getBalance()).isEqualTo(Optional.of(Wei.fromHexString("0x01")));
+    assertTrue(accountOverride1.getStateDiff().isPresent());
+    assertThat(accountOverride1.getStateDiff().get().get(STORAGE_KEY)).isEqualTo(STORAGE_VALUE);
+
+    final AccountOverride accountOverride2 =
+        accountOverrideParam.get(Address.fromHexString(ADDRESS_HEX2));
+    assertThat(accountOverride2.getNonce()).isEqualTo(Optional.of(99L));
+    assertThat(accountOverride2.getBalance()).isEqualTo(Optional.of(Wei.fromHexString("0xFF")));
+    assertTrue(accountOverride2.getStateDiff().isPresent());
+    assertThat(accountOverride2.getStateDiff().get().get(STORAGE_KEY)).isEqualTo(STORAGE_VALUE);
   }
 
   private JsonRpcRequest readJsonAsJsonRpcRequest(final String json) throws java.io.IOException {
