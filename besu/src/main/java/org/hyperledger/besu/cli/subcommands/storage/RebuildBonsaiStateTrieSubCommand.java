30c421f4cf556cb6dafae8a413008a1aef794b56
garyschulte <garyschulte@gmail.com>
2024-12-02 14:21:31 -0800

    added unit test for state rebuild command
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- besu/src/main/java/org/hyperledger/besu/cli/subcommands/storage/RebuildBonsaiStateTrieSubCommand.java
+++ besu/src/main/java/org/hyperledger/besu/cli/subcommands/storage/RebuildBonsaiStateTrieSubCommand.java
@@ -21,6 +21,7 @@ import static org.hyperledger.besu.ethereum.trie.diffbased.common.storage.DiffBa
 import org.hyperledger.besu.cli.util.VersionProvider;
 import org.hyperledger.besu.controller.BesuController;
 import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.ethereum.core.BlockHeader;
 import org.hyperledger.besu.ethereum.core.MiningConfiguration;
 import org.hyperledger.besu.ethereum.rlp.RLP;
 import org.hyperledger.besu.ethereum.rlp.RLPInput;
@@ -28,7 +29,8 @@ import org.hyperledger.besu.ethereum.trie.MerkleTrie;
 import org.hyperledger.besu.ethereum.trie.diffbased.bonsai.storage.BonsaiWorldStateKeyValueStorage;
 import org.hyperledger.besu.ethereum.trie.patricia.StoredMerklePatriciaTrie;
 import org.hyperledger.besu.ethereum.trie.patricia.StoredNodeFactory;
-import org.hyperledger.besu.ethereum.worldstate.DataStorageConfiguration;
+import org.hyperledger.besu.ethereum.worldstate.FlatDbMode;
+import org.hyperledger.besu.plugin.services.storage.DataStorageFormat;
 import org.hyperledger.besu.plugin.services.storage.SegmentedKeyValueStorageTransaction;
 
 import java.util.function.Function;
@@ -68,98 +70,142 @@ public class RebuildBonsaiStateTrieSubCommand implements Runnable {
     // spec.commandLine().usage(System.out);
     try (final BesuController controller = createController()) {
 
-      var storageProvider = controller.getStorageProvider();
+      var storageConfig = controller.getDataStorageConfiguration();
 
-      // TODO: assuming bonsai, fix or qualify
+      if (!storageConfig.getDataStorageFormat().equals(DataStorageFormat.BONSAI)) {
+        LOG.error("Data Storage format is not BONSAI.  Refusing to rebuild state trie.");
+        System.exit(-1);
+      }
+
       var worldStateStorage =
           (BonsaiWorldStateKeyValueStorage)
-              storageProvider.createWorldStateStorage(
-                  DataStorageConfiguration.DEFAULT_BONSAI_CONFIG);
+              controller
+                  .getStorageProvider()
+                  .createWorldStateStorage(controller.getDataStorageConfiguration());
 
-      var header = controller.getProtocolContext().getBlockchain().getChainHeadHeader();
-      // TODO: add a check to ensure the flat db state root and block hash either match or are
-      // absent from state trie
+      if (!worldStateStorage.getFlatDbMode().equals(FlatDbMode.FULL)) {
+        LOG.error("Database is not fully flattened. Refusing to rebuild state trie.");
+        System.exit(-1);
+      }
 
-      // truncate the TRIE_BRANCH_STORAGE column family,
-      //   subsequently rewrite blockhash and state root after we rebuild the trie
-      worldStateStorage.clearTrie();
+      final BlockHeader header =
+          controller.getProtocolContext().getBlockchain().getChainHeadHeader();
 
-      // rebuild the trie by inserting everything into a StoredMerklePatriciaTrie
-      // and incrementally (naively) commit after each account while streaming
-      // TODO: optimize to incrementally commit tx after a certain threshold
-
-      final var wss = worldStateStorage.getComposedWorldStateStorage();
-      final var accountTrie =
-          new StoredMerklePatriciaTrie<>(
-              new StoredNodeFactory<>(
-                  (location, hash) -> worldStateStorage.getAccount(Hash.wrap(hash)),
-                  Function.identity(),
-                  Function.identity()),
-              MerkleTrie.EMPTY_TRIE_NODE_HASH);
-
-      final var flatdb = worldStateStorage.getFlatDbStrategy();
-      flatdb
-          .accountsToPairStream(wss, Bytes32.ZERO)
-          .forEach(
-              accountPair -> {
-                final SegmentedKeyValueStorageTransaction perAccountTx =
-                    worldStateStorage.getComposedWorldStateStorage().startTransaction();
-
-                // if the account has storage, write the account storage trie values
-                if (isNonEmptyStorage(accountPair.getSecond())) {
-                  // create account storage trie
-                  var accountStorageTrie =
-                      new StoredMerklePatriciaTrie<>(
-                          new StoredNodeFactory<>(
-                              (location, hash) ->
-                                  worldStateStorage.getAccountStorageTrieNode(
-                                      Hash.wrap(accountPair.getFirst()), location, hash),
-                              Function.identity(),
-                              Function.identity()),
-                          MerkleTrie.EMPTY_TRIE_NODE_HASH);
-
-                  // put into account trie
-                  flatdb
-                      .storageToPairStream(
-                          wss,
-                          Hash.wrap(accountPair.getFirst()),
-                          Bytes32.ZERO,
-                          HASH_LAST,
-                          Function.identity())
-                      .forEach(
-                          storagePair -> {
-                            accountStorageTrie.put(storagePair.getFirst(), storagePair.getSecond());
-                          });
-
-                  // commit the account storage trie
-                  accountStorageTrie.commit(
-                      (location, hash, value) ->
-                          perAccountTx.put(
-                              TRIE_BRANCH_STORAGE,
-                              Bytes.concatenate(accountPair.getFirst(), location).toArrayUnsafe(),
-                              value.toArrayUnsafe()));
-                }
-
-                // write the account info
-                accountTrie.put(accountPair.getFirst(), accountPair.getSecond());
-
-                // commit the account trie
-                accountTrie.commit(
-                    (location, hash, value) ->
-                        perAccountTx.put(
-                            TRIE_BRANCH_STORAGE, location.toArrayUnsafe(), value.toArrayUnsafe()));
-
-                LOG.info("committing accountHash {}", accountPair.getFirst());
-                perAccountTx.commit();
+      worldStateStorage
+          .getWorldStateRootHash()
+          // we want state root hash to either be empty or same the same as chain head
+          .filter(root -> !root.equals(header.getStateRoot()))
+          .ifPresent(
+              foundRoot -> {
+                LOG.error(
+                    "Chain head {} does not match state root {}.  Refusing to rebuild state trie.",
+                    header.getStateRoot(),
+                    foundRoot);
+                System.exit(-1);
               });
 
-      // rewrite state root and blockhash
-      var tx = worldStateStorage.getComposedWorldStateStorage().startTransaction();
-      tx.put(TRIE_BRANCH_STORAGE, WORLD_ROOT_HASH_KEY, header.getStateRoot().toArrayUnsafe());
-      tx.put(TRIE_BRANCH_STORAGE, WORLD_BLOCK_HASH_KEY, header.getBlockHash().toArrayUnsafe());
-      LOG.info("committing blockhash and stateroot {}", header.toLogString());
-      tx.commit();
+      // rebuild trie:
+      var newHash = rebuildTrie(worldStateStorage);
+
+      // write state root and block hash from the header:
+      if (!header.getStateRoot().equals(newHash)) {
+        LOG.error(
+            "Catastrophic: calculated state root {} after state rebuild, was expecting {}.",
+            newHash,
+            header.getStateRoot());
+        System.exit(-1);
+      }
+      writeStateRootAndBlockHash(header, worldStateStorage);
     }
+  }
+
+  Hash rebuildTrie(final BonsaiWorldStateKeyValueStorage worldStateStorage) {
+    // truncate the TRIE_BRANCH_STORAGE column family,
+    //   subsequently rewrite blockhash and state root after we rebuild the trie
+    worldStateStorage.clearTrie();
+
+    // rebuild the trie by inserting everything into a StoredMerklePatriciaTrie
+    // and incrementally (naively) commit after each account while streaming
+    // TODO: optimize to incrementally commit tx after a certain threshold
+
+    final var wss = worldStateStorage.getComposedWorldStateStorage();
+    final var accountTrie =
+        new StoredMerklePatriciaTrie<>(
+            new StoredNodeFactory<>(
+                // this may be inefficient, and we can read through an incrementally committing tx
+                // instead
+                worldStateStorage::getAccountStateTrieNode,
+                Function.identity(),
+                Function.identity()),
+            MerkleTrie.EMPTY_TRIE_NODE_HASH);
+
+    final var flatdb = worldStateStorage.getFlatDbStrategy();
+    flatdb
+        .accountsToPairStream(wss, Bytes32.ZERO)
+        .forEach(
+            accountPair -> {
+              final SegmentedKeyValueStorageTransaction perAccountTx =
+                  worldStateStorage.getComposedWorldStateStorage().startTransaction();
+
+              // if the account has storage, write the account storage trie values
+              if (isNonEmptyStorage(accountPair.getSecond())) {
+                // create account storage trie
+                var accountStorageTrie =
+                    new StoredMerklePatriciaTrie<>(
+                        new StoredNodeFactory<>(
+                            (location, hash) ->
+                                worldStateStorage.getAccountStorageTrieNode(
+                                    Hash.wrap(accountPair.getFirst()), location, hash),
+                            Function.identity(),
+                            Function.identity()),
+                        MerkleTrie.EMPTY_TRIE_NODE_HASH);
+
+                // put into account trie
+                flatdb
+                    .storageToPairStream(
+                        wss,
+                        Hash.wrap(accountPair.getFirst()),
+                        Bytes32.ZERO,
+                        HASH_LAST,
+                        Function.identity())
+                    .forEach(
+                        storagePair -> {
+                          accountStorageTrie.put(storagePair.getFirst(), storagePair.getSecond());
+                        });
+
+                // commit the account storage trie
+                accountStorageTrie.commit(
+                    (location, hash, value) ->
+                        perAccountTx.put(
+                            TRIE_BRANCH_STORAGE,
+                            Bytes.concatenate(accountPair.getFirst(), location).toArrayUnsafe(),
+                            value.toArrayUnsafe()));
+              }
+
+              // write the account info
+              accountTrie.put(accountPair.getFirst(), accountPair.getSecond());
+
+              // commit the account trie
+              accountTrie.commit(
+                  (location, hash, value) ->
+                      perAccountTx.put(
+                          TRIE_BRANCH_STORAGE, location.toArrayUnsafe(), value.toArrayUnsafe()));
+
+              LOG.info("committing accountHash {}", accountPair.getFirst());
+              perAccountTx.commit();
+            });
+
+    // perhaps not the safest cast
+    return Hash.wrap(accountTrie.getRootHash());
+  }
+
+  void writeStateRootAndBlockHash(
+      final BlockHeader header, final BonsaiWorldStateKeyValueStorage worldStateStorage) {
+    var tx = worldStateStorage.getComposedWorldStateStorage().startTransaction();
+    tx.put(TRIE_BRANCH_STORAGE, WORLD_ROOT_HASH_KEY, header.getStateRoot().toArrayUnsafe());
+    tx.put(TRIE_BRANCH_STORAGE, WORLD_BLOCK_HASH_KEY, header.getBlockHash().toArrayUnsafe());
+    LOG.info("committing blockhash and stateroot {}", header.toLogString());
+    tx.commit();
   }
 
   private boolean isNonEmptyStorage(final Bytes accountRLP) {
@@ -174,7 +220,7 @@ public class RebuildBonsaiStateTrieSubCommand implements Runnable {
     final Hash storageRoot = Hash.wrap(in.readBytes32());
     // final Hash codeHash = Hash.wrap(in.readBytes32());
 
-    in.leaveList();
+    //    in.leaveList();
     return !storageRoot.equals(Hash.EMPTY_TRIE_HASH);
   }
 
---
+++ besu/src/test/java/org/hyperledger/besu/cli/subcommands/storage/RebuildBonsaiStateTrieSubCommandTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright contributors to Besu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.cli.subcommands.storage;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.hyperledger.besu.ethereum.ProtocolContext;
+import org.hyperledger.besu.ethereum.chain.Blockchain;
+import org.hyperledger.besu.ethereum.core.BlockchainSetupUtil;
+import org.hyperledger.besu.ethereum.mainnet.HeaderValidationMode;
+import org.hyperledger.besu.ethereum.mainnet.ProtocolSchedule;
+import org.hyperledger.besu.ethereum.trie.diffbased.bonsai.worldview.BonsaiWorldState;
+import org.hyperledger.besu.ethereum.worldstate.WorldStateArchive;
+import org.hyperledger.besu.plugin.services.storage.DataStorageFormat;
+import org.hyperledger.besu.testutil.BlockTestUtil;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This test only exercises bonsai worldstate since forest is essentially a no-op for moving the
+ * worldstate.
+ */
+public class RebuildBonsaiStateTrieSubCommandTest {
+
+  Blockchain blockchain;
+  WorldStateArchive archive;
+  ProtocolContext protocolContext;
+  ProtocolSchedule protocolSchedule;
+  BlockchainSetupUtil blockchainSetupUtil;
+  RebuildBonsaiStateTrieSubCommand command = new RebuildBonsaiStateTrieSubCommand();
+
+  @BeforeEach
+  public void setup() throws Exception {
+    setupBonsaiBlockchain();
+    blockchain = blockchainSetupUtil.getBlockchain();
+    protocolContext = blockchainSetupUtil.getProtocolContext();
+    protocolSchedule = blockchainSetupUtil.getProtocolSchedule();
+    archive = blockchainSetupUtil.getWorldArchive();
+  }
+
+  void setupBonsaiBlockchain() {
+    blockchainSetupUtil =
+        BlockchainSetupUtil.createForEthashChain(
+            // Mainnet is a more robust test resource, but it takes upwards of 1 minute to generate
+            // BlockTestUtil.getMainnetResources(),
+            BlockTestUtil.getSnapTestChainResources(), DataStorageFormat.BONSAI);
+    blockchainSetupUtil.importAllBlocks(
+        HeaderValidationMode.LIGHT_DETACHED_ONLY, HeaderValidationMode.LIGHT);
+  }
+
+  @Test
+  public void assertStateRootMatchesAfterRebuild() {
+    var worldstate = (BonsaiWorldState) archive.getMutable();
+    var headRootHash = worldstate.rootHash();
+
+    // drop the trie:
+    worldstate.getWorldStateStorage().clearTrie();
+
+    // rebuild the trie:
+    var newHash = command.rebuildTrie(worldstate.getWorldStateStorage());
+
+    assertThat(newHash).isEqualTo(headRootHash);
+  }
+}
