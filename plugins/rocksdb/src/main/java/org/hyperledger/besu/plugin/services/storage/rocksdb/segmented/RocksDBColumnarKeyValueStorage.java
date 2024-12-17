52446c428beaba205efeaa7b989436f3d2d54c62
Ameziane H. <ameziane.hamlat@consensys.net>
2024-12-17 14:27:09 +0100

    Change the strategy levelCompactionDynamicLevelBytes is set
    Signed-off-by: Ameziane H. <ameziane.hamlat@consensys.net>

--- plugins/rocksdb/src/main/java/org/hyperledger/besu/plugin/services/storage/rocksdb/segmented/RocksDBColumnarKeyValueStorage.java
+++ plugins/rocksdb/src/main/java/org/hyperledger/besu/plugin/services/storage/rocksdb/segmented/RocksDBColumnarKeyValueStorage.java
@@ -51,10 +51,12 @@ import org.rocksdb.ColumnFamilyDescriptor;
 import org.rocksdb.ColumnFamilyHandle;
 import org.rocksdb.ColumnFamilyOptions;
 import org.rocksdb.CompressionType;
+import org.rocksdb.ConfigOptions;
 import org.rocksdb.DBOptions;
 import org.rocksdb.Env;
 import org.rocksdb.LRUCache;
 import org.rocksdb.Options;
+import org.rocksdb.OptionsUtil;
 import org.rocksdb.ReadOptions;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
@@ -161,7 +163,13 @@ public abstract class RocksDBColumnarKeyValueStorage implements SegmentedKeyValu
           .forEach(trimmedSegments::remove);
       columnDescriptors =
           trimmedSegments.stream()
-              .map(segment -> createColumnDescriptor(segment, configuration))
+              .map(segment -> {
+                  try {
+                      return createColumnDescriptor(segment, configuration);
+                  } catch (RocksDBException e) {
+                      throw new RuntimeException(e);
+                  }
+              })
               .collect(Collectors.toList());
 
       setGlobalOptions(configuration, stats);
@@ -182,16 +190,41 @@ public abstract class RocksDBColumnarKeyValueStorage implements SegmentedKeyValu
    * @return a column family descriptor
    */
   private ColumnFamilyDescriptor createColumnDescriptor(
-      final SegmentIdentifier segment, final RocksDBConfiguration configuration) {
+      final SegmentIdentifier segment, final RocksDBConfiguration configuration) throws RocksDBException {
 
-    BlockBasedTableConfig basedTableConfig = createBlockBasedTableConfig(segment, configuration);
+    ConfigOptions configOptions = new ConfigOptions();
+    DBOptions dbOptions = new DBOptions();
+    List<ColumnFamilyDescriptor> cfDescriptors = new ArrayList<>();
+
+      String latestOptionsFileName = OptionsUtil.getLatestOptionsFileName(configuration.getDatabaseDir().toString(), Env.getDefault());
+      LOG.trace("Latest OPTIONS file detected: " + latestOptionsFileName);
+
+      String optionsFilePath = configuration.getDatabaseDir().toString() + "/" + latestOptionsFileName;
+      OptionsUtil.loadOptionsFromFile(configOptions, optionsFilePath, dbOptions, cfDescriptors);
+
+      LOG.trace("RocksDB options loaded successfully from: " + optionsFilePath);
+
+      boolean dynamicLevelBytes = true;
+      if (!cfDescriptors.isEmpty()) {
+        Optional<ColumnFamilyOptions> matchedCfOptions = Optional.empty();
+        for (ColumnFamilyDescriptor descriptor : cfDescriptors) {
+          if (Arrays.equals(descriptor.getName(), segment.getId())) {
+            matchedCfOptions = Optional.of(descriptor.getOptions());
+            break;
+          }
+        }
+         if (matchedCfOptions.isPresent()) {
+           dynamicLevelBytes = matchedCfOptions.get().levelCompactionDynamicLevelBytes();
+           LOG.trace("dynamicLevelBytes is set to an existing value : " + dynamicLevelBytes);
+         }
+      }
+      BlockBasedTableConfig basedTableConfig = createBlockBasedTableConfig(segment, configuration);
 
     final var options =
         new ColumnFamilyOptions()
             .setTtl(0)
             .setCompressionType(CompressionType.LZ4_COMPRESSION)
-            .setTableFormatConfig(basedTableConfig);
-
+            .setTableFormatConfig(basedTableConfig).setLevelCompactionDynamicLevelBytes(dynamicLevelBytes);
     if (segment.containsStaticData()) {
       options
           .setEnableBlobFiles(true)
