4c35f39a6d31e16355a21a57ffc2155ef0050d82
witty <131909329+0xwitty@users.noreply.github.com>
2024-12-18 15:04:32 +0300

    Fix array handling and improve variable naming in DCO check script
    Signed-off-by: witty <131909329+0xwitty@users.noreply.github.com>

--- scripts/dco_check.sh
+++ scripts/dco_check.sh
@@ -15,16 +15,18 @@
 ##
 
 status=0
-while IFS= read -r -a line; do
+my_array=()
+while IFS= read -r line; do
   my_array+=( "$line" )
-  done < <( git branch -r | grep -v origin/HEAD )
+done < <( git branch -r | grep -v origin/HEAD )
+
 for branch in "${my_array[@]}"
 do
   branch=$(echo "$branch" | xargs)
   echo "Checking commits in branch $branch for commits missing DCO..."
-  while read -r results; do
+  while read -r commit_info; do
     status=1
-    commit_hash="$(echo "$results" | cut -d' ' -f1)"
+    commit_hash="$(echo "$commit_info" | cut -d' ' -f1)"
     >&2 echo "$commit_hash is missing Signed-off-by line."
   done < <(git log "$branch" --no-merges --pretty="%H %ae" --grep 'Signed-off-by' --invert-grep -- )
 done
