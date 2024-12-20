eaba024b83d8786b4b5ef82516155d12aab28e2a
Luis Pinto <luis.pinto@consensys.net>
2024-12-20 11:29:35 +0000

    make gradle dependency verification on verkle lenient
    Signed-off-by: Luis Pinto <luis.pinto@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/chain/GenesisState.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/chain/GenesisState.java
@@ -333,7 +333,7 @@ public final class GenesisState {
     return isVerkleAtGenesis(genesis);
   }
 
-  private static boolean isVerkleAtGenesis(final GenesisConfigFile genesis) {
+  private static boolean isVerkleAtGenesis(final GenesisConfig genesis) {
     final OptionalLong verkleTimestamp = genesis.getConfigOptions().getVerkleTime();
     if (verkleTimestamp.isPresent()) {
       return genesis.getTimestamp() >= verkleTimestamp.getAsLong();
