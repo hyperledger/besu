6cfd297cde8954c423428cc437a0ed2362bcb58a
Karim Taam <karim.t2am@gmail.com>
2025-01-30 16:10:41 +0100

    add transaction hash for debug trace responses
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ethereum/api/src/integration-test/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/DebugTraceTransactionIntegrationTest.java
+++ ethereum/api/src/integration-test/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/DebugTraceTransactionIntegrationTest.java
@@ -24,7 +24,7 @@ import org.hyperledger.besu.ethereum.api.jsonrpc.internal.JsonRpcRequestContext;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.JsonRpcMethod;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcResponse;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.response.JsonRpcSuccessResponse;
-import org.hyperledger.besu.ethereum.api.jsonrpc.internal.results.DebugTraceTransactionResult;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.results.DebugTraceTransactionDetails;
 import org.hyperledger.besu.plugin.services.rpc.RpcResponseType;
 import org.hyperledger.besu.testutil.BlockTestUtil;
 
@@ -70,8 +70,8 @@ public class DebugTraceTransactionIntegrationTest {
 
     final JsonRpcResponse response = method.response(request);
     assertThat(response.getType()).isEqualTo(RpcResponseType.SUCCESS);
-    DebugTraceTransactionResult debugTraceTransactionResult =
-        (DebugTraceTransactionResult) ((JsonRpcSuccessResponse) response).getResult();
+    DebugTraceTransactionDetails debugTraceTransactionResult =
+        (DebugTraceTransactionDetails) ((JsonRpcSuccessResponse) response).getResult();
     assertThat(debugTraceTransactionResult.getGas()).isEqualTo(23705L);
     assertThat(debugTraceTransactionResult.getReturnValue()).isEmpty();
     assertThat(debugTraceTransactionResult.failed()).isFalse();
