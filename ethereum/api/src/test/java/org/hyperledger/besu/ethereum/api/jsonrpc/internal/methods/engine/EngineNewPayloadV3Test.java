928f10f0cdb25a84e09e1280af3d78478fe1d259
Karim Taam <karim.t2am@gmail.com>
2025-02-19 17:38:48 +0100

    clean and fix build
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineNewPayloadV3Test.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineNewPayloadV3Test.java
@@ -125,7 +125,7 @@ public class EngineNewPayloadV3Test extends EngineNewPayloadV2Test {
             .timestamp(pragueHardfork.milestone())
             .buildHeader();
 
-    var resp = resp(mockEnginePayload(pragueHeader, emptyList(), null));
+    var resp = resp(mockEnginePayload(pragueHeader, emptyList()));
     final JsonRpcError jsonRpcError = fromErrorResp(resp);
     assertThat(jsonRpcError.getCode()).isEqualTo(UNSUPPORTED_FORK.getCode());
     verify(engineCallListener, times(1)).executionEngineCalled();
@@ -138,7 +138,7 @@ public class EngineNewPayloadV3Test extends EngineNewPayloadV2Test {
             .timestamp(cancunHardfork.milestone() - 1)
             .buildHeader();
 
-    var resp = resp(mockEnginePayload(shanghaiHeader, emptyList(), null));
+    var resp = resp(mockEnginePayload(shanghaiHeader, emptyList()));
     final JsonRpcError jsonRpcError = fromErrorResp(resp);
     assertThat(jsonRpcError.getCode()).isEqualTo(UNSUPPORTED_FORK.getCode());
     verify(engineCallListener, times(1)).executionEngineCalled();
@@ -204,7 +204,7 @@ public class EngineNewPayloadV3Test extends EngineNewPayloadV2Test {
         setupValidPayload(
             new BlockProcessingResult(Optional.of(new BlockProcessingOutputs(null, List.of()))),
             Optional.empty());
-    final EnginePayloadParameter payload = mockEnginePayload(mockHeader, emptyList(), null);
+    final EnginePayloadParameter payload = mockEnginePayload(mockHeader, emptyList());
 
     ValidationResult<RpcErrorType> res =
         method.validateParameters(
--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineNewPayloadV4Test.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineNewPayloadV4Test.java
@@ -164,7 +164,7 @@ public class EngineNewPayloadV4Test extends EngineNewPayloadV3Test {
         setupValidPayload(
             new BlockProcessingResult(Optional.of(new BlockProcessingOutputs(null, List.of()))),
             Optional.empty());
-    final EnginePayloadParameter payload = mockEnginePayload(mockHeader, emptyList(), null);
+    final EnginePayloadParameter payload = mockEnginePayload(mockHeader, emptyList());
 
     ValidationResult<RpcErrorType> res =
         method.validateParameters(
@@ -182,7 +182,7 @@ public class EngineNewPayloadV4Test extends EngineNewPayloadV3Test {
         setupValidPayload(
             new BlockProcessingResult(Optional.of(new BlockProcessingOutputs(null, List.of()))),
             Optional.empty());
-    final EnginePayloadParameter payload = mockEnginePayload(mockHeader, emptyList(), null);
+    final EnginePayloadParameter payload = mockEnginePayload(mockHeader, emptyList());
 
     ValidationResult<RpcErrorType> res =
         method.validateParameters(
