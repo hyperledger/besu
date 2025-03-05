978204610ff585232aa1e0efedd1ba360ab52088
Matthew Whitehead <matthew.whitehead@kaleido.io>
2025-03-05 17:27:19 +0000

    Test compile fix
    Signed-off-by: Matthew Whitehead <matthew.whitehead@kaleido.io>

--- ethereum/blockcreation/src/test/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreatorTest.java
+++ ethereum/blockcreation/src/test/java/org/hyperledger/besu/ethereum/blockcreation/AbstractBlockCreatorTest.java
@@ -359,7 +359,7 @@ abstract class AbstractBlockCreatorTest {
     return new CreateOn(
         new TestBlockCreator(
             miningConfiguration,
-            __ -> Address.ZERO,
+            (__, ___, ____) -> Address.ZERO,
             __ -> Bytes.fromHexString("deadbeef"),
             transactionPool,
             executionContextTestFixture.getProtocolContext(),
