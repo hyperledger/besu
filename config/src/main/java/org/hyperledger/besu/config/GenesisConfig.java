53b66b6ab96a56a036559f895153fc3f78b20df7
Marcos Serradilla Diez <marcos@io.builders>
2025-03-05 19:50:56 +0100

    Add logging for missing gas limit in genesis config
    Introduce a debug log message when gas limit or gas target is missing in the genesis config options and an IllegalArgumentException is thrown. This improves debugging by providing clearer insight into configuration issues.
    
    Signed-off-by: Marcos Serradilla Diez <marcos@io.builders>

--- config/src/main/java/org/hyperledger/besu/config/GenesisConfig.java
+++ config/src/main/java/org/hyperledger/besu/config/GenesisConfig.java
@@ -27,9 +27,13 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** The Genesis config file. */
 public class GenesisConfig {
+
+  private static final Logger LOG = LoggerFactory.getLogger(GenesisConfig.class);
 
   /** The constant DEFAULT. */
   public static final GenesisConfig DEFAULT =
@@ -128,10 +132,17 @@ public class GenesisConfig {
       overridesRef.put("baseFeePerGas", optBaseFee.get().toShortHexString());
     }
 
-    final long optGasLimit = getGasLimit();
-    if (optGasLimit > 0) {
-      overridesRef = overridesRef.isEmpty() ? new HashMap<>() : overridesRef;
-      overridesRef.put("gasLimit", Long.toString(optGasLimit));
+    try {
+      final long optGasLimit = getGasLimit();
+      if (optGasLimit > 0) {
+        overridesRef = overridesRef.isEmpty() ? new HashMap<>() : overridesRef;
+        overridesRef.put("gasLimit", Long.toString(optGasLimit));
+      }
+    } catch (final IllegalArgumentException illegalArgumentException) {
+      LOG.atDebug()
+          .setMessage("GasLimit or gasTarget not present in genesis config options")
+          .setCause(illegalArgumentException)
+          .log();
     }
 
     return JsonGenesisConfigOptions.fromJsonObjectWithOverrides(config, overridesRef);
