b9734eee043b01245949e61982b86202b98d9623
Bhanu Pulluri <bhanu.pulluri@kaleido.io>
2025-03-12 15:06:41 -0400

    Add fastrecovery checking to mock testing
    Signed-off-by: Bhanu Pulluri <bhanu.pulluri@kaleido.io>

--- besu/src/test/java/org/hyperledger/besu/cli/CommandTestAbstract.java
+++ besu/src/test/java/org/hyperledger/besu/cli/CommandTestAbstract.java
@@ -283,6 +283,7 @@ public abstract class CommandTestAbstract {
     when(mockControllerBuilder.isParallelTxProcessingEnabled(false))
         .thenReturn(mockControllerBuilder);
     when(mockControllerBuilder.isEarlyRoundChangeEnabled(false)).thenReturn(mockControllerBuilder);
+    when(mockControllerBuilder.isFastRecoveryEnabled(false)).thenReturn(mockControllerBuilder);
     when(mockControllerBuilder.storageProvider(any())).thenReturn(mockControllerBuilder);
     when(mockControllerBuilder.requiredBlocks(any())).thenReturn(mockControllerBuilder);
     when(mockControllerBuilder.reorgLoggingThreshold(anyLong())).thenReturn(mockControllerBuilder);
