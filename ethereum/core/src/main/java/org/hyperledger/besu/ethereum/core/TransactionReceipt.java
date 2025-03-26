7c09da4c9cb20309bf09bec72976145293d6feca
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2025-03-26 16:57:00 +1100

    Fix spotless
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/TransactionReceipt.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/TransactionReceipt.java
@@ -17,7 +17,6 @@ package org.hyperledger.besu.ethereum.core;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.datatypes.TransactionType;
 import org.hyperledger.besu.ethereum.mainnet.TransactionReceiptType;
-import org.hyperledger.besu.ethereum.rlp.RLPOutput;
 import org.hyperledger.besu.evm.log.Log;
 import org.hyperledger.besu.evm.log.LogsBloomFilter;
 
