61f812a913ad1c2570b93efbb605eabca6bb0db4
Sally MacFarlane <macfarla.github@gmail.com>
2025-04-08 12:59:02 +1000

    imports
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/AbstractPreexistingNodeTest.java
+++ acceptance-tests/tests/src/test/java/org/hyperledger/besu/tests/acceptance/AbstractPreexistingNodeTest.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
