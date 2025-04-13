90c3ad903b9655780f4310ed5a2ff1bf48433c60
wizardbeard <ramonjlongiii@gmail.com>
2025-04-13 03:39:43 -0700

    feat: TypeFormatter in Kotlin

--- besu/src/main/java/org/hyperledger/besu/cli/converter/TypeFormatter.kt
+++ besu/src/main/java/org/hyperledger/besu/cli/converter/TypeFormatter.kt
@@ -12,20 +12,20 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-package org.hyperledger.besu.cli.converter;
+package org.hyperledger.besu.cli.converter
 
 /**
  * This interface can be used to give a converter the capability to format the converted value back
  * to its CLI form
  *
  * @param <V> the type of the CLI converted runtime value
- */
-public interface TypeFormatter<V> {
-  /**
-   * Format a converted value back to its CLI form
-   *
-   * @param value the converted value
-   * @return the textual CLI form of the value
-   */
-  String format(V value);
+</V> */
+interface TypeFormatter<V> {
+    /**
+     * Format a converted value back to its CLI form
+     *
+     * @param value the converted value
+     * @return the textual CLI form of the value
+     */
+    fun format(value: V): String?
 }
