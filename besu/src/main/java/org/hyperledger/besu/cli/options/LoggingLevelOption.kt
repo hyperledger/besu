2cf5845ee9f9c0adca71ec6dbe7558b4337b0d9b
wizardbeard <ramonjlongiii@gmail.com>
2025-04-13 04:21:37 -0700

    feat: LoggingLevelOption in Kotlin

---
+++ besu/src/main/java/org/hyperledger/besu/cli/options/LoggingLevelOption.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright contributors to Hyperledger Besu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.cli.options
+
+import picocli.CommandLine
+
+/** The Logging level CLI option.  */
+class LoggingLevelOption
+/** Default Constructor.  */
+internal constructor() {
+    /** The Picocli CommandSpec. Visible for testing. Injected by Picocli framework at runtime.  */
+    @JvmField
+    @CommandLine.Spec
+    var spec: CommandLine.Model.CommandSpec? = null
+
+    /**
+     * Gets log level.
+     *
+     * @return the log level
+     */
+    @set:CommandLine.Option(
+        names = ["--logging", "-l"],
+        paramLabel = "<LOG VERBOSITY LEVEL>",
+        description = ["Logging verbosity levels: OFF, ERROR, WARN, INFO, DEBUG, TRACE, ALL"]
+    )
+    var logLevel: String? = null
+        /**
+         * Sets log level.
+         *
+         * @param logLevel the log level
+         */
+        set(logLevel) {
+            if ("FATAL".equals(logLevel, ignoreCase = true)) {
+                println("FATAL level is deprecated")
+                field = "ERROR"
+            } else if (ACCEPTED_VALUES.contains(logLevel!!.uppercase())) {
+                field = logLevel.uppercase()
+            } else {
+                throw CommandLine.ParameterException(
+                    spec!!.commandLine(), "Unknown logging value: $logLevel"
+                )
+            }
+        }
+
+    companion object {
+        /**
+         * Create logging level option.
+         *
+         * @return the logging level option
+         */
+        @JvmStatic
+        fun create(): LoggingLevelOption {
+            return LoggingLevelOption()
+        }
+
+        private val ACCEPTED_VALUES = setOf("OFF", "ERROR", "WARN", "INFO", "DEBUG", "TRACE", "ALL")
+    }
+}
