2fb4f6c73240a249059a62a2a19cff74775350b6
wizardbeard <ramonjlongiii@gmail.com>
2025-04-13 05:26:46 -0700

    feat: ConfigDefaultValueProviderStrategy in Kotlin

---
+++ besu/src/main/java/org/hyperledger/besu/cli/util/ConfigDefaultValueProviderStrategy.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright contributors to Hyperledger Besu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.cli.util
+
+import com.google.common.annotations.VisibleForTesting
+import picocli.CommandLine
+import java.io.File
+import java.io.InputStream
+import java.util.*
+
+/** Custom Config option search and run handler.  */
+class ConfigDefaultValueProviderStrategy
+/**
+ * Instantiates a new Config option search and run handler.
+ *
+ * @param resultHandler the result handler
+ * @param environment the environment variables map
+ */(private val resultHandler: CommandLine.IExecutionStrategy, private val environment: Map<String?, String?>) :
+    CommandLine.IExecutionStrategy {
+    @Throws(CommandLine.ExecutionException::class, CommandLine.ParameterException::class)
+    override fun execute(parseResult: CommandLine.ParseResult): Int {
+        val commandLine = parseResult.commandSpec().commandLine()
+        commandLine.setDefaultValueProvider(
+            createDefaultValueProvider(
+                commandLine,
+                ConfigFileFinder().findConfiguration(environment, parseResult),
+                ProfileFinder().findConfiguration(environment, parseResult)
+            )
+        )
+        commandLine.setExecutionStrategy(resultHandler)
+        return commandLine.execute(*parseResult.originalArgs().toTypedArray<String>())
+    }
+
+    /**
+     * Create default value provider default value provider.
+     *
+     * @param commandLine the command line
+     * @param configFile the config file
+     * @param profile the profile file
+     * @return the default value provider
+     */
+    @VisibleForTesting
+    fun createDefaultValueProvider(
+        commandLine: CommandLine?,
+        configFile: Optional<out File?>,
+        profile: Optional<out InputStream?>
+    ): CommandLine.IDefaultValueProvider {
+        val providers: MutableList<CommandLine.IDefaultValueProvider> = ArrayList()
+        providers.add(EnvironmentVariableDefaultProvider(environment))
+
+        configFile.ifPresent { config: File? ->
+            if (config!!.exists()) {
+                providers.add(TomlConfigurationDefaultProvider.fromFile(commandLine, config))
+            }
+        }
+
+        profile.ifPresent { p: InputStream? ->
+            providers.add(
+                TomlConfigurationDefaultProvider.fromInputStream(
+                    commandLine,
+                    p
+                )
+            )
+        }
+        return CascadingDefaultProvider(providers)
+    }
+}
