70d4d30bb530e0b9112f335b981d5ab852276bb5
wizardbeard <ramonjlongiii@gmail.com>
2025-04-13 02:43:05 -0700

    feat: RlpBlockExport in Kotlin

---
+++ besu/src/main/java/org/hyperledger/besu/chainexport/RlpBlockExporter.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.chainexport
+
+import org.hyperledger.besu.ethereum.chain.Blockchain
+import org.hyperledger.besu.ethereum.core.Block
+import org.hyperledger.besu.ethereum.rlp.RLP
+import org.hyperledger.besu.ethereum.rlp.RLPOutput
+import java.io.FileOutputStream
+import java.io.IOException
+
+/** The Rlp block exporter.  */
+class RlpBlockExporter
+/**
+ * Instantiates a new Rlp block exporter.
+ *
+ * @param blockchain the blockchain
+ */
+    (blockchain: Blockchain) : BlockExporter(blockchain) {
+    @Throws(IOException::class)
+    override fun exportBlock(outputStream: FileOutputStream?, block: Block?) {
+        val rlp = RLP.encode { out: RLPOutput? ->
+            block?.writeTo(
+                out
+            )
+        }
+        outputStream?.write(rlp.toArrayUnsafe())
+    }
+}
