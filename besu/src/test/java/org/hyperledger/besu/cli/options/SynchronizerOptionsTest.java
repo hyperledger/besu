7b61541f826e10695b4cad78f2abfbb0f89a65b3
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2025-04-28 11:57:34 +1000

    Undo change
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- besu/src/test/java/org/hyperledger/besu/cli/options/SynchronizerOptionsTest.java
+++ besu/src/test/java/org/hyperledger/besu/cli/options/SynchronizerOptionsTest.java
@@ -14,8 +14,6 @@
  */
 package org.hyperledger.besu.cli.options;
 
-import static org.hyperledger.besu.ethereum.eth.sync.snapsync.SnapSyncConfiguration.DEFAULT_BYTECODE_COUNT_PER_REQUEST;
-
 import org.hyperledger.besu.ethereum.eth.sync.SynchronizerConfiguration;
 import org.hyperledger.besu.ethereum.eth.sync.snapsync.ImmutableSnapSyncConfiguration;
 import org.hyperledger.besu.ethereum.eth.sync.snapsync.SnapSyncConfiguration;
@@ -77,7 +75,8 @@ public class SynchronizerOptionsTest
                 .trienodeCountPerRequest(
                     SnapSyncConfiguration.DEFAULT_TRIENODE_COUNT_PER_REQUEST + 2)
                 .storageCountPerRequest(SnapSyncConfiguration.DEFAULT_STORAGE_COUNT_PER_REQUEST + 2)
-                .bytecodeCountPerRequest(DEFAULT_BYTECODE_COUNT_PER_REQUEST + 2)
+                .bytecodeCountPerRequest(
+                    SnapSyncConfiguration.DEFAULT_BYTECODE_COUNT_PER_REQUEST + 2)
                 .isSnapServerEnabled(Boolean.TRUE)
                 .isSnapSyncTransactionIndexingEnabled(Boolean.TRUE)
                 .build())
