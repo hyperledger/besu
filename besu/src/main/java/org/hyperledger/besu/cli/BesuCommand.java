ecc9ee07df2258d7a70f76822da0df546abed504
daniellehrner <daniel.lehrner@consensys.net>
2025-05-22 11:46:18 +0200

    enable parallel tx processing by default
    Signed-off-by: daniellehrner <daniel.lehrner@consensys.net>

--- besu/src/main/java/org/hyperledger/besu/cli/BesuCommand.java
+++ besu/src/main/java/org/hyperledger/besu/cli/BesuCommand.java
@@ -1880,7 +1880,7 @@ public class BesuCommand implements DefaultCommandValues, Runnable {
       final PathBasedExtraStorageConfiguration subStorageConfiguration =
           getDataStorageConfiguration().getPathBasedExtraStorageConfiguration();
       besuControllerBuilder.isParallelTxProcessingEnabled(
-          subStorageConfiguration.getUnstable().isParallelTxProcessingEnabled());
+          subStorageConfiguration.getParallelTxProcessingEnabled());
     }
     return besuControllerBuilder;
   }
