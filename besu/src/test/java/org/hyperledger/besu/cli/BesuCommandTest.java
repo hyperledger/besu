0edf8e162f77d5de49a99dc6e75eebaf6e02593e
Sally MacFarlane <macfarla.github@gmail.com>
2025-05-22 10:49:59 +1000

    added test
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- besu/src/test/java/org/hyperledger/besu/cli/BesuCommandTest.java
+++ besu/src/test/java/org/hyperledger/besu/cli/BesuCommandTest.java
@@ -256,6 +256,14 @@ public class BesuCommandTest extends CommandTestAbstract {
     assertThat(commandErrorOutput.toString(UTF_8)).isEmpty();
   }
 
+  @Test
+  public void callingUnstableSubCommandMustNotError() {
+    parseCommand("-X");
+    final String expectedOutputStart = "Unstable options";
+    assertThat(commandOutput.toString(UTF_8)).startsWith(expectedOutputStart);
+    assertThat(commandErrorOutput.toString(UTF_8)).isEmpty();
+  }
+
   // Testing default values
   @Test
   public void callingBesuCommandWithoutOptionsMustSyncWithDefaultValues() {
