c197b1e2db7ed4b85ad091bf31ff0ea08545d152
Matilda Clerke <matilda.clerke@consensys.net>
2025-05-26 11:29:17 +1000

    Add stack trace to start of pruning debug log to debug source of multiple calls per expected call
    Signed-off-by: Matilda Clerke <matilda.clerke@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/chain/ChainDataPruner.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/chain/ChainDataPruner.java
@@ -113,6 +113,12 @@ public class ChainDataPruner implements BlockAddedObserver {
   }
 
   private void preMergePruningAction() {
+    StringBuilder sb = new StringBuilder();
+    for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+      sb.append(element.toString());
+      sb.append("\n");
+    }
+    final String originatingStackTrace = sb.toString();
     pruningExecutor.submit(
         () -> {
           try {
@@ -121,7 +127,10 @@ public class ChainDataPruner implements BlockAddedObserver {
             final long expectedNewPruningMark =
                 Math.min(storedPruningMark + pruningQuantity, mergeBlock);
             LOG.debug(
-                "Attempting to prune blocks {} to {}", storedPruningMark, expectedNewPruningMark);
+                "Attempting to prune blocks {} to {}. Started from \n{}",
+                storedPruningMark,
+                expectedNewPruningMark,
+                originatingStackTrace);
             final KeyValueStorageTransaction pruningTransaction = prunerStorage.startTransaction();
             final BlockchainStorage.Updater updater = blockchainStorage.updater();
             for (long blockNumber = storedPruningMark;
