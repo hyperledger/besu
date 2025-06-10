d7895e04b31972c93e49117b42b92d4883746bbe
garyschulte <garyschulte@gmail.com>
2025-06-09 22:16:42 -0700

    add caching and native requirement for p256verify / secp256r1
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- ethereum/evmtool/src/main/java/org/hyperledger/besu/evmtool/BenchmarkSubCommand.java
+++ ethereum/evmtool/src/main/java/org/hyperledger/besu/evmtool/BenchmarkSubCommand.java
@@ -65,7 +65,6 @@ public class BenchmarkSubCommand implements Runnable {
     Bls12(new BLS12Benchmark()),
     p256Verify(new P256VerifyBenchmark());
 
-
     final BenchmarkExecutor benchmarkExecutor;
 
     Benchmark(final BenchmarkExecutor benchmarkExecutor) {
