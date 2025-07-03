013db900bb17b6c502c035d85db6e7cd8179edbc
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2025-07-03 13:50:59 +1000

    Fix spotless
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineGetBlobsV2.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/EngineGetBlobsV2.java
@@ -34,8 +34,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.vertx.core.Vertx;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class EngineGetBlobsV2 extends ExecutionEngineJsonRpcMethod {
 
