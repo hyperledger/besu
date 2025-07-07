066b1163ba045d73077b6d9dfeae6094406102ba
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2025-07-07 12:34:43 +1000

    Fix spotless
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- evm/src/main/java/org/hyperledger/besu/evm/operation/CountLeadingZerosOperation.java
+++ evm/src/main/java/org/hyperledger/besu/evm/operation/CountLeadingZerosOperation.java
@@ -14,13 +14,13 @@
  */
 package org.hyperledger.besu.evm.operation;
 
-import org.apache.tuweni.bytes.Bytes32;
 import org.hyperledger.besu.evm.EVM;
 import org.hyperledger.besu.evm.frame.MessageFrame;
 import org.hyperledger.besu.evm.gascalculator.GasCalculator;
 import org.hyperledger.besu.evm.internal.Words;
 
 import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
 
 /** The CLZ operation. */
 public class CountLeadingZerosOperation extends AbstractFixedCostOperation {
