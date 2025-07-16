1c91baf1dea962e65cf2e219211423719857cf4b
Usman Saleem <usman@usmans.info>
2025-07-16 17:14:14 +1000

    feat: Wiring framework for tracer config options in debug_trace* methods (#8952)
    Prepares the necessary wiring framework for implementing "tracer" options as per #8326 in debug_trace* methods.
    
    The wiring entrypoint is DebugTraceTransactionStepFactory. Subsequent PRs can wire necessary "tracer" result classes in it.
    
    TransactionTraceParams has been modified to include "tracer" and "tracerConfig" options which are required to specify non-default tracers and their options.
    
    TraceOptions has been renamed to OpCodeTracerConfig. It represents options for the default struct/opcode tracer.
    ---------
    
    Signed-off-by: Usman Saleem <usman@usmans.info>
    Signed-off-by: jflo <justin+github@florentine.us>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/AbstractTraceByHash.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/AbstractTraceByHash.java
@@ -24,7 +24,7 @@ import org.hyperledger.besu.ethereum.api.jsonrpc.internal.results.tracing.flat.F
 import org.hyperledger.besu.ethereum.api.query.BlockchainQueries;
 import org.hyperledger.besu.ethereum.api.query.TransactionWithMetadata;
 import org.hyperledger.besu.ethereum.core.Block;
-import org.hyperledger.besu.ethereum.debug.TraceOptions;
+import org.hyperledger.besu.ethereum.debug.OpCodeTracerConfig;
 import org.hyperledger.besu.ethereum.mainnet.ProtocolSchedule;
 import org.hyperledger.besu.ethereum.vm.DebugOperationTracer;
 
@@ -84,7 +84,7 @@ public abstract class AbstractTraceByHash implements JsonRpcMethod {
                     .trace(
                         mutableWorldState,
                         block,
-                        new DebugOperationTracer(new TraceOptions(false, false, true), false))
+                        new DebugOperationTracer(new OpCodeTracerConfig(false, false, true), false))
                     .map(BlockTrace::getTransactionTraces)
                     .orElse(Collections.emptyList())
                     .stream()
