934b9edeedfa0e65182f075bbec70a05d8292755
Simon Dudley <simon.dudley@consensys.net>
2025-08-08 21:08:33 +1000

    Apply suggestion from @macfarla
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>
    Signed-off-by: Simon Dudley <simon.dudley@consensys.net>

--- evm/src/main/java/org/hyperledger/besu/evm/precompile/P256VerifyPrecompiledContract.java
+++ evm/src/main/java/org/hyperledger/besu/evm/precompile/P256VerifyPrecompiledContract.java
@@ -78,7 +78,7 @@ public class P256VerifyPrecompiledContract extends AbstractPrecompiledContract {
   /**
    * Is native BoringSSL boolean.
    *
-   * @return the boolean
+   * @return the boolean indicating whether to use the BoringSSL native library implementation, if it is available
    */
   public static boolean isNativeBoringSSL() {
     return useNativeBoringSSL;
