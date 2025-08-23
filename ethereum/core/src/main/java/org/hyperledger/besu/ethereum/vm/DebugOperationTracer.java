1844c25d77213fad41c8608d3af9690a33613bc6
Usman Saleem <usman@usmans.info>
2025-08-23 23:48:47 +1000

    Frame status debug log
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/vm/DebugOperationTracer.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/vm/DebugOperationTracer.java
@@ -93,6 +93,10 @@ public class DebugOperationTracer implements OperationTracer {
     final Optional<Bytes[]> memory = captureMemory(frame);
     final Optional<Bytes[]> stackPostExecution = captureStack(frame);
 
+    if ("CALL".equals(opcode)) {
+        LOG.trace(" . . tracePostExecution: CALL Frame Status: {}", frame.getState());
+    }
+
     if (lastFrame != null) {
       lastFrame.setGasRemainingPostExecution(gasRemaining);
     }
