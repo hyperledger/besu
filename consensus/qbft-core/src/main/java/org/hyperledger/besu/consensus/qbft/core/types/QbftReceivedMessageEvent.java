d68d186679e0bef01f635b0a0aa5a23a629d4744
Jason Frame <jason.frame@consensys.net>
2025-08-27 14:36:05 +1000

    create QbftReceivedMessageEvent
    Signed-off-by: Jason Frame <jason.frame@consensys.net>

---
+++ consensus/qbft-core/src/main/java/org/hyperledger/besu/consensus/qbft/core/types/QbftReceivedMessageEvent.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright contributors to Besu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.consensus.qbft.core.types;
+
+import org.hyperledger.besu.consensus.common.bft.events.BftEvent;
+import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.ethereum.p2p.rlpx.wire.MessageData;
+
+/** The QBFT received message event interface. */
+public interface QbftReceivedMessageEvent extends BftEvent {
+
+  /**
+   * Gets message.
+   *
+   * @return the message data
+   */
+  MessageData getMessage();
+
+  /**
+   * Gets sender.
+   *
+   * @return the sender address
+   */
+  Address getSender();
+}
