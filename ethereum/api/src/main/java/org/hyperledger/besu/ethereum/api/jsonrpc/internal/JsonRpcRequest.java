de2449c29e0610e68e945f5f5c663f2b3d10ec35
jflo <justin+github@florentine.us>
2025-09-05 11:32:46 -0400

    revert jsonrpc scope change
    Signed-off-by: jflo <justin+github@florentine.us>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/JsonRpcRequest.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/JsonRpcRequest.java
@@ -106,7 +106,7 @@ public class JsonRpcRequest {
   }
 
   @JsonSetter("id")
-  public void setId(final JsonRpcRequestId id) {
+  protected void setId(final JsonRpcRequestId id) {
     // If an id is explicitly set, it is not a notification
     isNotification = false;
     this.id = id;
