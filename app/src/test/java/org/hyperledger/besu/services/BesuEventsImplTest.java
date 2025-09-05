88657e3699b11f9c7f2c876a81cab8d6eedf397e
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2025-09-05 13:11:30 +1000

    Fix tests
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- app/src/test/java/org/hyperledger/besu/services/BesuEventsImplTest.java
+++ app/src/test/java/org/hyperledger/besu/services/BesuEventsImplTest.java
@@ -442,7 +442,7 @@ public class BesuEventsImplTest {
   @Test
   public void transactionDroppedEventFiresAfterSubscribe() {
     final AtomicReference<Transaction> result = new AtomicReference<>();
-    serviceImpl.addTransactionDroppedListener(result::set);
+    serviceImpl.addTransactionDroppedListener((tx, reason) -> result.set(tx));
 
     assertThat(result.get()).isNull();
     // sending a replacement with higher gas should drop the previous one
@@ -456,7 +456,7 @@ public class BesuEventsImplTest {
   @Test
   public void transactionDroppedEventDoesNotFireAfterUnsubscribe() {
     final AtomicReference<Transaction> result = new AtomicReference<>();
-    final long id = serviceImpl.addTransactionDroppedListener(result::set);
+    final long id = serviceImpl.addTransactionDroppedListener((tx, reason) -> result.set(tx));
 
     assertThat(result.get()).isNull();
     transactionPool.addTransactionViaApi(TX1);
