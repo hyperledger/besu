ba6f025a4fbb8990c7a29c7270bd056d83c1fa52
Ameziane H. <ameziane.hamlat@consensys.net>
2025-09-07 20:27:41 +0200

    spotless.
    Signed-off-by: Ameziane H. <ameziane.hamlat@consensys.net>

--- evm/src/main/java/org/hyperledger/besu/evm/precompile/BigIntegerModularExponentiationPrecompiledContract.java
+++ evm/src/main/java/org/hyperledger/besu/evm/precompile/BigIntegerModularExponentiationPrecompiledContract.java
@@ -121,7 +121,6 @@ public class BigIntegerModularExponentiationPrecompiledContract
     if (LibArithmetic.ENABLED) {
       final int baseOffset = clampedToInt(BASE_OFFSET);
       final int baseLength = clampedToInt(length_of_BASE);
-      final int modulusOffset = clampedToInt(BASE_OFFSET + length_of_BASE + length_of_EXPONENT);
       final int modulusLength = clampedToInt(length_of_MODULUS);
       if ((extractLastByte(input, baseOffset, baseLength) & 1) != 1) {
         return computeNative(input, modulusLength);
