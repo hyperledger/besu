8ab806b217e7088adb9323d18405ae735523e590
Luis Pinto <luis.pinto@consensys.net>
2025-09-16 14:33:19 +0100

    increase test coverage
    Signed-off-by: Luis Pinto <luis.pinto@consensys.net>

--- datatypes/src/test/java/org/hyperledger/besu/datatypes/UInt256ArithTest.java
+++ datatypes/src/test/java/org/hyperledger/besu/datatypes/UInt256ArithTest.java
@@ -42,18 +42,19 @@ public class UInt256ArithTest {
       final Bytes numerator = Bytes.wrap(numArray);
       final Bytes denominator = Bytes.wrap(denomArray);
 
-      final BigInteger numBigInt = new BigInteger(1, numArray);
-      final BigInteger denomBigInt = new BigInteger(1, denomArray);
+      final BigInteger numBigInt = bytesToBigInt(numerator, Sign.UNSIGNED);
+      final BigInteger denomBigInt = bytesToBigInt(denominator, Sign.UNSIGNED);
+
       if (denomBigInt.equals(BigInteger.ZERO)) {
         continue;
       }
 
-      final byte[] bytesResult = UInt256Arith.divide(false, numerator, denominator).toArrayUnsafe();
-      final BigInteger result = new BigInteger(1, bytesResult);
+      final Bytes bytesResult = UInt256Arith.divide(false, numerator, denominator);
+      final BigInteger result = bytesToBigInt(bytesResult, Sign.UNSIGNED);
 
       assertNotNull(bytesResult);
       assertEquals(
-          UInt256.SIZE, bytesResult.length, "division result got " + bytesResult.length + " bytes");
+          UInt256.SIZE, bytesResult.size(), "division result got " + bytesResult.size() + " bytes");
       assertEquals(
           numBigInt.divide(denomBigInt),
           result,
@@ -77,22 +78,19 @@ public class UInt256ArithTest {
       final Bytes numerator = Bytes.wrap(numArray);
       final Bytes denominator = Bytes.wrap(denomArray);
 
-      final BigInteger numBigInt =
-          numerator.size() < UInt256.SIZE ? new BigInteger(1, numArray) : new BigInteger(numArray);
-      final BigInteger denomBigInt =
-          denominator.size() < UInt256.SIZE
-              ? new BigInteger(1, denomArray)
-              : new BigInteger(denomArray);
+      final BigInteger numBigInt = bytesToBigInt(numerator, Sign.SIGNED);
+      final BigInteger denomBigInt = bytesToBigInt(denominator, Sign.SIGNED);
+
       if (denomBigInt.equals(BigInteger.ZERO)) {
         continue;
       }
 
-      final byte[] bytesResult = UInt256Arith.divide(true, numerator, denominator).toArrayUnsafe();
-      final BigInteger result = new BigInteger(bytesResult);
+      final Bytes bytesResult = UInt256Arith.divide(true, numerator, denominator);
+      final BigInteger result = bytesToBigInt(bytesResult, Sign.SIGNED);
 
       assertNotNull(bytesResult);
       assertEquals(
-          UInt256.SIZE, bytesResult.length, "division result got " + bytesResult.length + " bytes");
+          UInt256.SIZE, bytesResult.size(), "division result got " + bytesResult.size() + " bytes");
       assertEquals(
           numBigInt.divide(denomBigInt),
           result,
@@ -104,23 +102,23 @@ public class UInt256ArithTest {
     }
   }
 
+  enum Sign {
+    SIGNED,
+    UNSIGNED
+  };
+
   @ParameterizedTest
   @MethodSource("testCases")
-  void divideSingle(final String numerator, final String denominator) {
+  void divide(final String numerator, final String denominator, final Sign sign) {
     final Bytes numeratorBytes = Bytes.fromHexString(numerator);
     final Bytes denominatorBytes = Bytes.fromHexString(denominator);
 
-    final BigInteger numBigInt =
-        numeratorBytes.size() < UInt256.SIZE
-            ? new BigInteger(1, numeratorBytes.toArrayUnsafe())
-            : new BigInteger(numeratorBytes.toArrayUnsafe());
-    final BigInteger denomBigInt =
-        denominatorBytes.size() < UInt256.SIZE
-            ? new BigInteger(1, denominatorBytes.toArrayUnsafe())
-            : new BigInteger(denominatorBytes.toArrayUnsafe());
+    final BigInteger numBigInt = bytesToBigInt(numeratorBytes, sign);
+    final BigInteger denomBigInt = bytesToBigInt(denominatorBytes, sign);
 
-    final Bytes resultBytes = UInt256Arith.divide(true, numeratorBytes, denominatorBytes);
-    final BigInteger result = new BigInteger(resultBytes.toArrayUnsafe());
+    final Bytes resultBytes =
+        UInt256Arith.divide(sign == Sign.SIGNED, numeratorBytes, denominatorBytes);
+    final BigInteger result = bytesToBigInt(resultBytes, sign);
 
     final BigInteger expectedResult = numBigInt.divide(denomBigInt);
 
@@ -138,24 +136,52 @@ public class UInt256ArithTest {
         Bytes.fromHexString(denominator), denominatorBytes, "Original value has been modified");
   }
 
+  private static BigInteger bytesToBigInt(final Bytes bytes, final Sign sign) {
+    // bytes can be shorter, so it's treated as left padded with zeros
+    if (bytes.size() < UInt256.SIZE) {
+      return new BigInteger(1, bytes.toArrayUnsafe());
+    }
+    return sign == Sign.SIGNED
+        ? new BigInteger(bytes.toArrayUnsafe())
+        : new BigInteger(1, bytes.toArrayUnsafe());
+  }
+
   static Collection<Object[]> testCases() {
-    return Arrays.asList(
-        new Object[][] {
-          {"0x00", "0x01"},
-          {"0x50", "0x21"},
-          {
-            "0x120d7a733f5016ad9fae51cb9896e15a96147719fe0379d0cb2642a6951e0a5c",
-            "0x007cdab49aba612fb02bd738a74c76789bc9a911c90296502a35df43e939e6e2"
-          },
-          {"0xa7f576de3a6c", "0xfffffffffef1c296a4c6"},
-          {"0xffffffffffffffffffffffff6bacfb1469f9a4d5674a85b75f951d72d7a58e4a", "0x020000"},
-          {"0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "0x01"},
-          {"0x01", "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"},
-          {"0x1598209296af93c13b2f5fde7d8e99", "0x09244c1368"},
-          {
-            "0xfffffffffffffff9309d38241af6a2545b52958d000000000000000000000000",
-            "0xb17217f7d1cf79abc9e3b398"
-          }
-        });
+    return Arrays.stream(
+            new Object[][] {
+              {"0x00", "0x01"},
+              {"0x50", "0x21"},
+              {
+                "0x120d7a733f5016ad9fae51cb9896e15a96147719fe0379d0cb2642a6951e0a5c",
+                "0x007cdab49aba612fb02bd738a74c76789bc9a911c90296502a35df43e939e6e2"
+              },
+              {"0xa7f576de3a6c", "0xfffffffffef1c296a4c6"},
+              {"0xffffffffffffffffffffffff6bacfb1469f9a4d5674a85b75f951d72d7a58e4a", "0x020000"},
+              {"0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "0x01"},
+              {"0x01", "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"},
+              {"0x1598209296af93c13b2f5fde7d8e99", "0x09244c1368"},
+              {
+                "0xfffffffffffffff9309d38241af6a2545b52958d000000000000000000000000",
+                "0xb17217f7d1cf79abc9e3b398"
+              },
+              {"0xa7f576de3a6c", "0xa7f576de3a6c"},
+              {"0x9c2c35e6c180771cda86cde561fe7609b9e89e8e5b", "0x993951396a774e675e93bea2e77c"},
+              {
+                "0xa73fc792edbfb1038115f77a37613b8f5b64837e28768c9dd90828",
+                "0x0700b2d7adda7612da7f95"
+              },
+              {"0xbf1256135bb3f72de074d0f237", "0x8b63235ac1765530"},
+              {"0x5b35862b0027a502b1d4cbc4a09e25", "0x932542f4003763"}
+            })
+        .flatMap(
+            inputs ->
+                Arrays.stream(Sign.values())
+                    .map(
+                        sign -> {
+                          Object[] newInputs = Arrays.copyOf(inputs, inputs.length + 1);
+                          newInputs[inputs.length] = sign;
+                          return newInputs;
+                        }))
+        .toList();
   }
 }
