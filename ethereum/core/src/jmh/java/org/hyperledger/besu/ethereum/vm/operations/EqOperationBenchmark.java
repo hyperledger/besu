14ffed16e57e33e22e1ebb13576c211393fcc8b5
Luis Pinto <luis.pinto@consensys.net>
2025-09-29 14:29:58 +0100

    create different Bytes instances in benchmarks
    Signed-off-by: Luis Pinto <luis.pinto@consensys.net>

--- ethereum/core/src/jmh/java/org/hyperledger/besu/ethereum/vm/operations/EqOperationBenchmark.java
+++ ethereum/core/src/jmh/java/org/hyperledger/besu/ethereum/vm/operations/EqOperationBenchmark.java
@@ -68,7 +68,7 @@ public class EqOperationBenchmark extends BinaryOperationBenchmark {
         break;
       case BYTES1_EQUAL:
         BenchmarkHelper.fillPool(aPool, () -> 1);
-        bPool = aPool;
+        bPool = Arrays.stream(aPool).map(bytes -> Bytes.wrap(bytes.toArrayUnsafe())).toArray(Bytes[]::new);
         break;
       case BYTES1_NOT_EQUAL:
         BenchmarkHelper.fillPools(
@@ -77,7 +77,7 @@ public class EqOperationBenchmark extends BinaryOperationBenchmark {
       case BYTES1_EQUAL_WITH_ZEROS:
         BenchmarkHelper.fillPool(aPool, () -> 1);
         aPool = Arrays.stream(aPool).map(Bytes32::leftPad).toArray(Bytes[]::new);
-        bPool = aPool;
+        bPool = Arrays.stream(aPool).map(bytes -> Bytes.wrap(bytes.toArrayUnsafe())).toArray(Bytes[]::new);
         break;
       case BYTES1_NOT_EQUAL_WITH_ZEROS:
         BenchmarkHelper.fillPools(
@@ -87,7 +87,7 @@ public class EqOperationBenchmark extends BinaryOperationBenchmark {
         break;
       case BYTES16_EQUAL:
         BenchmarkHelper.fillPool(aPool, () -> 16);
-        bPool = aPool;
+        bPool = Arrays.stream(aPool).map(bytes -> Bytes.wrap(bytes.toArrayUnsafe())).toArray(Bytes[]::new);
         break;
       case BYTES16_NOT_EQUAL:
         BenchmarkHelper.fillPools(
@@ -96,7 +96,7 @@ public class EqOperationBenchmark extends BinaryOperationBenchmark {
       case BYTES16_EQUAL_WITH_ZEROS:
         BenchmarkHelper.fillPool(aPool, () -> 16);
         aPool = Arrays.stream(aPool).map(Bytes32::leftPad).toArray(Bytes[]::new);
-        bPool = aPool;
+        bPool = Arrays.stream(aPool).map(bytes -> Bytes.wrap(bytes.toArrayUnsafe())).toArray(Bytes[]::new);
         break;
       case BYTES16_NOT_EQUAL_WITH_ZEROS:
         BenchmarkHelper.fillPools(
@@ -148,7 +148,7 @@ public class EqOperationBenchmark extends BinaryOperationBenchmark {
             return Bytes.wrap(newPool);
           }
         ).toArray(Bytes[]::new);
-        bPool = aPool;
+        bPool = Arrays.stream(aPool).map(bytes -> Bytes.wrap(bytes.toArrayUnsafe())).toArray(Bytes[]::new);
     }
     index = 0;
   }
