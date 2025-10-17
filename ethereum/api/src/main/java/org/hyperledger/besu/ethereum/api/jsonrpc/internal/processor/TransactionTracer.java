6af47f1268515b075128d723dafae7f0bdce756d
Luis Pinto <luis.pinto@consensys.net>
2025-10-17 10:40:53 +0100

    Enable/disable trace fields for RPCs that dump traces to files (#9183)
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    Allows for enabling and disabling stack, storage, returnData, etc.. for these 2 specific RPC endpoints: debug_traceStandardBlockToFile and debug_traceStandardBadBlockToFile. It does not change any previous defaults for memory which is shown by default.
    
    Example of usage:
    
    curl --location --globoff 'http://localhost:8545' --data '{
        "jsonrpc": "2.0",
        "method": "debug_standardTraceBlockToFile",
        "params": [
            "0x2476b23a03dd87f272724a12213dc4a1ca8f13a1aa9d128a690e1b7d7d312e5e",
            {
                "txHash": "0xdfd5028da0f2f306ae71dc558c26bb1935b13ec3a0e909223eaa77bf761189a8",
                "disableStack": true,
                "disableMemory": false,
                "disableStorage": false
            }
        ],
        "id": 1
    }'
    
    curl --location --globoff 'http://localhost:8545/' --data '{
        "jsonrpc": "2.0",
        "method": "debug_standardTraceBadBlockToFile",
        "params": [
            "0x2476b23a03dd87f272724a12213dc4a1ca8f13a1aa9d128a690e1b7d7d312e5e",
            {
                "txHash": "0xdfd5028da0f2f306ae71dc558c26bb1935b13ec3a0e909223eaa77bf761189a8",
                "disableStack": true,
                "disableMemory": false,
                "disableStorage": false
            }
        ],
        "id": 1
    }'
    
    Signed-off-by: Ifeoluwa Sanni <pheobeayo@gmail.com>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/processor/TransactionTracer.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/processor/TransactionTracer.java
@@ -29,8 +29,10 @@ import org.hyperledger.besu.ethereum.mainnet.ImmutableTransactionValidationParam
 import org.hyperledger.besu.ethereum.mainnet.MainnetTransactionProcessor;
 import org.hyperledger.besu.ethereum.processing.TransactionProcessingResult;
 import org.hyperledger.besu.ethereum.vm.DebugOperationTracer;
+import org.hyperledger.besu.evm.tracing.OpCodeTracerConfigBuilder;
+import org.hyperledger.besu.evm.tracing.OpCodeTracerConfigBuilder.OpCodeTracerConfig;
 import org.hyperledger.besu.evm.tracing.OperationTracer;
-import org.hyperledger.besu.evm.tracing.StandardJsonTracer;
+import org.hyperledger.besu.evm.tracing.StreamingOperationTracer;
 import org.hyperledger.besu.evm.worldstate.WorldUpdater;
 
 import java.io.File;
@@ -94,11 +96,26 @@ public class TransactionTracer {
         transactionTraceParams
             .map(TransactionTraceParams::getTransactionHash)
             .map(Hash::fromHexString);
-    final boolean showMemory =
-        transactionTraceParams
-            .map(TransactionTraceParams::traceOptions)
-            .map(options -> options.opCodeTracerConfig().traceMemory())
-            .orElse(true);
+    final OpCodeTracerConfig opCodeTracerConfig =
+        OpCodeTracerConfigBuilder.createFrom(OpCodeTracerConfig.DEFAULT)
+            .traceMemory(
+                transactionTraceParams
+                    .map(TransactionTraceParams::disableMemoryNullable)
+                    .map(Boolean.FALSE::equals)
+                    .orElse(true))
+            .traceStack(
+                transactionTraceParams
+                    .map(TransactionTraceParams::disableStackNullable)
+                    .map(Boolean.FALSE::equals)
+                    .orElse(true))
+            .traceStorage(
+                transactionTraceParams
+                    .map(TransactionTraceParams::disableStorageNullable)
+                    .map(Boolean.FALSE::equals)
+                    .orElse(false))
+            .traceReturnData(true)
+            .eip3155Strict(true)
+            .build();
 
     if (!Files.isDirectory(traceDir) && !traceDir.toFile().mkdirs()) {
       throw new RuntimeException(
@@ -134,7 +151,7 @@ public class TransactionTracer {
                             stackedUpdater,
                             transaction,
                             transactionProcessor,
-                            new StandardJsonTracer(out, showMemory, true, true, false, true),
+                            new StreamingOperationTracer(out, opCodeTracerConfig),
                             blobGasPrice);
                     out.println(
                         summaryTrace(
@@ -204,7 +221,7 @@ public class TransactionTracer {
     summaryLine.put("output", result.getOutput().toUnprefixedHexString());
     summaryLine.put(
         "gasUsed",
-        StandardJsonTracer.shortNumber(
+        StreamingOperationTracer.shortNumber(
             UInt256.valueOf(transaction.getGasLimit() - result.getGasRemaining())));
     summaryLine.put("time", timer);
     return summaryLine.toString();
