bb558b3fb7002f0201861d514c369745044fb738
Usman Saleem <usman@usmans.info>
2025-10-29 09:21:16 +1000

    fix: testDocker gradle task
     -- download goss/dgoss binaries to test docker image from gradle instead in CI only
    
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- docker/test.sh
+++ docker/test.sh
@@ -14,39 +14,50 @@
 ## SPDX-License-Identifier: Apache-2.0
 ##
 
+set -e  # Exit immediately if any command fails
+
+# Detect architecture if not already set
+if [ -z "$architecture" ]; then
+  case $(uname -m) in
+    x86_64)
+      architecture="amd64"
+      ;;
+    aarch64|arm64)
+      architecture="arm64"
+      ;;
+    arm*)
+      architecture="arm64"
+      ;;
+    *)
+      echo "Unsupported architecture: $(uname -m)"
+      exit 1
+      ;;
+  esac
+fi
+
 export TEST_PATH=./tests
 export GOSS_PATH=$TEST_PATH/goss-linux-${architecture}
 export GOSS_OPTS="$GOSS_OPTS --format junit"
 export GOSS_FILES_STRATEGY=cp
+
 DOCKER_IMAGE=$1
 DOCKER_FILE="${2:-$PWD/Dockerfile}"
 
-i=0
-
-## Checks on the Dockerfile
-GOSS_FILES_PATH=$TEST_PATH/00 \
-bash $TEST_PATH/dgoss dockerfile $DOCKER_IMAGE $DOCKER_FILE \
-> ./reports/00.xml || i=`expr $i + 1`
-# fail fast if we dont pass static checks
-if [[ $i != 0 ]]; then exit $i; fi
-
 # Test for normal startup with ports opened
-# we test that things listen on the right interface/port, not what interface the advertise
-# hence we dont set p2p-host=0.0.0.0 because this sets what its advertising to devp2p; the important piece is that it defaults to listening on all interfaces
+echo "Running test 01: normal startup with ports opened"
 GOSS_FILES_PATH=$TEST_PATH/01 \
 bash $TEST_PATH/dgoss run --sysctl net.ipv6.conf.all.disable_ipv6=1 $DOCKER_IMAGE \
 --network=dev \
 --rpc-http-enabled \
 --rpc-ws-enabled \
 --graphql-http-enabled \
-> ./reports/01.xml || i=`expr $i + 1`
-
-if [[ $i != 0 ]]; then exit $i; fi
+> ./reports/01.xml
 
 # Test for directory permissions
+echo "Running test 02: directory permissions"
 GOSS_FILES_PATH=$TEST_PATH/02 \
 bash $TEST_PATH/dgoss run --sysctl net.ipv6.conf.all.disable_ipv6=1 -v besu-data:/var/lib/besu $DOCKER_IMAGE --data-path=/var/lib/besu \
 --network=dev \
-> ./reports/02.xml || i=`expr $i + 1`
+> ./reports/02.xml
 
-exit $i
+echo "All tests passed successfully"
