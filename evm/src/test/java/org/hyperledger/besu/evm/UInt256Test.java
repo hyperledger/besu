bdbf06fff4a4630160a0088164bc6cdbe64aabda
Luis Pinto <luis.pinto@consensys.net>
2025-10-31 14:55:49 +0000

    add more tests for division
    Signed-off-by: Luis Pinto <luis.pinto@consensys.net>

--- evm/src/test/java/org/hyperledger/besu/evm/UInt256Test.java
+++ evm/src/test/java/org/hyperledger/besu/evm/UInt256Test.java
@@ -17,11 +17,15 @@ package org.hyperledger.besu.evm;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Random;
 
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class UInt256Test {
   static final int SAMPLE_SIZE = 300;
@@ -370,6 +374,89 @@ public class UInt256Test {
       }
       assertThat(bytesToBigInt(result, Sign.UNSIGNED)).isEqualTo(expected);
     }
+  }
+
+  @Test
+  public void signedDiv() {
+    final Random random = new Random(97712);
+    for (int i = 0; i < SAMPLE_SIZE; i++) {
+      int aSize = random.nextInt(1, 33);
+      int bSize = random.nextInt(1, 33);
+      byte[] aArray = new byte[aSize];
+      byte[] bArray = new byte[bSize];
+      random.nextBytes(aArray);
+      random.nextBytes(bArray);
+      UInt256 a = UInt256.fromBytesBE(aArray);
+      UInt256 b = UInt256.fromBytesBE(bArray);
+      BigInteger aInt = bytesToBigInt(aArray, Sign.SIGNED);
+      BigInteger bInt = bytesToBigInt(bArray, Sign.SIGNED);
+      Bytes32 quotient = Bytes32.leftPad(Bytes.wrap(a.signedDiv(b).toBytesBE()));
+      BigInteger expected = BigInteger.ZERO;
+      if (BigInteger.ZERO.compareTo(bInt) != 0) {
+        expected = aInt.divide(bInt);
+      }
+      assertThat(bytesToBigInt(quotient, Sign.SIGNED)).isEqualTo(expected);
+    }
+  }
+
+  @ParameterizedTest
+  @MethodSource("testCases")
+  void div(final String numerator, final String denominator, final Sign sign) {
+    final UInt256 a = UInt256.fromBytesBE(Bytes.fromHexString(numerator).toArray());
+    final UInt256 b = UInt256.fromBytesBE(Bytes.fromHexString(denominator).toArray());
+
+    final BigInteger aBigInt = bytesToBigInt(Bytes.fromHexString(numerator), sign);
+    final BigInteger bBigInt = bytesToBigInt(Bytes.fromHexString(denominator), sign);
+
+    final Bytes32 quotient = switch (sign) {
+      case UNSIGNED -> Bytes32.leftPad(Bytes.wrap(a.div(b).toBytesBE()));
+      case SIGNED -> Bytes32.leftPad(Bytes.wrap(a.signedDiv(b).toBytesBE()));
+    };
+
+    BigInteger expected = BigInteger.ZERO;
+    if (BigInteger.ZERO.compareTo(bBigInt) != 0) {
+      expected = aBigInt.divide(bBigInt);
+    }
+    assertThat(bytesToBigInt(quotient, sign)).isEqualTo(expected);
+  }
+
+  static Collection<Object[]> testCases() {
+    return Arrays.stream(
+        new Object[][] {
+          {"0x00", "0x01"},
+          {"0x50", "0x21"},
+          {
+            "0x120d7a733f5016ad9fae51cb9896e15a96147719fe0379d0cb2642a6951e0a5c",
+            "0x007cdab49aba612fb02bd738a74c76789bc9a911c90296502a35df43e939e6e2"
+          },
+          {"0xa7f576de3a6c", "0xfffffffffef1c296a4c6"},
+          {"0xffffffffffffffffffffffff6bacfb1469f9a4d5674a85b75f951d72d7a58e4a", "0x020000"},
+          {"0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "0x01"},
+          {"0x01", "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"},
+          {"0x1598209296af93c13b2f5fde7d8e99", "0x09244c1368"},
+          {
+            "0xfffffffffffffff9309d38241af6a2545b52958d000000000000000000000000",
+            "0xb17217f7d1cf79abc9e3b398"
+          },
+          {"0xa7f576de3a6c", "0xa7f576de3a6c"},
+          {"0x9c2c35e6c180771cda86cde561fe7609b9e89e8e5b", "0x993951396a774e675e93bea2e77c"},
+          {
+            "0xa73fc792edbfb1038115f77a37613b8f5b64837e28768c9dd90828",
+            "0x0700b2d7adda7612da7f95"
+          },
+          {"0xbf1256135bb3f72de074d0f237", "0x8b63235ac1765530"},
+          {"0x5b35862b0027a502b1d4cbc4a09e25", "0x932542f4003763"}
+        })
+      .flatMap(
+        inputs ->
+          Arrays.stream(Sign.values())
+            .map(
+              sign -> {
+                Object[] newInputs = Arrays.copyOf(inputs, inputs.length + 1);
+                newInputs[inputs.length] = sign;
+                return newInputs;
+              }))
+      .toList();
   }
 
   private static BigInteger bytesToBigInt(final Bytes bytes, final Sign sign) {
