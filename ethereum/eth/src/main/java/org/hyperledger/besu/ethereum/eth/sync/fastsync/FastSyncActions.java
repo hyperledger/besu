4709e3da0dae9b12f27f7f066d344d6dcedccc22
Matilda Clerke <matilda.clerke@consensys.net>
2025-11-05 15:28:27 +1100

    Remove RetryingGetHeaderFromPeerByNumberTask
    Signed-off-by: Matilda Clerke <matilda.clerke@consensys.net>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/fastsync/FastSyncActions.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/sync/fastsync/FastSyncActions.java
@@ -147,8 +147,6 @@ public class FastSyncActions {
                             new PivotBlockRetriever(
                                     protocolSchedule,
                                     ethContext,
-                                    metricsSystem,
-                                    syncConfig,
                                     currentState.getPivotBlockNumber().getAsLong(),
                                     syncConfig.getSyncMinimumPeerCount(),
                                     syncConfig.getSyncPivotDistance())
