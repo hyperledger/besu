261072e9f5bd11de29c1399bc6f4440289a655ba
Ali <alijakparov.kz@gmail.com>
2025-12-08 12:54:06 -0800

    fix compile errors
    Signed-off-by: Ali Zhagparov <alijakparov.kz@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/BonsaiArchiveWorldStateProvider.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/BonsaiArchiveWorldStateProvider.java
@@ -107,7 +107,7 @@ public class BonsaiArchiveWorldStateProvider extends BonsaiWorldStateProvider {
                 worldState ->
                     rollMutableArchiveStateToBlockHash( // This is a tiny action for archive
                         // state
-                        (PathBasedWorldState) worldState, queryParams.getBlockHeader().getHash()))
+                        (PathBasedWorldState) worldState, queryParams.getBlockHeader().getBlockHash()))
             .map(MutableWorldState::freezeStorage);
       }
       return super.getWorldState(queryParams);
