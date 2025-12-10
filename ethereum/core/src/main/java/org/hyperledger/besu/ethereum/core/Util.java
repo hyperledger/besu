f88af42c0c1a139f508bcf4f99d18c6ad1fbcdc9
Ali <alijakparov.kz@gmail.com>
2025-12-09 19:20:54 -0800

    add to log string to util
    Signed-off-by: Ali Zhagparov <alijakparov.kz@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/Util.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/Util.java
@@ -24,10 +24,12 @@ import org.hyperledger.besu.ethereum.trie.MerkleTrie;
 import org.hyperledger.besu.ethereum.trie.patricia.SimpleMerklePatriciaTrie;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.units.bigints.UInt256;
+import org.hyperledger.besu.plugin.data.BlockHeader;
 
 public class Util {
 
@@ -93,5 +95,9 @@ public class Util {
               trie.put(indexKey(i), bytes.get(i));
             });
     return Hash.wrap(trie.getRootHash());
+  }
+
+  public static String toLogString(BlockHeader blockHeader) {
+      return blockHeader.getNumber() + " (" + blockHeader.getBlockHash() + ")";
   }
 }
