8c21d158e2af2d0654f60df9f2af7d6fe5be08bd
Sally MacFarlane <macfarla.github@gmail.com>
2026-02-12 08:40:54 +1000

    added test to demonstrate the problem
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

---
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/parameters/TransactionTraceParamsTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright contributors to Hyperledger Besu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.ethereum.api.jsonrpc.internal.parameters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.hyperledger.besu.ethereum.debug.TraceOptions;
+import org.hyperledger.besu.evm.tracing.OpCodeTracerConfigBuilder.OpCodeTracerConfig;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+public class TransactionTraceParamsTest {
+
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  @Test
+  public void emptyOptionsObjectShouldMatchDefaultTraceOptions() throws Exception {
+    // Demonstrates that passing {} should be equivalent to passing no options at all.
+    // TraceOptions.DEFAULT has: traceStorage=true, traceMemory=false, traceStack=true
+    final OpCodeTracerConfig defaultConfig = TraceOptions.DEFAULT.opCodeTracerConfig();
+
+    // Parse an empty JSON options object — simulates debug_traceTransaction(hash, {})
+    final TransactionTraceParams emptyParams = MAPPER.readValue("{}", TransactionTraceParams.class);
+    final OpCodeTracerConfig emptyParamsConfig = emptyParams.traceOptions().opCodeTracerConfig();
+
+    // These should be identical, but currently traceMemory differs:
+    // DEFAULT has traceMemory=false, but empty {} produces traceMemory=true
+    assertThat(emptyParamsConfig.traceMemory())
+        .describedAs(
+            "traceMemory should match DEFAULT when no disableMemory is specified. "
+                + "DEFAULT=%s, empty options=%s",
+            defaultConfig.traceMemory(), emptyParamsConfig.traceMemory())
+        .isEqualTo(defaultConfig.traceMemory());
+
+    assertThat(emptyParamsConfig.traceStorage())
+        .describedAs("traceStorage should match DEFAULT")
+        .isEqualTo(defaultConfig.traceStorage());
+
+    assertThat(emptyParamsConfig.traceStack())
+        .describedAs("traceStack should match DEFAULT")
+        .isEqualTo(defaultConfig.traceStack());
+  }
+}
