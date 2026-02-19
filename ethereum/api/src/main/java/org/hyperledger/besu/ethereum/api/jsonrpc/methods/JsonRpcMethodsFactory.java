bb99511109827e8653be0f7e94f837892a5f0d3c
daniellehrner <daniel.lehrner@consensys.net>
2026-02-19 10:14:22 +0100

    stream block traces on op code level
    Signed-off-by: daniellehrner <daniel.lehrner@consensys.net>

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/JsonRpcMethodsFactory.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/JsonRpcMethodsFactory.java
@@ -113,8 +113,7 @@ public class JsonRpcMethodsFactory {
                   transactionPool,
                   synchronizer,
                   dataDir,
-                  transactionSimulator,
-                  ethScheduler),
+                  transactionSimulator),
               new ExecutionEngineJsonRpcMethods(
                   miningCoordinator,
                   protocolSchedule,
