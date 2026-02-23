2bea3436b326935bd0ec541103edc12726c68873
Karim Taam <karim.t2am@gmail.com>
2026-02-23 12:23:50 +0400

    clean code
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/mainnet/BodyValidationTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/mainnet/BodyValidationTest.java
@@ -14,28 +14,18 @@
  */
 package org.hyperledger.besu.ethereum.mainnet;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.datatypes.RequestType;
-import org.hyperledger.besu.datatypes.StorageSlotKey;
 import org.hyperledger.besu.ethereum.core.BlockBody;
 import org.hyperledger.besu.ethereum.core.BlockHeader;
-import org.hyperledger.besu.ethereum.core.BlockHeaderTestFixture;
 import org.hyperledger.besu.ethereum.core.Request;
-import org.hyperledger.besu.ethereum.mainnet.block.access.list.BlockAccessList;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
-import org.apache.tuweni.units.bigints.UInt256;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -94,129 +84,5 @@ public final class BodyValidationTest {
         .isEqualTo(
             Bytes32.fromHexString(
                 "0x0e53a6857da18cf29c6ae28be10a333fc0eaafbd3f425f09e5e81f29e4d3d766"));
-  }
-
-  @Test
-  public void blockAccessListValidator_shouldPassWithinLimit() {
-    // Create a BAL with 2 addresses and 3 storage keys (5 items total)
-    final Address addr1 = Address.fromHexString("0x1000000000000000000000000000000000000001");
-    final Address addr2 = Address.fromHexString("0x2000000000000000000000000000000000000002");
-
-    final BlockAccessList.AccountChanges account1 =
-        new BlockAccessList.AccountChanges(
-            addr1,
-            List.of(
-                new BlockAccessList.SlotChanges(
-                    new StorageSlotKey(UInt256.ONE),
-                    List.of(new BlockAccessList.StorageChange(1, UInt256.valueOf(100))))),
-            List.of(
-                new BlockAccessList.SlotRead(new StorageSlotKey(UInt256.valueOf(2))),
-                new BlockAccessList.SlotRead(new StorageSlotKey(UInt256.valueOf(3)))),
-            List.of(),
-            List.of(),
-            List.of());
-
-    final BlockAccessList.AccountChanges account2 =
-        new BlockAccessList.AccountChanges(
-            addr2, List.of(), List.of(), List.of(), List.of(), List.of());
-
-    final BlockAccessList bal = new BlockAccessList(List.of(account1, account2));
-    final BlockAccessListValidator validator = createValidatorWithAmsterdamGasCalculator();
-    final BlockHeader header =
-        new BlockHeaderTestFixture()
-            .gasLimit(30_000_000L)
-            .balHash(BodyValidation.balHash(bal))
-            .buildHeader();
-
-    // bal_items = 5, max_items = 15000 → should pass
-    Assertions.assertThat(validator.validate(Optional.of(bal), header)).isTrue();
-  }
-
-  @Test
-  public void blockAccessListValidator_shouldFailWhenExceedingLimit() {
-    final Address addr1 = Address.fromHexString("0x1000000000000000000000000000000000000001");
-
-    final BlockAccessList.AccountChanges account1 =
-        new BlockAccessList.AccountChanges(
-            addr1,
-            List.of(
-                new BlockAccessList.SlotChanges(
-                    new StorageSlotKey(UInt256.ONE),
-                    List.of(new BlockAccessList.StorageChange(1, UInt256.valueOf(100))))),
-            List.of(
-                new BlockAccessList.SlotRead(new StorageSlotKey(UInt256.valueOf(2))),
-                new BlockAccessList.SlotRead(new StorageSlotKey(UInt256.valueOf(3))),
-                new BlockAccessList.SlotRead(new StorageSlotKey(UInt256.valueOf(4))),
-                new BlockAccessList.SlotRead(new StorageSlotKey(UInt256.valueOf(5))),
-                new BlockAccessList.SlotRead(new StorageSlotKey(UInt256.valueOf(6)))),
-            List.of(),
-            List.of(),
-            List.of());
-
-    final BlockAccessList bal = new BlockAccessList(List.of(account1));
-    final BlockAccessListValidator validator = createValidatorWithAmsterdamGasCalculator();
-    final BlockHeader header =
-        new BlockHeaderTestFixture()
-            .gasLimit(10_000L)
-            .balHash(BodyValidation.balHash(bal))
-            .buildHeader();
-
-    // bal_items = 7, max_items = 5 → should fail
-    Assertions.assertThat(validator.validate(Optional.of(bal), header)).isFalse();
-  }
-
-  @Test
-  public void blockAccessListValidator_shouldPassAtExactLimit() {
-    final Address addr1 = Address.fromHexString("0x1000000000000000000000000000000000000001");
-    final Address addr2 = Address.fromHexString("0x2000000000000000000000000000000000000002");
-
-    final BlockAccessList.AccountChanges account1 =
-        new BlockAccessList.AccountChanges(
-            addr1,
-            List.of(
-                new BlockAccessList.SlotChanges(
-                    new StorageSlotKey(UInt256.ONE),
-                    List.of(new BlockAccessList.StorageChange(1, UInt256.valueOf(100))))),
-            List.of(),
-            List.of(),
-            List.of(),
-            List.of());
-
-    final BlockAccessList.AccountChanges account2 =
-        new BlockAccessList.AccountChanges(
-            addr2, List.of(), List.of(), List.of(), List.of(), List.of());
-
-    final BlockAccessList bal = new BlockAccessList(List.of(account1, account2));
-    final BlockAccessListValidator validator = createValidatorWithAmsterdamGasCalculator();
-    final BlockHeader header =
-        new BlockHeaderTestFixture()
-            .gasLimit(6_000L)
-            .balHash(BodyValidation.balHash(bal))
-            .buildHeader();
-
-    // bal_items = 3, max_items = 3 → should pass
-    Assertions.assertThat(validator.validate(Optional.of(bal), header)).isTrue();
-  }
-
-  @Test
-  public void blockAccessListValidator_shouldPassWithEmptyBAL() {
-    final BlockAccessList bal = new BlockAccessList(List.of());
-    final BlockAccessListValidator validator = createValidatorWithAmsterdamGasCalculator();
-    final BlockHeader header =
-        new BlockHeaderTestFixture()
-            .gasLimit(30_000_000L)
-            .balHash(BodyValidation.balHash(bal))
-            .buildHeader();
-
-    Assertions.assertThat(validator.validate(Optional.of(bal), header)).isTrue();
-  }
-
-  private static BlockAccessListValidator createValidatorWithAmsterdamGasCalculator() {
-    final ProtocolSchedule protocolSchedule = mock(ProtocolSchedule.class);
-    final ProtocolSpec protocolSpec = mock(ProtocolSpec.class);
-    when(protocolSpec.getGasCalculator())
-        .thenReturn(new org.hyperledger.besu.evm.gascalculator.AmsterdamGasCalculator());
-    when(protocolSchedule.getByBlockHeader(any())).thenReturn(protocolSpec);
-    return new MainnetBlockAccessListValidator(protocolSchedule);
   }
 }
