59e87e5cc868d8de1eb7099a9cfc7ce0ba5db79f
Ali Zhagparov <alijakparov.kz@gmail.com>
2026-03-02 22:01:51 -0800

    compare depth
    Signed-off-by: Ali <alijakparov.kz@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/vm/DebugOperationTracer.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/vm/DebugOperationTracer.java
@@ -286,7 +286,9 @@ public class DebugOperationTracer implements OperationTracer {
   private Optional<Bytes[]> captureMemory(final MessageFrame frame) {
     if (!options.traceMemory() || frame.memoryWordSize() == 0) {
       return Optional.empty();
-    } else if (frame.getMaybeUpdatedMemory().isEmpty() && lastFrame != null) {
+    } else if (frame.getMaybeUpdatedMemory().isEmpty()
+        && lastFrame != null
+        && lastFrame.getDepth() == frame.getDepth()) {
       return lastFrame.getMemory();
     }
     final Bytes[] memoryContents = new Bytes[frame.memoryWordSize()];
