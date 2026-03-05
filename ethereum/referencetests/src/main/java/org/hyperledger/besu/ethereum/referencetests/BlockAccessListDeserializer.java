1c1f8d549e8336490e97a029ba01c37993b5203d
Karim Taam <karim.t2am@gmail.com>
2026-03-05 15:41:28 +0400

    address review comments
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ethereum/referencetests/src/main/java/org/hyperledger/besu/ethereum/referencetests/BlockAccessListDeserializer.java
+++ ethereum/referencetests/src/main/java/org/hyperledger/besu/ethereum/referencetests/BlockAccessListDeserializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright contributors to Hyperledger Besu.
+ * Copyright contributors to Besu.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -23,57 +23,76 @@ import java.util.Spliterators;
 import java.util.stream.StreamSupport;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
 
 public class BlockAccessListDeserializer extends JsonDeserializer<BlockAccessList>
     implements ContextualDeserializer {
 
-  private static final ObjectMapper MAPPER = new ObjectMapper();
-  private String propertyName;
+  private final String propertyName;
+
+  public BlockAccessListDeserializer() {
+    this.propertyName = null;
+  }
+
+  private BlockAccessListDeserializer(final String propertyName) {
+    this.propertyName = propertyName;
+  }
 
   @Override
   public JsonDeserializer<?> createContextual(
       final DeserializationContext ctxt, final BeanProperty property) {
-    if (property != null) {
-      this.propertyName = property.getName();
-    }
-    return this;
+    final String name = property != null ? property.getName() : null;
+    return new BlockAccessListDeserializer(name);
   }
 
   @Override
   public BlockAccessList deserialize(final JsonParser p, final DeserializationContext ctxt)
       throws IOException {
-    final JsonNode node = p.getCodec().readTree(p);
-    final JsonNode balNode = findArrayNode(node, propertyName);
+    final ObjectCodec codec = p.getCodec();
+    final JsonNode node = codec.readTree(p);
+
+    final JsonNode balNode = findArrayNode(node, propertyName, true);
     if (balNode == null) {
       return null;
     }
-    try {
-      final List<AccountChangesJson> list =
-          MAPPER.readerForListOf(AccountChangesJson.class).readValue(balNode);
-      return AccountChangesJson.toBlockAccessList(list);
+
+    final JavaType listType =
+        ctxt.getTypeFactory().constructCollectionType(List.class, AccountChangesJson.class);
+    final List<AccountChangesJson> list;
+    try (JsonParser nodeParser = balNode.traverse(codec)) {
+      list = ctxt.readValue(nodeParser, listType);
     } catch (final IOException e) {
-      return null;
+      throw ctxt.weirdStringException(
+          balNode.toString(),
+          BlockAccessList.class,
+          "Failed to parse blockAccessList: " + e.getMessage());
     }
+
+    return AccountChangesJson.toBlockAccessList(list);
   }
 
-  private JsonNode findArrayNode(final JsonNode node, final String fieldName) {
+  private JsonNode findArrayNode(
+      final JsonNode node, final String fieldName, final boolean isRoot) {
     if (node == null || node.isNull()) {
       return null;
     }
-    if (node.isArray()) {
+    if (isRoot && node.isArray()) {
       return node;
     }
-    if (node.has(fieldName)) {
-      return node.get(fieldName);
+    if (fieldName != null && node.has(fieldName)) {
+      final JsonNode child = node.get(fieldName);
+      if (child.isArray()) {
+        return child;
+      }
     }
     return StreamSupport.stream(Spliterators.spliteratorUnknownSize(node.fields(), 0), false)
-        .map(e -> findArrayNode(e.getValue(), fieldName))
+        .map(e -> findArrayNode(e.getValue(), fieldName, false))
         .filter(Objects::nonNull)
         .findFirst()
         .orElse(null);
