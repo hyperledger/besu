300cf3c36b2291277e0f1632d1093152254f8f1d
Simon Dudley <simon.dudley@consensys.net>
2026-03-27 15:02:58 +1000

    Replace Add impl with UnsupprtedOperationException
    Signed-off-by: Simon Dudley <simon.dudley@consensys.net>

--- evm/src/main/java/org/hyperledger/besu/evm/operation/v2/AddOperationV2.java
+++ evm/src/main/java/org/hyperledger/besu/evm/operation/v2/AddOperationV2.java
@@ -28,6 +28,7 @@ import org.hyperledger.besu.evm.operation.Operation;
  */
 public class AddOperationV2 extends AbstractFixedCostOperationV2 {
 
+  @SuppressWarnings("UnusedVariable")
   private static final Operation.OperationResult ADD_SUCCESS =
       new Operation.OperationResult(3, null);
 
@@ -52,41 +53,9 @@ public class AddOperationV2 extends AbstractFixedCostOperationV2 {
    * @param frame the message frame
    * @return the operation result
    */
+  @SuppressWarnings("DoNotCallSuggester")
   public static Operation.OperationResult staticOperation(
       final MessageFrame frame, final long[] s) {
-    if (!frame.stackHasItems(2)) return UNDERFLOW_RESPONSE;
-    frame.setTopV2(add(s, frame.stackTopV2()));
-    return ADD_SUCCESS;
-  }
-
-  /** ADD: s[top-2] = s[top-1] + s[top-2], return top-1. */
-  private static int add(final long[] s, final int top) {
-    final int a = (top - 1) << 2;
-    final int b = (top - 2) << 2;
-    // Fast path: both values fit in a single limb (common case)
-    if ((s[a] | s[a + 1] | s[a + 2] | s[b] | s[b + 1] | s[b + 2]) == 0) {
-      long z = s[a + 3] + s[b + 3];
-      s[b + 2] = ((s[a + 3] & s[b + 3]) | ((s[a + 3] | s[b + 3]) & ~z)) >>> 63;
-      s[b + 3] = z;
-      return top - 1;
-    }
-    long a0 = s[a + 3], a1 = s[a + 2], a2 = s[a + 1], a3 = s[a];
-    long b0 = s[b + 3], b1 = s[b + 2], b2 = s[b + 1], b3 = s[b];
-    long z0 = a0 + b0;
-    long c = ((a0 & b0) | ((a0 | b0) & ~z0)) >>> 63;
-    long t1 = a1 + b1;
-    long c1 = ((a1 & b1) | ((a1 | b1) & ~t1)) >>> 63;
-    long z1 = t1 + c;
-    c = c1 | (((t1 & c) | ((t1 | c) & ~z1)) >>> 63);
-    long t2 = a2 + b2;
-    long c2 = ((a2 & b2) | ((a2 | b2) & ~t2)) >>> 63;
-    long z2 = t2 + c;
-    c = c2 | (((t2 & c) | ((t2 | c) & ~z2)) >>> 63);
-    long z3 = a3 + b3 + c;
-    s[b] = z3;
-    s[b + 1] = z2;
-    s[b + 2] = z1;
-    s[b + 3] = z0;
-    return top - 1;
+    throw new UnsupportedOperationException("ADD operation not yet implemented for evm v2");
   }
 }
